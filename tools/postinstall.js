b43c7316bc273179fe6ff3b84165c9a49d3783ea
Victor Vlasenko <victor.vlasenko@sysgears.com>
2017-03-08 15:28:10 +0200

    Use heroku-postbuild script instead of postinstall

--- tools/postinstall.js
+++
@@ -1,5 +0,0 @@
-const execSync = require('child_process').execSync;
-
-if (process.env.NODE_ENV === 'production') {
-  execSync('npm run clean && npm run build && npm run migrate && npm run seed', { stdio: [0, 1, 2] });
-}

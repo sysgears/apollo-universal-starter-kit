9bd5736e659136811a5e6ad05b44ede60c6063e7
Alexander Vetrov <alexandr.vetrov@sysgears.com>
2017-03-09 12:41:42 +0200

    Remove unnecessary test cases

--- tests/ui/graphql/counter.test.js
+++ tests/ui/graphql/counter.test.js
@@ -2,6 +2,7 @@ import chai from "chai";
 import chaiAsPromised from "chai-as-promised";
 import {mockServer} from "graphql-tools";
 import schema from "../../../src/server/api/schema_def.graphqls";
+import AMOUNT_QUERY from '!raw!../../../src/ui/graphql/CountGet.graphql'
 import ADD_COUNT_MUTATION from "!raw!../../../src/ui/graphql/CountAddMutation.graphql";
 
 chai.use(chaiAsPromised);
@@ -22,17 +23,17 @@ const mockedServer = mockServer(schema, {
     })
 });
 
-describe('Add count mutation', () => {
+describe('Counter', () => {
+    it('should get the current value of a counter', () => {
+        return mockedServer
+            .query(AMOUNT_QUERY)
+            .then(value => value.data.count.amount)
+            .should.eventually.equal(counterValue)
+    });
     it('should return a new value of a counter after a mutation', () => {
         return mockedServer
             .query(ADD_COUNT_MUTATION, {"amount": increasedValue})
             .then(value => value.data.addCount.amount)
             .should.eventually.equal(increasedValue)
-    });
-    it('should not update a nonexistent value', () => {
-        return mockedServer
-            .query(ADD_COUNT_MUTATION, {"nonexistent": increasedValue})
-            .then(value => value.errors)
-            .should.eventually.have.lengthOf(1)
     });
 });

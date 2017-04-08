25bf5fbc9aadb9c943f5c30abf872ed0deb802ed
Victor Vlasenko <victor.vlasenko@sysgears.com>
2017-04-08 15:20:06 +0300

    Disable GraphiQL link in prod mode when persistGraphQL is on
    Ref: #117

--- src/ui/components/nav_bar.jsx
+++ src/ui/components/nav_bar.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { Container, Row, Navbar, Nav, NavItem } from 'reactstrap';
 
+import { app as settings } from '../../../package.json';
+
 const NavBar = () => (
   <Navbar color="faded" light>
     <Container>
@@ -12,11 +14,11 @@ const NavBar = () => (
             <Link to="/posts" className="nav-link">Posts</Link>
           </NavItem>
         </Nav>
-        <Nav className="ml-auto" navbar>
+        {(!settings.persistGraphQL || __DEV__) && <Nav className="ml-auto" navbar>
           <NavItem>
             <a href="/graphiql">GraphiQL</a>
           </NavItem>
-        </Nav>
+        </Nav>}
       </Row>
     </Container>
   </Navbar>

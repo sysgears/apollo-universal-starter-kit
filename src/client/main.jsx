87e706fa6d7b5da2d828d6fa6e63a3023a336e4b
Victor Vlasenko <victor.vlasenko@sysgears.com>
2017-04-12 11:22:15 +0300

    Rework front end reload on back end change.
    Made implementation simpler and more stable.
    
    Fixes: #125

--- src/client/main.jsx
+++ src/client/main.jsx
@@ -12,9 +12,6 @@ import { app as settings } from '../../package.json';
 
 import '../ui/styles/styles.scss';
 
-// Virtual module, see webpack-virtual-modules usage in webpack.run.js
-export { default as backendReloadCount } from 'backend_reload'; // eslint-disable-line import/no-unresolved
-
 // Favicon.ico should not be hashed, since some browsers expect it to be exactly on /favicon.ico URL
 require('!file-loader?name=[name].[ext]!../assets/favicon.ico'); // eslint-disable-line import/no-webpack-loader-syntax
 

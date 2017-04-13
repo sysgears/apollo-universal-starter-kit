b2e20a572a796b7cc97a428ebe42e7e3705b29c2
Victor Vlasenko <victor.vlasenko@sysgears.com>
2017-04-13 12:21:34 +0300

    Update GraphQL tests to use latest Apollo mocking API

--- src/server/modules/counter/tests/graphql.test.js
+++ src/server/modules/counter/tests/graphql.test.js
@@ -1,11 +1,12 @@
 import chai from "chai";
 import chaiAsPromised from "chai-as-promised";
-import { mockServer } from "graphql-tools";
+import { graphql } from "graphql";
+import { makeExecutableSchema, addMockFunctionsToSchema } from "graphql-tools";
 
 import AMOUNT_QUERY from '!raw-loader!client/modules/counter/graphql/count_get.graphql';
 import ADD_COUNT_MUTATION from "!raw-loader!client/modules/counter/graphql/count_add_mutation.graphql";
 import rootSchema from "server/api/root_schema.graphqls";
-import schema from "../schema.graphqls";
+import counterSchema from "../schema.graphqls";
 
 chai.use(chaiAsPromised);
 chai.should();
@@ -13,7 +14,7 @@ chai.should();
 const counterValue = 10;
 const increasedValue = 11;
 
-const mockedServer = mockServer(rootSchema + schema, {
+const mocks = {
   Count: () => ({
     amount: counterValue
   }),
@@ -22,18 +23,20 @@ const mockedServer = mockServer(rootSchema + schema, {
       amount: amount
     })
   })
-});
+};
+
+const schema = makeExecutableSchema({ typeDefs: [rootSchema, counterSchema] });
+
+addMockFunctionsToSchema({schema, mocks});
 
 describe('Counter', () => {
   it('should get the current value of a counter', () => {
-    return mockedServer
-      .query(AMOUNT_QUERY)
+    return graphql(schema, AMOUNT_QUERY)
       .then(value => value.data.count.amount)
       .should.eventually.equal(counterValue);
   });
   it('should return a new value of a counter after a mutation', () => {
-    return mockedServer
-      .query(ADD_COUNT_MUTATION, { "amount": increasedValue })
+    return graphql(schema, ADD_COUNT_MUTATION, null, null, { "amount": increasedValue })
       .then(value => value.data.addCount.amount)
       .should.eventually.equal(increasedValue);
   });

4df74130bfd42ae49ecc220b404ad926c469f37a
Victor Vlasenko <victor.vlasenko@sysgears.com>
2017-05-11 18:32:32 +0300

    Set PORT for tests from npm scripts

--- src/server/test-helpers/integration_setup.js
+++ src/server/test-helpers/integration_setup.js
@@ -19,14 +19,11 @@ before(async () => {
   await knex.migrate.latest();
   await knex.seed.run();
 
-  const TEST_PORT = 7070;
-  process.env['PORT'] = TEST_PORT;
-
   server = require('../api_server').default;
-  const wsClient = new subscriptions.SubscriptionClient(`ws://localhost:${TEST_PORT}`, {}, WebSocket);
+  const wsClient = new subscriptions.SubscriptionClient(`ws://localhost:${process.env['PORT']}`, {}, WebSocket);
 
   const networkInterface = subscriptions.addGraphQLSubscriptions(
-    createNetworkInterface({ uri: `http://localhost:${TEST_PORT}/graphql` }),
+    createNetworkInterface({ uri: `http://localhost:${process.env['PORT']}/graphql` }),
     wsClient
   );
 

2f9f9e5122d1b14becf66cd4ae2b33173ea2215e
Victor Vlasenko <victor.vlasenko@sysgears.com>
2017-05-20 18:28:33 +0300

    Implement Expo entry point for mobile bundles

--- src/mobile/index.js
+++ src/mobile/index.js
@@ -1,1 +1,26 @@
-console.log('Hello, from React Native!');
+import Expo from 'expo';
+import React from 'react';
+import { View } from 'react-native';
+
+const App = () => {
+  return React.createElement('div', null, 'Hello, from React Native!');
+};
+
+// we don't want this to require transformation
+class AwakeInDevApp extends React.Component {
+  render() {
+    return React.createElement(
+      View,
+      {
+        style: {
+          flex: 1,
+        },
+      },
+      React.createElement(App),
+      React.createElement(process.env.NODE_ENV === 'development' ? Expo.KeepAwake : View)
+    );
+  }
+}
+
+Expo.registerRootComponent(AwakeInDevApp);
+

7284d97f1d1ab4f956f716d81da631c7b212c070
Victor Vlasenko <victor.vlasenko@sysgears.com>
2017-06-02 11:38:12 +0300

    Add support for older Node versions

--- tools/webpack.entry.js
+++ tools/webpack.entry.js
@@ -1,9 +1,10 @@
 require('babel-register')({
-  presets: ['es2015', 'flow'],
+  presets: ['es2015', 'stage-0', 'flow'],
   ignore: /node_modules(?!\/haul-cli)/,
   retainLines: true,
   sourceMaps: 'inline',
 });
+require('babel-polyfill');
 
 if (process.argv.indexOf('--webpack-config') >= 0 ||
   process.argv.indexOf('--ext') >= 0) {

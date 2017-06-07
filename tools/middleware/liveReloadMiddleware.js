4ae30d148116fa47460e08753cb64fe2d614b8a8
Victor Vlasenko <victor.vlasenko@sysgears.com>
2017-06-07 20:04:13 +0300

    Use slower but safe Live Reload RN impl for now

--- tools/middleware/liveReloadMiddleware.js
+++ tools/middleware/liveReloadMiddleware.js
@@ -1,33 +1,51 @@
+const safeWay = true;
+
+function notifyWatcher(watcher) {
+  const headers = {
+    'Content-Type': 'application/json; charset=UTF-8',
+  };
+
+  watcher.res.writeHead(205, headers);
+  watcher.res.end(JSON.stringify({ changed: true }));
+}
+
 function liveReloadMiddleware(compiler) {
   let watchers = [];
+  let notify = false;
 
   compiler.plugin('done', () => {
-    const headers = {
-      'Content-Type': 'application/json; charset=UTF-8',
-    };
+    if (safeWay) {
+      notify = true;
+    } else {
+      watchers.forEach(watcher => {
+        notifyWatcher(watcher);
+      });
 
-    watchers.forEach(watcher => {
-      watcher.res.writeHead(205, headers);
-      watcher.res.end(JSON.stringify({ changed: true }));
-    });
-
-    watchers = [];
+      watchers = [];
+    }
   });
 
   return (req, res, next) => {
     if (req.path === '/onchange') {
       const watcher = { req, res };
 
-      watchers.push(watcher);
-
-      req.on('close', () => {
-        for (let i = 0; i < watchers.length; i++) {
-          if (watchers[i] && watchers[i].req === req) {
-            watchers.splice(i, 1);
-            break;
-          }
+      if (safeWay) {
+        if (notify) {
+          notifyWatcher(watcher);
+          notify = false;
         }
-      });
+      } else {
+        watchers.push(watcher);
+
+        req.on('close', () => {
+          for (let i = 0; i < watchers.length; i++) {
+            if (watchers[i] && watchers[i].req === req) {
+              watchers.splice(i, 1);
+              break;
+            }
+          }
+        });
+      }
     } else {
       next();
     }

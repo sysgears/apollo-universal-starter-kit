afddba4d429a5ff5230482e8da717665656f4646
Ujjwal <ujjwal.mairh@gmail.com>
2017-06-08 17:15:26 +0300

    Non existing router link

---
+++ src/client/modules/page-not-found/containers/page-not-found.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import Helmet from 'react-helmet';
+import { Button } from 'reactstrap';
+
+const pageNotFound = () => (
+  <section className="text-center mt-4 mb-4">
+    <Helmet
+      title='Apollo Starter Kit - Page not found'
+      meta={[{
+        name: 'description',
+        content: 'Apollo Starter Kit - Page not found'
+      }]}/>
+    <h2>Page not found - 404</h2>
+    <Link to="/">
+      <Button className='home-link' color="primary">Go to Homepage</Button>
+    </Link>
+  </section>
+);
+
+export default pageNotFound;

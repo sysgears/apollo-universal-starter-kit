e84b2c80e4818896cea1c746809031d87a834a80
Ujjwal <ujjwal.mairh@gmail.com>
2017-06-19 11:03:00 +0300

    Add support for DB other than SQlite for seeding

--- src/server/database/seeds/post.js
+++ src/server/database/seeds/post.js
@@ -5,13 +5,13 @@ export async function seed(knex, Promise) {
   ]);
 
   await Promise.all([...Array(20).keys()].map(async ii => {
-    const post = await knex('post').insert({
+    const post = await knex('post').returning('id').insert({
       title: `Post title ${ii + 1}`,
       content: `Post content ${ii + 1}`
     });
 
     await Promise.all([...Array(2).keys()].map(async jj => {
-      return knex('comment').insert({
+      return knex('comment').returning('id').insert({
         post_id: post[0],
         content: `Comment title ${jj + 1} for post ${post[0]}`
       });

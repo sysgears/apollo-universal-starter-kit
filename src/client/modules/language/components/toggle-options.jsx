46ea687929f409be67e7acf4b415cc70190f9a9a
Ujjwal <ujjwal.mairh@gmail.com>
2017-07-12 15:59:17 +0300

    WIP - intl

---
+++ src/client/modules/language/components/toggle-options.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { injectIntl, intlShape } from 'react-intl';
+
+const ToggleOption = ({ value, message, intl }) => (
+  <option value={value}>
+    {message ? intl.formatMessage(message) : value}
+  </option>
+);
+
+ToggleOption.propTypes = {
+  value: PropTypes.string.isRequired,
+  message: PropTypes.object,
+  intl: intlShape.isRequired,
+};
+
+export default injectIntl(ToggleOption);

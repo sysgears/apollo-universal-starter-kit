3cac021c3e663ad1791515b93164e81cfbb54a02
Ujjwal <ujjwal.mairh@gmail.com>
2017-09-18 14:43:24 +0300

    lint - prettier fix

--- settings.js
+++ settings.js
@@ -4,7 +4,7 @@ import { app as settings } from './app.json';
 const envSettings = Object.assign(
   {},
   _.pickBy(settings, (v, k) => k !== 'env'),
-  _.get(settings, "env." + process.env.NODE_ENV)
+  _.get(settings, 'env.' + process.env.NODE_ENV)
 );
 
 export default envSettings;

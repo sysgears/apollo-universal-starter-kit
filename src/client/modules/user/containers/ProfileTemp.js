6f698308b4ef9a6f7e7f00b10ff69fab79342a01
Ujjwal <ujjwal.mairh@gmail.com>
2017-09-18 15:09:26 +0300

    temp name casing

---
+++ src/client/modules/user/containers/ProfileTemp.js
@@ -0,0 +1,32 @@
+/*eslint-disable no-unused-vars*/
+// React
+import React from 'react';
+import PropTypes from 'prop-types';
+
+// Apollo
+import { graphql, compose } from 'react-apollo';
+
+// Components
+import ProfileView from '../components/ProfileView.jsx';
+
+import CURRENT_USER from '../graphql/currentUser.graphql';
+
+class Profile extends React.Component {
+  render() {
+    return <ProfileView {...this.props} />;
+  }
+}
+
+Profile.propTypes = {
+  loading: PropTypes.bool.isRequired,
+  currentUser: PropTypes.object
+};
+
+export default compose(
+  graphql(CURRENT_USER, {
+    options: { fetchPolicy: 'network-only' },
+    props({ data: { loading, currentUser } }) {
+      return { loading, currentUser };
+    }
+  })
+)(Profile);
---
+++ src/mobile/AppTemp.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import { ApolloProvider } from 'react-apollo';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { reducer as formReducer } from 'redux-form';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import ApolloClient from 'apollo-client';
+import { SubscriptionClient } from 'subscriptions-transport-ws';
+
+import modules from '../client/modules';
+import MainScreenNavigator from '../client/app/Routes';
+
+const networkInterface = new SubscriptionClient(
+  __BACKEND_URL__.replace(/^http/, 'ws'),
+  {
+    reconnect: true
+  }
+);
+
+const client = new ApolloClient({
+  networkInterface
+});
+
+const store = createStore(
+  combineReducers({
+    apollo: client.reducer(),
+    form: formReducer,
+
+    ...modules.reducers
+  }),
+  {}, // initial state
+  composeWithDevTools(applyMiddleware(client.middleware()))
+);
+
+export default class Main extends Component {
+  render() {
+    return (
+      <ApolloProvider store={store} client={client}>
+        <MainScreenNavigator />
+      </ApolloProvider>
+    );
+  }
+}

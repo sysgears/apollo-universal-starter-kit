1e41e0db08432b6b39819abab3aa215065aadcc6
Victor Vlasenko <victor.vlasenko@sysgears.com>
2017-09-19 17:45:00 +0300

    Add hello world Angular 4 component

--- src/client/index.ts
+++ src/client/index.ts
@@ -1,5 +1,19 @@
-(function () {
-    const str: string = "web client";
+import { Component, NgModule } from "@angular/core";
+import { platformBrowserDynamic } from "@angular/platform-browser-dynamic";
+import { BrowserModule } from "@angular/platform-browser";
 
-    console.log(`Hello, ${str}`);
-})();
+@Component({
+  selector: "#content",
+  template: "<h1>Hello World!</h1><br/><a href='/graphiql'>GraphiQL</a>"
+})
+export class AppComponent {
+}
+
+@NgModule({
+  imports: [BrowserModule],
+  bootstrap: [AppComponent],
+  declarations: [AppComponent]
+})
+export class AppModule {}
+
+platformBrowserDynamic().bootstrapModule(AppModule);

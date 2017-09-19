a921401ff62847386b0aecc8acf83c77c88d7d08
Victor Vlasenko <victor.vlasenko@sysgears.com>
2017-09-19 15:26:10 +0300

    Add ISettings type to shut up IDE warning

--- settings.ts
+++ settings.ts
@@ -2,10 +2,14 @@ import * as _ from "lodash";
 
 const settings = require("./app.json").app;
 
-const envSettings: any = Object.assign(
+type ISettings = {
+  apolloLogging: boolean,
+}
+
+const envSettings: ISettings = Object.assign(
   {},
   _.pickBy(settings, (v, k) => k !== "env"),
   _.get(settings, "env." + process.env.NODE_ENV)
-);
+) as ISettings;
 
-export default envSettings as any;
+export default envSettings;

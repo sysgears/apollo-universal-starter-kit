2e646230189193f0c6efff4e02ac5355189c42fa
Mitja Debeljak <mitjade@gmail.com>
2017-09-20 14:41:38 +0200

    extract pubsub into separate file

--- src/server/api/schema.js
+++ src/server/api/schema.js
@@ -1,13 +1,9 @@
 import { makeExecutableSchema, addErrorLoggingToSchema } from 'graphql-tools';
-import { addApolloLogging } from 'apollo-logger';
-import { PubSub } from 'graphql-subscriptions';
 
 import rootSchemaDef from './rootSchema.graphqls';
 import modules from '../modules';
+import pubsub from './pubsub';
 import log from '../../common/log';
-import settings from '../../../settings';
-
-export const pubsub = settings.apolloLogging ? addApolloLogging(new PubSub()) : new PubSub();
 
 const executableSchema = makeExecutableSchema({
   typeDefs: [rootSchemaDef].concat(modules.schemas),

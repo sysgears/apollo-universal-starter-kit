dc5364c04663411599919118510c81273ae8169a
Victor Vlasenko <victor.vlasenko@sysgears.com>
2017-09-26 09:27:52 +0300

    Upgrade to flow-bin@0.52

--- flow-typed/npm/body-parser_v1.x.x.js
+++ flow-typed/npm/body-parser_v1.x.x.js
@@ -4,43 +4,40 @@
 import type { Middleware, $Request, $Response } from 'express';
 
 declare type bodyParser$Options = {
-  inflate?: boolean,
-  limit?: number | string,
-  type?: string | string[] | ((req: $Request) => any),
-  verify?: (
-    req: $Request,
-    res: $Response,
-    buf: Buffer,
-    encoding: string
-  ) => void
+  inflate?: boolean;
+  limit?: number | string;
+  type?: string | string[] | ((req: $Request) => any);
+  verify?: (req: $Request, res: $Response, buf: Buffer, encoding: string) => void;
 };
 
 declare type bodyParser$OptionsText = bodyParser$Options & {
-  reviver?: (key: string, value: any) => any,
-  strict?: boolean
+  reviver?: (key: string, value: any) => any;
+  strict?: boolean;
 };
 
 declare type bodyParser$OptionsJson = bodyParser$Options & {
-  reviver?: (key: string, value: any) => any,
-  strict?: boolean
+  reviver?: (key: string, value: any) => any;
+  strict?: boolean;
 };
 
 declare type bodyParser$OptionsUrlencoded = bodyParser$Options & {
-  extended?: boolean,
-  parameterLimit?: number
+  extended?: boolean;
+  parameterLimit?: number;
 };
 
-declare module 'body-parser' {
-  declare type Options = bodyParser$Options;
-  declare type OptionsText = bodyParser$OptionsText;
-  declare type OptionsJson = bodyParser$OptionsJson;
-  declare type OptionsUrlencoded = bodyParser$OptionsUrlencoded;
+declare module "body-parser" {
 
-  declare function json(options?: OptionsJson): Middleware;
+    declare type Options = bodyParser$Options;
+    declare type OptionsText = bodyParser$OptionsText;
+    declare type OptionsJson = bodyParser$OptionsJson;
+    declare type OptionsUrlencoded = bodyParser$OptionsUrlencoded;
 
-  declare function raw(options?: Options): Middleware;
+    declare function json(options?: OptionsJson): Middleware;
 
-  declare function text(options?: OptionsText): Middleware;
+    declare function raw(options?: Options): Middleware;
 
-  declare function urlencoded(options?: OptionsUrlencoded): Middleware;
+    declare function text(options?: OptionsText): Middleware;
+
+    declare function urlencoded(options?: OptionsUrlencoded): Middleware;
+
 }

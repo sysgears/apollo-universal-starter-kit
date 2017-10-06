cc7c3d2ba9e7a47d5ebc4f30ec11ef5df0450795
Ujjwal <ujjwal.mairh@gmail.com>
2017-10-06 11:48:08 +0300

    Pass env vars as strings

--- config/mailer.js
+++ config/mailer.js
@@ -1,8 +1,8 @@
 export default {
-  host: process.env.EMAIL_HOST,
-  port: process.env.EMAIL_PORT,
+  host: `${process.env.EMAIL_HOST}`,
+  port: `${process.env.EMAIL_PORT}`,
   auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASSWORD
+    user: `${process.env.EMAIL_USER}`,
+    pass: `${process.env.EMAIL_PASSWORD}`
   }
 };
--- config/user.js
+++ config/user.js
@@ -1,5 +1,5 @@
 export default {
-  secret: process.env.AUTH_SECRET,
+  secret: `${process.env.AUTH_SECRET}`,
   confirm: true,
   sendConfirmationEmail: true,
   auth: {

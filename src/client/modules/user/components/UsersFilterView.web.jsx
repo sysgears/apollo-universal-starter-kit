6bf168184be1c6c0cbc135687464926ef31da512
Mitja Debeljak <mitjade@gmail.com>
2017-10-10 15:59:35 +0200

    Revert "debounce searchText (not working)"
    This reverts commit 9e75530e301a273efda6e19db82b609ab43b1bf7.

--- src/client/modules/user/components/UsersFilterView.web.jsx
+++ src/client/modules/user/components/UsersFilterView.web.jsx
@@ -2,22 +2,11 @@
 // React
 import React from 'react';
 import PropTypes from 'prop-types';
-import debounce from 'lodash';
 
 class UsersFilterView extends React.Component {
-  state = {
-    searchText: this.props.searchText
-  };
-
-  doSearch = debounce(() => {
-    const { onSearchTextChange } = this.props;
-    onSearchTextChange(this.state.searchText);
-  }, 300);
-
   handleSearch = event => {
-    this.setState({ searchText: event.target.value }, () => {
-      this.doSearch();
-    });
+    const { onSearchTextChange } = this.props;
+    onSearchTextChange(event.target.value);
   };
 
   handleIsAdmin = () => {
@@ -26,14 +15,14 @@ class UsersFilterView extends React.Component {
   };
 
   render() {
-    const { isAdmin } = this.props;
+    const { searchText, isAdmin } = this.props;
     return (
       <form className="form-inline">
         <label className="mr-sm-2">Filter: </label>
         <input
           type="text"
           className="form-control mb-2 mr-sm-2 mb-sm-0"
-          value={this.state.searchText}
+          value={searchText}
           onChange={this.handleSearch}
           placeholder="Search"
         />

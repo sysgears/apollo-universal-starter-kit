60572841774b868dfc66930ac59b510a40360994
Mitja Debeljak <mitjade@gmail.com>
2017-10-12 16:21:16 +0200

    fix for #426

--- src/server/modules/user/token.js
+++ src/server/modules/user/token.js
@@ -5,18 +5,6 @@ import { refreshTokens, tryLoginSerial } from './auth';
 export default (SECRET, User, jwt) => async (req, res, next) => {
   let token = req.universalCookies.get('x-token') || req.headers['x-token'];
 
-  // if header available
-  if (req.headers['x-token']) {
-    // check if header token matches cookie token
-    if (
-      req.headers['x-token'] !== req.universalCookies.get('x-token') ||
-      req.headers['x-refresh-token'] !== req.universalCookies.get('x-refresh-token')
-    ) {
-      // if x-token is not empty and not the same as cookie x-token revoke authentication
-      token = undefined;
-    }
-  }
-
   // if cookie available
   if (req.universalCookies.get('x-token')) {
     // check if header token matches cookie token

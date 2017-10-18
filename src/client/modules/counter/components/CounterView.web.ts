11ea697ca1b55a56489aa54d77049c69b43fa1ac
Evgenij Olshanskij <gekanchiko@gmail.com>
2017-10-18 15:33:55 +0300

    Fix the page reload bug

--- src/client/modules/counter/components/CounterView.web.ts
+++ src/client/modules/counter/components/CounterView.web.ts
@@ -1,4 +1,4 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component, NgZone, OnDestroy, OnInit } from '@angular/core';
 import { CounterService } from '../containers/Counter';
 import { COUNTER } from '../reducers/actionTypes';
 import { counterStore } from '../reducers/index';
@@ -13,7 +13,7 @@ export default class CounterView implements OnInit, OnDestroy {
   public counter: any;
   public reduxCount: number;
 
-  constructor(private counterService: CounterService) {
+  constructor(private counterService: CounterService, private ngZone: NgZone) {
     this.setReduxCount();
   }
 
@@ -51,8 +51,10 @@ export default class CounterView implements OnInit, OnDestroy {
   };
 
   private getCounterCb = (res: any) => {
-    this.counter = res.data.counter;
-    this.loading = res.loading;
+    this.ngZone.run(() => {
+      this.counter = res.data.counter;
+      this.loading = res.loading || false;
+    });
   };
 
   private addCounterCb = (res: any) => {

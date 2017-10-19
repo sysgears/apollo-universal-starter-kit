988cf3ceff7564ab522f5d994a862540d9c9fbb0
Evgenij Olshanskij <gekanchiko@gmail.com>
2017-10-19 10:46:05 +0300

    Modify buttons

--- src/client/modules/pageNotFound/containers/PageNotFound.ts
+++ src/client/modules/pageNotFound/containers/PageNotFound.ts
@@ -6,7 +6,7 @@ import '../../../styles/styles.scss';
   template: `
     <section class="text-center mt-4 mb-4">
     <h2>Page not found - 404</h2>
-    <a href="/" class="home-link btn btn-primary">Go to Homepage</a>
+    <a href="/" class="btn-primary" ngbButtonLabel>Go to Homepage</a>
   </section>
   `
 })

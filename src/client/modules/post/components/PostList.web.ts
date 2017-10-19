8ef1b3d51a8e75184fb53f8f6eded766d7fb7758
Vsevolod Bondar <vsevolod.bondar@sysgears.com>
2017-10-19 11:03:14 +0300

    Update delete post functionality

--- src/client/modules/post/components/PostList.web.ts
+++ src/client/modules/post/components/PostList.web.ts
@@ -1,4 +1,5 @@
 import { Component, NgZone, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs/Subscription';
 import { PostService } from '../containers/Post';
 
 @Component({
@@ -37,12 +38,20 @@ export class PostList implements OnInit, OnDestroy {
   public loading: boolean = true;
   public posts: any;
   public endCursor = 0;
+  private subscription: Subscription = null;
 
   constructor(private postService: PostService, private ngZone: NgZone) {}
 
   public ngOnInit() {
-    this.postService.subscribeToPostList(this.endCursor);
-    this.postService.getPosts(this.getPostsCb);
+    this.subscription = this.postService.getPosts().subscribe({
+      next: ({ data, loading }) => {
+        this.ngZone.run(() => {
+          this.posts = data.posts;
+          this.loading = loading;
+          this.endCursor = this.posts.pageInfo.endCursor;
+        });
+      }
+    });
   }
 
   public renderPosts() {
@@ -52,7 +61,7 @@ export class PostList implements OnInit, OnDestroy {
   }
 
   public loadMoreRows() {
-    this.postService.loadMoreRows(this.posts.pageInfo.endCursor);
+    this.postService.loadMoreRows(this.endCursor);
   }
 
   public hasNextPage() {
@@ -64,103 +73,6 @@ export class PostList implements OnInit, OnDestroy {
   }
 
   public ngOnDestroy(): void {
-    this.postService.unsubscribe();
+    this.subscription.unsubscribe();
   }
-
-  /* Callbacks */
-
-  private getPostsCb = (res: any) => {
-    this.ngZone.run(() => {
-      this.posts = res.data.posts;
-      this.loading = res.loading;
-      this.endCursor = this.posts.pageInfo.endCursor;
-    });
-  };
 }
-
-// import React from 'react';
-// import PropTypes from 'prop-types';
-// import Helmet from 'react-helmet';
-// import { Link } from 'react-router-dom';
-// import { ListGroup, ListGroupItem, Button } from 'reactstrap';
-
-// import PageLayout from '../../../app/PageLayout';
-//
-// function renderPosts(posts, deletePost) {
-//     return posts.edges.map(({ node: { id, title } }) => {
-//         return (
-//             <ListGroupItem className="d-flex justify-content-between" key={id}>
-//         <div>
-//             <Link className="post-link" to={`/post/${id}`}>
-//         {title}
-//         </Link>
-//         </div>
-//         <div>
-//         <span className="badge badge-secondary delete-button" onClick={deletePost(id)}>
-//             Delete
-//             </span>
-//             </div>
-//             </ListGroupItem>
-//     );
-//     });
-// }
-//
-// function renderLoadMore(posts, loadMoreRows) {
-//     if (posts.pageInfo.hasNextPage) {
-//         return (
-//             <Button id="load-more" color="primary" onClick={loadMoreRows}>
-//             Load more ...
-//         </Button>
-//     );
-//     }
-// }
-//
-// const PostList = ({ loading, posts, deletePost, loadMoreRows }) => {
-//     const renderMetaData = () => (
-//         <Helmet
-//             title="Apollo Starter Kit - Posts list"
-//     meta={[
-//         {
-//             name: 'description',
-//             content: 'Apollo Fullstack Starter Kit - List of all posts example page'
-//         }
-//         ]}
-//     />
-// );
-//
-//     if (loading) {
-//         return (
-//             <PageLayout>
-//                 {renderMetaData()}
-//             <div className="text-center">Loading...</div>
-//         </PageLayout>
-//     );
-//     } else {
-//         return (
-//             <PageLayout>
-//                 {renderMetaData()}
-//             <h2>Posts</h2>
-//             <Link to="/post/0">
-//         <Button color="primary">Add</Button>
-//             </Link>
-//             <h1 />
-//             <ListGroup>{renderPosts(posts, deletePost)}</ListGroup>
-//             <div>
-//             <small>
-//                 ({posts.edges.length} / {posts.totalCount})
-//         </small>
-//         </div>
-//         {renderLoadMore(posts, loadMoreRows)}
-//         </PageLayout>
-//     );
-//     }
-// };
-//
-// PostList.propTypes = {
-//     loading: PropTypes.bool.isRequired,
-//     posts: PropTypes.object,
-//     deletePost: PropTypes.func.isRequired,
-//     loadMoreRows: PropTypes.func.isRequired
-// };
-
-// export default PostList;
--- src/client/modules/post/containers/Post.ts
+++ src/client/modules/post/containers/Post.ts
@@ -1,12 +1,8 @@
 import { Injectable } from '@angular/core';
 import { Apollo, ApolloQueryObservable } from 'apollo-angular';
 
-import { Observable } from 'rxjs/Observable';
-import { Subscription } from 'rxjs/Subscription';
-
 import * as DELETE_POST from '../graphql/DeletePost.graphql';
 import * as POSTS_QUERY from '../graphql/PostsQuery.graphql';
-import * as POSTS_SUBSCRIPTION from '../graphql/PostsSubscription.graphql';
 
 export function AddPost(prev: any, node: any) {
   // ignore if duplicate
@@ -34,28 +30,15 @@ export function AddPost(prev: any, node: any) {
 
 @Injectable()
 export class PostService {
-  private subsOnUpdate: Subscription;
-  private subsOnLoad: Subscription;
-  private subsOnDelete: Subscription;
   private getPostsQuery: ApolloQueryObservable<any>;
 
   constructor(private apollo: Apollo) {}
 
-  public subscribeToPostList(endCursor: any) {
-    this.subsOnUpdate = this.apollo
-      .subscribe({
-        query: POSTS_SUBSCRIPTION,
-        variables: { endCursor }
-      })
-      .subscribe();
-  }
-
-  public getPosts(callback: (result: any) => any) {
-    this.getPostsQuery = this.apollo.watchQuery({
+  public getPosts() {
+    return (this.getPostsQuery = this.apollo.watchQuery({
       query: POSTS_QUERY,
       variables: { limit: 10, after: 0 }
-    });
-    this.subsOnLoad = this.subscribe(this.getPostsQuery, callback);
+    }));
   }
 
   public loadMoreRows(endCursor: number) {
@@ -83,7 +66,7 @@ export class PostService {
   }
 
   public deletePost(id: number) {
-    this.subsOnDelete = this.apollo
+    const subsOnDelete = this.apollo
       .mutate({
         mutation: DELETE_POST,
         variables: { id },
@@ -107,206 +90,6 @@ export class PostService {
         }
       })
       .subscribe();
-    this.subsOnDelete.unsubscribe();
-  }
-
-  public unsubscribe() {
-    this.subsOnUpdate.unsubscribe();
-    this.subsOnLoad.unsubscribe();
-  }
-
-  private subscribe(observable: Observable<any>, cb: (result: Observable<any>) => any): Subscription {
-    const subscription = observable.subscribe({
-      next: result => {
-        try {
-          cb(result);
-        } catch (e) {
-          setImmediate(() => {
-            subscription.unsubscribe();
-          });
-        }
-      }
-    });
-    return subscription;
+    subsOnDelete.unsubscribe();
   }
 }
-
-// import React from 'react';
-// import PropTypes from 'prop-types';
-// import { graphql, compose } from 'react-apollo';
-// import update from 'immutability-helper';
-//
-// import PostList from '../components/PostList';
-//
-// import POSTS_QUERY from '../graphql/PostsQuery.graphql';
-// import POSTS_SUBSCRIPTION from '../graphql/PostsSubscription.graphql';
-// import DELETE_POST from '../graphql/DeletePost.graphql';
-//
-// export function AddPost(prev, node) {
-//     // ignore if duplicate
-//     if (node.id !== null && prev.posts.edges.some(post => node.id === post.cursor)) {
-//         return prev;
-//     }
-//
-//     const edge = {
-//         cursor: node.id,
-//         node: node,
-//         __typename: 'PostEdges'
-//     };
-//
-//     return update(prev, {
-//         posts: {
-//             totalCount: {
-//                 $set: prev.posts.totalCount + 1
-//             },
-//             edges: {
-//                 $unshift: [edge]
-//             }
-//         }
-//     });
-// }
-//
-// function DeletePost(prev, id) {
-//     const index = prev.posts.edges.findIndex(x => x.node.id === id);
-//
-//     // ignore if not found
-//     if (index < 0) {
-//         return prev;
-//     }
-//
-//     return update(prev, {
-//         posts: {
-//             totalCount: {
-//                 $set: prev.posts.totalCount - 1
-//             },
-//             edges: {
-//                 $splice: [[index, 1]]
-//             }
-//         }
-//     });
-// }
-//
-// class Post extends React.Component {
-//     constructor(props) {
-//         super(props);
-//
-//         this.subscription = null;
-//     }
-//
-//     componentWillReceiveProps(nextProps) {
-//         if (!nextProps.loading) {
-//             const endCursor = this.props.posts ? this.props.posts.pageInfo.endCursor : 0;
-//             const nextEndCursor = nextProps.posts.pageInfo.endCursor;
-//
-//             // Check if props have changed and, if necessary, stop the subscription
-//             if (this.subscription && endCursor !== nextEndCursor) {
-//                 this.subscription();
-//                 this.subscription = null;
-//             }
-//
-//             // Subscribe or re-subscribe
-//             if (!this.subscription) {
-//                 this.subscribeToPostList(nextEndCursor);
-//             }
-//         }
-//     }
-//
-//     subscribeToPostList = endCursor => {
-//         const { subscribeToMore } = this.props;
-//
-//         this.subscription = subscribeToMore({
-//             document: POSTS_SUBSCRIPTION,
-//             variables: { endCursor },
-//             updateQuery: (prev, { subscriptionData: { data: { postsUpdated: { mutation, node } } } }) => {
-//                 let newResult = prev;
-//
-//                 if (mutation === 'CREATED') {
-//                     newResult = AddPost(prev, node);
-//                 } else if (mutation === 'DELETED') {
-//                     newResult = DeletePost(prev, node.id);
-//                 }
-//
-//                 return newResult;
-//             }
-//         });
-//     };
-//
-//     componentWillUnmount() {
-//         if (this.subscription) {
-//             // unsubscribe
-//             this.subscription();
-//         }
-//     }
-//
-//     render() {
-//         return <PostList {...this.props} />;
-//     }
-// }
-//
-// Post.propTypes = {
-//     loading: PropTypes.bool.isRequired,
-//     posts: PropTypes.object,
-//     deletePost: PropTypes.func.isRequired,
-//     loadMoreRows: PropTypes.func.isRequired,
-//     subscribeToMore: PropTypes.func.isRequired
-// };
-//
-// export default compose(
-//     graphql(POSTS_QUERY, {
-//         options: () => {
-//             return {
-//                 variables: { limit: 10, after: 0 }
-//             };
-//         },
-//         props: ({ data }) => {
-//             const { loading, posts, fetchMore, subscribeToMore } = data;
-//             const loadMoreRows = () => {
-//                 return fetchMore({
-//                     variables: {
-//                         after: posts.pageInfo.endCursor
-//                     },
-//                     updateQuery: (previousResult, { fetchMoreResult }) => {
-//                         const totalCount = fetchMoreResult.posts.totalCount;
-//                         const newEdges = fetchMoreResult.posts.edges;
-//                         const pageInfo = fetchMoreResult.posts.pageInfo;
-//
-//                         return {
-//                             // By returning `cursor` here, we update the `fetchMore` function
-//                             // to the new cursor.
-//                             posts: {
-//                                 totalCount,
-//                                 edges: [...previousResult.posts.edges, ...newEdges],
-//                                 pageInfo,
-//                                 __typename: 'Posts'
-//                             }
-//                         };
-//                     }
-//                 });
-//             };
-//
-//             return { loading, posts, subscribeToMore, loadMoreRows };
-//         }
-//     }),
-//     graphql(DELETE_POST, {
-//         props: ({ mutate }) => ({
-//             deletePost(id) {
-//                 return () =>
-//                     mutate({
-//                         variables: { id },
-//                         optimisticResponse: {
-//                             __typename: 'Mutation',
-//                             deletePost: {
-//                                 id: id,
-//                                 __typename: 'Post'
-//                             }
-//                         },
-//                         updateQueries: {
-//                             posts: (prev, { mutationResult: { data: { deletePost } } }) => {
-//                                 return DeletePost(prev, deletePost.id);
-//                             }
-//                         }
-//                     });
-//             }
-//         })
-//     })
-// )(Post);

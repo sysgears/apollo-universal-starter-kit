f2ac889e36c1b4491f8594f04dfcb266c8c34f6d
mitjade <mitjade@users.noreply.github.com>
2017-10-22 18:17:53 +0200

    404 status if page not found (#457)
    * 404 status if page not found

--- src/client/modules/pageNotFound/containers/PageNotFound.jsx
+++ src/client/modules/pageNotFound/containers/PageNotFound.jsx
@@ -1,29 +1,37 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import Helmet from 'react-helmet';
 import { Button } from 'reactstrap';
 import PageLayout from '../../../app/PageLayout';
 
-const PageNotFound = () => (
-  <PageLayout>
-    <section className="text-center mt-4 mb-4">
-      <Helmet
-        title="Apollo Starter Kit - Page not found"
-        meta={[
-          {
-            name: 'description',
-            content: 'Apollo Starter Kit - Page not found'
-          }
-        ]}
-      />
-      <h2>Page not found - 404</h2>
-      <Link to="/">
-        <Button className="home-link" color="primary">
-          Go to Homepage
-        </Button>
-      </Link>
-    </section>
-  </PageLayout>
-);
+const PageNotFound = ({ staticContext = {} }) => {
+  staticContext.pageNotFound = true;
+  return (
+    <PageLayout>
+      <section className="text-center mt-4 mb-4">
+        <Helmet
+          title="Apollo Starter Kit - Page not found"
+          meta={[
+            {
+              name: 'description',
+              content: 'Apollo Starter Kit - Page not found'
+            }
+          ]}
+        />
+        <h2>Page not found - 404</h2>
+        <Link to="/">
+          <Button className="home-link" color="primary">
+            Go to Homepage
+          </Button>
+        </Link>
+      </section>
+    </PageLayout>
+  );
+};
+
+PageNotFound.propTypes = {
+  staticContext: PropTypes.object
+};
 
 export default PageNotFound;

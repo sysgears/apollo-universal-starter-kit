5ef22422cbc5128e04706fa8d8032c96be4d2d26
Evgenij Olshanskij <gekanchiko@gmail.com>
2017-10-26 12:22:15 +0300

    Add types for 'password' as well as 'passport-facebook' packages

--- typings/typings.d.ts
+++ typings/typings.d.ts
@@ -26,6 +26,23 @@ declare module '*.json' {
   export = value;
 }
 
+declare module 'passport-facebook-ext' {
+  import { StrategyOption } from 'passport-facebook';
+
+  // Since there is no 'scope' variable in the StrategyOption interface
+  // we create the extension
+  export interface StrategyOptionWithScope extends StrategyOption {
+    clientID: string;
+    clientSecret: string;
+    callbackURL: string;
+    scope: string[];
+
+    scopeSeparator?: string;
+    enableProof?: boolean;
+    profileFields?: string[];
+  }
+}
+
 interface Window {
   __APOLLO_STATE__?: any;
 }

3f0e676c87db10072bd6e2d410096847973fa539
Vsevolod Bondar <vsevolod.bondar@sysgears.com>
2017-11-08 10:07:08 +0200

    Remove unused files from post module

--- src/client/modules/post/components/PostCommentsView.web.ts
+++ src/client/modules/post/components/PostCommentsView.web.ts
@@ -62,3 +62,90 @@ export default class PostCommentsView implements OnInit, OnDestroy {
     this.postCommentsService.deleteComment(id, this.post.id).subscribe();
   }
 }
+
+// import React from "react";
+// import PropTypes from "prop-types";
+// import { ListGroup, ListGroupItem, Button } from "reactstrap";
+//
+// import PostCommentForm from "./PostCommentForm";
+//
+// function renderComments(comments, onCommentSelect, comment, deleteComment) {
+//   return comments.map(({ id, content }) => {
+//     return (
+//       <ListGroupItem className="d-flex justify-content-between" key={id}>
+//     {content}
+//     <div>
+//     <Button color="primary" size="sm" className="badge badge-secondary edit-comment" onClick={() => onCommentSelect({ id, content })} href="#">
+//       Edit
+//       </Button>{' '}
+//       <Button
+//     color="primary"
+//     size="sm"
+//     className="badge badge-secondary delete-comment"
+//     onClick={() => onCommentDelete(comment, deleteComment, onCommentSelect, id)}
+//     href="#"
+//       >
+//       Delete
+//       </Button>
+//       </div>
+//       </ListGroupItem>
+//   );
+//   });
+// }
+//
+// function onCommentDelete(comment, deleteComment, onCommentSelect, id) {
+//   if (comment.id === id) {
+//     onCommentSelect({ id: null, content: "" });
+//   }
+//
+//   deleteComment(id);
+// }
+//
+// const onSubmit = (comment, postId, addComment, editComment, onCommentSelect, onFormSubmitted) => values => {
+//   if (comment.id === null) {
+//     addComment(values.content, postId);
+//   } else {
+//     editComment(comment.id, values.content);
+//   }
+//
+//   onCommentSelect({ id: null, content: "" });
+//   onFormSubmitted();
+// };
+//
+// const PostCommentsView = ({
+//                             postId,
+//                             comment,
+//                             addComment,
+//                             editComment,
+//                             comments,
+//                             onCommentSelect,
+//                             deleteComment,
+//                             onFormSubmitted
+//                           }) => {
+//   return (
+//     <div>
+//       <h3>Comments</h3>
+//     <PostCommentForm
+//   postId={postId}
+//   onSubmit={onSubmit(comment, postId, addComment, editComment, onCommentSelect, onFormSubmitted)}
+//   initialValues={comment}
+//   />
+//   <h1 />
+//   <ListGroup>{renderComments(comments, onCommentSelect, comment, deleteComment)}</ListGroup>
+//   </div>
+// );
+// };
+//
+// PostCommentsView.propTypes = {
+//   postId: PropTypes.number.isRequired,
+//   comments: PropTypes.array.isRequired,
+//   comment: PropTypes.object.isRequired,
+//   addComment: PropTypes.func.isRequired,
+//   editComment: PropTypes.func.isRequired,
+//   deleteComment: PropTypes.func.isRequired,
+//   onCommentSelect: PropTypes.func.isRequired,
+//   onFormSubmitted: PropTypes.func.isRequired,
+//   subscribeToMore: PropTypes.func.isRequired
+// };
+//
+// export default PostCommentsView;
--- src/client/modules/post/containers/PostComments.ts
+++ src/client/modules/post/containers/PostComments.ts
@@ -146,3 +146,192 @@ export default class PostCommentsService {
     });
   }
 }
+
+// import React from 'react';
+// import PropTypes from 'prop-types';
+// import { connect } from 'react-redux';
+// import { graphql, compose } from 'react-apollo';
+// import update from 'immutability-helper';
+// import { reset } from 'redux-form';
+//
+// import PostCommentsView from '../components/PostCommentsView';
+//
+// import ADD_COMMENT from '../graphql/AddComment.graphql';
+// import EDIT_COMMENT from '../graphql/EditComment.graphql';
+// import DELETE_COMMENT from '../graphql/DeleteComment.graphql';
+// import COMMENT_SUBSCRIPTION from '../graphql/CommentSubscription.graphql';
+//
+// function AddComment(prev, node) {
+//   // ignore if duplicate
+//   if (node.id !== null && prev.post.comments.some(comment => node.id === comment.id)) {
+//     return prev;
+//   }
+//
+//   return update(prev, {
+//     post: {
+//       comments: {
+//         $push: [node]
+//       }
+//     }
+//   });
+// }
+//
+// function DeleteComment(prev, id) {
+//   const index = prev.post.comments.findIndex(x => x.id === id);
+//
+//   // ignore if not found
+//   if (index < 0) {
+//     return prev;
+//   }
+//
+//   return update(prev, {
+//     post: {
+//       comments: {
+//         $splice: [[index, 1]]
+//       }
+//     }
+//   });
+// }
+//
+// class PostComments extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.subscription = null;
+//   }
+//
+//   componentWillReceiveProps(nextProps) {
+//     // Check if props have changed and, if necessary, stop the subscription
+//     if (this.subscription && this.props.postId !== nextProps.postId) {
+//       this.subscription = null;
+//     }
+//
+//     // Subscribe or re-subscribe
+//     if (!this.subscription) {
+//       this.subscribeToCommentList(nextProps.postId);
+//     }
+//   }
+//
+//   subscribeToCommentList = postId => {
+//     const { subscribeToMore } = this.props;
+//
+//     this.subscription = subscribeToMore({
+//       document: COMMENT_SUBSCRIPTION,
+//       variables: { postId },
+//       updateQuery: (prev, { subscriptionData: { commentUpdated: { mutation, id, node } } }) => {
+//         let newResult = prev;
+//
+//         if (mutation === 'CREATED') {
+//           newResult = AddComment(prev, node);
+//         } else if (mutation === 'DELETED') {
+//           newResult = DeleteComment(prev, id);
+//         }
+//
+//         return newResult;
+//       }
+//     });
+//   };
+//
+//   componentWillUnmount() {
+//     this.props.onCommentSelect({ id: null, content: '' });
+//
+//     if (this.subscription) {
+//       // unsubscribe
+//       this.subscription();
+//     }
+//   }
+//
+//   render() {
+//     return <PostCommentsView {...this.props} />;
+//   }
+// }
+//
+// PostComments.propTypes = {
+//   postId: PropTypes.number.isRequired,
+//   comments: PropTypes.array.isRequired,
+//   comment: PropTypes.object.isRequired,
+//   addComment: PropTypes.func.isRequired,
+//   editComment: PropTypes.func.isRequired,
+//   deleteComment: PropTypes.func.isRequired,
+//   onCommentSelect: PropTypes.func.isRequired,
+//   onFormSubmitted: PropTypes.func.isRequired,
+//   subscribeToMore: PropTypes.func.isRequired
+// };
+//
+// const PostCommentsWithApollo = compose(
+//   graphql(ADD_COMMENT, {
+//     props: ({ mutate }) => ({
+//       addComment: (content, postId) =>
+//         mutate({
+//           variables: { input: { content, postId } },
+//           optimisticResponse: {
+//             __typename: 'Mutation',
+//             addComment: {
+//               __typename: 'Comment',
+//               id: null,
+//               content: content
+//             }
+//           },
+//           updateQueries: {
+//             post: (prev, { mutationResult: { data: { addComment } } }) => {
+//               if (prev.post) {
+//                 return AddComment(prev, addComment);
+//               }
+//             }
+//           }
+//         })
+//     })
+//   }),
+//   graphql(EDIT_COMMENT, {
+//     props: ({ ownProps: { postId }, mutate }) => ({
+//       editComment: (id, content) =>
+//         mutate({
+//           variables: { input: { id, postId, content } },
+//           optimisticResponse: {
+//             __typename: 'Mutation',
+//             editComment: {
+//               __typename: 'Comment',
+//               id: id,
+//               content: content
+//             }
+//           }
+//         })
+//     })
+//   }),
+//   graphql(DELETE_COMMENT, {
+//     props: ({ ownProps: { postId }, mutate }) => ({
+//       deleteComment: id =>
+//         mutate({
+//           variables: { input: { id, postId } },
+//           optimisticResponse: {
+//             __typename: 'Mutation',
+//             deleteComment: {
+//               __typename: 'Comment',
+//               id: id
+//             }
+//           },
+//           updateQueries: {
+//             post: (prev, { mutationResult: { data: { deleteComment } } }) => {
+//               if (prev.post) {
+//                 return DeleteComment(prev, deleteComment.id);
+//               }
+//             }
+//           }
+//         })
+//     })
+//   })
+// )(PostComments);
+//
+// export default connect(
+//   state => ({ comment: state.post.comment }),
+//   dispatch => ({
+//     onCommentSelect(comment) {
+//       dispatch({
+//         type: 'COMMENT_SELECT',
+//         value: comment
+//       });
+//     },
+//     onFormSubmitted() {
+//       dispatch(reset('comment'));
+//     }
+//   })
+// )(PostCommentsWithApollo);

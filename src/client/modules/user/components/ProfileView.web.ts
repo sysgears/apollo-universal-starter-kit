0f0d7c13bd549cc5f32b459430f880892284df90
Evgenij Olshanskij <gekanchiko@gmail.com>
2017-11-09 10:23:07 +0200

    Improve a Profile page

--- src/client/modules/user/components/ProfileView.web.ts
+++ src/client/modules/user/components/ProfileView.web.ts
@@ -6,11 +6,15 @@ import ProfileService from '../containers/Profile';
   selector: 'profile-view',
   template: `
     <div *ngIf="loading" class="text-center">Loading...</div>
-    <div id="content" *ngIf="!loading" class="container">
+    <div id="content" *ngIf="!loading && currentUser" class="container">
       <h2>Profile</h2>
       <p>username: {{currentUser.username}}</p>
       <p>email: {{currentUser.email}}</p>
-      <p>is admin: {{currentUser.isAdmin}}</p>
+      <p>role: {{currentUser.role}}</p>
+      <p *ngIf="currentUser.profile">name: {{currentUser.profile.fullName}}</p>
+    </div>
+    <div *ngIf="!loading && !currentUser" class="container">
+      <h2>No current user logged in</h2>
     </div>
   `
 })

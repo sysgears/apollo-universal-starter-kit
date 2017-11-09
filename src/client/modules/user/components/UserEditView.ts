a61d56491136d2c53d0e22e23173a1902fbc214a
Evgenij Olshanskij <gekanchiko@gmail.com>
2017-11-09 11:39:25 +0200

    Improve the user edit form behaviour

--- src/client/modules/user/components/UserEditView.ts
+++ src/client/modules/user/components/UserEditView.ts
@@ -4,6 +4,24 @@ import { assign, pick } from 'lodash';
 import settings from '../../../../../settings';
 import UserEditService from '../containers/UserEdit';
 
+export enum InputType {
+  INPUT = 0,
+  SELECTOR = 1,
+  RADIO_BUTTON = 2
+}
+
+export interface FormInput {
+  id: string;
+  name: string;
+  value: string;
+  type?: string;
+  placeholder?: string;
+  inputType: InputType;
+  options?: any[];
+  minLength?: number;
+  required?: boolean;
+}
+
 @Component({
   selector: 'users-edit-view',
   template: `
@@ -17,12 +35,13 @@ import UserEditService from '../containers/UserEdit';
         </div>
       </div>
 
-      <user-form [onSubmit]="onSubmit" [user]="user" [loading]="loading"></user-form>
+      <user-form [onSubmit]="onSubmit" [user]="user" [loading]="loading" [form]="form"></user-form>
     </div>
   `
 })
 export default class UsersEditView implements OnInit, OnDestroy {
   public user: any = {};
+  public form: FormInput[];
   public loading: boolean = true;
   public title: string;
   public errors: any[] = [];
@@ -39,6 +58,7 @@ export default class UsersEditView implements OnInit, OnDestroy {
       this.userEditService.user(p.id, ({ data: { user }, loading }: any) => {
         this.ngZone.run(() => {
           this.user = user ? assign({}, user, user.profile) : {};
+          this.form = this.createForm(Number(p.id) === 0);
           this.loading = loading;
           this.title = user ? 'Edit User' : 'Create User';
         });
@@ -76,5 +96,83 @@ export default class UsersEditView implements OnInit, OnDestroy {
         this.router.navigateByUrl('users');
       });
     }
+  };
+
+  private createForm = (withPassword: boolean) => {
+    return [
+      {
+        id: 'username-input',
+        name: 'username',
+        value: 'Username',
+        type: 'text',
+        placeholder: 'Username',
+        inputType: InputType.INPUT,
+        minLength: 1,
+        required: true
+      },
+      {
+        id: 'email-input',
+        name: 'email',
+        value: 'Email',
+        type: 'email',
+        placeholder: 'Email',
+        inputType: InputType.INPUT,
+        minLength: 3,
+        required: true
+      },
+      {
+        id: 'role-selector',
+        name: 'role',
+        value: 'Role',
+        inputType: InputType.SELECTOR,
+        options: ['user', 'admin'],
+        required: true
+      },
+      {
+        id: 'active-radio',
+        name: 'isActive',
+        value: 'Is Active',
+        inputType: InputType.RADIO_BUTTON,
+        required: true
+      },
+      {
+        id: 'first-name-input',
+        name: 'firstName',
+        value: 'First Name',
+        type: 'text',
+        placeholder: 'First Name',
+        inputType: InputType.INPUT,
+        required: true
+      },
+      {
+        id: 'last-name-input',
+        name: 'lastName',
+        value: 'Last Name',
+        type: 'text',
+        placeholder: 'Last Name',
+        inputType: InputType.INPUT,
+        required: true
+      },
+      {
+        id: 'password-input',
+        name: 'password',
+        value: 'Password',
+        type: 'password',
+        placeholder: 'Password',
+        inputType: InputType.INPUT,
+        minLength: 5,
+        required: withPassword
+      },
+      {
+        id: 'passwordConfirmation-input',
+        name: 'passwordConfirmation',
+        value: 'Password Confirmation',
+        type: 'password',
+        placeholder: 'Password Confirmation',
+        inputType: InputType.INPUT,
+        minLength: 5,
+        required: withPassword
+      }
+    ];
   };
 }
--- src/client/modules/user/components/UserForm.web.ts
+++ src/client/modules/user/components/UserForm.web.ts
@@ -1,29 +1,12 @@
 import { Component, Input, OnDestroy, OnInit } from '@angular/core';
-
-enum InputType {
-  INPUT = 0,
-  SELECTOR = 1,
-  RADIO_BUTTON = 2
-}
-
-interface FormInput {
-  id: string;
-  name: string;
-  value: string;
-  type?: string;
-  placeholder?: string;
-  inputType: InputType;
-  options?: any[];
-  minLength?: number;
-  required?: boolean;
-}
+import { FormInput } from './UserEditView';
 
 @Component({
   selector: 'user-form',
   template: `
     <div *ngIf="loading">Loading...</div>
     <form *ngIf="!loading && user" name="userForm" #userForm="ngForm" (ngSubmit)="onSubmit(user)">
-      <div [ngClass]="{'form-group': fi.inputType !== 2, 'form-check': fi.inputType === 2}" *ngFor="let fi of formInputs">
+      <div [ngClass]="{'form-group': fi.inputType !== 2, 'form-check': fi.inputType === 2}" *ngFor="let fi of form">
 
         <span *ngIf="fi.inputType === 0">
           <label for="{{fi.id}}">{{fi.value}}</label>
@@ -36,7 +19,7 @@ interface FormInput {
                  #name="ngModel"
                  minlength="{{fi.minLength}}"
                  pattern="{{pattern(fi.name)}}"
-                 required />
+                 [required]="fi.required" />
 
           <div *ngIf="name.invalid && (name.dirty || name.touched)">
             <small [hidden]="!name.errors.required">
@@ -67,7 +50,7 @@ interface FormInput {
         </span>
 
       </div>
-      <button type="submit" id="register-submit-btn" class="btn btn-primary" [disabled]="!userForm.form.valid">Save</button>
+      <button type="submit" id="register-submit-btn" class="btn btn-primary" [disabled]="userForm.form.invalid">Save</button>
     </form>
   `,
   styles: ['small {color: brown}']
@@ -76,13 +59,12 @@ export default class UserForm implements OnInit, OnDestroy {
   @Input() public onSubmit: any;
   @Input() public loading: boolean;
   @Input() public user: any;
+  @Input() public form: FormInput[];
 
-  public formInputs: FormInput[];
   public pattern: any;
   public patternMsg: any;
 
   constructor() {
-    this.formInputs = this.getForm();
     this.pattern = this.getPattern;
     this.patternMsg = this.getPatternMsg;
   }
@@ -91,6 +73,7 @@ export default class UserForm implements OnInit, OnDestroy {
 
   public ngOnDestroy(): void {}
 
+  // Some of the form patterns must be generalized and taken out of a form component
   private getPattern = (fieldName: string) => {
     let pat: any = null;
 
@@ -117,83 +100,5 @@ export default class UserForm implements OnInit, OnDestroy {
     }
 
     return patMsg;
-  };
-
-  private getForm = (): FormInput[] => {
-    return [
-      {
-        id: 'username-input',
-        name: 'username',
-        value: 'Username',
-        type: 'text',
-        placeholder: 'Username',
-        inputType: InputType.INPUT,
-        minLength: 1,
-        required: true
-      },
-      {
-        id: 'email-input',
-        name: 'email',
-        value: 'Email',
-        type: 'email',
-        placeholder: 'Email',
-        inputType: InputType.INPUT,
-        minLength: 3,
-        required: true
-      },
-      {
-        id: 'role-selector',
-        name: 'role',
-        value: 'Role',
-        inputType: InputType.SELECTOR,
-        options: ['user', 'admin'],
-        required: true
-      },
-      {
-        id: 'active-radio',
-        name: 'isActive',
-        value: 'Is Active',
-        inputType: InputType.RADIO_BUTTON,
-        required: true
-      },
-      {
-        id: 'first-name-input',
-        name: 'firstName',
-        value: 'First Name',
-        type: 'text',
-        placeholder: 'First Name',
-        inputType: InputType.INPUT,
-        required: true
-      },
-      {
-        id: 'last-name-input',
-        name: 'lastName',
-        value: 'Last Name',
-        type: 'text',
-        placeholder: 'Last Name',
-        inputType: InputType.INPUT,
-        required: true
-      },
-      {
-        id: 'password-input',
-        name: 'password',
-        value: 'Password',
-        type: 'password',
-        placeholder: 'Password',
-        inputType: InputType.INPUT,
-        minLength: 5,
-        required: true
-      },
-      {
-        id: 'passwordConfirmation-input',
-        name: 'passwordConfirmation',
-        value: 'Password Confirmation',
-        type: 'password',
-        placeholder: 'Password Confirmation',
-        inputType: InputType.INPUT,
-        minLength: 5,
-        required: true
-      }
-    ];
   };
 }

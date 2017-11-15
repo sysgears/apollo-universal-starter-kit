4afd5c954334aec561a9bb7003266c2e2eb77b1e
Mitja Debeljak <mitjade@gmail.com>
2017-11-15 10:28:24 +0100

    use `View` to style layout

--- src/client/modules/user/components/LoginView.web.jsx
+++ src/client/modules/user/components/LoginView.web.jsx
@@ -1,11 +1,8 @@
-// Web only component
-
-// React
 import React from 'react';
 import PropTypes from 'prop-types';
 import Helmet from 'react-helmet';
 import { SubmissionError } from 'redux-form';
-import { Container, Row, Col } from 'reactstrap';
+import { StyleSheet, View } from 'react-native';
 
 import { PageLayout, Card, CardGroup, CardTitle, CardText } from '../../common/components/web';
 import LoginForm from '../components/LoginForm';
@@ -41,33 +38,39 @@ class LoginView extends React.PureComponent {
     return (
       <PageLayout>
         {renderMetaData()}
-        <Container>
-          <Row>
-            <Col xs={{ size: 6, offset: 3 }}>
-              <Row>
-                <Col>
-                  <h1 className="text-center">Sign In</h1>
-                  <LoginForm onSubmit={this.onSubmit(login)} />
-                </Col>
-              </Row>
-              <Row>
-                <Col>
-                  <Card>
-                    <CardGroup>
-                      <CardTitle>Available logins:</CardTitle>
-                      <CardText>admin@example.com:admin</CardText>
-                      <CardText>user@example.com:user</CardText>
-                    </CardGroup>
-                  </Card>
-                </Col>
-              </Row>
-            </Col>
-          </Row>
-        </Container>
+        <View style={styles.layout}>
+          <View style={styles.box}>
+            <h1 className="text-center">Sign In</h1>
+            <LoginForm onSubmit={this.onSubmit(login)} />
+          </View>
+          <View style={styles.box}>
+            <Card>
+              <CardGroup>
+                <CardTitle>Available logins:</CardTitle>
+                <CardText>admin@example.com:admin</CardText>
+                <CardText>user@example.com:user</CardText>
+              </CardGroup>
+            </Card>
+          </View>
+        </View>
       </PageLayout>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  layout: {
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'stretch' //'center'
+  },
+  box: {
+    paddingLeft: 200,
+    paddingRight: 200
+    //width: 600
+  }
+});
 
 LoginView.propTypes = {
   login: PropTypes.func.isRequired,

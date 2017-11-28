d7f3aa358d6a9a015596fd96d659d48e5b88fb0f
Mitja Debeljak <mitjade@gmail.com>
2017-11-28 11:41:19 +0100

    refactor post list to use FlatList

--- src/client/modules/post/components/PostList.jsx
+++ src/client/modules/post/components/PostList.jsx
@@ -1,71 +1,46 @@
 /*eslint-disable react/display-name*/
 import React from 'react';
 import PropTypes from 'prop-types';
-import { StyleSheet, Text, View, ListView, ScrollView, Button } from 'react-native';
+import { StyleSheet, FlatList, Text, View, Button } from 'react-native';
 
-// Row comparison function
-const rowHasChanged = (r1, r2) => r1.id !== r2.id;
+class PostList extends React.PureComponent {
+  keyExtractor = item => item.node.id;
 
-// DataSource template object
-const ds = new ListView.DataSource({ rowHasChanged });
-
-const renderRow = (deletePost, navigation) => rowData => {
-  return (
-    <View style={styles.row}>
-      <Button
-        title={rowData.title}
-        onPress={() =>
-          navigation.navigate('PostEdit', {
-            id: rowData.id
-          })
-        }
-      />
-      <Button title="Delete" onPress={() => deletePost(rowData.id)} />
-    </View>
-  );
-};
-
-function renderLoadMore(posts, loadMoreRows) {
-  if (posts.pageInfo.hasNextPage) {
+  renderItem = ({ item: { node: { id, title } } }) => {
+    const { deletePost, navigation } = this.props;
     return (
       <View style={styles.row}>
-        <Text>
-          ({posts.edges.length} / {posts.totalCount})
-        </Text>
-        <Button title="Load More ..." onPress={loadMoreRows} />
+        <Button title={title} onPress={() => navigation.navigate('PostEdit', { id })} />
+        <Button title="Delete" onPress={() => deletePost(id)} />
       </View>
     );
+  };
+
+  render() {
+    const { loading, posts, loadMoreRows } = this.props;
+
+    if (loading) {
+      return (
+        <View style={styles.container}>
+          <Text>Loading...</Text>
+        </View>
+      );
+    } else {
+      return (
+        <FlatList
+          data={posts.edges}
+          keyExtractor={this.keyExtractor}
+          renderItem={this.renderItem}
+          onEndReached={() => {
+            if (posts.pageInfo.hasNextPage) {
+              return loadMoreRows();
+            }
+          }}
+        />
+      );
+    }
   }
 }
-
-const PostList = ({ loading, posts, deletePost, loadMoreRows, navigation }) => {
-  if (loading) {
-    return (
-      <View style={styles.container}>
-        <Text>Loading...</Text>
-      </View>
-    );
-  } else {
-    const rows = posts.edges.reduce((prev, { node }) => {
-      prev.push(node);
-      return prev;
-    }, []);
-
-    const dataSource = ds.cloneWithRows(rows);
-
-    return (
-      <ScrollView>
-        <ListView
-          style={styles.container}
-          dataSource={dataSource}
-          renderRow={renderRow(deletePost, navigation)}
-          removeClippedSubviews={false}
-        />
-        {renderLoadMore(posts, loadMoreRows)}
-      </ScrollView>
-    );
-  }
-};
 
 PostList.propTypes = {
   loading: PropTypes.bool.isRequired,

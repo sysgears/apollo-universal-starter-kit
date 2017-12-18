2d630152721c543eff64681ce2d3761415fe9ca6
Victor Vlasenko <victor.vlasenko@sysgears.com>
2017-12-18 07:28:52 +0200

    Simplify webpack.config.lint.js

--- packages/mobile/webpack.config.lint.js
+++ packages/mobile/webpack.config.lint.js
@@ -1,6 +1,1 @@
-const path = require('path');
-const createWebpackConfig = require('spinjs').createWebpackConfig;
-
-const config = createWebpackConfig(path.join(__dirname, '.spinrc.json'), 'android');
-
-module.exports = config;
+module.exports = require('spinjs').createWebpackConfig(require.resolve('./.spinrc.json'), 'android');

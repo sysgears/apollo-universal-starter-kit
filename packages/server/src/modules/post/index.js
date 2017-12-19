284d4975ef7140404861413baec008c6397122d3
mitjade <mitjade@gmail.com>
2017-12-19 12:05:57 +0100

    switch from dataloader to graphql-resolve-batch (#580)

--- packages/server/src/modules/post/index.js
+++ packages/server/src/modules/post/index.js
@@ -1,5 +1,3 @@
-import DataLoader from 'dataloader';
-
 import Post from './sql';
 import schema from './schema.graphql';
 import createResolvers from './resolvers';
@@ -9,14 +7,5 @@ import Feature from '../connector';
 export default new Feature({
   schema,
   createResolversFunc: createResolvers,
-  createContextFunc: () => {
-    const post = new Post();
-
-    return {
-      Post: post,
-      loaders: {
-        getCommentsForPostIds: new DataLoader(post.getCommentsForPostIds)
-      }
-    };
-  }
+  createContextFunc: () => ({ Post: new Post() })
 });

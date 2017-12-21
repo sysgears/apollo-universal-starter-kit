a3bcaf8037fd06623a34e124d238f35e68c003b0
Yuri Heiko <yuriheiko@gmail.com>
2017-12-21 12:55:14 +0200

    Create the CardGroup component

--- src/client/modules/ui-antd/components/CardGroup.ts
+++ src/client/modules/ui-antd/components/CardGroup.ts
@@ -3,9 +3,9 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'card-group',
   template: `
-     <div class="card-body">
-         <ng-content></ng-content>
-     </div>
-	`
+    <div>
+      <ng-content></ng-content>
+    </div>
+  `
 })
 export default class CardGroup {}

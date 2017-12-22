9cb9c1262ab44dc1aff5de60e709f8d332327512
Yuri Heiko <yuriheiko@gmail.com>
2017-12-22 14:48:26 +0200

    Refactor the CardText and CardTitle components

--- src/client/modules/ui-antd/components/CardText.ts
+++ src/client/modules/ui-antd/components/CardText.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'card-text',
   template: `
-    <p>
+    <p style="-webkit-margin-after: 1em;">
       <ng-content></ng-content>
     </p>
   `
--- src/client/modules/ui-antd/components/CardTitle.ts
+++ src/client/modules/ui-antd/components/CardTitle.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'card-title',
   template: `
-    <h1>
+    <h1 style="-webkit-margin-after: 0.67em;">
       <ng-content></ng-content>
     </h1>
   `

7b04f83e4b0171f595cb5b3a6ab012c0a6ee537b
Yuri Heiko <yuriheiko@gmail.com>
2017-12-27 10:04:37 +0200

    Refactor the RenderCheckBox component

--- src/client/modules/ui-antd/components/RenderCheckBox.ts
+++ src/client/modules/ui-antd/components/RenderCheckBox.ts
@@ -4,22 +4,33 @@ import { FormInput } from './Form';
 @Component({
   selector: 'render-checkbox',
   template: `
-    <label for="{{input.id}}" class="ant-checkbox-wrapper">
-        <span class="ant-checkbox" [ngClass]="{'ant-checkbox-checked': reduxForm?.value[input.name]}" >
-          <input type="checkbox"
-                 id="{{input.id}}"
-                 name="{{input.name}}"
-                 [ngrxFormControlState]="reduxForm.controls[input.name]"
-                 [ngrxEnableFocusTracking]="true"
-                 class="ant-checkbox-input"
-                 (ngModelChange)="changed({ id: input.id, value: $event })"
-                 [(ngModel)]="reduxForm?.value[input.name]"/>
-          <span class="ant-checkbox-inner"></span>
-        </span>
-      {{input.label}}
-    </label>
+    <div class="ant-form-item-label">
+      <label class="" *ngIf="input.label" for="{{input.id}}">{{input.label}}</label>
+    </div>
+    <div class="ant-form-item-control-wrapper">
+      <div class="ant-form-item-control ">
+        <div>
+          <label class="ant-checkbox-wrapper">
+            <span class="ant-checkbox" [ngClass]="{'ant-checkbox-checked': reduxForm?.value[input.name]}">
+              <input type="checkbox"
+                     id="{{input.id}}"
+                     name="{{input.name}}"
+                     [ngrxFormControlState]="reduxForm.controls[input.name]"
+                     [ngrxEnableFocusTracking]="true"
+                     class="ant-checkbox-input"
+                     (ngModelChange)="changed({ id: input.id, value: $event })"
+                     [(ngModel)]="reduxForm?.value[input.name]"/>
+              <span class="ant-checkbox-inner"></span>
+            </span>
+            {{input.label}}
+          </label>
+        </div>
+      </div>
+    </div>
 
-    <div *ngIf="reduxForm.controls[input.name].isInvalid && (reduxForm.controls[input.name].isDirty || reduxForm.controls[input.name].isTouched)">
+
+    <div
+        *ngIf="reduxForm.controls[input.name].isInvalid && (reduxForm.controls[input.name].isDirty || reduxForm.controls[input.name].isTouched)">
       <small [hidden]="!reduxForm.controls[input.name].errors[input.name]">
         {{reduxForm.controls[input.name].errors[input.name]}}
       </small>

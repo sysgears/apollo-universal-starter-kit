b08faf9aafbad9ef82378b81a2a86b16ffe264d3
Yuri Heiko <yuriheiko@gmail.com>
2018-01-19 18:08:43 +0200

    Refactor the Table component in order to use the native Ant Design classes

--- src/client/modules/ui-antd/components/Table.ts
+++ src/client/modules/ui-antd/components/Table.ts
@@ -27,39 +27,51 @@ export enum ElemType {
 @Component({
   selector: 'ausk-table',
   template: `
-    <nz-table #nzTable [nzIsPagination]="false" [nzDataSource]="rows">
-      <thead nz-thead>
-      <tr>
-        <th nz-th *ngFor="let column of columns; index as i;"
-            [class]="'w-' + (columns.length === 2 ? 100 : 100 / columns.length) +
-                  (column.columnSpan > 1 ? ' text-center' : '')"
-            [attr.width]="column.width"
-            [attr.colSpan]="column.columnSpan">
-          {{column.title}}
-          <ausk-link *ngIf="column.sorting" (click)="column.sorting($event, column.value)">
-            <span [innerHTML]="column.iconRender(column.value)"></span>
-          </ausk-link>
-        </th>
-      </tr>
-      </thead>
-      <tbody nz-tbody>
-      <tr nz-tbody-tr *ngFor="let row of rows">
-        <td nz-td *ngFor="let cell of row">
-          <ausk-link *ngIf="cell.type === 0" [to]="cell.link">
-            {{cell.text}}
-          </ausk-link>
+    <div class="ant-table-wrapper">
+      <div class="ant-spin-nested-loading">
+        <div class="ant-spin-container">
+          <div class="ant-table ant-table-large ant-table-scroll-position-left">
+            <div class="ant-table-content">
+              <div class="ant-table-body">
+                <table class="">
+                  <colgroup>
+                    <col *ngFor="let column of columns" [attr.width]="column.width">
+                  </colgroup>
+                  <thead class="ant-table-thead">
+                  <tr>
+                    <th *ngFor="let column of columns" class="" [attr.colSpan]="column.columnSpan">
+                      <span>{{column.title}}</span>
+                      <ausk-link *ngIf="column.sorting" (click)="column.sorting($event, column.value)">
+                        <span [innerHTML]="column.iconRender(column.value)"></span>
+                      </ausk-link>
+                    </th>
+                  </tr>
+                  </thead>
+                  <tbody class="ant-table-tbody">
+                  <tr class="ant-table-row  ant-table-row-level-0" *ngFor="let row of rows">
+                    <td *ngFor="let cell of row" class="">
+                      <ausk-link *ngIf="cell.type === 0" [to]="cell.link">
+                        {{cell.text}}
+                      </ausk-link>
 
-          <ausk-button *ngIf="cell.type === 1" [click]="cell.callback" [btnStyle]="cell.className" [btnSize]="buttonSize()">
-            {{cell.text}}
-          </ausk-button>
+                      <ausk-button *ngIf="cell.type === 1" [click]="cell.callback" [btnStyle]="cell.className"
+                                   [btnSize]="buttonSize()">
+                        {{cell.text}}
+                      </ausk-button>
 
-          <span *ngIf="cell.type === 2">
-            {{cell.text}}
-          </span>
-        </td>
-      </tr>
-      </tbody>
-    </nz-table>
+                      <span *ngIf="cell.type === 2">
+                            {{cell.text}}
+                          </span>
+                    </td>
+                  </tr>
+                  </tbody>
+                </table>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   `
 })
 export default class Table {

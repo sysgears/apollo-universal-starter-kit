760ee23c85359e471fe05f357ee1aded2d41b70c
Sergey Gilevich <kentserg9453@gmail.com>
2018-02-13 13:22:45 +0200

    Add module facebook to server side

--- packages/server/src/modules/user/modules/jwt/index.js
+++ packages/server/src/modules/user/modules/jwt/index.js
@@ -13,57 +13,14 @@ import confirmMiddleware from './confirm';
 import Feature from '../../../connector';
 import scopes from './auth/scopes';
 import settings from '../../../../../../../settings';
+import { facebookStategy, facebookAuth } from '../facebook';
 
 const SECRET = settings.user.secret;
 
 const User = new UserDAO();
 
 if (settings.user.auth.facebook.enabled) {
-  passport.use(
-    new FacebookStrategy(
-      {
-        clientID: settings.user.auth.facebook.clientID,
-        clientSecret: settings.user.auth.facebook.clientSecret,
-        callbackURL: '/auth/facebook/callback',
-        scope: settings.user.auth.facebook.scope,
-        profileFields: settings.user.auth.facebook.profileFields
-      },
-      async function(accessToken, refreshToken, profile, cb) {
-        const { id, username, displayName, emails: [{ value }] } = profile;
-        try {
-          let user = await User.getUserByFbIdOrEmail(id, value);
-
-          if (!user) {
-            const isActive = true;
-            const [createdUserId] = await User.register({
-              username: username ? username : displayName,
-              email: value,
-              password: id,
-              isActive
-            });
-
-            await User.createFacebookAuth({
-              id,
-              displayName,
-              userId: createdUserId
-            });
-
-            user = await User.getUser(createdUserId);
-          } else if (!user.fbId) {
-            await User.createFacebookAuth({
-              id,
-              displayName,
-              userId: user.id
-            });
-          }
-
-          return cb(null, pick(user, ['id', 'username', 'role', 'email']));
-        } catch (err) {
-          return cb(err, {});
-        }
-      }
-    )
-  );
+  facebookStategy(User);
 }
 
 if (settings.user.auth.google.enabled) {
@@ -191,38 +148,7 @@ export default new Feature({
 
     // Setup Facebook OAuth
     if (settings.user.auth.facebook.enabled) {
-      app.use(passport.initialize());
-
-      app.get('/auth/facebook', passport.authenticate('facebook'));
-
-      app.get('/auth/facebook/callback', passport.authenticate('facebook', { session: false }), async function(
-        req,
-        res
-      ) {
-        const user = await User.getUserWithPassword(req.user.id);
-        const refreshSecret = SECRET + user.password;
-        const [token, refreshToken] = await createTokens(req.user, SECRET, refreshSecret);
-
-        req.universalCookies.set('x-token', token, {
-          maxAge: 60 * 60 * 24 * 7,
-          httpOnly: true
-        });
-        req.universalCookies.set('x-refresh-token', refreshToken, {
-          maxAge: 60 * 60 * 24 * 7,
-          httpOnly: true
-        });
-
-        req.universalCookies.set('r-token', token, {
-          maxAge: 60 * 60 * 24 * 7,
-          httpOnly: false
-        });
-        req.universalCookies.set('r-refresh-token', refreshToken, {
-          maxAge: 60 * 60 * 24 * 7,
-          httpOnly: false
-        });
-
-        res.redirect('/profile');
-      });
+      facebookAuth(app, SECRET, User);
     }
 
     // Setup Google OAuth

fd3ebfb01c1a8a181037e30660d11c064b58e534
Sergey Gilevich <kentserg9453@gmail.com>
2018-03-02 14:37:42 +0200

    Disable swipe for TabNavigator

--- packages/client/src/app/Routes.jsx
+++ packages/client/src/app/Routes.jsx
@@ -2,8 +2,15 @@ import { TabNavigator } from 'react-navigation';
 
 import modules from '../modules';
 
-const MainScreenNavigator = TabNavigator({
-  ...modules.tabItems
-});
+const MainScreenNavigator = TabNavigator(
+  {
+    ...modules.tabItems
+  },
+  {
+    navigationOptions: {
+      swipeEnabled: false
+    }
+  }
+);
 
 export default MainScreenNavigator;

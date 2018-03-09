86213d44183bfa2275c58bcc8d4494cc7f48cef0
Sergey Gilevich <kentserg9453@gmail.com>
2018-03-09 15:31:13 +0200

    Add @types/react-router-dom do dependencies. Add RenderField, Option, RenderCheckBox

---
+++ packages/client/src/modules/common/components/web/ui-bootstrap/components/RenderCheckBox.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { FormGroup, Label, Input, FormFeedback } from 'reactstrap';
+
+interface Meta {
+  touched: boolean;
+  error: string;
+}
+
+interface RenderCheckBoxProps {
+  input: any;
+  label: string;
+  type: string;
+  meta: Meta;
+}
+
+const RenderCheckBox = ({ input, label, type, meta: { touched, error } }: RenderCheckBoxProps) => {
+  let color = 'normal';
+  if (touched && error) {
+    color = 'danger';
+  }
+
+  return (
+    <FormGroup color={color} check>
+      <Label check>
+        <Input {...input} placeholder={label} type={type} /> {label}
+        {touched && (error && <FormFeedback>{error}</FormFeedback>)}
+      </Label>
+    </FormGroup>
+  );
+};
+
+export default RenderCheckBox;

7085181a6613fa8d13c775ea0ef8ab36a5b1b8f7
Sergey Gilevich <kentserg9453@gmail.com>
2018-03-13 17:25:57 +0200

    Migrate index files for favicon module to TypeScript

--- packages/client/src/modules/favicon/index.web.ts
+++ packages/client/src/modules/favicon/index.web.ts
@@ -1,5 +1,5 @@
 // Favicon.ico should not be hashed, since some browsers expect it to be exactly on /favicon.ico URL
-require('!file-loader?name=[name].[ext]!./assets/favicon.ico'); // eslint-disable-line import/no-webpack-loader-syntax
+require('!file-loader?name=[name].[ext]!./assets/favicon.ico'); // tslint:disable-line
 
 // Require all files from assets dir recursively addding them into assets.json
 let req = require.context('!file-loader?name=[hash].[ext]!./assets', true, /.*/);

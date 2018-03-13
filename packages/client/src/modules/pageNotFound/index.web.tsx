6d8e21238c9675382671a0b51b17257907842c3e
Sergey Gilevich <kentserg9453@gmail.com>
2018-03-13 17:24:45 +0200

    Migrate index files for pageNotFound module to TypeScript

--- packages/client/src/modules/pageNotFound/index.web.tsx
+++ packages/client/src/modules/pageNotFound/index.web.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Route } from 'react-router-dom';
 
 import PageNotFound from './containers/PageNotFound';
-import Feature from '../connector';
+import Feature from '../connector.web';
 
 export default new Feature({
   route: <Route component={PageNotFound} />

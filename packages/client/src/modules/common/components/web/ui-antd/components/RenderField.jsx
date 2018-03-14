27275f0f49db8ecfd969094b6affeaffff50d58c
Mitja Debeljak <mitjade@gmail.com>
2018-03-14 13:55:48 +0100

    update renderField

--- packages/client/src/modules/common/components/web/ui-antd/components/RenderField.jsx
+++ packages/client/src/modules/common/components/web/ui-antd/components/RenderField.jsx
@@ -1,68 +1,31 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import DomainSchema from '@domain-schema/core';
+import Form from 'antd/lib/form';
+import Input from 'antd/lib/input';
 
-import { FormItem, Input } from './index';
+const FormItem = Form.Item;
 
-export default class RenderField extends React.Component {
-  static propTypes = {
-    input: PropTypes.object,
-    label: PropTypes.string,
-    type: PropTypes.string,
-    formItemLayout: PropTypes.object,
-    hasTypeOf: PropTypes.func.isRequired,
-    meta: PropTypes.object
-  };
-
-  handleChange = e => {
-    const { input: { onChange, name }, hasTypeOf } = this.props;
-    //console.log('RenderField: handleChange');
-    //console.log('name:', name);
-    let value = e.target.value;
-    //console.log('value:', value);
-    if (hasTypeOf(Number)) {
-      value = value !== '' ? parseFloat(value) : '';
-      //console.log('RenderField: hasTypeOf(Number) TRUE');
-    }
-
-    onChange(name, value);
-  };
-
-  render() {
-    const {
-      input: { onChange, ...inputRest },
-      label,
-      type,
-      formItemLayout,
-      hasTypeOf,
-      meta: { touched, error }
-    } = this.props;
-
-    let validateStatus = '';
-    if (touched && error) {
-      validateStatus = 'error';
-    }
-
-    let input = {
-      ...inputRest
-    };
-
-    if (hasTypeOf(DomainSchema.Float)) {
-      //console.log('RenderField: hasTypeOf(Float) TRUE');
-      input = {
-        step: '0.01',
-        ...inputRest
-      };
-    } else if (hasTypeOf(Number)) {
-      //console.log('RenderField: hasTypeOf(Number) TRUE');
-    }
-
-    return (
-      <FormItem label={label} {...formItemLayout} validateStatus={validateStatus} help={touched && error}>
-        <div>
-          <Input {...input} placeholder={label} type={type} onChange={this.handleChange} />
-        </div>
-      </FormItem>
-    );
+const RenderField = ({ input, label, type, meta: { touched, error }, placeholder }) => {
+  let validateStatus = '';
+  if (touched && error) {
+    validateStatus = 'error';
   }
-}
+
+  return (
+    <FormItem label={label} validateStatus={validateStatus} help={touched && error}>
+      <div>
+        <Input {...input} placeholder={label || placeholder} type={type} />
+      </div>
+    </FormItem>
+  );
+};
+
+RenderField.propTypes = {
+  input: PropTypes.object,
+  label: PropTypes.string,
+  placeholder: PropTypes.string,
+  type: PropTypes.string,
+  meta: PropTypes.object
+};
+
+export default RenderField;

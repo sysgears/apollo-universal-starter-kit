83f14d6e8f666fe24eaa059d0b5605934c789949
Sergey Gilevich <kentserg9453@gmail.com>
2018-03-15 10:52:49 +0200

    Remove index from paths

--- packages/client/src/modules/counter/index.web.tsx
+++ packages/client/src/modules/counter/index.web.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { Route } from 'react-router-dom';
 
 import Counter from './containers/Counter.web';
-import resolvers from './resolvers/index';
-import reducers from './reducers/index';
+import resolvers from './resolvers';
+import reducers from './reducers';
 
 import Feature from '../connector.web';
 

e9a9458da7b9bc20d43dbc38e4bec45a8437f5a2
Aleksey Zadorozhniy <zadorozhniyal@gmail.com>
2018-03-23 15:47:58 +0200

    Replace buttons on dropdown in the LanguagePicker

--- packages/client/src/modules/i18n/components/LanguagePicker.jsx
+++ packages/client/src/modules/i18n/components/LanguagePicker.jsx
@@ -1,29 +1,44 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Button } from 'reactstrap';
+import { Dropdown, DropdownToggle, DropdownMenu, DropdownItem } from 'reactstrap';
 
-const LanguagePicker = ({ i18n }) => {
-  const langs = Object.keys(i18n.store.data);
-  return (
-    <section className="lang-section">
-      {langs.length > 1 &&
-        langs.map(lang => (
-          <Button
-            key={lang}
-            color="link"
-            size="sm"
-            active={i18n.language === lang}
-            onClick={() => i18n.changeLanguage(lang)}
-          >
-            {lang.toUpperCase()}
-          </Button>
-        ))}
-    </section>
-  );
-};
+export default class LanguagePicker extends React.Component {
+  constructor(props) {
+    super(props);
+    this.toggle = this.toggle.bind(this);
+    this.state = {
+      dropdownOpen: false
+    };
+  }
+
+  toggle() {
+    this.setState({
+      dropdownOpen: !this.state.dropdownOpen
+    });
+  }
+
+  render() {
+    const { i18n } = this.props;
+    const langs = Object.keys(i18n.store.data);
+    return (
+      <section className="lang-section">
+        {langs.length > 1 && (
+          <Dropdown size="sm" isOpen={this.state.dropdownOpen} toggle={this.toggle}>
+            <DropdownToggle caret>{i18n.language.toUpperCase()}</DropdownToggle>
+            <DropdownMenu>
+              {langs.map(lang => (
+                <DropdownItem key={lang} onClick={() => i18n.changeLanguage(lang)}>
+                  {lang.toUpperCase()}
+                </DropdownItem>
+              ))}
+            </DropdownMenu>
+          </Dropdown>
+        )}
+      </section>
+    );
+  }
+}
 
 LanguagePicker.propTypes = {
   i18n: PropTypes.object.isRequired
 };
-
-export default LanguagePicker;

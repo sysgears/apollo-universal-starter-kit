61f83f04f26901e7ca150f22712a1942338f76fb
Sergey Gilevich <kentserg9453@gmail.com>
2018-03-28 14:48:09 +0300

    Add types folder to Upload module and create base interfaces

---
+++ packages/client/src/modules/upload/types/index.ts
@@ -0,0 +1,25 @@
+/**
+ * Errors
+ */
+interface Error {
+  error: string;
+}
+
+/* --- COMPONENT PROPS --- */
+
+/**
+ * Mutation props
+ */
+interface UploadOption {
+  uploadFiles: (files: any[]) => Promise<boolean | Error>;
+  removeFile: (id: number) => Promise<boolean | Error>;
+}
+
+/**
+ * Query props
+ */
+interface UploadQueryResult {
+  files: any[];
+}
+
+interface UploadProps extends UploadOption, UploadQueryResult {}

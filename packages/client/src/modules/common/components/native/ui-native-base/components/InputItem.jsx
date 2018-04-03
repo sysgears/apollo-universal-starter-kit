1db48dd909ac10dc2295b2d01584835913993cc9
Sergey Gilevich <kentserg9453@gmail.com>
2018-04-03 15:10:48 +0300

    Remove label from input item. Add implementation for show input errors

--- packages/client/src/modules/common/components/native/ui-native-base/components/InputItem.jsx
+++ packages/client/src/modules/common/components/native/ui-native-base/components/InputItem.jsx
@@ -1,26 +1,32 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { StyleSheet } from 'react-native';
-import { Item, Label, Input, Icon } from 'native-base';
+import { StyleSheet, Text, View } from 'react-native';
+import { Item, Input } from 'native-base';
 
 const InputItem = ({ children, error, ...props }) => {
   return (
-    <Item fixedLabel style={styles.item} error={error}>
-      <Label>{children}</Label>
-      <Input {...props} />
-      {error && <Icon name="close-circle" />}
-    </Item>
+    <View>
+      <Item fixedLabel style={styles.item} error={!!error}>
+        <Input {...props} />
+      </Item>
+      {!!error && <Text style={styles.errorText}>{error}</Text>}
+    </View>
   );
 };
 
 InputItem.propTypes = {
   children: PropTypes.node,
-  error: PropTypes.bool
+  error: PropTypes.string
 };
 
 const styles = StyleSheet.create({
   item: {
     paddingLeft: 10
+  },
+  errorText: {
+    color: 'red',
+    textAlign: 'center',
+    margin: 3
   }
 });
 

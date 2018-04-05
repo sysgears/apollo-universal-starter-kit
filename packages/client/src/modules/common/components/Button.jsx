213b942d56b5f332369a57ce4650e87415891afd
Sergey Gilevich <kentserg9453@gmail.com>
2018-04-05 09:47:45 +0300

    Add optional property bgColor for Button

--- packages/client/src/modules/common/components/Button.jsx
+++ packages/client/src/modules/common/components/Button.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { StyleSheet, Text, TouchableOpacity } from 'react-native';
 
-const Button = ({ children, onPress, disabled }) => {
-  const { buttonStyle, textStyle } = styles;
+const Button = ({ children, onPress, disabled, bgColor }) => {
+  const { buttonStyle, textStyle } = styles(bgColor);
 
   return (
     <TouchableOpacity onPress={onPress} style={buttonStyle} activeOpacity={disabled ? 0.5 : 1}>
@@ -12,27 +12,29 @@ const Button = ({ children, onPress, disabled }) => {
   );
 };
 
-const styles = StyleSheet.create({
-  buttonStyle: {
-    backgroundColor: '#0275d8',
-    borderRadius: 5,
-    paddingLeft: 10,
-    paddingRight: 10
-  },
-  textStyle: {
-    alignSelf: 'center',
-    color: '#FFF',
-    fontSize: 16,
-    fontWeight: '600',
-    paddingTop: 10,
-    paddingBottom: 10
-  }
-});
+const styles = bgColor =>
+  StyleSheet.create({
+    buttonStyle: {
+      backgroundColor: bgColor || '#0275d8',
+      borderRadius: 5,
+      paddingLeft: 10,
+      paddingRight: 10
+    },
+    textStyle: {
+      alignSelf: 'center',
+      color: '#FFF',
+      fontSize: 16,
+      fontWeight: '600',
+      paddingTop: 10,
+      paddingBottom: 10
+    }
+  });
 
 Button.propTypes = {
   children: PropTypes.node,
   onPress: PropTypes.func,
-  disabled: PropTypes.bool
+  disabled: PropTypes.bool,
+  bgColor: PropTypes.string
 };
 
 export default Button;

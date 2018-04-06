0763ef8ed466c58cc8e79457f72c233cd971cc65
Sergey Gilevich <kentserg9453@gmail.com>
2018-04-06 13:09:15 +0300

    Add ProfileView for mobile platform

--- packages/client/src/modules/user/components/ProfileView.native.jsx
+++ packages/client/src/modules/user/components/ProfileView.native.jsx
@@ -1,35 +1,55 @@
 /*eslint-disable no-unused-vars*/
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ScrollView } from 'react-native';
 import PropTypes from 'prop-types';
 
+import { Card, CardItem, CardText, CardTitle, CardHeader } from '../../common/components/native';
+import SubscriptionProfile from '../../subscription/containers/SubscriptionProfile';
+
 import { withLoadedUser } from '../';
+import settings from '../../../../../../settings';
+
+const renderProfileItem = (title, value) => (
+  <CardItem>
+    <CardText style={styles.title}>{title.toUpperCase()}</CardText>
+    <CardText>{value}</CardText>
+  </CardItem>
+);
 
 const ProfileView = ({ currentUserLoading, currentUser }) => {
   return (
     <View style={styles.container}>
-      <View style={styles.element}>
-        {currentUserLoading ? (
-          <Text style={styles.box}>Loading...</Text>
-        ) : (
-          <Text style={styles.box}>
-            Hello User {currentUser ? currentUser.fullName || currentUser.username : null}!
-          </Text>
-        )}
-      </View>
+      {currentUserLoading ? (
+        <Text style={styles.box}>Loading...</Text>
+      ) : (
+        <ScrollView contentContainerStyle={styles.scrollContainer}>
+          <Card>
+            <CardHeader title="Profile info" />
+            {renderProfileItem('User Name: ', currentUser.username)}
+            {renderProfileItem('Email: ', currentUser.email)}
+            {renderProfileItem('Role: ', currentUser.role)}
+            {currentUser.profile && currentUser.profile.fullName
+              ? renderProfileItem('Full Name: ', currentUser.profile.fullName)
+              : null}
+          </Card>
+        </ScrollView>
+      )}
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center'
+  scrollContainer: {
+    paddingTop: 10,
+    paddingHorizontal: 20
   },
-  element: {
-    paddingTop: 30
+  container: {
+    flex: 1
+  },
+  title: {
+    fontSize: 12,
+    fontWeight: '600',
+    color: '#686b70'
   },
   box: {
     textAlign: 'center',

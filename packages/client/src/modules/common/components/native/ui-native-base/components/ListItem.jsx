8f226a14605e27595d35222c93727567b48e9af9
Sergey Gilevich <kentserg9453@gmail.com>
2018-04-11 12:26:00 +0300

    Add click action for ListItem

--- packages/client/src/modules/common/components/native/ui-native-base/components/ListItem.jsx
+++ packages/client/src/modules/common/components/native/ui-native-base/components/ListItem.jsx
@@ -2,12 +2,18 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { ListItem as ListItemComponent } from 'native-base';
 
-const ListItem = ({ children, ...props }) => {
-  return <ListItemComponent {...props}>{children}</ListItemComponent>;
+const ListItem = ({ children, onPress, onClick, ...props }) => {
+  return (
+    <ListItemComponent {...props} onPress={onPress || onClick}>
+      {children}
+    </ListItemComponent>
+  );
 };
 
 ListItem.propTypes = {
-  children: PropTypes.node
+  children: PropTypes.node,
+  onClick: PropTypes.func,
+  onPress: PropTypes.func
 };
 
 export default ListItem;

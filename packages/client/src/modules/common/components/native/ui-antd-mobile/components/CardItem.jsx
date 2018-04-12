18d8bd1edee5e04af4e734084290b767edc74ced
Sergey Gilevich <kentserg9453@gmail.com>
2018-04-12 11:05:47 +0300

    Update CardItem

--- packages/client/src/modules/common/components/native/ui-antd-mobile/components/CardItem.jsx
+++ packages/client/src/modules/common/components/native/ui-antd-mobile/components/CardItem.jsx
@@ -1,23 +1,24 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { View } from 'react-native';
+import { View, StyleSheet } from 'react-native';
 
 const CardItem = ({ children, style }) => {
-  return <View style={{ ...styles.item, ...style }}>{children}</View>;
+  return <View style={[styles.item, style]}>{children}</View>;
 };
 
 CardItem.propTypes = {
   children: PropTypes.node,
-  style: PropTypes.object
+  style: PropTypes.number
 };
 
-const styles = {
+const styles = StyleSheet.create({
   item: {
     flexDirection: 'row',
     alignItems: 'center',
     paddingHorizontal: 15,
-    marginVertical: 15
+    paddingTop: 15,
+    paddingBottom: 15
   }
-};
+});
 
 export default CardItem;

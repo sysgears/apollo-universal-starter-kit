ca3f5934b0db3ee97e5ab312220868aa94daca14
Sergey Gilevich <kentserg9453@gmail.com>
2018-04-13 12:14:40 +0300

    Update Select component

--- packages/client/src/modules/common/components/native/ui-native-base/components/Select.jsx
+++ packages/client/src/modules/common/components/native/ui-native-base/components/Select.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Picker } from 'native-base';
+import { Platform } from 'react-native';
+import { Picker, Item } from 'native-base';
 
 const Select = ({ data, onValueChange, selectedValue, value, onChange, ...props }) => {
-  return (
+  return Platform.OS === 'ios' ? (
+    <Item>
+      <Picker onValueChange={onValueChange || onChange} selectedValue={selectedValue || value} {...props}>
+        {data.map((option, idx) => <Picker.Item key={idx} label={option.label} value={option.value} />)}
+      </Picker>
+    </Item>
+  ) : (
     <Picker onValueChange={onValueChange || onChange} selectedValue={selectedValue || value} {...props}>
       {data.map((option, idx) => <Picker.Item key={idx} label={option.label} value={option.value} />)}
     </Picker>

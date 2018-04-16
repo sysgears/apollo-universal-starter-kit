4721ef9fb4dcdcf7c7075c590e8e2515cacedb44
Victor Vlasenko <victor.vlasenko@sysgears.com>
2018-04-16 17:41:13 +0300

    Add server GraphQL operations logging

--- packages/server/src/middleware/graphql.js
+++ packages/server/src/middleware/graphql.js
@@ -1,4 +1,5 @@
 import { graphqlExpress } from 'apollo-server-express';
+import { formatResponse } from 'apollo-logger';
 import 'isomorphic-fetch';
 
 import schema from '../api/schema';
@@ -15,6 +16,8 @@ export default async (req, res, next) => {
       context: { ...context, req, res },
       debug: false,
       formatError: error => log.error('GraphQL execution error:', error),
+      formatResponse: (response, options) =>
+        settings.app.logging.apolloLogging ? formatResponse(response, options) : response,
       tracing: !!settings.engine.engineConfig.apiKey,
       cacheControl: !!settings.engine.engineConfig.apiKey
     }))(req, res, next);

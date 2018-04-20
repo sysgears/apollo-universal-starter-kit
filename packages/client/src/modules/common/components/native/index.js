6b7270a107b844df010152fb9cdf946749fd8ee8
Sergey Gilevich <kentserg9453@gmail.com>
2018-04-20 12:59:03 +0300

    Add Modal to index file

--- packages/client/src/modules/common/components/native/index.js
+++ packages/client/src/modules/common/components/native/index.js
@@ -2,5 +2,6 @@
 export * from './ui-native-base/components';
 //export * from './ui-antd-mobile/components';
 export { default as RenderField } from './RenderField';
+export { default as Modal } from './Modal';
 export { default as createTabBarIconWrapper } from './createTabBarIconWrapper';
 export * from './ButtonTypes';

6bedbd80fe194565fe5cf3985f71af1045e088e7
Sergey Gilevich <kentserg9453@gmail.com>
2018-04-20 12:56:29 +0300

    Update user seed

--- packages/server/src/database/seeds/002_user.js
+++ packages/server/src/database/seeds/002_user.js
@@ -9,7 +9,7 @@ export async function seed(knex, Promise) {
     .insert({
       username: 'admin',
       email: 'admin@example.com',
-      password_hash: await bcrypt.hash('admin', 12),
+      password_hash: await bcrypt.hash('admin123', 12),
       role: 'admin',
       is_active: true
     });
@@ -17,9 +17,9 @@ export async function seed(knex, Promise) {
   await knex('user')
     .returning('id')
     .insert({
-      username: 'user1',
+      username: 'user',
       email: 'user@example.com',
-      password_hash: await bcrypt.hash('user', 12),
+      password_hash: await bcrypt.hash('user1234', 12),
       role: 'user',
       is_active: true
     });

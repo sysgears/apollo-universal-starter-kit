7c877a250dbdac65b2fc22fb8b857639efe84ddd
Aleksey Zadorozhniy <zadorozhniyal@gmail.com>
2018-04-26 12:05:39 +0300

    Add domain form to contact

--- packages/client/src/modules/contact/components/ContactForm.web.jsx
+++ packages/client/src/modules/contact/components/ContactForm.web.jsx
@@ -1,62 +1,65 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { withFormik } from 'formik';
+//eslint-disable-next-line import/no-extraneous-dependencies
+import DomainSchema from '@domain-schema/core';
+//eslint-disable-next-line import/no-extraneous-dependencies
+import { DomainSchemaFormik, FieldTypes, FormSchema } from '@domain-schema/formik';
 
 import translate from '../../../i18n';
-import Field from '../../../utils/FieldAdapter';
-import { Form, RenderField, Button, Alert } from '../../common/components/web';
-import { email, minLength, required, validateForm } from '../../../../../common/validation';
 
-const contactFormSchema = {
-  name: [required, minLength(3)],
-  email: [required, email],
-  content: [required, minLength(10)]
-};
-
-const validate = values => validateForm(values, contactFormSchema);
-
-const ContactForm = ({ values, handleSubmit, error, sent, t }) => {
-  return (
-    <Form name="contact" onSubmit={handleSubmit}>
-      {sent && <Alert color="success">{t('form.submitMsg')}</Alert>}
-      <Field name="name" component={RenderField} type="text" label={t('form.field.name')} value={values.name} />
-      <Field name="email" component={RenderField} type="text" label={t('form.field.email')} value={values.email} />
-      <Field
-        name="content"
-        component={RenderField}
-        type="textarea"
-        label={t('form.field.content')}
-        value={values.content}
-      />
-      <div className="text-center">
-        {error && <Alert color="error">{error}</Alert>}
-        <Button color="primary" type="submit">
-          {t('form.btnSubmit')}
-        </Button>
-      </div>
-    </Form>
+const ContactForm = ({ onSubmit, t }) => {
+  const contactFormSchema = new DomainSchema(
+    class extends FormSchema {
+      __ = { name: 'ContactForm' };
+      name = {
+        type: String,
+        fieldType: FieldTypes.input,
+        input: {
+          label: t('form.field.name')
+        },
+        min: 3
+      };
+      email = {
+        type: String,
+        fieldType: FieldTypes.input,
+        input: {
+          type: 'email',
+          label: t('form.field.email')
+        },
+        email: true
+      };
+      content = {
+        type: String,
+        fieldType: FieldTypes.input,
+        input: {
+          type: 'textarea',
+          label: t('form.field.content')
+        },
+        min: 10
+      };
+      setSubmitBtn() {
+        return {
+          label: t('form.btnSubmit'),
+          color: 'primary'
+        };
+      }
+      setBtnsWrapperProps() {
+        return {
+          className: 'text-center'
+        };
+      }
+    }
   );
+
+  const contactForm = new DomainSchemaFormik(contactFormSchema);
+  const ContactFormComponent = contactForm.generateForm(onSubmit);
+
+  return <ContactFormComponent onSubmit={async values => await onSubmit(values)} />;
 };
 
 ContactForm.propTypes = {
-  handleSubmit: PropTypes.func,
   onSubmit: PropTypes.func,
-  submitting: PropTypes.bool,
-  error: PropTypes.string,
-  sent: PropTypes.bool,
-  values: PropTypes.object,
   t: PropTypes.func
 };
 
-const ContactFormWithFormik = withFormik({
-  enableReinitialize: true,
-  mapPropsToValues: () => ({ content: '', email: '', name: '' }),
-  async handleSubmit(values, { resetForm, props: { onSubmit } }) {
-    await onSubmit(values);
-    resetForm();
-  },
-  validate: values => validate(values),
-  displayName: 'ContactUsForm' // helps with React DevTools
-});
-
-export default translate('contact')(ContactFormWithFormik(ContactForm));
+export default translate('contact')(ContactForm);

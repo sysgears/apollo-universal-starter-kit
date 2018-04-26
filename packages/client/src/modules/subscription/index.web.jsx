5b07a17a6043b57146eecc1ce6191547b6b80e85
Sergey Gilevich <kentserg9453@gmail.com>
2018-04-26 12:14:25 +0300

    Fix bug with redirect from subscription page after logout

--- packages/client/src/modules/subscription/index.web.jsx
+++ packages/client/src/modules/subscription/index.web.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, NavLink } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 import translate from '../../i18n';
 import { MenuItem } from '../../modules/common/components/web';
@@ -7,7 +7,7 @@ import Subscription from './containers/Subscription';
 import SubscribersOnly from './containers/SubscribersOnly';
 import UpdateCard from './containers/UpdateCard';
 import { SubscriberRoute } from './containers/Auth';
-import { IfLoggedIn } from '../user';
+import { IfLoggedIn, AuthRoute } from '../user';
 import reducers from './reducers';
 import settings from '../../../../../settings';
 import resources from './locales';
@@ -22,9 +22,9 @@ const NavLinkWithI18n = translate('subscription')(({ t }) => (
 export default new Feature({
   route: settings.subscription.enabled
     ? [
-        <Route exact path="/subscription" component={Subscription} />,
-        <SubscriberRoute exact role="user" path="/subscribers-only" component={SubscribersOnly} />,
-        <SubscriberRoute exact role="user" path="/update-card" component={UpdateCard} />
+        <AuthRoute exact role="user" path="/subscription" component={Subscription} />,
+        <SubscriberRoute exact path="/subscribers-only" component={SubscribersOnly} />,
+        <SubscriberRoute exact path="/update-card" component={UpdateCard} />
       ]
     : [],
   navItem: settings.subscription.enabled ? (

835a256d48612b46f7ccf86924e68fed5eb3285f
Victor Vlasenko <victor.vlasenko@sysgears.com>
2018-04-30 21:31:21 +0300

    Don't use absolute API_URL during production

--- packages/client/.spinrc.js
+++ packages/client/.spinrc.js
@@ -44,7 +44,6 @@ const config = {
 config.options.devProxy = config.options.ssr;
 
 if (process.env.NODE_ENV === 'production') {
-  config.options.defines.__API_URL__ = '"https://apollo-universal-starter-kit.herokuapp.com/graphql"';
   // Generating source maps for production will slowdown compilation for roughly 25%
   config.options.sourceMap = false;
 }

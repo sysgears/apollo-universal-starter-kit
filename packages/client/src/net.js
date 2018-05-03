84f41ee091e6716730ba0ee2e251a8092d9ee46a
Victor Vlasenko <victor.vlasenko@sysgears.com>
2018-05-03 11:11:50 +0300

    Fix production URL to GraphQL API is relative URL used

--- packages/client/src/net.js
+++ packages/client/src/net.js
@@ -2,7 +2,5 @@ import url from 'url';
 
 export const isApiExternal = !!url.parse(__API_URL__).protocol;
 export const apiUrl = !isApiExternal
-  ? `${window.location.protocol}//${window.location.hostname}${
-      __DEV__ ? ':8080' : ':' + window.location.port || ''
-    }${__API_URL__}`
+  ? `${window.location.protocol}//${window.location.hostname}:8080${__API_URL__}`
   : __API_URL__;

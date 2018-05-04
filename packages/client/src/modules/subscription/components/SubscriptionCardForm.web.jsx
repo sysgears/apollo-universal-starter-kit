0c2ac1a0cc2e4804a4e1b43df8ce1f59c9cb4a83
Aleksey Zadorozhniy <zadorozhniyal@gmail.com>
2018-05-04 17:03:23 +0300

    Add domain form to the subscription card

--- packages/client/src/modules/subscription/components/SubscriptionCardForm.web.jsx
+++ packages/client/src/modules/subscription/components/SubscriptionCardForm.web.jsx
@@ -1,80 +1,74 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { withFormik } from 'formik';
 import { CardElement, injectStripe } from 'react-stripe-elements';
+//eslint-disable-next-line import/no-extraneous-dependencies
+import DomainSchema from '@domain-schema/core';
+//eslint-disable-next-line import/no-extraneous-dependencies
+import { DomainSchemaFormik, FieldTypes, FormSchema } from '@domain-schema/formik';
 
 import translate from '../../../i18n';
-import Field from '../../../utils/FieldAdapter';
-import { Form, RenderField, Button, Alert, Label } from '../../common/components/web';
-import { required, validateForm } from '../../../../../common/validation';
+import { Label } from '../../common/components/web';
 
-const commentFormSchema = {
-  name: [required]
-};
-
-const validate = values => validateForm(values, commentFormSchema);
+const subscriptionFormSchema = ({ action, t }) =>
+  new DomainSchema(
+    class extends FormSchema {
+      __ = { name: 'SubscriptionForm' };
+      name = {
+        type: String,
+        fieldType: FieldTypes.input,
+        input: {
+          label: t('card.name')
+        }
+      };
+      setSubmitBtn() {
+        return {
+          label: action,
+          color: 'primary',
+          style: { marginTop: 15 }
+        };
+      }
+    }
+  );
 
 class SubscriptionCardForm extends React.Component {
   static propTypes = {
-    submitting: PropTypes.bool,
     action: PropTypes.string.isRequired,
-    error: PropTypes.string,
-    handleSubmit: PropTypes.func,
     onSubmit: PropTypes.func,
-    values: PropTypes.object,
-    t: PropTypes.func
+    t: PropTypes.func,
+    stripe: PropTypes.object
   };
 
   render() {
-    const { handleSubmit, submitting, action, error, values, t } = this.props;
+    const subscriptionForm = new DomainSchemaFormik(subscriptionFormSchema(this.props));
+    const SubscriptionFormComponent = subscriptionForm.generateForm();
+
     return (
-      <Form name="subscription" onSubmit={handleSubmit}>
-        <Field
-          name="name"
-          component={RenderField}
-          type="text"
-          label={t('card.name')}
-          validate={required}
-          value={values.name}
-        />
-        <Label>{t('card.info')}</Label>
+      <React.Fragment>
+        <Label>{this.props.t('card.info')}</Label>
         <CardElement className="form-control" style={{ base: { lineHeight: '30px' } }} />
-        {error && <Alert color="error">{error}</Alert>}
-        <Button color="primary" type="submit" disabled={submitting} style={{ marginTop: 15 }}>
-          {action}
-        </Button>
-      </Form>
+        <SubscriptionFormComponent
+          onSubmit={async ({ name }) => {
+            const { stripe, onSubmit } = this.props;
+            const { token, error } = await stripe.createToken({ name });
+            if (error) return;
+
+            const {
+              id,
+              card: { exp_month, exp_year, last4, brand }
+            } = token;
+
+            await onSubmit({
+              token: id,
+              expiryMonth: exp_month,
+              expiryYear: exp_year,
+              last4,
+              brand
+            });
+          }}
+        />
+      </React.Fragment>
     );
   }
 }
 
-const SubscriptionFormWithFormik = withFormik({
-  mapPropsToValues: () => ({ name: '' }),
-  async handleSubmit(values, { resetForm, props }) {
-    const onSubmit = async ({ name }) => {
-      const { stripe } = props;
-      const { token, error } = await stripe.createToken({ name });
-      if (error) return;
-
-      const {
-        id,
-        card: { exp_month, exp_year, last4, brand }
-      } = token;
-
-      await this.props.onSubmit({
-        token: id,
-        expiryMonth: exp_month,
-        expiryYear: exp_year,
-        last4,
-        brand
-      });
-    };
-    await onSubmit(values);
-    resetForm({ name: '' });
-  },
-  validate: values => validate(values),
-  displayName: 'SubscriptionForm', // helps with React DevTools,
-  enableReinitialize: true
-});
-
-export default translate('subscription')(injectStripe(SubscriptionFormWithFormik(SubscriptionCardForm)));
+export default translate('subscription')(injectStripe(SubscriptionCardForm));

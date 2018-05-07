429b4e4bad4e2402fb09f2a3f7e727b009cc841f
Victor Vlasenko <victor.vlasenko@sysgears.com>
2018-05-07 10:38:28 +0300

    Avoid using resetStore as it is buggy atm.

--- packages/client/src/modules/user/access/index.js
+++ packages/client/src/modules/user/access/index.js
@@ -4,11 +4,11 @@ import session from './session';
 import Feature from './connector';
 
 const login = client => {
-  return client.resetStore();
+  return client.cache.reset();
 };
 
 const logout = client => {
-  return client.resetStore();
+  return client.cache.reset();
 };
 
 export default new Feature(jwt, session, {

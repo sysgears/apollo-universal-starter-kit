a0f40ce5821f309b854122d82e7d1a367b45891e
Sergey Gilevich <kentserg9453@gmail.com>
2018-05-08 17:31:38 +0300

    Fix header button position

--- packages/client/src/modules/common/components/native/ui-native-base/components/IconButton.jsx
+++ packages/client/src/modules/common/components/native/ui-native-base/components/IconButton.jsx
@@ -3,9 +3,9 @@ import PropTypes from 'prop-types';
 import { Button, Icon } from 'native-base';
 import { Ionicons } from '@expo/vector-icons';
 
-const IconButton = ({ iconName, iconColor, iconSize, ...props }) => {
+const IconButton = ({ iconName, style, iconColor, iconSize, ...props }) => {
   return (
-    <Button transparent {...props}>
+    <Button transparent style={[{ alignSelf: 'center' }, style]} {...props}>
       <Icon>
         <Ionicons name={iconName} size={iconSize} color={iconColor} />
       </Icon>
@@ -16,7 +16,8 @@ const IconButton = ({ iconName, iconColor, iconSize, ...props }) => {
 IconButton.propTypes = {
   iconName: PropTypes.string,
   iconSize: PropTypes.number,
-  iconColor: PropTypes.string
+  iconColor: PropTypes.string,
+  style: PropTypes.oneOfType([PropTypes.number, PropTypes.object])
 };
 
 export default IconButton;

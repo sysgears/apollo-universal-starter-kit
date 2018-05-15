df4f5ecf378e444870d84da9bf642f70fb1c532f
Vsevolod Bondar <vsevolod.bondar@sysgears.com>
2018-05-15 16:01:05 +0300

    Add new test wrappers. Update test utils.

--- packages/client/src/testHelpers/testUtils.js
+++ packages/client/src/testHelpers/testUtils.js
@@ -1,7 +1,40 @@
-import { Simulate } from 'react-testing-library';
+import { render, renderIntoDocument, Simulate, wait, waitForElement, fireEvent, cleanup } from 'react-testing-library';
 
-export * from 'react-testing-library';
+const find = (container, selector) => {
+  return container.querySelector(selector);
+};
 
-export const click = element => {
+const findAll = (container, selector) => {
+  return container.querySelectorAll(selector);
+};
+
+const click = element => {
   Simulate.click(element, { button: 0 });
+};
+
+const change = (element, value) => {
+  Simulate.change(element, value);
+};
+
+const submit = element => {
+  Simulate.submit(element);
+};
+
+const updateContent = container => {
+  return find(container, '#content');
+};
+
+export {
+  render,
+  renderIntoDocument,
+  wait,
+  waitForElement,
+  fireEvent,
+  cleanup,
+  find,
+  findAll,
+  updateContent,
+  click,
+  change,
+  submit
 };

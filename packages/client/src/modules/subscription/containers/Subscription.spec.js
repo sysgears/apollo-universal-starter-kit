0ea7e0c6594d46af679d84bcccf89974bf594588
Vsevolod Bondar <vsevolod.bondar@sysgears.com>
2018-05-16 14:53:17 +0300

    Fix tests for the subscription client module.

--- packages/client/src/modules/subscription/containers/Subscription.spec.js
+++ packages/client/src/modules/subscription/containers/Subscription.spec.js
@@ -3,18 +3,41 @@ import { expect } from 'chai';
 import { step } from 'mocha-steps';
 
 // Components and helpers
+import settings from '../../../../../../settings';
 import Renderer from '../../../testHelpers/Renderer';
-import { updateContent } from '../../../testHelpers/testUtils';
+import { updateContent, isElementExist, wait } from '../../../testHelpers/testUtils';
 
-describe('Subscription UI works', () => {
-  const renderer = new Renderer({});
-  let app;
-  let content;
+const { enabled, stripePublishableKey } = settings.subscription;
 
-  step('Subscription page renders on mount', () => {
-    app = renderer.render();
-    renderer.history.push('/subscription');
-    content = updateContent(app.container);
-    expect(content).to.not.be.empty;
+if (enabled && stripePublishableKey !== '') {
+  const mocks = {
+    Query: () => ({
+      currentUser() {
+        return {
+          id: 1,
+          username: 'user',
+          role: 'user',
+          isActive: true,
+          email: 'user@example.com',
+          profile: null,
+          auth: null,
+          __typename: 'User'
+        };
+      }
+    })
+  };
+
+  describe('Subscription UI works', () => {
+    const renderer = new Renderer(mocks, {});
+    let app;
+    let content;
+
+    step('Subscription page renders on mount', async () => {
+      app = renderer.render();
+      await wait(() => isElementExist(app.container, 'a[href="/subscribers-only"]'));
+      renderer.history.push('/subscription');
+      content = updateContent(app.container);
+      expect(content).to.not.be.empty;
+    });
   });
-});
+}

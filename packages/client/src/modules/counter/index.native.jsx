b68f80c66f7c6970be7fe397b0254455132a725e
Vsevolod Bondar <vsevolod.bondar@sysgears.com>
2018-05-17 14:53:10 +0300

    Fix app styles for mobile platforms.

--- packages/client/src/modules/counter/index.native.jsx
+++ packages/client/src/modules/counter/index.native.jsx
@@ -22,7 +22,8 @@ export default new Feature({
             headerTitle: <HeaderTitleWithI18n i18nKey="title" style="subTitle" />,
             headerLeft: (
               <IconButton iconName="menu" iconSize={32} iconColor="#0275d8" onPress={() => navigation.openDrawer()} />
-            )
+            ),
+            headerStyle: { backgroundColor: '#fff' }
           })
         }
       }),

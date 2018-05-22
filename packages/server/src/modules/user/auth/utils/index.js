a0a2b4614983e84805b5738495ba4baf44319065
Sergey Gilevich <kentserg9453@gmail.com>
2018-05-22 13:40:46 +0300

    Update creating apollo client

--- packages/server/src/modules/user/auth/utils/index.js
+++ packages/server/src/modules/user/auth/utils/index.js
@@ -1,18 +1,15 @@
 import { SchemaLink } from 'apollo-link-schema';
 import modules from '../../../../modules';
-import schema from '../../../../api/schema';
 import { isApiExternal, apiUrl } from '../../../../net';
 import createApolloClient from '../../../../../../common/createApolloClient';
 import CURRENT_USER_QUERY from '../../../../../../client/src/modules/user/graphql/CurrentUserQuery.graphql';
 
 export default async function getCurrentUser(req, res) {
-  const clientModules = require('../../../../../../client/src/modules').default;
+  const schema = require('../../../../api/schema').default;
   const schemaLink = new SchemaLink({ schema, context: await modules.createContext(req, res) });
   const client = createApolloClient({
     apiUrl,
-    createNetLink: !isApiExternal ? () => schemaLink : undefined,
-    links: clientModules.link,
-    clientResolvers: clientModules.resolvers
+    createNetLink: !isApiExternal ? () => schemaLink : undefined
   });
 
   return await client.query({ query: CURRENT_USER_QUERY });

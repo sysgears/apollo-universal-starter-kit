73e6d3e2f1661edff5406d0dfcd2fdeb813e67e8
Sergey Ivzhenko <sergiiivzhenko@gmail.com>
2018-05-25 10:22:39 +0300

    Add chat package, remove web components

--- packages/client/src/modules/chat/containers/Chat.native.jsx
+++ packages/client/src/modules/chat/containers/Chat.native.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { View, Text } from 'react-native';
+import { GiftedChat } from 'react-native-gifted-chat';
 import translate from '../../../i18n';
-import ChatView from '../components/ChatView.native';
+// import ChatView from '../components/ChatView.native';
 
-@translate('pagination')
+@translate('chat')
 class Chat extends React.Component {
   static propTypes = {
     t: PropTypes.func,
@@ -12,12 +12,42 @@ class Chat extends React.Component {
     loadData: PropTypes.func
   };
 
+  state = {
+    messages: []
+  };
+
+  componentWillMount() {
+    this.setState({
+      messages: [
+        {
+          _id: 1,
+          text: 'Hello developer',
+          createdAt: new Date(),
+          user: {
+            _id: 2,
+            name: 'React Native',
+            avatar: 'https://placeimg.com/140/140/any'
+          }
+        }
+      ]
+    });
+  }
+
+  onSend(messages = []) {
+    this.setState(previousState => ({
+      messages: GiftedChat.append(previousState.messages, messages)
+    }));
+  }
+
   render() {
     return (
-      <View>
-        <Text>chat container</Text>
-        <ChatView />
-      </View>
+      <GiftedChat
+        messages={this.state.messages}
+        onSend={messages => this.onSend(messages)}
+        user={{
+          _id: 1
+        }}
+      />
     );
   }
 }

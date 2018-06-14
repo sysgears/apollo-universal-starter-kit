45bda7a3f61749a3f4da17b330da6c9d69b2b9d4
Dmytro Tverdoklib <nepal-tda@outlook.com>
2018-06-14 12:03:36 +0300

    Delete unused string

--- packages/server/src/modules/common/i18n.js
+++ packages/server/src/modules/common/i18n.js
@@ -3,7 +3,6 @@ import i18n from 'i18n';
 import settings from '../../../../../settings';
 
 export default (language = i18n.getLocale(), moduleName, messageKey) => {
-  console.log('CURRENT_LOCALE', i18n.getLocale());
   const clientLanguage = language.split('-')[0]; // Supports both options 'en' and 'en-US'
   const translation = i18n.getCatalog(clientLanguage)[moduleName][messageKey];
 

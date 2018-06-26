7e39471820c1a14f473d877d334d3b91fc305ff7
Victor Vlasenko <victor.vlasenko@sysgears.com>
2018-06-26 09:29:32 +0300

    Enable GraphQL logging during tests

--- config/app.js
+++ config/app.js
@@ -2,7 +2,7 @@ export default {
   name: 'Apollo Starter Kit',
   logging: {
     debugSQL: false,
-    apolloLogging: ['test', 'production'].indexOf(process.env.NODE_ENV) < 0
+    apolloLogging: ['production'].indexOf(process.env.NODE_ENV) < 0
   },
   // Check here for Windows and Mac OS X: https://code.visualstudio.com/docs/editor/command-line#_opening-vs-code-with-urls
   // Use this protocol handler for Linux: https://github.com/sysgears/vscode-handler

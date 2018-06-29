305cc6206e0e958ef0f0f25da0b196e20c45ed7e
Oleksandr Samofan <samofan77@gmail.com>
2018-06-29 17:48:53 +0300

    Add Switch, InputNumber field for native form

--- packages/client/src/modules/common/util.jsx
+++ packages/client/src/modules/common/util.jsx
@@ -1,19 +1,50 @@
 import React from 'react';
 import { pick, capitalize } from 'lodash';
 import Field from '../../utils/FieldAdapter';
-import { RenderField } from './components/native';
+import { RenderField, RenderSwitch } from './components/native';
 
-export const createFormFields = (schema, values) => {
+export const createFormFields = (schema, values, setFieldValue) => {
   let fields = [];
   for (const key of schema.keys()) {
-    if (key !== 'id') {
+    const value = Array.isArray(schema.values[key]) ? schema.values[key][0] : schema.values[key];
+    const hasTypeOf = targetType => value.type === targetType || value.type.prototype instanceof targetType;
+    if (key === 'id') {
+      continue;
+    }
+    if (hasTypeOf(Boolean)) {
       fields.push(
         <Field
+          label={capitalize(key)}
           name={key}
           key={key}
           value={values[key]}
-          component={RenderField}
+          component={RenderSwitch}
+          placeholder={capitalize(key)}
+          onValueChange={val => setFieldValue(key, val)}
+        />
+      );
+    } else if (hasTypeOf(String)) {
+      fields.push(
+        <Field
+          label={capitalize(key)}
+          name={key}
+          key={key}
+          value={values[key]}
           type="text"
+          component={RenderField}
+          placeholder={capitalize(key)}
+        />
+      );
+    } else if (hasTypeOf(Number)) {
+      fields.push(
+        <Field
+          keyboardType="numeric"
+          label={capitalize(key)}
+          name={key}
+          key={key}
+          value={`${values[key]}`}
+          type="number"
+          component={RenderField}
           placeholder={capitalize(key)}
         />
       );

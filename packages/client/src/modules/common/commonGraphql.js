04661f7afc79eebe040e33e7d85efb933a1cc345
Oleksandr Samofan <samofan77@gmail.com>
2018-07-02 10:11:47 +0300

    Move commonGraphql file

---
+++ packages/client/src/modules/common/commonGraphql.js
@@ -0,0 +1,11 @@
+import ProductQuery from '../product/containers/ProductQuery';
+import ProductTypeQuery from '../productType/containers/ProductTypeQuery';
+import CategoryQuery from '../category/containers/CategoryQuery';
+import SubCategoryQuery from '../subCategory/containers/SubCategoryQuery';
+
+export default {
+  ProductQuery,
+  CategoryQuery,
+  ProductTypeQuery,
+  SubCategoryQuery
+};

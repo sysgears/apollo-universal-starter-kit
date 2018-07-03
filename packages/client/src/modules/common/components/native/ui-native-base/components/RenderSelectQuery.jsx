4c0de4d763265417b11db0f0cb5e2da6a095909a
Oleksandr Samofan <samofan77@gmail.com>
2018-07-03 13:21:49 +0300

    Add and change native components for Form fields

---
+++ packages/client/src/modules/common/components/native/ui-native-base/components/RenderSelectQuery.jsx
@@ -0,0 +1,79 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { pascalize } from 'humps';
+import { View } from 'react-native';
+import { RenderSelect } from './index';
+import schemaQueries from '../../../../commonGraphql';
+
+const handleSelect = (selectedValue, edges, onChangeValue) => {
+  let selectedItem = edges && Array.isArray(edges) ? edges.find(item => item.value == selectedValue) : '';
+  onChangeValue(selectedItem ? { id: selectedItem.value } : '');
+};
+
+const RenderSelectQuery = ({
+  input: { value: inputValue, onChange, onBlur, defaultValue, ...inputRest },
+  label,
+  schema,
+  customStyles,
+  onChange: onChangeValue,
+  value,
+  ...props
+}) => {
+  const pascalizeSchemaName = pascalize(schema.name);
+
+  const formatedValue = value && value != '' && typeof value !== 'undefined' ? value.id : '';
+
+  const Query = schemaQueries[`${pascalizeSchemaName}Query`];
+
+  let defaultStyle = {
+    container: {},
+    itemContainer: {},
+    itemTitle: {},
+    itemAction: {
+      flexDirection: 'column',
+      flex: 2,
+      justifyContent: 'center',
+      alignItems: 'flex-end'
+    }
+  };
+  if (customStyles) {
+    defaultStyle = customStyles;
+  }
+
+  return (
+    <Query limit={10}>
+      {({ loading, data }) => {
+        if (!loading || data) {
+          let computedData = data.edges
+            ? data.edges.map(item => {
+                return { label: item.name, value: item.id };
+              })
+            : null;
+          let computedProps = {
+            renderSelectStyles: defaultStyle,
+            value: formatedValue,
+            data: computedData,
+            onChange: selectedValue => handleSelect(selectedValue, computedData, onChangeValue),
+            ...inputRest,
+            ...props
+          };
+          return <RenderSelect {...computedProps} />;
+        } else {
+          return <View />;
+        }
+      }}
+    </Query>
+  );
+};
+
+RenderSelectQuery.propTypes = {
+  input: PropTypes.object,
+  label: PropTypes.string,
+  schema: PropTypes.object,
+  customStyles: PropTypes.object,
+  formType: PropTypes.string,
+  onChange: PropTypes.func.required,
+  value: PropTypes.any
+};
+
+export default RenderSelectQuery;
--- packages/client/src/modules/common/util.jsx
+++ packages/client/src/modules/common/util.jsx
@@ -1,17 +1,62 @@
 import React from 'react';
 import { pick, capitalize } from 'lodash';
 import Field from '../../utils/FieldAdapter';
-import { RenderField, RenderSwitch } from './components/native';
+import { RenderField, RenderSwitch, RenderSelectQuery } from './components/native';
 
 export const createFormFields = (schema, values, setFieldValue) => {
   let fields = [];
   for (const key of schema.keys()) {
-    const value = Array.isArray(schema.values[key]) ? schema.values[key][0] : schema.values[key];
-    const hasTypeOf = targetType => value.type === targetType || value.type.prototype instanceof targetType;
+    const value = schema.values[key];
+    const type = Array.isArray(value.type) ? value.type[0] : value.type;
+    const hasTypeOf = targetType => type === targetType || type.prototype instanceof targetType;
+    const inputStyle = { fontSize: 16 };
+    const switchStyle = {
+      itemTitle: {
+        fontSize: 16
+      }
+    };
     if (key === 'id') {
       continue;
     }
-    if (hasTypeOf(Boolean)) {
+    if (type.isSchema) {
+      fields.push(
+        <Field
+          schema={type}
+          label={capitalize(key)}
+          name={key}
+          key={key}
+          value={values[key]}
+          component={RenderSelectQuery}
+          placeholder={capitalize(key)}
+          onChange={selectedValue => setFieldValue(key, selectedValue)}
+        />
+      );
+    } else if (hasTypeOf(String)) {
+      fields.push(
+        <Field
+          name={key}
+          key={key}
+          value={values[key]}
+          component={RenderField}
+          type="text"
+          style={inputStyle}
+          placeholder={capitalize(key)}
+        />
+      );
+    } else if (hasTypeOf(Number)) {
+      fields.push(
+        <Field
+          keyboardType="numeric"
+          name={key}
+          key={key}
+          value={`${values[key]}`}
+          type="number"
+          component={RenderField}
+          style={inputStyle}
+          placeholder={capitalize(key)}
+        />
+      );
+    } else if (hasTypeOf(Boolean)) {
       fields.push(
         <Field
           label={capitalize(key)}
@@ -20,32 +65,8 @@ export const createFormFields = (schema, values, setFieldValue) => {
           value={values[key]}
           component={RenderSwitch}
           placeholder={capitalize(key)}
-          onValueChange={val => setFieldValue(key, val)}
-        />
-      );
-    } else if (hasTypeOf(String)) {
-      fields.push(
-        <Field
-          label={capitalize(key)}
-          name={key}
-          key={key}
-          value={values[key]}
-          type="text"
-          component={RenderField}
-          placeholder={capitalize(key)}
-        />
-      );
-    } else if (hasTypeOf(Number)) {
-      fields.push(
-        <Field
-          keyboardType="numeric"
-          label={capitalize(key)}
-          name={key}
-          key={key}
-          value={`${values[key]}`}
-          type="number"
-          component={RenderField}
-          placeholder={capitalize(key)}
+          onChange={selectedValue => setFieldValue(key, selectedValue)}
+          style={switchStyle}
         />
       );
     }

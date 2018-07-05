de43af0026392af6800926529a05c16bbadc7fa2
Oleksandr Samofan <samofan77@gmail.com>
2018-07-05 17:34:06 +0300

    Update generatedSchema file

--- packages/server/src/modules/common/generatedSchemas.js
+++ packages/server/src/modules/common/generatedSchemas.js
@@ -1,5 +1,11 @@
 import { ProductSchema } from '../product/schema';
+import { CategorySchema } from '../category/schema';
+import { ProductTypeSchema } from '../productType/schema';
+import { SubCategorySchema } from '../subCategory/schema';
 
 export default {
-  ProductSchema
+  ProductSchema,
+  CategorySchema,
+  ProductTypeSchema,
+  SubCategorySchema
 };

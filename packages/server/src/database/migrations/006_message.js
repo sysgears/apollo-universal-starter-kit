92911c7b579e51a78381625edfb2cb29f67b49d5
Roman Peskov <peskovrp@gmail.com>
2018-07-10 10:19:34 +0300

    Add table attachment in DB

--- packages/server/src/database/migrations/006_message.js
+++ packages/server/src/database/migrations/006_message.js
@@ -1,14 +1,25 @@
-export async function up(knex) {
-  return knex.schema.createTable('message', table => {
-    table.increments();
-    table.string('text').notNull();
-    table.integer('userId');
-    table.string('uuid');
-    table.integer('reply');
-    table.timestamps(false, true);
-  });
+export async function up(knex, Promise) {
+  return Promise.all([
+    knex.schema
+      .createTable('message', table => {
+        table.increments();
+        table.string('text').notNull();
+        table.integer('userId');
+        table.string('uuid');
+        table.integer('reply');
+        table.timestamps(false, true);
+      })
+      .createTable('attachment', table => {
+        table.increments();
+        table.string('name').notNull();
+        table.string('type').notNull();
+        table.integer('size').notNull();
+        table.string('path').notNull();
+        table.timestamps(false, true);
+      })
+  ]);
 }
 
-export async function down(knex) {
-  return knex.schema.dropTable('message');
+export async function down(knex, Promise) {
+  return Promise.all([knex.schema.dropTable('attachment'), knex.schema.dropTable('message')]);
 }

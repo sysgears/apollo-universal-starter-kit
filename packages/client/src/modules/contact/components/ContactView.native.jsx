00783e67050903971f1b51496fbd4965e3454e4f
Dmytro Tverdoklib <nepal-tda@outlook.com>
2018-07-18 14:41:01 +0300

    Update contact to use <Mutation> element

--- packages/client/src/modules/contact/components/ContactView.native.jsx
+++ packages/client/src/modules/contact/components/ContactView.native.jsx
@@ -5,29 +5,16 @@ import { StyleSheet, View } from 'react-native';
 import translate from '../../../i18n';
 import ContactForm from './ContactForm';
 
-const onSubmit = (contact, t) => async values => {
-  const result = await contact(values);
-
-  if (result.errors) {
-    let submitError = {
-      _error: t('errorMsg')
-    };
-    result.errors.map(error => (submitError[error.field] = error.message));
-    throw submitError;
-  }
-};
-
-const ContactView = ({ contact, t }) => {
+const ContactView = ({ onSubmit }) => {
   return (
     <View style={styles.container}>
-      <ContactForm onSubmit={onSubmit(contact, t)} />
+      <ContactForm onSubmit={onSubmit} />
     </View>
   );
 };
 
 ContactView.propTypes = {
-  contact: PropTypes.func.isRequired,
-  t: PropTypes.func
+  onSubmit: PropTypes.func.isRequired
 };
 
 const styles = StyleSheet.create({

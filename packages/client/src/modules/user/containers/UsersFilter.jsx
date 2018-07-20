86f41a7ea7abdf9118c6a558771b736b9510a47e
Mykyta Lyzhov <lyzhovnik@gmail.com>
2018-07-20 13:56:04 +0300

    Replace UserFilter logic from component to container

--- packages/client/src/modules/user/containers/UsersFilter.jsx
+++ packages/client/src/modules/user/containers/UsersFilter.jsx
@@ -1,6 +1,7 @@
 // React
 import React from 'react';
 import { compose } from 'react-apollo';
+import PropTypes from 'prop-types';
 
 // Components
 import UsersFilterView from '../components/UsersFilterView';
@@ -8,8 +9,43 @@ import UsersFilterView from '../components/UsersFilterView';
 import { withUsersState, withFilterUpdating } from './UserOperations';
 
 class UsersFilter extends React.Component {
+  static propTypes = {
+    filter: PropTypes.object,
+    role: PropTypes.string,
+    isActive: PropTypes.bool,
+    onSearchTextChange: PropTypes.func.isRequired,
+    onRoleChange: PropTypes.func.isRequired,
+    onIsActiveChange: PropTypes.func.isRequired,
+    t: PropTypes.func
+  };
+
+  handleSearch = searchText => {
+    const { onSearchTextChange } = this.props;
+    onSearchTextChange(searchText);
+  };
+
+  handleRole = role => {
+    const { onRoleChange } = this.props;
+    onRoleChange(role);
+  };
+
+  handleIsActive = () => {
+    const {
+      onIsActiveChange,
+      filter: { isActive }
+    } = this.props;
+    onIsActiveChange(!isActive);
+  };
+
   render() {
-    return <UsersFilterView {...this.props} />;
+    const filterProps = {
+      ...this.props,
+      handleSearch: this.handleSearch,
+      handleRole: this.handleRole,
+      handleIsActive: this.handleIsActive
+    };
+
+    return <UsersFilterView {...filterProps} />;
   }
 }
 

4adf3bc1d113721aedc76ee23640bfbcf0626cc5
Oleksandr Samofan <samofan77@gmail.com>
2018-07-23 12:26:50 +0300

    Replace formik on  domain-schema/formik in FormView. Fix render number input bootsrap. Increase versions

--- packages/client/src/modules/common/components/crud/FormView.web.jsx
+++ packages/client/src/modules/common/components/crud/FormView.web.jsx
@@ -1,66 +1,31 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Formik } from 'formik';
-import DomainValidator from '@domain-schema/validation';
+import { DomainSchemaFormik } from '@domain-schema/formik';
+import { onSubmit } from '../../../../utils/crud';
+import { Form, Button, RenderField, RenderSwitch, RenderSelectQuery, RenderDate, RenderNumber } from '../web';
 
-import { onSubmit, mapFormPropsToValues } from '../../../../utils/crud';
-import { createFormFields } from '../../util';
-import { Form, FormItem, Button } from '../web';
-import { computeDomainValidationErrors } from '../../../../../../common/validation';
+const FormView = ({ schema, updateEntry, createEntry, title, data }) => {
+  const schemaFormik = new DomainSchemaFormik(schema);
+  schemaFormik.setFormComponents({
+    input: RenderField,
+    checkbox: RenderSwitch,
+    number: RenderNumber,
+    date: RenderDate,
+    select: RenderSelectQuery,
+    button: Button,
+    form: Form
+  });
+  const Formik = schemaFormik.generateForm({
+    label: 'Submit',
+    disableOnInvalid: false
+  });
 
-const tailFormItemLayout = {
-  wrapperCol: {
-    xs: {
-      span: 24,
-      offset: 0
-    },
-    sm: {
-      span: 16,
-      offset: 6
-    }
-  }
-};
-
-const formItemLayout = {
-  labelCol: {
-    xs: { span: 24 },
-    sm: { span: 6 }
-  },
-  wrapperCol: {
-    xs: { span: 24 },
-    sm: { span: 12 }
-  }
-};
-
-const FormView = ({ schema, updateEntry, createEntry, title, customFields, data }) => {
-  const domainValidator = new DomainValidator();
   return (
     <Formik
-      initialValues={mapFormPropsToValues({ schema, data: data ? data.node : null })}
-      validate={values => {
-        let rawErrors = domainValidator.validate(schema, values);
-        return computeDomainValidationErrors(rawErrors);
-      }}
+      values={data ? data.node : null}
       onSubmit={async values => {
         await onSubmit({ schema, values, updateEntry, createEntry, title, data: data ? data.node : null });
       }}
-      render={({ values, handleChange, handleBlur, handleSubmit }) => (
-        <Form name="post" onSubmit={handleSubmit}>
-          {createFormFields({
-            handleChange,
-            handleBlur,
-            schema,
-            values,
-            formItemLayout,
-            customFields
-          })}
-          <FormItem {...tailFormItemLayout}>
-            <Button color="primary" type="submit">
-              Save
-            </Button>
-          </FormItem>
-        </Form>
-      )}
     />
   );
 };
@@ -70,7 +35,6 @@ FormView.propTypes = {
   schema: PropTypes.object.isRequired,
   createEntry: PropTypes.func.isRequired,
   title: PropTypes.string.isRequired,
-  customFields: PropTypes.object,
   data: PropTypes.object
 };
 

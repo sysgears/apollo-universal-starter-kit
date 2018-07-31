6364aa7246262bc746d2d35a7b34f3506445934b
Oleksandr Samofan <samofan77@gmail.com>
2018-07-31 11:53:42 +0300

    Add crud and helpers from cli-crud

--- packages/server/src/sql/crud.js
+++ packages/server/src/sql/crud.js
@@ -1,14 +1,17 @@
 import _ from 'lodash';
 import uuidv4 from 'uuid';
-import { camelize, decamelizeKeys, camelizeKeys } from 'humps';
+import { decamelize, decamelizeKeys, camelize, camelizeKeys, pascalize } from 'humps';
+import knexnest from 'knexnest';
+import parseFields from 'graphql-parse-fields';
+import moment from 'moment';
 
+import FieldError from '../../../common/FieldError';
 import knex from './connector';
-
-import { orderedFor } from './helpers';
-
+import { selectBy, orderedFor } from './helpers';
 import selectAdapter from './select';
-
 import log from '../../../common/log';
+
+const dateFormat = 'YYYY-MM-DD';
 
 export function createWithIdGenAdapter(options) {
   const T = options.table;
@@ -402,4 +405,401 @@ export function deleteRelationAdapter(options) {
       throw e;
     }
   };
+}
+
+export default class Crud {
+  getTableName() {
+    return decamelize(this.schema.__.tableName ? this.schema.__.tableName : this.schema.name);
+  }
+
+  getFullTableName() {
+    return `${this.schema.__.tablePrefix}${this.getTableName()}`;
+  }
+
+  getSchema() {
+    return this.schema;
+  }
+
+  normalizeFields(data) {
+    //console.log('normalizeFields: ', data);
+    for (const key of _.keys(data)) {
+      if (this.schema.values.hasOwnProperty(key)) {
+        const value = this.schema.values[key];
+        const hasTypeOf = targetType => value.type === targetType || value.type.prototype instanceof targetType;
+        if (hasTypeOf(Date)) {
+          data[key] = moment(data[key]).format('YYYY-MM-DD');
+        }
+      }
+    }
+    //console.log('normalizeFields: ', data);
+    return data;
+  }
+
+  _getList({ limit, offset, orderBy, filter }, info) {
+    const baseQuery = knex(`${this.getFullTableName()} as ${this.getTableName()}`);
+    const select = selectBy(this.schema, info, false);
+    const queryBuilder = select(baseQuery);
+
+    if (limit) {
+      queryBuilder.limit(limit);
+    }
+
+    if (offset) {
+      queryBuilder.offset(offset);
+    }
+
+    if (orderBy && orderBy.column) {
+      let column = orderBy.column;
+      let order = 'asc';
+      if (orderBy.order) {
+        order = orderBy.order;
+      }
+      //console.log('orderBy, column:', column);
+      //console.log('orderBy, order:', order);
+
+      for (const key of this.schema.keys()) {
+        if (column === key) {
+          const value = this.schema.values[key];
+          if (value.type.isSchema) {
+            const tableName = decamelize(value.type.__.tableName ? value.type.__.tableName : value.type.name);
+            let sortBy = 'name';
+            for (const remoteKey of value.type.keys()) {
+              const remoteValue = value.type.values[remoteKey];
+              if (remoteValue.sortBy) {
+                sortBy = remoteKey;
+              }
+            }
+            column = `${tableName}.${sortBy}`;
+          } else {
+            column = `${this.getTableName()}.${decamelize(column)}`;
+          }
+        }
+      }
+      //console.log('column:', column);
+      //console.log('order:', order);
+      queryBuilder.orderBy(column, order);
+    } else {
+      queryBuilder.orderBy(`${this.getTableName()}.id`);
+    }
+
+    if (filter) {
+      //console.log('filter: ', filter);
+      const schema = this.schema;
+      const tableName = this.getTableName();
+      queryBuilder.where(function() {
+        for (const key of schema.keys()) {
+          const value = schema.values[key];
+          const hasTypeOf = targetType => value.type === targetType || value.type.prototype instanceof targetType;
+          //console.log('filter, key: ', key);
+          let filterKey = key;
+
+          if (value.type.isSchema) {
+            filterKey = `${key}Id`;
+          }
+
+          //console.log('filter, filterKey: ', filterKey);
+
+          let filterValue = filter[filterKey];
+          if (hasTypeOf(Date)) {
+            if (filter[`${filterKey}_lte`] && filter[`${filterKey}_lte`] !== '') {
+              const filterValue_lte = moment(filter[`${filterKey}_lte`]).format(dateFormat);
+              this.andWhere(`${tableName}.${decamelize(filterKey)}`, '>=', `${filterValue_lte}`);
+            }
+            if (filter[`${filterKey}_gte`] && filter[`${filterKey}_gte`] !== '') {
+              const filterValue_gte = moment(filter[`${filterKey}_gte`]).format(dateFormat);
+              this.andWhere(`${tableName}.${decamelize(filterKey)}`, '<=', `${filterValue_gte}`);
+            }
+          } else if (hasTypeOf(Boolean)) {
+            if (_.has(filter, filterKey) && filter[filterKey] !== '') {
+              if (filterValue === true) {
+                filterValue = 1;
+              } else {
+                filterValue = 0;
+              }
+              this.andWhere(`${tableName}.${decamelize(filterKey)}`, '=', `${filterValue}`);
+            }
+          } else {
+            if (_.has(filter, filterKey) && filter[filterKey] !== '') {
+              this.andWhere(`${tableName}.${decamelize(filterKey)}`, '=', `${filterValue}`);
+            }
+          }
+        }
+      });
+
+      if (_.has(filter, 'searchText') && filter.searchText !== '') {
+        queryBuilder.where(function() {
+          for (const key of schema.keys()) {
+            const value = schema.values[key];
+            if (value.searchText) {
+              this.orWhere(`${tableName}.${decamelize(key)}`, 'like', `%${filter.searchText}%`);
+            }
+          }
+        });
+      }
+    }
+
+    return knexnest(queryBuilder);
+  }
+
+  async getPaginated(args, info) {
+    const edges = await this._getList(args, parseFields(info).edges);
+    const { count } = await this.getTotal();
+    //console.log('getPaginated, edges:', edges);
+    //console.log('getPaginated, count:', edges);
+
+    return {
+      edges,
+      pageInfo: {
+        totalCount: count,
+        hasNextPage: edges && edges.length === args.limit
+      }
+    };
+  }
+
+  getList(args, info) {
+    return this._getList(args, parseFields(info));
+  }
+
+  getTotal() {
+    return knex(`${this.getFullTableName()}`)
+      .countDistinct('id as count')
+      .first();
+  }
+
+  _get({ where }, info) {
+    const baseQuery = knex(`${this.getFullTableName()} as ${this.getTableName()}`);
+    const select = selectBy(this.schema, info, true);
+
+    const tableName = this.getTableName();
+    baseQuery.where(function() {
+      Object.keys(where).map(key => {
+        this.andWhere(`${tableName}.${decamelize(key)}`, '=', where[key]);
+      });
+    });
+
+    return knexnest(select(baseQuery));
+  }
+
+  _getMany(
+    {
+      where: { id_in }
+    },
+    info
+  ) {
+    const baseQuery = knex(`${this.getFullTableName()} as ${this.getTableName()}`);
+    const select = selectBy(this.schema, info);
+
+    baseQuery.whereIn('id', [...id_in]);
+
+    return knexnest(select(baseQuery));
+  }
+
+  async get(args, info) {
+    const node = await this._get(args, parseFields(info).node);
+    return { node };
+  }
+
+  _create(data) {
+    return knex(`${this.getFullTableName()}`)
+      .insert(decamelizeKeys(this.normalizeFields(data)))
+      .returning('id');
+  }
+
+  async create({ data }, ctx, info) {
+    try {
+      const e = new FieldError();
+      e.throwIf();
+
+      // extract nested entries from data
+      let nestedEntries = [];
+      for (const key of this.schema.keys()) {
+        const value = this.schema.values[key];
+        if (value.type.constructor === Array && data[key]) {
+          nestedEntries.push({ key, data: data[key] });
+          delete data[key];
+        } else if (key === 'rank') {
+          const total = await this.getTotal();
+          data[key] = total.count + 1;
+        }
+      }
+
+      const [id] = await this._create(data);
+
+      // create nested entries
+      if (nestedEntries.length > 0) {
+        nestedEntries.map(nested => {
+          if (nested.data.create) {
+            nested.data.create.map(async create => {
+              create[`${camelize(this.schema.name)}Id`] = id;
+              await ctx[pascalize(nested.key)]._create(create);
+            });
+          }
+        });
+      }
+
+      return await this.get({ where: { id } }, info);
+    } catch (e) {
+      return { errors: e };
+    }
+  }
+
+  _update({ data, where }) {
+    return knex(`${this.getFullTableName()}`)
+      .update(decamelizeKeys(this.normalizeFields(data)))
+      .where(where);
+  }
+
+  async update(args, ctx, info) {
+    try {
+      const e = new FieldError();
+      e.throwIf();
+
+      // extract nested entries from data
+      let nestedEntries = [];
+      for (const key of this.schema.keys()) {
+        const value = this.schema.values[key];
+        if (value.type.constructor === Array && args.data[key]) {
+          nestedEntries.push({ key: value.type[0].name, data: args.data[key] });
+          delete args.data[key];
+        }
+      }
+
+      await this._update(args);
+
+      // create, update, delete nested entries
+      if (nestedEntries.length > 0) {
+        nestedEntries.map(nested => {
+          if (nested.data.create) {
+            nested.data.create.map(async create => {
+              create[`${this.getTableName()}Id`] = args.where.id;
+              await ctx[pascalize(nested.key)]._create(create);
+            });
+          }
+          if (nested.data.update) {
+            nested.data.update.map(async update => {
+              await ctx[pascalize(nested.key)]._update(update);
+            });
+          }
+          if (nested.data.delete) {
+            nested.data.delete.map(async where => {
+              await ctx[pascalize(nested.key)]._delete({ where });
+            });
+          }
+        });
+      }
+
+      return await this.get(args, info);
+    } catch (e) {
+      return { errors: e };
+    }
+  }
+
+  _delete({ where }) {
+    return knex(`${this.getFullTableName()}`)
+      .where(where)
+      .del();
+  }
+
+  async delete(args, info) {
+    try {
+      const e = new FieldError();
+
+      const node = await this.get(args, info);
+
+      if (!node) {
+        e.setError('delete', 'Node does not exist.');
+        e.throwIf();
+      }
+
+      const isDeleted = await this._delete(args);
+
+      if (isDeleted) {
+        return node;
+      } else {
+        e.setError('delete', 'Could not delete Node. Please try again later.');
+        e.throwIf();
+      }
+    } catch (e) {
+      return { errors: e };
+    }
+  }
+
+  _sort({ data }) {
+    return Promise.all([
+      this._update({ data: { rank: data[2] }, where: { id: data[0] } }),
+      this._update({ data: { rank: data[3] }, where: { id: data[1] } })
+    ]);
+  }
+
+  async sort(args) {
+    try {
+      const e = new FieldError();
+      e.throwIf();
+
+      const sortCount = await this._sort(args);
+      const count = sortCount.reduce((total, num) => total + num);
+      if (count > 1) {
+        return { count };
+      } else {
+        e.setError('sort', 'Could not sort Node. Please try again later.');
+        e.throwIf();
+      }
+    } catch (e) {
+      return { errors: e };
+    }
+  }
+
+  _updateMany({ data, where: { id_in } }) {
+    return knex(`${this.getFullTableName()}`)
+      .update(decamelizeKeys(this.normalizeFields(data)))
+      .whereIn('id', id_in);
+  }
+
+  async updateMany(args) {
+    try {
+      const e = new FieldError();
+      const updateCount = await this._updateMany(args);
+
+      if (updateCount > 0) {
+        return { count: updateCount };
+      } else {
+        e.setError('update', 'Could not update any of selected Node. Please try again later.');
+        e.throwIf();
+      }
+    } catch (e) {
+      return { errors: e };
+    }
+  }
+
+  _deleteMany({ where: { id_in } }) {
+    return knex(`${this.getFullTableName()}`)
+      .whereIn('id', id_in)
+      .del();
+  }
+
+  async deleteMany(args) {
+    try {
+      const e = new FieldError();
+
+      const deleteCount = await this._deleteMany(args);
+
+      if (deleteCount > 0) {
+        return { count: deleteCount };
+      } else {
+        e.setError('delete', 'Could not delete any of selected Node. Please try again later.');
+        e.throwIf();
+      }
+    } catch (e) {
+      return { errors: e };
+    }
+  }
+
+  async getByIds(ids, by, Obj, info) {
+    info = parseFields(info);
+    info[`${by}Id`] = true;
+    const baseQuery = knex(`${Obj.getFullTableName()} as ${Obj.getTableName()}`);
+    const select = selectBy(Obj.getSchema(), info, false);
+    const res = await knexnest(select(baseQuery).whereIn(`${Obj.getTableName()}.${decamelize(by)}_id`, ids));
+    return orderedFor(res, ids, `${by}Id`, false);
+  }
 }
--- packages/server/src/sql/helpers.js
+++ packages/server/src/sql/helpers.js
@@ -1,4 +1,5 @@
 import { groupBy } from 'lodash';
+import { decamelize } from 'humps';
 import settings from '../../../../settings';
 
 export const returnId = knexTable => (settings.db.dbType === 'sqlite' ? knexTable : knexTable.returning('id'));
@@ -28,4 +29,76 @@ export const orderedFor = (rows, collection, field, singleObject) => {
     }
     return singleObject ? {} : [];
   });
+};
+
+export const orderedForArray = (rows, collection, field, arrayElement) => {
+  // return the rows ordered for the collection
+  const inGroupsOfField = groupBy(rows, field);
+  return collection.map(element => {
+    const elementArray = inGroupsOfField[element];
+    if (elementArray) {
+      return inGroupsOfField[element].map(elm => {
+        return elm[arrayElement];
+      });
+    }
+    return [];
+  });
+};
+
+const _getSelectFields = (fields, parentPath, parentKey, domainSchema, selectItems, joinNames, single) => {
+  for (const key of Object.keys(fields)) {
+    if (key !== '__typename') {
+      const value = domainSchema.values[key];
+      if (fields[key] === true) {
+        if (value && value.transient) {
+          continue;
+        }
+        const as = parentPath.length > 0 ? `${parentPath.join('_')}_${key}` : key;
+        const tableName = `${decamelize(domainSchema.__.tableName ? domainSchema.__.tableName : domainSchema.name)}`;
+        const fullTableName = parentKey !== null && parentKey !== tableName ? `${parentKey}_${tableName}` : tableName;
+        const arrayPrefix = single ? '' : '_';
+        selectItems.push(`${decamelize(fullTableName)}.${decamelize(key)} as ${arrayPrefix}${as}`);
+      } else {
+        if (value.type.constructor === Array) {
+          //console.log('Array');
+          //console.log(key);
+          //console.log(fields[key]);
+          //console.log(value.type[0].name);
+        } else {
+          if (!value.type.__.transient) {
+            joinNames.push({
+              key: decamelize(key),
+              prefix: value.type.__.tablePrefix ? value.type.__.tablePrefix : '',
+              name: decamelize(value.type.__.tableName ? value.type.__.tableName : value.type.name),
+              schemaName: decamelize(domainSchema.name)
+            });
+          }
+
+          parentPath.push(key);
+
+          _getSelectFields(fields[key], parentPath, decamelize(key), value.type, selectItems, joinNames, single);
+
+          parentPath.pop();
+        }
+      }
+    }
+  }
+};
+
+export const selectBy = (schema, fields, single = false) => {
+  // select fields
+  const parentPath = [];
+  const selectItems = [];
+  const joinNames = [];
+  _getSelectFields(fields, parentPath, null, schema, selectItems, joinNames, single);
+
+  return query => {
+    // join table names
+    joinNames.map(({ key, prefix, name, schemaName }) => {
+      const tableName = key !== null && key !== name ? `${key}_${name}` : name;
+      query.leftJoin(`${prefix}${name} as ${tableName}`, `${tableName}.id`, `${schemaName}.${key}_id`);
+    });
+
+    return query.select(selectItems);
+  };
 };

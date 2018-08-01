03f7fcf7067ff72d27343de614adbf4a57f1c63f
Dmytro Tverdoklib <nepal-tda@outlook.com>
2018-08-01 16:37:00 +0300

    Add support persisted query

--- packages/server/src/graphql.js
+++ packages/server/src/graphql.js
@@ -1,11 +1,13 @@
 import { ApolloServer } from 'apollo-server-express';
 import { formatResponse } from 'apollo-logger';
+import { MemcachedCache } from 'apollo-server-memcached';
 import 'isomorphic-fetch';
 
 import modules from './modules/index';
 import schema from './api/schema';
 import settings from '../../../settings';
 import log from '../../common/log';
+import { host } from './net';
 
 export default () => {
   return new ApolloServer({
@@ -23,6 +25,12 @@ export default () => {
           apiKey: settings.engine.apiKey
         }
       : false,
-    playground: false
+    playground: false,
+    persistedQueries: {
+      cache: new MemcachedCache(
+        [host],
+        { retries: 10, retry: 10000 } // Options
+      )
+    }
   });
 };

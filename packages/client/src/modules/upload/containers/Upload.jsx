3c1660b98605384a4236e1bd74bfe2757fe62dbf
Mykyta Lyzhov <lyzhovnik@gmail.com>
2018-08-06 18:40:11 +0300

    Move logic from views to containers

--- packages/client/src/modules/upload/containers/Upload.jsx
+++ packages/client/src/modules/upload/containers/Upload.jsx
@@ -5,14 +5,47 @@ import UploadView from '../components/UploadView';
 import FILES_QUERY from '../graphql/FilesQuery.graphql';
 import UPLOAD_FILES from '../graphql/UploadFiles.graphql';
 import REMOVE_FILE from '../graphql/RemoveFile.graphql';
+import translate from '../../../i18n';
 
 class Upload extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      error: null
+    };
+  }
+
+  handleUploadFiles = async files => {
+    const { uploadFiles } = this.props;
+    const result = await uploadFiles(files);
+
+    this.handleErrors(result);
+  };
+
+  handleRemoveFile = async id => {
+    const { removeFile } = this.props;
+    const result = await removeFile(id);
+
+    this.handleErrors(result);
+  };
+
+  handleErrors(result) {
+    this.setState({ error: result && result.error ? result.error : null });
+  }
+
   render() {
-    return <UploadView {...this.props} />;
+    return (
+      <UploadView
+        {...this.props}
+        error={this.state.error}
+        handleRemoveFile={this.handleRemoveFile}
+        handleUploadFiles={this.handleUploadFiles}
+      />
+    );
   }
 }
 
-const UploadWithApollo = compose(
+export default compose(
   graphql(FILES_QUERY, {
     options: () => {
       return {
@@ -21,6 +54,7 @@ const UploadWithApollo = compose(
     },
     props({ data: { loading, error, files, refetch } }) {
       if (error) throw new Error(error);
+
       return { loading, files, refetch };
     }
   }),
@@ -33,6 +67,7 @@ const UploadWithApollo = compose(
           } = await mutate({
             variables: { files }
           });
+
           refetch();
           return uploadFiles;
         } catch (e) {
@@ -50,6 +85,7 @@ const UploadWithApollo = compose(
           } = await mutate({
             variables: { id }
           });
+
           refetch();
           return removeFile;
         } catch (e) {
@@ -57,7 +93,6 @@ const UploadWithApollo = compose(
         }
       }
     })
-  })
+  }),
+  translate('upload')
 )(Upload);
-
-export default UploadWithApollo;

f80fd38338f7e386df3bbe300d79f406d78d00e8
Victor Vlasenko <victor.vlasenko@sysgears.com>
2018-08-06 15:10:57 +0300

    Drop unused server tests

--- packages/server/src/modules/upload/__tests__/Upload.spec.js
+++ packages/server/src/modules/upload/__tests__/Upload.spec.js
@@ -1,26 +1,12 @@
 /*eslint-disable no-unused-vars*/
-// General imports
-import chai from 'chai';
 import { step } from 'mocha-steps';
 
-// Helpers
-import { getServer, getApollo } from '../../../testHelpers/integrationSetup';
+import { getApollo } from '../../../testHelpers/integrationSetup';
 
 describe('Upload API works', () => {
-  let server, apollo;
+  let apollo;
 
   before(() => {
-    server = getServer();
     apollo = getApollo();
-  });
-
-  step('Has GraphQL Playground endpoint', () => {
-    return chai
-      .request(server)
-      .get('/gplayground')
-      .then(res => {
-        res.should.have.status(200);
-        res.body.should.be.eql({});
-      });
   });
 });

7153311ba56b0fd7c683c80a278915fb559706a2
Michael Fürmann <michael@spicyweb.de>
2018-08-07 11:31:12 +0200

    Add tests using authentication with different users

--- packages/server/src/modules/user/__tests__/User.spec.js
+++ packages/server/src/modules/user/__tests__/User.spec.js
@@ -1,12 +1,12 @@
 /*eslint-disable no-unused-vars*/
-import { expect } from 'chai';
+import chai, { expect } from 'chai';
 import { step } from 'mocha-steps';
 
 import { getApollo } from '../../../testHelpers/integrationSetup';
+import { login, logout } from '../../../testHelpers/session';
 
 import CURRENT_USER_QUERY from '../../../../../client/src/modules/user/graphql/CurrentUserQuery.graphql';
-import LOGIN from '../../../../../client/src/modules/user/graphql/Login.graphql';
-import LOGOUT from '../../../../../client/src/modules/user/access/session/graphql/Logout.graphql';
+import USER_QUERY from '../../../../../client/src/modules/user/graphql/UserQuery.graphql';
 
 describe('User API works', () => {
   let apollo;
@@ -21,17 +21,70 @@ describe('User API works', () => {
   });
 
   step('Siging in as ordinary user works', async () => {
-    await apollo.mutate({
-      mutation: LOGIN,
-      variables: { input: { usernameOrEmail: 'user', password: 'user1234' } }
-    });
+    await login('user', 'user1234');
     const result = await apollo.query({ query: CURRENT_USER_QUERY });
     expect(result.data.currentUser.username).to.equal('user');
   });
 
   step('Signing out as ordinary user works', async () => {
-    await apollo.mutate({ mutation: LOGOUT });
+    await logout();
     const result = await apollo.query({ query: CURRENT_USER_QUERY });
     expect(result.data).to.deep.equal({ currentUser: null });
+  });
+
+  step("Can't query user profiles as guest", async done => {
+    apollo
+      .query({ query: USER_QUERY, variables: { id: 1 } })
+      .then(() => done('This test is expected to throw an error'))
+      .catch(ex => {
+        // Check for values in the thrown graphQL error object here
+        // e.g. message, extensions.code or other
+        chai.expect(ex.graphQLErrors).to.containSubset([
+          {
+            extensions: { code: 'UNAUTHENTICATED' }
+          }
+        ]);
+        // Error received as expected, consider test as done
+        done();
+      })
+      .catch(done);
+  });
+
+  describe('Tests with authenticated user', () => {
+    before(async () => {
+      await login('user', 'user1234');
+    });
+    after(async () => {
+      await logout();
+    });
+
+    step('Can query own user profile', async () => {
+      const result = await apollo.query({ query: USER_QUERY, variables: { id: 2 } });
+      expect(result.data.user.user.username).to.equal('user');
+    });
+
+    step('Cannot query other users profile', async () => {
+      const result = await apollo.query({ query: USER_QUERY, variables: { id: 1 } });
+      expect(result.data.user.user).to.be.null;
+    });
+  });
+
+  describe('Tests with authenticated admin', () => {
+    before(async () => {
+      await login('admin', 'admin123');
+    });
+    after(async () => {
+      await logout();
+    });
+
+    step('Can query own user profile', async () => {
+      const result = await apollo.query({ query: USER_QUERY, variables: { id: 1 } });
+      expect(result.data.user.user.username).to.equal('admin');
+    });
+
+    step('Can query other users profile', async () => {
+      const result = await apollo.query({ query: USER_QUERY, variables: { id: 2 } });
+      expect(result.data.user.user.username).to.equal('user');
+    });
   });
 });

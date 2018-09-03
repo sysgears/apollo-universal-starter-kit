5f2ad0e43af3a51dcb185e1e92607b39e6ed9316
Mykyta Lyzhov <lyzhovnik@gmail.com>
2018-09-03 16:12:01 +0300

    Implement new Query component in the CreditCardInfo component

--- packages/client/src/modules/payments/stripe/recurring/components/CreditCardInfoView.tsx
+++ packages/client/src/modules/payments/stripe/recurring/components/CreditCardInfoView.tsx
@@ -5,35 +5,40 @@ import { Button, CardGroup, CardTitle, CardText } from '../../../../common/compo
 
 interface CardInfoViewProps {
   loading: boolean;
-  expiryMonth: number;
-  expiryYear: number;
-  last4: string;
-  brand: string;
+  creditCard: {
+    expiryMonth: number;
+    expiryYear: number;
+    last4: string;
+    brand: string;
+  };
   t: TranslateFunction;
 }
 
-export default ({ loading, expiryMonth, expiryYear, last4, brand, t }: CardInfoViewProps) => (
-  <div>
-    {!loading &&
-      expiryMonth &&
-      expiryYear &&
-      last4 &&
-      brand && (
-        <CardGroup>
-          <CardTitle>{t('card.title')}</CardTitle>
-          <CardText>
-            {t('card.text.card')}: {brand} ************
-            {last4}
-          </CardText>
-          <CardText>
-            {t('card.text.expires')}: {expiryMonth}/{expiryYear}
-          </CardText>
-          <CardText>
-            <Link to="/update-card">
-              <Button color="primary">{t('card.btnUpdate')}</Button>
-            </Link>
-          </CardText>
-        </CardGroup>
-      )}
-  </div>
-);
+export default ({ loading, t, creditCard }: CardInfoViewProps) => {
+  return (
+    <div>
+      {!loading &&
+        creditCard &&
+        creditCard.expiryMonth &&
+        creditCard.expiryYear &&
+        creditCard.last4 &&
+        creditCard.brand && (
+          <CardGroup>
+            <CardTitle>{t('card.title')}</CardTitle>
+            <CardText>
+              {t('card.text.card')}: {creditCard.brand} ************
+              {creditCard.last4}
+            </CardText>
+            <CardText>
+              {t('card.text.expires')}: {creditCard.expiryMonth}/{creditCard.expiryYear}
+            </CardText>
+            <CardText>
+              <Link to="/update-card">
+                <Button color="primary">{t('card.btnUpdate')}</Button>
+              </Link>
+            </CardText>
+          </CardGroup>
+        )}
+    </div>
+  );
+};
--- packages/client/src/modules/payments/stripe/recurring/containers/CreditCardInfo.js
+++ packages/client/src/modules/payments/stripe/recurring/containers/CreditCardInfo.js
@@ -1,46 +1,20 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { graphql, compose } from 'react-apollo';
+import { Query } from 'react-apollo';
 
 import CreditCardInfoView from '../components/CreditCardInfoView';
 import translate from '../../../../../i18n';
 
 import CREDIT_CARD_QUERY from '../graphql/CreditCardQuery.graphql';
 
-const CreditCardInfo = ({ loading, expiryMonth, expiryYear, last4, brand, t }) => (
-  <CreditCardInfoView
-    loading={loading}
-    expiryMonth={expiryMonth}
-    expiryYear={expiryYear}
-    last4={last4}
-    brand={brand}
-    t={t}
-  />
+const CreditCardInfo = ({ t }) => (
+  <Query query={CREDIT_CARD_QUERY} fetchPolicy="network-only">
+    {({ loading, data }) => <CreditCardInfoView loading={loading} t={t} creditCard={data.stripeSubscriptionCard} />}
+  </Query>
 );
 
 CreditCardInfo.propTypes = {
-  loading: PropTypes.bool.isRequired,
-  expiryMonth: PropTypes.number,
-  expiryYear: PropTypes.number,
-  last4: PropTypes.string,
-  brand: PropTypes.string,
   t: PropTypes.func
 };
 
-const CreditCardInfoWithApollo = compose(
-  graphql(CREDIT_CARD_QUERY, {
-    options: { fetchPolicy: 'network-only' },
-    props({ data: { loading, stripeSubscriptionCard } }) {
-      return {
-        loading,
-        expiryMonth: stripeSubscriptionCard && stripeSubscriptionCard.expiryMonth,
-        expiryYear: stripeSubscriptionCard && stripeSubscriptionCard.expiryYear,
-        last4: stripeSubscriptionCard && stripeSubscriptionCard.last4,
-        brand: stripeSubscriptionCard && stripeSubscriptionCard.brand
-      };
-    }
-  }),
-  translate('subscription')
-)(CreditCardInfo);
-
-export default CreditCardInfoWithApollo;
+export default translate('subscription')(CreditCardInfo);

2027d8b4f590b36d293d40c21c9fdc69bac8437c
Mykyta Lyzhov <lyzhovnik@gmail.com>
2018-09-21 20:14:03 +0300

    Remove react-native-credit-card from client package.json

--- packages/mobile/.spinrc.js
+++ packages/mobile/.spinrc.js
@@ -10,7 +10,7 @@ const config = {
       defines: {
         __CLIENT__: true
       },
-      enabled: true
+      enabled: false
     },
     ios: {
       entry: './src/index.ts',
@@ -20,7 +20,7 @@ const config = {
       defines: {
         __CLIENT__: true
       },
-      enabled: true
+      enabled: false
     },
     test: {
       stack: ['server'],

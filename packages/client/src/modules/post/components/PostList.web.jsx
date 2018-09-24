3f116b7a20f5e93538838a149d34b33ad665dd1b
Mykyta Lyzhov <lyzhovnik@gmail.com>
2018-09-24 15:54:11 +0300

    Implement showing no display message on the web

--- packages/client/src/modules/post/components/PostList.web.jsx
+++ packages/client/src/modules/post/components/PostList.web.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import PropTypes from 'prop-types';
 import Helmet from 'react-helmet';
 import { Link } from 'react-router-dom';
@@ -23,21 +23,6 @@ class PostList extends React.PureComponent {
     deletePost(id);
   };
 
-  renderMetaData = () => {
-    const { t } = this.props;
-    return (
-      <Helmet
-        title={`${settings.app.name} - ${t('list.title')}`}
-        meta={[
-          {
-            name: 'description',
-            content: `${settings.app.name} - ${t('list.meta')}`
-          }
-        ]}
-      />
-    );
-  };
-
   handlePageChange = (pagination, pageNumber) => {
     const {
       posts: {
@@ -45,71 +30,68 @@ class PostList extends React.PureComponent {
       },
       loadData
     } = this.props;
-    if (pagination === 'relay') {
-      loadData(endCursor + 1, 'add');
-    } else {
-      loadData((pageNumber - 1) * itemsNumber, 'replace');
-    }
+
+    pagination === 'relay' ? loadData(endCursor + 1, 'add') : loadData((pageNumber - 1) * itemsNumber, 'replace');
   };
 
   render() {
     const { loading, posts, t } = this.props;
-    if (loading && !posts) {
-      return (
-        <PageLayout>
-          {this.renderMetaData()}
-          <div className="text-center">{t('post.loadMsg')}</div>
-        </PageLayout>
-      );
-    } else {
-      const columns = [
-        {
-          title: t('list.column.title'),
-          dataIndex: 'title',
-          key: 'title',
-          render: (text, record) => (
-            <Link className="post-link" to={`/post/${record.id}`}>
-              {text}
-            </Link>
-          )
-        },
-        {
-          title: t('list.column.actions'),
-          key: 'actions',
-          width: 50,
-          render: (text, record) => (
-            <Button
-              color="primary"
-              size="sm"
-              className="delete-button"
-              onClick={() => this.handleDeletePost(record.id)}
-            >
-              {t('post.btn.del')}
-            </Button>
-          )
-        }
-      ];
-      return (
-        <PageLayout>
-          {this.renderMetaData()}
-          <h2>{t('list.subTitle')}</h2>
-          <Link to="/post/new">
-            <Button color="primary">{t('list.btn.add')}</Button>
+    const columns = [
+      {
+        title: t('list.column.title'),
+        dataIndex: 'title',
+        key: 'title',
+        render: (text, record) => (
+          <Link className="post-link" to={`/post/${record.id}`}>
+            {text}
           </Link>
-          <h1 />
-          <Table dataSource={posts.edges.map(({ node }) => node)} columns={columns} />
-          <Pagination
-            itemsPerPage={posts.edges.length}
-            handlePageChange={this.handlePageChange}
-            hasNextPage={posts.pageInfo.hasNextPage}
-            pagination={type}
-            total={posts.totalCount}
-            loadMoreText={t('list.btn.more')}
-            defaultPageSize={itemsNumber}
-          />
-        </PageLayout>
-      );
-    }
+        )
+      },
+      {
+        title: t('list.column.actions'),
+        key: 'actions',
+        width: 50,
+        render: (text, record) => (
+          <Button color="primary" size="sm" className="delete-button" onClick={() => this.handleDeletePost(record.id)}>
+            {t('post.btn.del')}
+          </Button>
+        )
+      }
+    ];
+    const Loading = () => <div className="text-center">{t('post.loadMsg')}</div>;
+    const NoPostsMessage = () => <div className="text-center">{t('post.noPostsMsg')}</div>;
+    const RenderPosts = () => (
+      <Fragment>
+        <Table dataSource={posts.edges.map(({ node }) => node)} columns={columns} />
+        <Pagination
+          itemsPerPage={posts.edges.length}
+          handlePageChange={this.handlePageChange}
+          hasNextPage={posts.pageInfo.hasNextPage}
+          pagination={type}
+          total={posts.totalCount}
+          loadMoreText={t('list.btn.more')}
+          defaultPageSize={itemsNumber}
+        />
+      </Fragment>
+    );
+
+    return (
+      <PageLayout>
+        {/* Render metadata */}
+        <Helmet
+          title={`${settings.app.name} - ${t('list.title')}`}
+          meta={[{ name: 'description', content: `${settings.app.name} - ${t('list.meta')}` }]}
+        />
+        <h2>{t('list.subTitle')}</h2>
+        <Link to="/post/new">
+          <Button color="primary">{t('list.btn.add')}</Button>
+        </Link>
+        {/* Render loader */}
+        {loading && !posts && <Loading />}
+        {/* Render main post content */}
+        {posts && posts.totalCount ? <RenderPosts /> : <NoPostsMessage />}
+      </PageLayout>
+    );
   }
 }
 

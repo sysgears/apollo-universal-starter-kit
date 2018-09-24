33117337b9693098c999b746afacfe094e98268b
Oleh Tsivan <oleh.tsivan@sysgears.com>
2018-09-24 12:45:15 +0300

    Refactored 'InjectionModules'

--- src/main/scala/InjectionModules.scala
+++ src/main/scala/InjectionModules.scala
@@ -1,13 +1,13 @@
 import akka.actor.ActorSystem
 import modules.{AkkaModule, ConfigModule, CounterModule, ExecutionModule}
-import scala.collection.JavaConverters._
 
 object InjectionModules {
 
-  def Modules(implicit actorSystem: ActorSystem) = Iterable(
-    new CounterModule,
-    new ConfigModule,
-    new AkkaModule,
-    new ExecutionModule
-  ).asJava
+  def Modules(implicit actorSystem: ActorSystem) =
+    java.util.Arrays.asList(
+      new CounterModule,
+      new ConfigModule,
+      new AkkaModule,
+      new ExecutionModule
+    )
 }

47e9b27e11e02dc8ec4d7014776c6d32fc96c9f3
Oleh Tsivan <oleh.tsivan@sysgears.com>
2018-09-25 11:16:55 +0300

    Added a 'serverCounter' query and an 'addServerCounter' mutation to 'Counter'

--- src/main/scala/graphql/GraphQL.scala
+++ src/main/scala/graphql/GraphQL.scala
@@ -1,10 +1,22 @@
 package graphql
 
-import sangria.schema.Schema
+import models.counter.Counter
+import sangria.schema.{ObjectType, Schema}
 
 object GraphQL {
   val maxQueryDepth = 15
   val maxQueryComplexity = 1000
 
-  val Schema: Schema[Unit, Unit] = ???
+  val Schema: Schema[GraphQLContext, Unit] = sangria.schema.Schema(
+    query = ObjectType(
+      name = "Query",
+      fields = Counter.GraphQL.Queries
+    ),
+    mutation = Some(
+      ObjectType(
+        name = "Mutation",
+        fields = Counter.GraphQL.Mutations
+      )
+    )
+  )
 }
--- src/main/scala/models/counter/Counter.scala
+++ src/main/scala/models/counter/Counter.scala
@@ -1,3 +1,38 @@
 package models.counter
 
+import graphql.GraphQLContext
+import sangria.macros.derive._
+import sangria.schema.{Argument, Field, IntType, ObjectType}
+
 case class Counter(amount: Int)
+
+object Counter {
+
+  object Types {
+    implicit val Сounter: ObjectType[Unit, Counter] = deriveObjectType(ObjectTypeName("Counter"))
+  }
+
+  object GraphQL {
+
+    val Queries: List[Field[GraphQLContext, Unit]] = List(
+      Field(
+        name = "serverCounter",
+        fieldType = Types.Сounter,
+        arguments = Nil,
+        resolve = sc => sc.ctx.counterResolver.serverCounter
+      )
+    )
+
+    val Mutations: List[Field[GraphQLContext, Unit]] = List(
+      Field(
+        name = "addServerCounter",
+        fieldType = Types.Сounter,
+        arguments = Argument(name = "amount", argumentType = IntType) :: Nil,
+        resolve = sc => {
+          val amount = sc.args.arg[Int]("amount")
+          sc.ctx.counterResolver.addServerCounter(amount)
+        }
+      )
+    )
+  }
+}

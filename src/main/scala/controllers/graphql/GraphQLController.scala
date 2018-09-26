d01045802530177801bd5ea40e3f56a776123b53
Dmitry Tverdokhleb <dmitriy.tverdokhleb@sysgears.com>
2018-09-26 16:21:59 +0300

    Move Http handler to separate module

--- src/main/scala/controllers/graphql/GraphQLController.scala
+++ src/main/scala/controllers/graphql/GraphQLController.scala
@@ -1,35 +1,29 @@
 package controllers.graphql
 
 import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
-import akka.http.scaladsl.marshalling.ToResponseMarshallable
-import akka.http.scaladsl.model.StatusCodes._
-import akka.http.scaladsl.model.sse.ServerSentEvent
 import akka.http.scaladsl.server.Directives._
 import akka.http.scaladsl.server.Route
 import akka.stream.ActorMaterializer
 import graphql.{GraphQL, GraphQLContext, GraphQLContextFactory}
 import javax.inject.{Inject, Singleton}
-import sangria.ast.OperationType.Subscription
-import sangria.execution.{ErrorWithResolver, Executor, QueryAnalysisError}
-import sangria.marshalling.sprayJson._
-import sangria.parser.{QueryParser, SyntaxError}
+import sangria.execution.Executor
 import sangria.renderer.SchemaRenderer
 import spray.json._
 
 import scala.concurrent.ExecutionContext
-import scala.util.control.NonFatal
-import scala.util.{Failure, Success}
 
 @Singleton
 class GraphQLController @Inject()(graphQlContextFactory: GraphQLContextFactory,
-                                  graphQlExecutor: Executor[GraphQLContext, Unit])
+                                  graphQlExecutor: Executor[GraphQLContext, Unit],
+                                  httpHandler: HttpHandler)
                                  (implicit val executionContext: ExecutionContext,
                                   implicit val actorMaterializer: ActorMaterializer) {
+
   val Routes: Route =
     path("graphql") {
       get {
         parameters('query, 'operation.?) { (query, operation) =>
-          handleQuery(query, operation)
+          httpHandler.handleQuery(query, operation)
         }
       } ~
         post {
@@ -43,64 +37,11 @@ class GraphQLController @Inject()(graphQlContextFactory: GraphQLContextFactory,
               case Some(obj: JsObject) => obj
               case _ => JsObject.empty
             }
-            handleQuery(query, operation, vars)
+            httpHandler.handleQuery(query, operation, vars)
           }
         }
     } ~
       (path("schema") & get) {
         complete(SchemaRenderer.renderSchema(GraphQL.Schema))
       }
-
-  private def handleQuery(query: String, operation: Option[String], variables: JsObject = JsObject.empty) = {
-    val ctx = graphQlContextFactory.createContextForRequest
-    QueryParser.parse(query) match {
-      case Success(queryAst) =>
-        queryAst.operationType(operation) match {
-          case Some(Subscription) =>
-            import akka.http.scaladsl.marshalling.sse.EventStreamMarshalling._
-            import sangria.execution.ExecutionScheme.Stream
-            import sangria.streaming.akkaStreams._
-            complete(
-              graphQlExecutor.prepare(queryAst, ctx, (), operation, variables)
-                .map {
-                  preparedQuery =>
-                    ToResponseMarshallable(preparedQuery.execute()
-                      .map(result => ServerSentEvent(result.compactPrint))
-                      .recover {
-                        case NonFatal(error) =>
-                          ServerSentEvent(error.getMessage)
-                      }
-                    )
-                }
-                .recover {
-                  case error: QueryAnalysisError => ToResponseMarshallable(BadRequest -> error.resolveError)
-                  case error: ErrorWithResolver => ToResponseMarshallable(InternalServerError -> error.resolveError)
-                }
-            )
-          case _ =>
-            complete(
-              graphQlExecutor.execute(queryAst, ctx, (), operation, variables)
-                .map(OK -> _)
-                .recover {
-                  case error: QueryAnalysisError => BadRequest -> error.resolveError
-                  case error: ErrorWithResolver => InternalServerError -> error.resolveError
-                }
-            )
-        }
-      case Failure(e: SyntaxError) =>
-        complete(
-          BadRequest,
-          JsObject(
-            "syntaxError" -> JsString(e.getMessage),
-            "locations" -> JsArray(
-              JsObject(
-                "line" -> JsNumber(e.originalError.position.line),
-                "column" -> JsNumber(e.originalError.position.column)
-              )
-            )
-          )
-        )
-      case Failure(_) => complete(InternalServerError)
-    }
-  }
 }

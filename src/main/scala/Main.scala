8fb9e41906fb3071e61a03387cdde0225d147ac8
Dmitry Tverdokhleb <dmitriy.tverdokhleb@sysgears.com>
2018-09-26 10:46:38 +0300

    Refactor code for injecting actor system

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -2,17 +2,17 @@ import InjectionModules.Modules
 import akka.actor.ActorSystem
 import akka.http.scaladsl.Http
 import akka.stream.ActorMaterializer
-import com.google.inject.{Guice, Injector}
+import com.google.inject.Guice
 import controllers.GraphQLController
 import net.codingwell.scalaguice.InjectorExtensions._
 
 object Main extends App {
 
-  implicit val system: ActorSystem = ActorSystem("scala-starter-kit")
-  implicit val materializer: ActorMaterializer = ActorMaterializer()
-
-  val injector: Injector = Guice.createInjector(Modules)
+  val injector = Guice.createInjector(Modules)
   val routes = injector.instance[GraphQLController].Routes
+
+  implicit val system: ActorSystem = injector.instance[ActorSystem]
+  implicit val materializer: ActorMaterializer = injector.instance[ActorMaterializer]
 
   Http().bindAndHandle(routes, "0.0.0.0")
 }

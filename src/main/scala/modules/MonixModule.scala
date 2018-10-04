ae15285e2ff0d15fa3b8ad5f0a267ba8b441960b
Oleh Tsivan <oleh.tsivan@sysgears.com>
2018-10-04 12:43:07 +0300

    Move bind of 'PublisherService' in different module

--- src/main/scala/modules/MonixModule.scala
+++ src/main/scala/modules/MonixModule.scala
@@ -1,17 +1,12 @@
 package modules
 
 import com.google.inject.AbstractModule
-import models.counter.Counter
 import monix.execution.Scheduler
 import net.codingwell.scalaguice.ScalaModule
-import services.publisher.PublisherService
-import services.publisher.monix.MonixPublisherServiceImpl
 
 class MonixModule extends AbstractModule with ScalaModule {
 
   override def configure {
     bind[Scheduler].toInstance(Scheduler.Implicits.global)
-
-    bind[PublisherService[Counter]].to[MonixPublisherServiceImpl[Counter]]
   }
 }

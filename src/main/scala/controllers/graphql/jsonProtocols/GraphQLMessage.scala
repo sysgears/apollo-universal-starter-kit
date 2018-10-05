cdaf5bd521248beb7e16602734f206ffbf1b0fd2
Oleh Tsivan <oleh.tsivan@sysgears.com>
2018-10-05 18:52:11 +0300

    Fix typo

--- src/main/scala/controllers/graphql/jsonProtocols/GraphQLMessage.scala
+++ src/main/scala/controllers/graphql/jsonProtocols/GraphQLMessage.scala
@@ -2,7 +2,7 @@ package controllers.graphql.jsonProtocols
 
 import spray.json.{DefaultJsonProtocol, JsObject, RootJsonFormat}
 
-case class GraphQLMessage(query: String, operationName: Option[String], variables: Option[JsObject])
+case class GraphQLMessage(query: String, operationName: Option[String] = None, variables: Option[JsObject] = None)
 
 object GraphQLMessageProtocol extends DefaultJsonProtocol {
   implicit val graphQLMessageFormat: RootJsonFormat[GraphQLMessage] = jsonFormat3(GraphQLMessage)

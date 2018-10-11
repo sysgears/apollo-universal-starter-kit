01440915d0f18cfa456e4388f5afb1c00736970f
Dmitry Tverdokhleb <dmitriy.tverdokhleb@sysgears.com>
2018-10-11 10:27:55 +0300

    Add OverflowStrategy for dropping old elements from source

--- src/main/scala/services/publisher/monix/MonixPublisherServiceImpl.scala
+++ src/main/scala/services/publisher/monix/MonixPublisherServiceImpl.scala
@@ -2,6 +2,7 @@ package services.publisher.monix
 
 import javax.inject.{Inject, Singleton}
 import monix.execution.Scheduler
+import monix.reactive.OverflowStrategy
 import monix.reactive.subjects.ConcurrentSubject
 import org.reactivestreams.Publisher
 import services.publisher.PublisherService
@@ -9,7 +10,7 @@ import services.publisher.PublisherService
 @Singleton
 class MonixPublisherServiceImpl[T] @Inject()(implicit val scheduler: Scheduler) extends PublisherService[T] {
 
-  lazy val sourceCounter: ConcurrentSubject[T, T] = ConcurrentSubject.publish[T]
+  lazy val sourceCounter: ConcurrentSubject[T, T] = ConcurrentSubject.publish[T](OverflowStrategy.DropOld(16))
 
   override def getPublisher: Publisher[T] = sourceCounter.toReactivePublisher[T]
 

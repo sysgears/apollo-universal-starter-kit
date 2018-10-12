b991c51c3386b469975678417433bc312ec41216
Oleh Tsivan <oleh.tsivan@sysgears.com>
2018-10-12 15:42:29 +0300

    Add new test in 'QuerySpec'

--- src/test/scala/controllers/graphql/counter/QuerySpec.scala
+++ src/test/scala/controllers/graphql/counter/QuerySpec.scala
@@ -3,55 +3,55 @@ package controllers.graphql.counter
 import akka.http.scaladsl.model.HttpEntity
 import akka.http.scaladsl.model.MediaTypes.`application/json`
 import akka.http.scaladsl.model.StatusCodes.OK
-import akka.http.scaladsl.server.Route
-import akka.http.scaladsl.testkit.ScalatestRouteTest
 import akka.util.ByteString
-import controllers.graphql.GraphQLController
-import controllers.graphql.Utils.resetCounter
+import controllers.graphql.TestHelper
 import controllers.graphql.jsonProtocols.GraphQLMessage
 import controllers.graphql.jsonProtocols.GraphQLMessageJsonProtocol._
 import models.counter.Counter
-import org.scalatest.{BeforeAndAfter, Matchers, WordSpec}
 import spray.json._
-import util.Injecting
 
-class QuerySpec extends WordSpec with Matchers with ScalatestRouteTest with Injecting with BeforeAndAfter {
+class QuerySpec extends TestHelper {
 
-  val routes: Route = inject[GraphQLController].Routes
-
-  val addServerCounterMutation = "mutation Increment { addServerCounter(amount: 2) { amount } }"
-  val addServerCounterGraphQLMessage = ByteString(GraphQLMessage(addServerCounterMutation, None, None).toJson.compactPrint)
+  val addServerCounterMutation = "mutation Increment { addServerCounter(amount: 1) { amount } }"
+  val addServerCounterGraphQLMessage = ByteString(GraphQLMessage(addServerCounterMutation).toJson.compactPrint)
   val addServerCounterEntity = HttpEntity(`application/json`, addServerCounterGraphQLMessage)
 
   val serverCounter = "query IncrementAndGet { serverCounter { amount } }"
-  val serverCounterGraphQLMessage = ByteString(GraphQLMessage(serverCounter, None, None).toJson.compactPrint)
+  val serverCounterGraphQLMessage = ByteString(GraphQLMessage(serverCounter).toJson.compactPrint)
   val serverCounterEntity = HttpEntity(`application/json`, serverCounterGraphQLMessage)
-
-  before {
-    resetCounter
-  }
-
-  after {
-    resetCounter
-  }
 
   "GraphQLController" must {
 
-    "increment and get amount of counter" in {
-      implicit val counterJsonReader = CounterJsonReader
+    implicit val counterJsonReader = CounterJsonReader
+
+    "increment amount of counter" in {
       Post("/graphql", addServerCounterEntity) ~> routes ~> check {
         val counter = responseAs[String].parseJson.convertTo[Counter]
 
         status shouldBe OK
         contentType.mediaType shouldBe `application/json`
-        counter.amount shouldBe 2
+        counter.amount shouldBe 1
       }
-      Post("/graphql", serverCounterEntity) ~> routes ~> check {
-        val counter = responseAs[String].parseJson.convertTo[Counter]
+    }
 
-        status shouldBe OK
-        contentType.mediaType shouldBe `application/json`
-        counter.amount shouldBe 2
+    "increment several times and then get an amount of counter" in {
+      Post("/graphql", addServerCounterEntity) ~> routes ~> check {
+
+        Post("/graphql", addServerCounterEntity) ~> routes ~> check {
+          val counter = responseAs[String].parseJson.convertTo[Counter]
+
+          status shouldBe OK
+          contentType.mediaType shouldBe `application/json`
+          counter.amount shouldBe 2
+        }
+
+        Post("/graphql", serverCounterEntity) ~> routes ~> check {
+          val counter = responseAs[String].parseJson.convertTo[Counter]
+
+          status shouldBe OK
+          contentType.mediaType shouldBe `application/json`
+          counter.amount shouldBe 2
+        }
       }
     }
   }

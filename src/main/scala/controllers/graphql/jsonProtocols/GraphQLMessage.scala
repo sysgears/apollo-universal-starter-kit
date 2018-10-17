6c007ca73d9c0e99ed501c08bd0390f4f9dbb76e
Dmitry Tverdokhleb <dmitriy.tverdokhleb@sysgears.com>
2018-10-17 11:09:10 +0300

    Add "json" prefix for marshalling/unmarshalling.

--- src/main/scala/controllers/graphql/jsonProtocols/GraphQLMessage.scala
+++ src/main/scala/controllers/graphql/jsonProtocols/GraphQLMessage.scala
@@ -4,6 +4,6 @@ import spray.json.{DefaultJsonProtocol, JsObject, RootJsonFormat}
 
 case class GraphQLMessage(query: String, operationName: Option[String], variables: Option[JsObject])
 
-object GraphQLMessageProtocol extends DefaultJsonProtocol {
-  implicit val graphQLMessageFormat: RootJsonFormat[GraphQLMessage] = jsonFormat3(GraphQLMessage)
+object GraphQLMessageJsonProtocol extends DefaultJsonProtocol {
+  implicit val graphQLMessageJsonFormat: RootJsonFormat[GraphQLMessage] = jsonFormat3(GraphQLMessage)
 }

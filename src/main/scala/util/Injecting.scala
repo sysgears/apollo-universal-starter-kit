424393a03c74ab14517531f0035909eaabd1655f
Oleh Tsivan <oleh.tsivan@sysgears.com>
2018-10-18 17:05:41 +0300

    Injecting dependency modules with help of reflection

--- src/main/scala/util/Injecting.scala
+++ src/main/scala/util/Injecting.scala
@@ -2,9 +2,13 @@ package util
 
 import java.lang.annotation.Annotation
 
+import _root_.util.ReflectionHelper.getInstances
 import com.google.inject.{Guice, Injector}
+import com.typesafe.config.ConfigFactory
 import net.codingwell.scalaguice.InjectorExtensions._
-import util.InjectionModules._
+import net.codingwell.scalaguice.ScalaModule
+
+import scala.collection.JavaConverters._
 
 trait Injecting {
   val injector: Injector = Injecting.injector
@@ -19,5 +23,12 @@ trait Injecting {
 }
 
 object Injecting {
-  val injector: Injector = Guice.createInjector(modules)
+
+  val injector: Injector = Guice.createInjector {
+    getInstances[ScalaModule](classesNames).asJava
+  }
+
+  private def classesNames: List[String] = {
+    ConfigFactory.load.getList("modules").asScala.map(_.render.drop(1).dropRight(1)).toList
+  }
 }

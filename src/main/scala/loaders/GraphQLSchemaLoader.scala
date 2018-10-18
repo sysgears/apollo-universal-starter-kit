6a1c356b0827f52b70a4ee72e014de666db8e119
Dmitry Tverdokhleb <dmitriy.tverdokhleb@sysgears.com>
2018-10-18 17:27:53 +0300

    Refactore GraphQLSchemaLoader

--- src/main/scala/loaders/GraphQLSchemaLoader.scala
+++ src/main/scala/loaders/GraphQLSchemaLoader.scala
@@ -3,33 +3,17 @@ package loaders
 import com.typesafe.config.ConfigFactory
 import graphql.{GraphQLContext, GraphQLSchema}
 import sangria.schema.Field
-import util.ReflectionHelper
+import util.ReflectionHelper.getInstances
 
 import scala.collection.JavaConverters._
 
 object GraphQLSchemaLoader {
 
-  private val graphQLSchemaModuleList = ReflectionHelper.getInstances[GraphQLSchema](
-    ConfigFactory.load.getList("graphql.schemas").asScala.map(
-      configValue => {
-        val str: java.lang.String = configValue.render
-        str.substring(1, str.length - 1)
-      }
-    ).toList
-  )
-
-  val queries: List[Field[GraphQLContext, Unit]] = graphQLSchemaModuleList.flatMap {
-    graphQLSchemaModule =>
-      graphQLSchemaModule.queries
+  private val graphQLSchemaModuleList = getInstances[GraphQLSchema] {
+    ConfigFactory.load.getList("graphql.schemas").asScala.map(_.render.drop(1).dropRight(1)).toList
   }
 
-  val mutations: List[Field[GraphQLContext, Unit]] = graphQLSchemaModuleList.flatMap {
-    graphQLSchemaModule =>
-      graphQLSchemaModule.mutations
-  }
-
-  val subscriptions: List[Field[GraphQLContext, Unit]] = graphQLSchemaModuleList.flatMap {
-    graphQLSchemaModule =>
-      graphQLSchemaModule.subscriptions
-  }
+  val queries: List[Field[GraphQLContext, Unit]] = graphQLSchemaModuleList.flatMap(_.queries)
+  val mutations: List[Field[GraphQLContext, Unit]] = graphQLSchemaModuleList.flatMap(_.mutations)
+  val subscriptions: List[Field[GraphQLContext, Unit]] = graphQLSchemaModuleList.flatMap(_.subscriptions)
 }

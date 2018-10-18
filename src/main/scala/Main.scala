81c1674495b484b8eba7698876f1e37b086bbea8
Oleh Tsivan <oleh.tsivan@sysgears.com>
2018-10-18 17:07:20 +0300

    Move 'Injecting' to another package

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -4,7 +4,7 @@ import akka.http.scaladsl.server.Directives._
 import akka.stream.ActorMaterializer
 import controllers.frontend.FrontendController
 import controllers.graphql.GraphQLController
-import util.Injecting
+import injection.Injecting
 
 object Main extends App with Injecting {
   val routes = inject[GraphQLController].Routes ~ inject[FrontendController].Routes
--- src/main/scala/injection/Injecting.scala
+++ src/main/scala/injection/Injecting.scala
@@ -1,4 +1,4 @@
-package util
+package injection
 
 import java.lang.annotation.Annotation
 

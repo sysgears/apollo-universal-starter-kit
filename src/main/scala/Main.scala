3c7217ea3e599e436623a4df459735a75691f755
Dmitry Tverdokhleb <dmitriy.tverdokhleb@sysgears.com>
2018-10-19 14:17:41 +0300

    Add import routes

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -2,11 +2,11 @@ import akka.actor.ActorSystem
 import akka.http.scaladsl.Http
 import akka.stream.ActorMaterializer
 import injection.Injecting
-import loaders.AkkaRoutesLoader
+import loaders.AkkaRoutesLoader.routes
 
 object Main extends App with Injecting {
   implicit val system: ActorSystem = inject[ActorSystem]
   implicit val materializer: ActorMaterializer = inject[ActorMaterializer]
 
-  Http().bindAndHandle(AkkaRoutesLoader.routes, "0.0.0.0")
+  Http().bindAndHandle(routes, "0.0.0.0")
 }

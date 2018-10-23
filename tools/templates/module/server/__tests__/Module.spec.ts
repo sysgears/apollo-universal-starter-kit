1765d11eea6043be1d262e98a06c643614cd2283
Mykyta Lyzhov <lyzhovnik@gmail.com>
2018-10-23 19:15:31 +0300

    Add test examples to templates

--- tools/templates/module/server/__tests__/Module.spec.ts
+++ tools/templates/module/server/__tests__/Module.spec.ts
@@ -1,12 +1,27 @@
-import chai from 'chai';
+import chai, { expect } from 'chai';
+import { Server } from 'http';
+import { ApolloClient } from 'apollo-client';
 import { step } from 'mocha-steps';
-import { getServer } from '../../../testHelpers/integrationSetup';
+import { getServer, getApollo } from '../../../testHelpers/integrationSetup';
+import gql from 'graphql-tag';
+
+const BACKEND_SUPPORTED_QUERIES_QUERY = gql`
+  query __schema {
+    __schema {
+      types {
+        name
+      }
+    }
+  }
+`;
 
 describe('$Module$ API works', () => {
-  let server: any;
+  let server: Server;
+  let apollo: ApolloClient<any>;
 
   before(() => {
     server = getServer();
+    apollo = getApollo();
   });
 
   step('Has GraphiQL endpoint', () => {
@@ -19,5 +34,10 @@ describe('$Module$ API works', () => {
         res.should.have.status(200);
         res.body.should.be.eql({});
       });
+  });
+
+  step('Can query to Graphql backend', async () => {
+    const result = await apollo.query({ query: BACKEND_SUPPORTED_QUERIES_QUERY });
+    expect(result.data).to.have.property('__schema');
   });
 });

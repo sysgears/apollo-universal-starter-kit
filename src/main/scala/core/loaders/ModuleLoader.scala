935d5c2bf4d41a540abef8e3abd248867211be7e
Dmitry Tverdokhleb <dmitriy.tverdokhleb@sysgears.com>
2018-10-25 18:09:03 +0300

    Add checking of IgnoreModule annotation in loaded annotations

--- src/main/scala/core/loaders/ModuleLoader.scala
+++ src/main/scala/core/loaders/ModuleLoader.scala
@@ -7,11 +7,13 @@ import core.graphql.GraphQLSchema
 import core.guice.injection.Injecting.injector
 import net.codingwell.scalaguice.ScalaModule
 import org.clapper.classutil.ClassFinder
+import util.Logger
 
-object ModuleLoader {
+object ModuleLoader extends Logger {
 
   private val classLoader = getClass.getClassLoader
   private val classes = ClassFinder(List(".").map(new File(_))).getClasses()
+  private val ignoreModuleName = classOf[IgnoreModule].getSimpleName
 
   val guiceModules: List[ScalaModule] = instantiateClasses[ScalaModule](classOf[ScalaModule].getName)
 
@@ -19,14 +21,24 @@ object ModuleLoader {
   lazy val graphQLSchemaModules: List[GraphQLSchema] = instantiateClassesWithInject[GraphQLSchema](classOf[GraphQLSchema].getName)
 
   private def instantiateClasses[T](classType: String) = {
-    loadClasses(classType).map(_.newInstance.asInstanceOf[T])
+    loadClasses(classType).map {
+      clazz =>
+        log.info(s"Instantiate a class: ${clazz.getName}")
+        clazz.newInstance.asInstanceOf[T]
+    }
   }
 
   private def instantiateClassesWithInject[T](classType: String) = {
-    loadClasses(classType).map(injector.getInstance(_).asInstanceOf[T])
+    loadClasses(classType).map {
+      clazz =>
+        log.info(s"Instantiate a class with inject: ${clazz.getName}")
+        injector.getInstance(clazz).asInstanceOf[T]
+    }
   }
 
   private def loadClasses(classType: String) = {
-    classes.filter(_.implements(classType)).map(clazz => classLoader.loadClass(clazz.name)).toList
+    classes.filter {
+      clazz => clazz.implements(classType) && !clazz.annotations.exists(_.descriptor.endsWith(ignoreModuleName + ";"))
+    }.map(clazz => classLoader.loadClass(clazz.name)).toList
   }
 }

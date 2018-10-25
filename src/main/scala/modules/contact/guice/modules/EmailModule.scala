34955c16dd7169254c161cf9861ef711437a800c
Dmitry Tverdokhleb <dmitriy.tverdokhleb@sysgears.com>
2018-10-25 11:09:39 +0300

    Refactor mailer injecting via Provider

--- src/main/scala/modules/contact/guice/modules/EmailModule.scala
+++ src/main/scala/modules/contact/guice/modules/EmailModule.scala
@@ -1,17 +1,27 @@
 package modules.contact.guice.modules
 
 import com.github.jurajburian.mailer.{Mailer, SessionFactory, SmtpAddress, SmtpStartTls}
-import com.google.inject.AbstractModule
-import com.typesafe.config.ConfigFactory
+import com.google.inject.{AbstractModule, Provides}
+import com.typesafe.config.Config
+import javax.inject.Singleton
 import net.codingwell.scalaguice.ScalaModule
 
 class EmailModule extends AbstractModule with ScalaModule {
 
-  override def configure() {
-    val config = ConfigFactory.load
-    val session = (SmtpAddress(config.getString("email.host"), config.getInt("email.port"))
-      :: SmtpStartTls()
-      :: SessionFactory()).session(Some(config.getString("email.user") -> config.getString("email.password")))
-    bind[Mailer].toInstance(Mailer(session))
+  @Provides
+  @Singleton
+  def provideMailer(config: Config): Mailer = {
+    val session = (
+      SmtpAddress(
+        config.getString("email.host"),
+        config.getInt("email.port")
+      )
+        :: SmtpStartTls()
+        :: SessionFactory()).session(
+      Some(
+        config.getString("email.user") -> config.getString("email.password")
+      )
+    )
+    Mailer(session)
   }
 }

596cf3052cf9d7a64a317f9dc725181c1920e03e
Oleh Tsivan <oleh.tsivan@sysgears.com>
2018-10-25 16:30:43 +0300

    Add 'Slick' definition for 'Counter'

--- src/main/scala/modules/counter/models/Counter.scala
+++ src/main/scala/modules/counter/models/Counter.scala
@@ -1,3 +1,18 @@
 package modules.counter.models
 
-case class Counter(amount: Int)
+import core.slick.SchemaInitializer
+import slick.lifted.Tag
+import slick.jdbc.SQLiteProfile.api.{Table => SlickTable, _}
+import slick.model.Column
+
+case class Counter(id: Option[Int] = None, amount: Int)
+
+object Counter extends ((Option[Int], Int) => Counter) {
+
+  class Table(tag: Tag) extends SlickTable[Counter](tag, "COUNTERS") {
+    val id = column[Int]("ID", O.PrimaryKey)
+    val amount = column[Int]("AMOUNT")
+
+    override def * = (id.?, amount).mapTo[Counter]
+  }
+}

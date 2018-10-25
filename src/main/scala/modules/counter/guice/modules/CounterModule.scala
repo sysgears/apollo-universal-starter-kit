197e27200e8d22a0d912dfceda846bb1ea6a402d
Oleh Tsivan <oleh.tsivan@sysgears.com>
2018-10-25 16:29:25 +0300

    Add bind of 'CounterRepo' to implementation

--- src/main/scala/modules/counter/guice/modules/CounterModule.scala
+++ src/main/scala/modules/counter/guice/modules/CounterModule.scala
@@ -1,18 +1,24 @@
 package modules.counter.guice.modules
 
 import akka.actor.{ActorRef, ActorSystem}
+import com.google.inject.Provides
 import com.google.inject.name.Named
-import com.google.inject.{AbstractModule, Provides, Singleton}
-import core.services.persistence.PersistenceCleanup
-import modules.counter.services.count.CounterPersistentActor
+import modules.counter.repositories.{CounterRepo, CounterRepoImpl}
+import modules.counter.services.count.CounterActor
 import net.codingwell.scalaguice.ScalaModule
 
-class CounterModule extends AbstractModule with ScalaModule {
+import scala.concurrent.ExecutionContext
+
+class CounterModule extends ScalaModule {
+
+  override def configure() {
+    bind[CounterRepo].to[CounterRepoImpl]
+  }
 
   @Provides
-  @Singleton
-  @Named(CounterPersistentActor.name)
-  def counterActor(actorSystem: ActorSystem, persistenceCleanup: PersistenceCleanup): ActorRef = {
-    actorSystem.actorOf(CounterPersistentActor.props(persistenceCleanup))
+  @Named(CounterActor.name)
+  def counterActor(actorSystem: ActorSystem, counterRepo: CounterRepo)
+                  (implicit executionContext: ExecutionContext): ActorRef = {
+    actorSystem.actorOf(CounterActor.props(counterRepo))
   }
 }

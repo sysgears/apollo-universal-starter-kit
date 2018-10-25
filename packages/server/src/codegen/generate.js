1a0cdcb6838c9c01004f9304496398dae84d3405
Kamil Kisiela <kamil.kisiela@gmail.com>
2018-10-25 09:37:48 +0200

    Watch mode

--- packages/server/src/codegen/generate.js
+++ packages/server/src/codegen/generate.js
@@ -1,11 +1,14 @@
 const { generate } = require('graphql-code-generator');
 const { resolve } = require('path');
 
+const watch = process.argv[2] === 'watch';
+
 generate(
   {
     schema: resolve(__dirname, 'schema.js'),
     template: 'graphql-codegen-typescript-template',
     out: resolve(__dirname, '../../typings/graphql.ts'),
+    watch,
     overwrite: true
   },
   true

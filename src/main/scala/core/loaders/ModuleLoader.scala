0e6645cf6a5af32889f8ad9f5f40804c659633cd
Dmitry Tverdokhleb <dmitriy.tverdokhleb@sysgears.com>
2018-10-26 12:03:52 +0300

    Optimize ModuleLoader for functional style

--- src/main/scala/core/loaders/ModuleLoader.scala
+++ src/main/scala/core/loaders/ModuleLoader.scala
@@ -6,7 +6,7 @@ import core.controllers.AkkaRoute
 import core.graphql.GraphQLSchema
 import core.guice.injection.Injecting.injector
 import net.codingwell.scalaguice.ScalaModule
-import org.clapper.classutil.ClassFinder
+import org.clapper.classutil.{ClassFinder, ClassInfo}
 import util.Logger
 
 object ModuleLoader extends Logger {
@@ -20,8 +20,12 @@ object ModuleLoader extends Logger {
   lazy val akkaRouteModules: List[AkkaRoute] = instantiateClassesWithInject[AkkaRoute](classOf[AkkaRoute].getName)
   lazy val graphQLSchemaModules: List[GraphQLSchema] = instantiateClassesWithInject[GraphQLSchema](classOf[GraphQLSchema].getName)
 
+  private def filter(classType: String)(clazz: ClassInfo) = {
+    clazz.implements(classType) && !clazz.annotations.exists(_.descriptor.endsWith(s"$ignoreModuleName;"))
+  }
+
   private def instantiateClasses[T](classType: String) = {
-    loadClasses(classType).map {
+    loadClasses(filter(classType)).map {
       clazz =>
         log.info(s"Instantiate a class: ${clazz.getName}")
         clazz.newInstance.asInstanceOf[T]
@@ -29,16 +33,14 @@ object ModuleLoader extends Logger {
   }
 
   private def instantiateClassesWithInject[T](classType: String) = {
-    loadClasses(classType).map {
+    loadClasses(filter(classType)).map {
       clazz =>
         log.info(s"Instantiate a class with inject: ${clazz.getName}")
         injector.getInstance(clazz).asInstanceOf[T]
     }
   }
 
-  private def loadClasses(classType: String) = {
-    classes.filter {
-      clazz => clazz.implements(classType) && !clazz.annotations.exists(_.descriptor.endsWith(ignoreModuleName + ";"))
-    }.map(clazz => classLoader.loadClass(clazz.name)).toList
+  private def loadClasses(filter: ClassInfo => Boolean) = {
+    classes.filter(filter).map(clazz => classLoader.loadClass(clazz.name)).toList
   }
 }

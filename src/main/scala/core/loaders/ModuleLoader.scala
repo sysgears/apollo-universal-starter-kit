7df5c64a81f2276bc0990ef559e858fefe29f42e
Dmitry Tverdokhleb <dmitriy.tverdokhleb@sysgears.com>
2018-10-26 14:27:55 +0300

    Refactor ModuleLoader

--- src/main/scala/core/loaders/ModuleLoader.scala
+++ src/main/scala/core/loaders/ModuleLoader.scala
@@ -1,46 +1,45 @@
 package core.loaders
 
-import java.io.File
-
+import common.Logger
+import common.classes.{ClassFilter, ClassFinder, ClassInstantiator}
 import core.controllers.AkkaRoute
 import core.graphql.GraphQLSchema
 import core.guice.injection.Injecting.injector
 import net.codingwell.scalaguice.ScalaModule
-import org.clapper.classutil.{ClassFinder, ClassInfo}
-import common.Logger
+import org.clapper.classutil.ClassInfo
 
-object ModuleLoader extends Logger {
+object ModuleLoader extends Logger
+  with ClassFinder
+  with ClassFilter
+  with ClassInstantiator {
 
-  private val classLoader = getClass.getClassLoader
-  private val classes = ClassFinder(List(".").map(new File(_))).getClasses()
+  private val projectClasses = findClasses
   private val ignoreModuleName = classOf[IgnoreModule].getSimpleName
 
-  val guiceModules: List[ScalaModule] = instantiateClasses[ScalaModule](classOf[ScalaModule].getName)
+  val guiceModules: List[ScalaModule] = getInstances[ScalaModule](classOf[ScalaModule].getName)
 
-  lazy val akkaRouteModules: List[AkkaRoute] = instantiateClassesWithInject[AkkaRoute](classOf[AkkaRoute].getName)
-  lazy val graphQLSchemaModules: List[GraphQLSchema] = instantiateClassesWithInject[GraphQLSchema](classOf[GraphQLSchema].getName)
+  lazy val akkaRouteModules: List[AkkaRoute] = getInjectedInstances[AkkaRoute](classOf[AkkaRoute].getName)
+  lazy val graphQLSchemaModules: List[GraphQLSchema] = getInjectedInstances[GraphQLSchema](classOf[GraphQLSchema].getName)
 
-  private def filter(classType: String)(clazz: ClassInfo) = {
-    clazz.implements(classType) && !clazz.annotations.exists(_.descriptor.endsWith(s"$ignoreModuleName;"))
+  private def filter(className: String)(clazz: ClassInfo) = {
+    clazz.implements(className) && !clazz.annotations.exists(_.descriptor.endsWith(s"$ignoreModuleName;"))
   }
 
-  private def instantiateClasses[T](classType: String) = {
-    loadClasses(filter(classType)).map {
+  private def getInstances[T](className: String) = {
+    val filteredClasses = filterClasses(projectClasses, filter(className))
+    instatiateClasses(filteredClasses, init = {
       clazz =>
         log.info(s"Instantiate a class: ${clazz.getName}")
         clazz.newInstance.asInstanceOf[T]
-    }
+    })
   }
 
-  private def instantiateClassesWithInject[T](classType: String) = {
-    loadClasses(filter(classType)).map {
+  private def getInjectedInstances[T](className: String) = {
+    val filteredClasses = filterClasses(projectClasses, filter(className))
+    instatiateClasses(filteredClasses, init = {
       clazz =>
         log.info(s"Instantiate a class with inject: ${clazz.getName}")
         injector.getInstance(clazz).asInstanceOf[T]
-    }
-  }
-
-  private def loadClasses(filter: ClassInfo => Boolean) = {
-    classes.filter(filter).map(clazz => classLoader.loadClass(clazz.name)).toList
+    })
   }
 }

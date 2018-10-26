43cf8fb721ede92d36e77b05954eaaf8829d06aa
Dmitry Tverdokhleb <dmitriy.tverdokhleb@sysgears.com>
2018-10-26 15:51:21 +0300

    Add dummy graphql query

--- src/main/scala/core/graphql/GraphQL.scala
+++ src/main/scala/core/graphql/GraphQL.scala
@@ -1,28 +1,55 @@
 package core.graphql
 
 import core.loaders.GraphQLSchemaLoader
-import sangria.schema.{ObjectType, Schema}
+import sangria.schema.{Field, IntType, ObjectType, Schema}
 
 object GraphQL {
+
   val maxQueryDepth = 15
   val maxQueryComplexity = 1000
 
-  val schema: Schema[Unit, Unit] = sangria.schema.Schema(
-    query = ObjectType(
-      name = "Query",
-      fields = GraphQLSchemaLoader.queries
-    ),
-    mutation = Some(
-      ObjectType(
-        name = "Mutation",
-        fields = GraphQLSchemaLoader.mutations
-      )
-    ),
-    subscription = Some(
-      ObjectType(
-        name = "Subscription",
-        fields = GraphQLSchemaLoader.subscriptions
+  private val dummy = ObjectType(
+    name = "Query",
+    fields = List[Field[Unit, Unit]](
+      Field(
+        name = "dummy",
+        fieldType = IntType,
+        resolve = _ => 0
       )
     )
+  )
+
+  val schema: Schema[Unit, Unit] = sangria.schema.Schema(
+    query =
+      if (GraphQLSchemaLoader.queries.isEmpty) {
+        dummy
+      } else {
+        ObjectType(
+          name = "Query",
+          fields = GraphQLSchemaLoader.queries
+        )
+      },
+    mutation =
+      if (GraphQLSchemaLoader.mutations.isEmpty) {
+        None
+      } else {
+        Some(
+          ObjectType(
+            name = "Mutation",
+            fields = GraphQLSchemaLoader.mutations
+          )
+        )
+      },
+    subscription =
+      if (GraphQLSchemaLoader.subscriptions.isEmpty) {
+        None
+      } else {
+        Some(
+          ObjectType(
+            name = "Subscription",
+            fields = GraphQLSchemaLoader.subscriptions
+          )
+        )
+      }
   )
 }

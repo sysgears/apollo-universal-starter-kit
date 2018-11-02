3b4e477c94e09bac8dd00f5ee41f4850c6b0bae3
Victor Vlasenko <victor.vlasenko@sysgears.com>
2018-11-02 13:17:03 +0200

    Simplify knexdata

--- config/db.js
+++ config/db.js
@@ -1,19 +1,14 @@
-const defaultConnection = {
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  socketPath: process.env.DB_SOCKET_PATH,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE,
-  ssl: process.env.DB_SSL,
-  multipleStatements: true,
-  charset: 'utf8'
-};
-
 const db = {
   client: process.env.DB_CLIENT || 'sqlite3',
   connection: {
-    development: { ...defaultConnection },
-    production: { ...defaultConnection }
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    socketPath: process.env.DB_SOCKET_PATH,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    ssl: process.env.DB_SSL,
+    multipleStatements: true,
+    charset: 'utf8'
   }
 };
 
@@ -22,12 +17,17 @@ if (process.env.NODE_ENV === 'test') {
 }
 
 if (db.client === 'sqlite3') {
-  db.connection.development = {
-    filename: './dev-db.sqlite3'
-  };
-  db.connection.production = {
-    filename: './prod-db.sqlite3'
-  };
+  db.connection = {
+    development: {
+      filename: './dev-db.sqlite3'
+    },
+    production: {
+      filename: './prod-db.sqlite3'
+    },
+    test: {
+      filename: ':memory:'
+    }
+  }[process.env.NODE_ENV || 'development'];
   db.pool = {
     afterCreate: (conn, cb) => {
       conn.run('PRAGMA foreign_keys = ON', cb);

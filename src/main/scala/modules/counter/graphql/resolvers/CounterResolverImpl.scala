40074d7dac16c250738c09c363de63130e22352e
Oleh Tsivan <oleh.tsivan@sysgears.com>
2018-11-08 11:13:12 +0200

    Use 'tell' pattern instead of 'ask' in 'CounterActor'

--- src/main/scala/modules/counter/graphql/resolvers/CounterResolverImpl.scala
+++ src/main/scala/modules/counter/graphql/resolvers/CounterResolverImpl.scala
@@ -1,44 +1,46 @@
 package modules.counter.graphql.resolvers
 
-import java.util.concurrent.TimeUnit.SECONDS
-
 import akka.NotUsed
 import akka.actor.ActorRef
-import akka.pattern.ask
-import akka.stream.scaladsl.Source
-import akka.util.Timeout
+import akka.stream.scaladsl.{Sink, Source}
+import akka.stream.{ActorMaterializer, OverflowStrategy}
 import com.google.inject.name.Named
+import common.Logger
 import core.services.publisher.{PublisherHelper, PublisherService}
 import javax.inject.Inject
 import modules.counter.models.Counter
 import modules.counter.services.count.CounterActor
 import modules.counter.services.count.CounterActor.{GetAmount, IncrementAndGet}
 import sangria.schema.Action
-import common.Logger
 
 import scala.concurrent.{ExecutionContext, Future}
 
 class CounterResolverImpl @Inject()(@Named(CounterActor.name) counterActor: ActorRef,
                                     publisherService: PublisherService[Counter])
-                                   (implicit executionContext: ExecutionContext) extends PublisherHelper[Counter]
+                                   (implicit executionContext: ExecutionContext,
+                                    materializer: ActorMaterializer) extends PublisherHelper[Counter]
   with Logger
   with CounterResolver {
 
-  implicit val timeout: Timeout = Timeout(5, SECONDS)
-
   def addServerCounter(amount: Int): Future[Counter] = withPublishing(publisherService) {
-    ask(counterActor, IncrementAndGet(amount)).mapTo[Counter]
+    sendMessageToActor(actorRef => counterActor ! IncrementAndGet(amount, actorRef))
   }
 
   def serverCounter: Future[Counter] = {
-    ask(counterActor, GetAmount).mapTo[Counter]
+    sendMessageToActor(actorRef => counterActor ! GetAmount(actorRef))
   }
 
   def counterUpdated: Source[Action[Unit, Counter], NotUsed] = {
     Source.fromPublisher(publisherService.getPublisher).map {
-      e =>
-        log.info(s"Sending event [$e] to client ...")
-        Action(e)
+      counter =>
+        log.info(s"Sending event [$counter] to client ...")
+        Action(counter)
     }
+  }
+
+  private def sendMessageToActor(f: ActorRef => Unit): Future[Counter] = {
+    Source.actorRef[Counter](0, OverflowStrategy.dropHead)
+      .mapMaterializedValue(f)
+      .runWith(Sink.head[Counter])
   }
 }

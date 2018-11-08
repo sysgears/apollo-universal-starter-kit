fd2e42dbe67ed0a0a60b3e8309a7b2f281377e27
Dmitry Tverdokhleb <dmitriy.tverdokhleb@sysgears.com>
2018-11-08 13:04:41 +0200

    Change Element type from FIELD to PARAMETER

--- src/main/scala/core/interceptor/InterceptorArg.java
+++ src/main/scala/core/interceptor/InterceptorArg.java
@@ -6,7 +6,7 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 @Retention(RetentionPolicy.RUNTIME)
-@Target(ElementType.FIELD)
+@Target(ElementType.PARAMETER)
 public @interface InterceptorArg {
     Class<? extends Interceptor>[] classes();
 }

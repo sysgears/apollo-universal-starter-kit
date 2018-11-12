4a7d5488a2ac3ccc609dc2dbf557e456d9c4ac6e
Dmitry Tverdokhleb <dmitriy.tverdokhleb@sysgears.com>
2018-11-12 17:48:58 +0200

    Add an implementation to JwtEncoderImpl

--- src/main/scala/modules/jwt/encoder/JwtEncoderImpl.scala
+++ src/main/scala/modules/jwt/encoder/JwtEncoderImpl.scala
@@ -2,8 +2,17 @@ package modules.jwt.encoder
 
 import java.time.Duration
 
-class JwtEncoderImpl extends JwtEncoder {
-  override def encode(content: String): String = ???
+import javax.inject.{Inject, Named}
+import pdi.jwt.{Jwt, JwtAlgorithm, JwtClaim}
 
-  override def encode(content: String, expiration: Duration): String = ???
+class JwtEncoderImpl @Inject()(@Named("jwt.secretKey") secretKey: String,
+                               algorithm: JwtAlgorithm) extends JwtEncoder {
+
+  override def encode(content: String): String = {
+    Jwt.encode(content, secretKey, algorithm)
+  }
+
+  override def encode(content: String, expiration: Duration): String = {
+    Jwt.encode(JwtClaim(content).issuedNow.expiresIn(expiration.toMillis), secretKey, algorithm)
+  }
 }

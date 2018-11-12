58e3d34649de488d9a3030b3cfacb2b172a93cdd
Oleh Tsivan <oleh.tsivan@sysgears.com>
2018-11-12 14:15:34 +0200

    Add 'AmbigousResult' and 'NotFound' errors

--- src/main/scala/common/models/Error.scala
+++ src/main/scala/common/models/Error.scala
@@ -3,3 +3,7 @@ package common.models
 class Error extends Exception
 
 case class InternalServerError(msg: String = "") extends Error
+
+case class AmbigousResult(msg: String = "") extends Error
+
+case class NotFound(msg: String = "") extends Error

fa293c2cacda8133a509f6211911416b1d1136ed
Dmitry Tverdokhleb <dmitriy.tverdokhleb@sysgears.com>
2018-11-12 17:59:23 +0200

    Add an implementation to JwtDecoder

--- src/main/scala/modules/jwt/decoder/JwtDecoderImpl.scala
+++ src/main/scala/modules/jwt/decoder/JwtDecoderImpl.scala
@@ -1,7 +1,14 @@
 package modules.jwt.decoder
 
+import javax.inject.{Inject, Named}
+import pdi.jwt.Jwt
+import pdi.jwt.algorithms.JwtHmacAlgorithm
+
 import scala.util.Try
 
-class JwtDecoderImpl extends JwtDecoder {
-  override def decode(token: String): Try[String] = ???
+class JwtDecoderImpl @Inject()(@Named("jwt.secretKey") secretKey: String,
+                               algorithm: JwtHmacAlgorithm) extends JwtDecoder {
+  override def decode(token: String): Try[String] = {
+    Jwt.decodeRaw(token, secretKey, Seq(algorithm))
+  }
 }

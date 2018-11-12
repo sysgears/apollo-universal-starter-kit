fe632ea54895ad65b273815ce85ea6295f3e1da6
Dmitry Tverdokhleb <dmitriy.tverdokhleb@sysgears.com>
2018-11-12 18:09:18 +0200

    Add an implementation to JwtValidator

--- src/main/scala/modules/jwt/validator/JwtValidator.scala
+++ src/main/scala/modules/jwt/validator/JwtValidator.scala
@@ -3,7 +3,7 @@ package modules.jwt.validator
 import scala.util.Try
 
 trait JwtValidator {
-  def validate(token: String): Try[Exception]
+  def validate(token: String): Try[Unit]
 
   def isValid(token: String): Boolean
 }
--- src/main/scala/modules/jwt/validator/JwtValidatorImpl.scala
+++ src/main/scala/modules/jwt/validator/JwtValidatorImpl.scala
@@ -1,9 +1,18 @@
 package modules.jwt.validator
 
+import javax.inject.{Inject, Named}
+import pdi.jwt.Jwt
+import pdi.jwt.algorithms.JwtHmacAlgorithm
+
 import scala.util.Try
 
-class JwtValidatorImpl extends JwtValidator {
-  override def validate(token: String): Try[Exception] = ???
+class JwtValidatorImpl @Inject()(@Named("jwt.secretKey") secretKey: String,
+                                 algorithm: JwtHmacAlgorithm) extends JwtValidator {
+  override def validate(token: String): Try[Unit] = {
+    Try(Jwt.validate(token, secretKey, Seq(algorithm)))
+  }
 
-  override def isValid(token: String): Boolean = ???
+  override def isValid(token: String): Boolean = {
+    Jwt.isValid(token, secretKey, Seq(algorithm))
+  }
 }

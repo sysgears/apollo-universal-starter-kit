85e81ab218602adac4cfc41ff868d65b300d0ec0
Serhii Reznychenko <serhii.reznychenko@sysgears.com>
2018-11-12 19:50:22 +0200

    Add sessionData field to UserContext

--- src/main/scala/core/graphql/UserContext.scala
+++ src/main/scala/core/graphql/UserContext.scala
@@ -2,9 +2,12 @@ package core.graphql
 
 import akka.http.scaladsl.model.HttpHeader
 import akka.http.scaladsl.model.headers.HttpCookie
+import modules.session.SessionData
 
+import scala.collection.mutable
 import scala.collection.mutable.ListBuffer
 
 case class UserContext(requestHeaders: List[HttpHeader] = List.empty,
                        newHeaders: ListBuffer[HttpHeader] = ListBuffer.empty,
-                       newCookies: ListBuffer[HttpCookie] = ListBuffer.empty)
+                       newCookies: ListBuffer[HttpCookie] = ListBuffer.empty,
+                       sessionData: SessionData = SessionData(mutable.Map.empty))

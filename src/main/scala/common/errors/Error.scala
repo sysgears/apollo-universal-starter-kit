210f3c6419a4765f39b6c12a74fcb0eb0d5dc58c
Konstantin Musienko <konstantin.musienko@sysgears.com>
2018-11-12 18:34:12 +0200

    Add AlreadyExists, AmbigousResult errors

--- src/main/scala/common/errors/Error.scala
+++ src/main/scala/common/errors/Error.scala
@@ -1,3 +1,9 @@
 package common.errors
 
 class Error extends Exception
+
+case class InternalServerError(msg: String = "") extends Error
+
+case class AmbigousResult(msg: String = "") extends Error
+
+case class AlreadyExists(msg: String = "") extends Error

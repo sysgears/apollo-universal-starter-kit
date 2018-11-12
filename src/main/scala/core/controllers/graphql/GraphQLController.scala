68fb47001387e979967eb6a4de1a0f1488f6f9b0
Konstantin Musienko <konstantin.musienko@sysgears.com>
2018-11-12 19:29:49 +0200

    Add 'public' endpoint by which uploaded files can be downloaded

--- src/main/scala/core/controllers/graphql/GraphQLController.scala
+++ src/main/scala/core/controllers/graphql/GraphQLController.scala
@@ -64,6 +64,9 @@ class GraphQLController @Inject()(graphQlExecutor: Executor[UserContext, Unit],
       } ~
       (path("graphiql") & get) {
         getFromResource("web/graphiql.html")
+      } ~
+      (path("public" / Segment) & get) { str =>
+        getFromResource(s"public/$str")
       }
 
   private def withHeaders(userCtx: UserContext)(ctxToRoute: UserContext => Route) =

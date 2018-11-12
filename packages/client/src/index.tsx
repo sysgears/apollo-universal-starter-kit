e910fbda6dfcf9fb9f74231b12524d3e7e090444
alekseyzadorozhniy <zadorozhniyal@gmail.com>
2018-11-12 14:31:36 +0200

    Add Hot Module Reload

--- packages/client/src/index.tsx
+++ packages/client/src/index.tsx
@@ -1,10 +1,13 @@
 import { ApplicationRef, NgModule, Component } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { bootloader, createInputTransfer, createNewHosts, hmrModule, removeNgStyles } from '@angularclass/hmr';
 
 // Virtual module, generated in-memory by spinjs, contains count of backend rebuilds
 // tslint:disable-next-line
 import 'backend_reload';
+
+import log from '../../common/log';
 
 @Component({
   selector: 'body div:first-child',
@@ -22,6 +25,62 @@ export class AppComponent {
 })
 class MainModule {
   constructor(public appRef: ApplicationRef) {}
+
+  public hmrOnInit(store: any) {
+    if (!store || !store.state) {
+      return;
+    }
+    log.debug('Updating front-end', store.state.data);
+    // inject AppStore here and update it
+    // this.AppStore.update(store.state)
+    if ('restoreInputValues' in store) {
+      store.restoreInputValues();
+    }
+    // change detection
+    this.appRef.tick();
+    delete store.state;
+    delete store.restoreInputValues;
+  }
+
+  public hmrOnDestroy(store: any) {
+    const cmpLocation = this.appRef.components.map(cmp => cmp.location.nativeElement);
+    // recreate elements
+    store.disposeOldHosts = createNewHosts(cmpLocation);
+    // inject your AppStore and grab state then set it on store
+    // var appState = this.AppStore.get()
+    store.state = { data: 'yolo' };
+    // store.state = Object.assign({}, appState)
+    // save input values
+    store.restoreInputValues = createInputTransfer();
+    // remove styles
+    removeNgStyles();
+  }
+
+  public hmrAfterDestroy(store: any) {
+    // display new elements
+    store.disposeOldHosts();
+    delete store.disposeOldHosts;
+    // anything you need done the component is removed
+  }
 }
 
-platformBrowserDynamic().bootstrapModule(MainModule);
+export function main() {
+  const result = platformBrowserDynamic().bootstrapModule(MainModule);
+  if (__DEV__) {
+    result.then((ngModuleRef: any) => {
+      return hmrModule(ngModuleRef, module);
+    });
+  }
+}
+
+if (__DEV__) {
+  if (module.hot) {
+    module.hot.accept('backend_reload', () => {
+      log.debug('Reloading front-end');
+      window.location.reload();
+    });
+  }
+}
+
+// boot on document ready
+bootloader(main);

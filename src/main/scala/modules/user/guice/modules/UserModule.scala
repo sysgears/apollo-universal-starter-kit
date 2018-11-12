064405f091c435e856b4b340595a8051e25cb8c2
Oleh Tsivan <oleh.tsivan@sysgears.com>
2018-11-12 17:42:33 +0200

    Add bind of 'UserResolver' to an implementation

--- src/main/scala/modules/user/guice/modules/UserModule.scala
+++ src/main/scala/modules/user/guice/modules/UserModule.scala
@@ -1,10 +1,12 @@
 package modules.user.guice.modules
 
+import modules.user.graphql.resolvers.{UserResolver, UserResolverImpl}
 import modules.user.repositories.{UserRepo, UserRepoImpl}
 import net.codingwell.scalaguice.ScalaModule
 
 class UserModule extends ScalaModule {
   override def configure() = {
+    bind[UserResolver].to[UserResolverImpl]
     bind[UserRepo].to[UserRepoImpl]
   }
 }

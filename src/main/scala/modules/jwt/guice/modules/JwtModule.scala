09eccb225bb5667b5dfca93b759da527681e9128
Dmitry Tverdokhleb <dmitriy.tverdokhleb@sysgears.com>
2018-11-12 11:24:30 +0200

    Add binding of jwt modules

--- src/main/scala/modules/jwt/guice/modules/JwtModule.scala
+++ src/main/scala/modules/jwt/guice/modules/JwtModule.scala
@@ -1,10 +1,15 @@
 package modules.jwt.guice.modules
 
+import modules.jwt.decoder.{JwtDecoder, JwtDecoderImpl}
+import modules.jwt.encoder.{JwtEncoder, JwtEncoderImpl}
+import modules.jwt.validator.{JwtValidator, JwtValidatorImpl}
 import net.codingwell.scalaguice.ScalaModule
 
 class JwtModule extends ScalaModule {
 
   override def configure() {
-
+    bind[JwtEncoder].to[JwtEncoderImpl]
+    bind[JwtDecoder].to[JwtDecoderImpl]
+    bind[JwtValidator].to[JwtValidatorImpl]
   }
 }

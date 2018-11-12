b69b9e36e3bdaa01ccdf593a3fd87a87553bdc43
Serhii Reznychenko <serhii.reznychenko@sysgears.com>
2018-11-12 19:40:21 +0200

    Separate the session and the jwt session, which extends it

--- src/main/scala/modules/session/JWTSession.scala
+++ src/main/scala/modules/session/JWTSession.scala
@@ -1,19 +1,12 @@
 package modules.session
 
-import akka.http.scaladsl.server.{Directive0, Directive1}
-import com.softwaremill.session.SessionDirectives.{invalidateSession, optionalSession, setSession}
-import com.softwaremill.session.SessionOptions.{oneOff, usingCookies}
 import com.softwaremill.session._
 import org.json4s.JValue
 
-trait JWTSession[T] {
-  val defaultSessionConfig: SessionConfig = SessionConfig.default("c05ll3lesrinf39t7mc5h6un6r0c69lgfno69dsak3vabeqamouq4328cuaekros401ajdpkh60rrtpd8ro24rbuqmgtnd1ebag6ljnb65i8a55d482ok7o0nch0bfbe")
+trait JWTSession extends Session[SessionData] {
+  val sessionConfig: SessionConfig = SessionConfig.default("c05ll3lesrinf39t7mc5h6un6r0c69lgfno69dsak3vabeqamouq4328cuaekros401ajdpkh60rrtpd8ro24rbuqmgtnd1ebag6ljnb65i8a55d482ok7o0nch0bfbe")
 
-  implicit val serializer: SessionSerializer[T, JValue] = JValueSessionSerializer.caseClass[T]
-  implicit val encoder: JwtSessionEncoder[T] = new JwtSessionEncoder[T]
-  implicit val manager: SessionManager[T] = new SessionManager(defaultSessionConfig)
-
-  def withNewSession(newSession: T): Directive0 = setSession(oneOff, usingCookies, newSession)
-  val withOptionalSession: Directive1[Option[T]] = optionalSession(oneOff, usingCookies)
-  val withInvalidateSession: Directive0 = invalidateSession(oneOff, usingCookies)
+  implicit val serializer: SessionSerializer[SessionData, JValue] = JValueSessionSerializer.caseClass[SessionData]
+  implicit val encoder: JwtSessionEncoder[SessionData] = new JwtSessionEncoder[SessionData]
+  implicit val manager: SessionManager[SessionData] = new SessionManager(sessionConfig)
 }

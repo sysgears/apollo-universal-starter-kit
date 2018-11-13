2079473f9cd45fce99f675a54bb0152c16256975
Oleh Tsivan <oleh.tsivan@sysgears.com>
2018-11-13 17:48:53 +0200

    Update 'CounterSchema' to use 'DispatcherActor'

--- src/main/scala/modules/counter/graphql/schema/CounterSchema.scala
+++ src/main/scala/modules/counter/graphql/schema/CounterSchema.scala
@@ -1,17 +1,31 @@
 package modules.counter.graphql.schema
 
+import akka.actor.{ActorRef, ActorSystem}
 import akka.stream.ActorMaterializer
+import akka.stream.scaladsl.Source
+import com.google.inject.name.Named
 import common.Logger
 import core.graphql.{GraphQLSchema, UserContext}
+import core.services.publisher.PublisherService
 import javax.inject.Inject
+import common.actors.DispatcherActor
+import common.graphql.GraphQLUtil
 import modules.counter.graphql.resolvers.CounterResolver
 import modules.counter.models.Counter
+import modules.counter.services.count.CounterActor.GetAmount
 import sangria.macros.derive.{ExcludeFields, ObjectTypeName, deriveObjectType}
-import sangria.schema.{Argument, Field, IntType, ObjectType}
+import sangria.schema.{Action, Argument, Field, IntType, ObjectType}
 import sangria.streaming.akkaStreams._
 
-class CounterSchema @Inject()(counterResolver: CounterResolver)
-                             (implicit val materializer: ActorMaterializer) extends GraphQLSchema with Logger {
+import scala.concurrent.ExecutionContext
+
+class CounterSchema @Inject()(@Named(DispatcherActor.name) dispatcherActor: ActorRef,
+                              publisherService: PublisherService[Counter])
+                             (implicit val materializer: ActorMaterializer,
+                              actorSystem: ActorSystem,
+                              executionContext: ExecutionContext) extends GraphQLSchema
+  with Logger
+  with GraphQLUtil {
 
   object Types {
     implicit val counter: ObjectType[Unit, Counter] = deriveObjectType(ObjectTypeName("Counter"), ExcludeFields("id"))
@@ -21,7 +35,11 @@ class CounterSchema @Inject()(counterResolver: CounterResolver)
     Field(
       name = "serverCounter",
       fieldType = Types.counter,
-      resolve = _ => counterResolver.serverCounter
+      resolve = sc => sendMessageToDispatcher[Counter](
+        input = GetAmount,
+        userContext = sc.ctx,
+        resolverActor = CounterResolver.name
+      )
     )
   )
 
@@ -32,7 +50,16 @@ class CounterSchema @Inject()(counterResolver: CounterResolver)
       arguments = Argument(name = "amount", argumentType = IntType) :: Nil,
       resolve = sc => {
         val amount = sc.args.arg[Int]("amount")
-        counterResolver.addServerCounter(amount)
+        sendMessageToDispatcher[Counter](
+          input = amount,
+          userContext = sc.ctx,
+          resolverActor = CounterResolver.name
+        ).map {
+          counter => {
+            publisherService.publish(counter)
+            counter
+          }
+        }
       }
     )
   )
@@ -41,7 +68,11 @@ class CounterSchema @Inject()(counterResolver: CounterResolver)
     Field.subs(
       name = "counterUpdated",
       fieldType = Types.counter,
-      resolve = _ => counterResolver.counterUpdated
+      resolve = _ => Source.fromPublisher(publisherService.getPublisher).map {
+        counter =>
+          log.info(s"Sending event [$counter] to client ...")
+          Action(counter)
+      }
     )
   )
 }

fca6461f256d9e69b3aca37d75b3897b53a9c96e
Oleh Tsivan <oleh.tsivan@sysgears.com>
2018-11-13 17:28:29 +0200

    Remove unnecessary parameter

--- src/main/scala/modules/counter/services/count/CounterActor.scala
+++ src/main/scala/modules/counter/services/count/CounterActor.scala
@@ -1,6 +1,6 @@
 package modules.counter.services.count
 
-import akka.actor.{Actor, ActorLogging, ActorRef}
+import akka.actor.{Actor, ActorLogging}
 import akka.pattern._
 import com.google.inject.Inject
 import modules.counter.models.Counter
@@ -11,23 +11,24 @@ import scala.concurrent.ExecutionContext
 
 object CounterActor {
 
-  case class GetAmount(sender: ActorRef)
+  object GetAmount
 
-  case class IncrementAndGet(amount: Int, sender: ActorRef)
+  case class IncrementAndGet(amount: Int)
 
   final val name = "CounterActor"
 }
 
 class CounterActor @Inject()(counterRepo: CounterRepo)
-                            (implicit executionContext: ExecutionContext) extends Actor with ActorLogging {
+                            (implicit executionContext: ExecutionContext) extends Actor
+  with ActorLogging {
 
   private val defaultId = 1
 
   override def receive: Receive = {
     case incrementAndGet: IncrementAndGet =>
       log.info(s"Received message: [ $incrementAndGet ]")
-      counterRepo.inc(Counter(Some(defaultId), incrementAndGet.amount)).pipeTo(incrementAndGet.sender)
+      counterRepo.inc(Counter(Some(defaultId), incrementAndGet.amount)).pipeTo(sender)
 
-    case getAmount: GetAmount => counterRepo.find(defaultId).pipeTo(getAmount.sender)
+    case GetAmount => counterRepo.find(defaultId).pipeTo(sender)
   }
 }

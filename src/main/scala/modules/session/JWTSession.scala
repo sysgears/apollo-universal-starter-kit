fc7c2b009fb7f865be7febec8572fd663678a774
Serhii Reznychenko <serhii.reznychenko@sysgears.com>
2018-11-13 20:19:21 +0200

    Remove implementation from JWTSession fields

--- src/main/scala/modules/session/JWTSession.scala
+++ src/main/scala/modules/session/JWTSession.scala
@@ -3,10 +3,7 @@ package modules.session
 import com.softwaremill.session._
 import org.json4s.JValue
 
-trait JWTSession extends Session[SessionData] {
-  val sessionConfig: SessionConfig = SessionConfig.default("c05ll3lesrinf39t7mc5h6un6r0c69lgfno69dsak3vabeqamouq4328cuaekros401ajdpkh60rrtpd8ro24rbuqmgtnd1ebag6ljnb65i8a55d482ok7o0nch0bfbe")
-
-  implicit val serializer: SessionSerializer[SessionData, JValue] = JValueSessionSerializer.caseClass[SessionData]
-  implicit val encoder: JwtSessionEncoder[SessionData] = new JwtSessionEncoder[SessionData]
-  implicit val manager: SessionManager[SessionData] = new SessionManager(sessionConfig)
+trait JWTSession[T] extends Session[T] {
+  implicit val serializer: SessionSerializer[T, JValue]
+  implicit val encoder: JwtSessionEncoder[T]
 }

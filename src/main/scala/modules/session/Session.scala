16c4554eed05bfba0209a7068f380cf71dbf761e
Serhii Reznychenko <serhii.reznychenko@sysgears.com>
2018-11-13 20:08:51 +0200

    Remove methods to manage session from Session class

--- src/main/scala/modules/session/Session.scala
+++ src/main/scala/modules/session/Session.scala
@@ -1,16 +1,10 @@
 package modules.session
 
-import akka.http.scaladsl.server.{Directive0, Directive1}
-import com.softwaremill.session.SessionDirectives.{invalidateSession, optionalSession, setSession}
-import com.softwaremill.session.SessionOptions.{oneOff, usingCookies}
-import com.softwaremill.session._
+import com.softwaremill.session.{InMemoryRefreshTokenStorage, _}
 
 trait Session[T] {
   val sessionConfig: SessionConfig
 
   implicit val manager: SessionManager[T]
-
-  def withNewSession(value: T): Directive0 = setSession(oneOff, usingCookies, value)
-  val withOptionalSession: Directive1[Option[T]] = optionalSession(oneOff, usingCookies)
-  val withInvalidateSession: Directive0 = invalidateSession(oneOff, usingCookies)
+  implicit val refreshTokenStorage: InMemoryRefreshTokenStorage[T]
 }

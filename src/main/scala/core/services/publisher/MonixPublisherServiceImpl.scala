d81a45a52d2117b7a7932011d04c33465b63cc9f
Oleh Tsivan <oleh.tsivan@sysgears.com>
2018-11-13 16:14:02 +0200

    Fix typo

--- src/main/scala/core/services/publisher/MonixPublisherServiceImpl.scala
+++ src/main/scala/core/services/publisher/MonixPublisherServiceImpl.scala
@@ -10,12 +10,12 @@ import common.Logger
 @Singleton
 class MonixPublisherServiceImpl[T] @Inject()(implicit val scheduler: Scheduler) extends PublisherService[T] with Logger {
 
-  lazy val sourceCounter: ConcurrentSubject[T, T] = ConcurrentSubject.publish[T](OverflowStrategy.DropOld(16))
+  lazy val source: ConcurrentSubject[T, T] = ConcurrentSubject.publish[T](OverflowStrategy.DropOld(16))
 
-  override def getPublisher: Publisher[T] = sourceCounter.toReactivePublisher[T]
+  override def getPublisher: Publisher[T] = source.toReactivePublisher[T]
 
   override def publish(event: T): Unit = {
     log.info(s"Event [$event] is publishing...")
-    sourceCounter.onNext(event)
+    source.onNext(event)
   }
 }

7d1ff34b2bfe47d346789cd1fd28f7059a6cb28e
Serhii Reznychenko <serhii.reznychenko@sysgears.com>
2018-11-13 20:13:22 +0200

    Refactor SessionData value type to immutable Map

--- src/main/scala/modules/session/SessionData.scala
+++ src/main/scala/modules/session/SessionData.scala
@@ -2,17 +2,14 @@ package modules.session
 
 import com.softwaremill.session.MultiValueSessionSerializer
 
-import scala.collection.mutable
 import scala.util.Try
 
-case class SessionData(value: mutable.Map[String, String])
+case class SessionData(value: Map[String, String])
 
 object SessionData {
   implicit def serializer: MultiValueSessionSerializer[SessionData] =
     new MultiValueSessionSerializer(
-      toMap = data =>
-        data.value.toMap,
-      fromMap = data =>
-        Try(SessionData(mutable.Map(data.toSeq: _*)))
+      toMap = data => data.value,
+      fromMap = data => Try(SessionData(data))
     )
 }

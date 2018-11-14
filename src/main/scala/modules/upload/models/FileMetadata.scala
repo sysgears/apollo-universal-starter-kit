af1163864b5245f0b747970877a2fdb43a6b64ea
Konstantin Musienko <konstantin.musienko@sysgears.com>
2018-11-14 18:54:36 +0200

    Add json protocol for the FileMetadata entity

--- src/main/scala/modules/upload/models/FileMetadata.scala
+++ src/main/scala/modules/upload/models/FileMetadata.scala
@@ -1,6 +1,7 @@
 package modules.upload.models
 import slick.lifted.Tag
 import slick.jdbc.SQLiteProfile.api.{Table => SlickTable, _}
+import spray.json.{DefaultJsonProtocol, JsonFormat}
 
 case class FileMetadata(id: Option[Int] = None, name: String, contentType: String, size: Long, path: String)
 
@@ -22,4 +23,8 @@ object FileMetadata extends ((Option[Int], String, String, Long, String) => File
 
     override def * = (id.?, name, contentType, size, path).mapTo[FileMetadata]
   }
+}
+
+object FileMetadataJsonProtocol extends DefaultJsonProtocol {
+  implicit val fileMetadataFormat: JsonFormat[FileMetadata] = jsonFormat5(FileMetadata)
 }

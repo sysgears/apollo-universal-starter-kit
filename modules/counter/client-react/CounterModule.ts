64983ecad5d46ecc369820f7504264296435c335
Victor Vlasenko <victor.vlasenko@sysgears.com>
2018-11-14 17:10:27 +0200

    Module-first architecture (#907)

---
+++ modules/counter/client-react/CounterModule.ts
@@ -0,0 +1,16 @@
+import React from 'react';
+import ClientModule, { ClientModuleShape } from '@module/module-client-react';
+
+export interface CounterModuleShape extends ClientModuleShape {
+  counterComponent?: Array<React.ReactElement<any>>;
+}
+
+interface CounterModule extends CounterModuleShape {}
+
+class CounterModule extends ClientModule {
+  constructor(...modules: CounterModuleShape[]) {
+    super(...modules);
+  }
+}
+
+export default CounterModule;
--- packages/common/modules/validation/index.ts
+++
@@ -1,8 +0,0 @@
-import CommonModule from '../CommonModule';
-import resources from './locales';
-
-export * from './validation';
-
-export default new CommonModule({
-  localization: [{ ns: 'validation', resources }]
-});

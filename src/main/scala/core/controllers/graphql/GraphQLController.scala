3156fde9eefcbaf34291efa4d4cc339b4446d1f8
Serhii Reznychenko <serhii.reznychenko@sysgears.com>
2018-11-15 19:53:07 +0200

    Integrate 'withChanges' method in the GraphQLController class

--- src/main/scala/core/controllers/graphql/GraphQLController.scala
+++ src/main/scala/core/controllers/graphql/GraphQLController.scala
@@ -11,7 +11,7 @@ import core.controllers.graphql.jsonProtocols.GraphQLMessage
 import core.controllers.graphql.jsonProtocols.GraphQLMessageJsonProtocol._
 import core.graphql.{GraphQL, UserContext}
 import javax.inject.Inject
-import modules.session.{JWTSessionImpl, SessionData}
+import modules.session.JWTSessionImpl
 import sangria.execution.Executor
 import sangria.renderer.SchemaRenderer
 
@@ -33,22 +33,29 @@ class GraphQLController @Inject()(graphQlExecutor: Executor[UserContext, Unit],
             handleWebSocketMessagesForProtocol(webSocketHandler.handleMessages, GraphQLMessage.graphQlWebsocketProtocol)
           } ~
             post {
-              withHeaders(UserContext(request.headers.toList)) {
-                withSession(_) {
-                  userCtx =>
-                    entity(as[GraphQLMessage]) {
-                      graphQlMessage =>
-                        onComplete(httpHandler.handleQuery(graphQlMessage, userCtx)) {
-                          response: Try[ToResponseMarshallable] => complete(response)
-                        }
-                    } ~
-                      entity(as[Seq[GraphQLMessage]]) {
-                        graphQlMessages =>
-                          onComplete(httpHandler.handleBatchQuery(graphQlMessages, userCtx)) {
-                            response: Try[ToResponseMarshallable] => complete(response)
+              session.withOptional {
+                maybeSession =>
+                  withHeaders(UserContext(requestHeaders = request.headers.toList, session = maybeSession)) {
+                    userCtx =>
+                      entity(as[GraphQLMessage]) {
+                        graphQlMessage =>
+                          onComplete(httpHandler.handleQuery(graphQlMessage, userCtx)) {
+                            response: Try[ToResponseMarshallable] =>
+                              session.withChanges(maybeSession, userCtx.session) {
+                                complete(response)
+                              }
                           }
-                      }
-                }
+                      } ~
+                        entity(as[Seq[GraphQLMessage]]) {
+                          graphQlMessages =>
+                            onComplete(httpHandler.handleBatchQuery(graphQlMessages, userCtx)) {
+                              response: Try[ToResponseMarshallable] =>
+                                session.withChanges(maybeSession, userCtx.session) {
+                                  complete(response)
+                                }
+                            }
+                        }
+                  }
               }
             }
       }
@@ -65,19 +72,4 @@ class GraphQLController @Inject()(graphQlExecutor: Executor[UserContext, Unit],
       userCtx.newHeaders.toList ++
       userCtx.newCookies.toList.map(`Set-Cookie`(_))
     )(ctxToRoute(userCtx))
-
-  //FIXME: find out how to create a session after setting data to newSession in UserContext
-  private def withSession(userCtx: UserContext)(ctxToRoute: UserContext => Route) = {
-    session.withOptional {
-      case Some(storedSession) => ctxToRoute(userCtx.copy(session = Some(storedSession)))
-      case None =>
-        ctxToRoute.andThen {
-          route =>
-            if (userCtx.session.isEmpty) route
-            else {
-              session.withNew(SessionData(""))(route)
-            }
-        }(userCtx)
-    }
-  }
 }

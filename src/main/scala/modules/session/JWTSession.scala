8799c05a8385696fe3297ccc2791032f40c34388
Serhii Reznychenko <serhii.reznychenko@sysgears.com>
2018-11-15 20:01:11 +0200

    Remove serializer field from the JWTSession class

--- src/main/scala/modules/session/JWTSession.scala
+++ src/main/scala/modules/session/JWTSession.scala
@@ -1,9 +1,7 @@
 package modules.session
 
 import com.softwaremill.session._
-import org.json4s.JValue
 
 trait JWTSession[T] extends Session[T] {
-  implicit val serializer: SessionSerializer[T, JValue]
   implicit val encoder: JwtSessionEncoder[T]
 }
--- src/main/scala/modules/session/JWTSessionImpl.scala
+++ src/main/scala/modules/session/JWTSessionImpl.scala
@@ -5,25 +5,21 @@ import com.google.inject.Inject
 import com.softwaremill.session._
 import com.softwaremill.session.SessionDirectives.{invalidateSession, optionalSession, setSession}
 import com.softwaremill.session.SessionOptions.{refreshable, usingCookies}
-import org.json4s.JValue
-import org.slf4j.LoggerFactory
+import modules.session.SessionData._
+import common.Logger
 
 import scala.concurrent.ExecutionContext
 
-class JWTSessionImpl @Inject()(implicit val executionContext: ExecutionContext)
-  extends JWTSession[SessionData] {
+class JWTSessionImpl @Inject()(implicit val executionContext: ExecutionContext) extends JWTSession[SessionData]
+  with Logger {
 
   override val sessionConfig: SessionConfig = SessionConfig.fromConfig()
 
-  override implicit val serializer: SessionSerializer[SessionData, JValue] = JValueSessionSerializer.caseClass[SessionData]
   override implicit val encoder: JwtSessionEncoder[SessionData] = new JwtSessionEncoder[SessionData]
   override implicit val manager: SessionManager[SessionData] = new SessionManager(sessionConfig)
+  override implicit val refreshTokenStorage: InMemoryRefreshTokenStorage[SessionData] = (msg: String) => log.info(msg)
 
-  override implicit val refreshTokenStorage: InMemoryRefreshTokenStorage[SessionData] = new InMemoryRefreshTokenStorage[SessionData] {
-    def log(msg: String): Unit = LoggerFactory.getLogger(this.getClass.getName).info(msg)
-  }
-
-  def withNew(value: SessionData): Directive0 = setSession(refreshable, usingCookies, value)
+  def withNew(sessionData: SessionData): Directive0 = setSession(refreshable, usingCookies, sessionData)
 
   def withOptional: Directive1[Option[SessionData]] = optionalSession(refreshable, usingCookies)
 

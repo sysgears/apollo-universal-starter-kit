2a44f2fae9d8625f6c65cd3fd903a592280bdec9
Oleh Tsivan <oleh.tsivan@sysgears.com>
2018-11-19 16:24:54 +0200

    Delete unused functionality

--- src/main/scala/core/services/publisher/PublishSubscribeService.scala
+++ src/main/scala/core/services/publisher/PublishSubscribeService.scala
@@ -2,14 +2,11 @@ package core.services.publisher
 
 import akka.NotUsed
 import akka.stream.scaladsl.Source
-import org.reactivestreams.Publisher
 import sangria.schema.Action
 
 trait PublishSubscribeService[T] {
 
   def publish(event: T)
-
-  def getPublisher: Publisher[T]
 
   def subscribe: Source[Action[Nothing, T], NotUsed]
 }
--- src/main/scala/core/services/publisher/PublishSubscribeServiceImpl.scala
+++ src/main/scala/core/services/publisher/PublishSubscribeServiceImpl.scala
@@ -7,7 +7,6 @@ import javax.inject.{Inject, Singleton}
 import monix.execution.Scheduler
 import monix.reactive.OverflowStrategy
 import monix.reactive.subjects.ConcurrentSubject
-import org.reactivestreams.Publisher
 import sangria.schema.Action
 
 @Singleton
@@ -15,12 +14,10 @@ class PublishSubscribeServiceImpl[T] @Inject()(implicit val scheduler: Scheduler
 
   lazy val source: ConcurrentSubject[T, T] = ConcurrentSubject.publish[T](OverflowStrategy.DropOld(16))
 
-  override def getPublisher: Publisher[T] = source.toReactivePublisher[T]
-
   override def publish(event: T): Unit = source.onNext(event)
 
   override def subscribe: Source[Action[Nothing, T], NotUsed] = {
-    Source.fromPublisher(getPublisher).map {
+    Source.fromPublisher(source.toReactivePublisher[T]).map {
       element =>
         log.info(s"Sending event [$element] to client ...")
         Action(element)

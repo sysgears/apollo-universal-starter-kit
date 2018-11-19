2381442999e429d386ff34d868fe4597ef1db26a
Konstantin Musienko <konstantin.musienko@sysgears.com>
2018-11-19 12:56:28 +0200

    Move error classes to a separate files

--- src/main/scala/common/errors/Error.scala
+++ src/main/scala/common/errors/Error.scala
@@ -1,9 +1,3 @@
 package common.errors
 
 class Error extends Exception
-
-case class InternalServerError(msg: String = "") extends Error
-
-case class AmbigousResult(msg: String = "") extends Error
-
-case class AlreadyExists(msg: String = "") extends Error

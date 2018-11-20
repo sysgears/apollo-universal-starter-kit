46cb488954ccc316cfa29c9d4a267565b97b91e1
Serhii Reznychenko <serhii.reznychenko@sysgears.com>
2018-11-20 13:33:59 +0200

    Add 'service' property to choose which email service to use in the MailModule

--- src/main/scala/modules/mail/guice/modules/MailModule.scala
+++ src/main/scala/modules/mail/guice/modules/MailModule.scala
@@ -1,23 +1,26 @@
 package modules.mail.guice.modules
 
 import com.github.jurajburian.mailer.{Mailer, SessionFactory, SmtpAddress, SmtpStartTls}
-import com.google.inject.Provides
+import com.google.inject.{Inject, Provides, Singleton}
 import com.typesafe.config.Config
 import net.codingwell.scalaguice.ScalaModule
 
-class MailModule extends ScalaModule {
+class MailModule @Inject()(config: Config) extends ScalaModule {
+
+  val service: String = config.getString("email.service")
 
   @Provides
-  def provideMailer(config: Config): Mailer = {
+  @Singleton
+  def provideMailer: Mailer = {
     val session = (
       SmtpAddress(
-        config.getString("email.host"),
-        config.getInt("email.port")
+        config.getString(s"email.$service.host"),
+        config.getInt(s"email.$service.port")
       )
         :: SmtpStartTls()
         :: SessionFactory()).session(
       Some(
-        config.getString("email.user") -> config.getString("email.password")
+        config.getString(s"email.$service.user") -> config.getString(s"email.$service.password")
       )
     )
     Mailer(session)

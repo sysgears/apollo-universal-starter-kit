e815859801dec293fed7cf88ff631f24bea1b644
pavel.grudina <pavel.grudina@sysgears.com>
2018-11-22 17:43:54 +0200

    fixed bug with return types

--- packages/server-scala/src/main/scala/modules/pagination/repositories/DataObjectRepo.scala
+++ packages/server-scala/src/main/scala/modules/pagination/repositories/DataObjectRepo.scala
@@ -2,8 +2,8 @@ package modules.pagination.repositories
 
 import common.errors.{AmbigousResult, NotFound}
 import javax.inject.{Inject, Singleton}
-import modules.pagination.{PaginatedResult, Pagination, PaginationParams}
 import modules.pagination.model.DataObject
+import modules.pagination.{PaginatedResult, Pagination, PaginationUtil}
 import slick.jdbc.SQLiteProfile.api._
 
 import scala.concurrent.{ExecutionContext, Future}
@@ -18,11 +18,11 @@ trait DataObjectRepo {
 
   def delete(id: Long): Future[Int]
 
-  def getPaginatedObjectsList(paginationParams: PaginationParams): Future[PaginatedResult[DataObject]]
+  def getPaginatedObjectsList(paginationParams: Pagination): Future[PaginatedResult[DataObject]]
 }
 
 @Singleton
-class DataObjectRepoImpl @Inject()(db: Database)(implicit executionContext: ExecutionContext) extends DataObjectRepo with Pagination {
+class DataObjectRepoImpl @Inject()(db: Database)(implicit executionContext: ExecutionContext) extends DataObjectRepo with PaginationUtil {
 
   def query = TableQuery[DataObject.Table]
 
@@ -34,7 +34,7 @@ class DataObjectRepoImpl @Inject()(db: Database)(implicit executionContext: Exec
 
   override def delete(id: Long): Future[Int] = db.run(Actions.delete(id))
 
-  override def getPaginatedObjectsList(paginationParams: PaginationParams): Future[PaginatedResult[DataObject]] = db.run(Actions.getPaginatedObjectsList(paginationParams))
+  override def getPaginatedObjectsList(paginationParams: Pagination): Future[PaginatedResult[DataObject]] = db.run(Actions.getPaginatedObjectsList(paginationParams))
 
   object Actions {
     def save(dataObject: DataObject): DBIO[DataObject] = {
@@ -56,16 +56,17 @@ class DataObjectRepoImpl @Inject()(db: Database)(implicit executionContext: Exec
 
     def delete(id: Long): DBIO[Int] = query.filter(_.id === id).delete
 
-    def getPaginatedObjectsList(paginationParams: PaginationParams): DBIO[PaginatedResult[DataObject]] = {
+    def getPaginatedObjectsList(paginationParams: Pagination): DBIO[PaginatedResult[DataObject]] = {
       val (offset, limit) = (paginationParams.offset, paginationParams.limit)
       val paginatedQuery = withPagination(query, paginationParams)
       for {
-        objects <- paginatedQuery.result
-        totalCount <- query.result
+        totalCount <- query.size.result
+        paginatedResult <- paginatedQuery.result
       } yield PaginatedResult(
-        totalCount.length,
-        entities = objects.toList,
-        hasNextPage = (totalCount.length - (offset + limit)) > 0)
+        totalCount = totalCount,
+        entities = paginatedResult.toList,
+        hasNextPage = (totalCount - (offset + limit)) > 0
+      )
     }
   }
 

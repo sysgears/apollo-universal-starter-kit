1755b2a3a52c8315606b8f36f8dc6dffb850aa3f
Dmitry Tverdokhleb <dmitriy.tverdokhleb@sysgears.com>
2018-11-22 11:42:59 +0200

    Rework Counter module on CRUD repository

---
+++ packages/server-scala/src/main/scala/modules/counter/models/CounterTable.scala
@@ -0,0 +1,17 @@
+package modules.counter.models
+
+import com.byteslounge.slickrepo.meta.Keyed
+import modules.counter.models.CounterTable.name
+import slick.jdbc.SQLiteProfile.api.{Table => SlickTable, _}
+import slick.lifted.Tag
+
+class CounterTable(tag: Tag) extends SlickTable[Counter](tag, name) with Keyed[Int] {
+  val id = column[Int]("ID", O.PrimaryKey)
+  val amount = column[Int]("AMOUNT")
+
+  override def * = (id.?, amount).mapTo[Counter]
+}
+
+object CounterTable {
+  val name = "COUNTER"
+}
--- packages/server-scala/src/main/scala/modules/counter/services/count/CounterActor.scala
+++ packages/server-scala/src/main/scala/modules/counter/services/count/CounterActor.scala
@@ -2,13 +2,15 @@ package modules.counter.services.count
 
 import akka.actor.{Actor, ActorLogging}
 import akka.pattern._
+import com.byteslounge.slickrepo.repository.Repository
 import com.google.inject.Inject
 import common.ActorNamed
+import common.DatabaseExecutor._
+import common.errors.InternalServerError
 import modules.counter.models.Counter
-import modules.counter.repositories.CounterRepo
 import modules.counter.services.count.CounterActor.{GetAmount, IncrementAndGet}
 
-import scala.concurrent.ExecutionContext
+import scala.concurrent.{ExecutionContext, Future}
 
 object CounterActor extends ActorNamed {
 
@@ -17,19 +19,27 @@ object CounterActor extends ActorNamed {
   object GetAmount
 
   case class IncrementAndGet(amount: Int)
+
 }
 
-class CounterActor @Inject()(counterRepo: CounterRepo)
+class CounterActor @Inject()(counterRepository: Repository[Counter, Int])
                             (implicit executionContext: ExecutionContext) extends Actor
   with ActorLogging {
-
   private val defaultId = 1
 
   override def receive: Receive = {
     case incrementAndGet: IncrementAndGet =>
       log.info(s"Received message: [ $incrementAndGet ]")
-      counterRepo.inc(Counter(Some(defaultId), incrementAndGet.amount)).pipeTo(sender)
+      (for {
+        optionCounter <- counterRepository.findOne(defaultId).run
+        counter <- if (optionCounter.nonEmpty) Future(optionCounter.get) else Future.failed(InternalServerError())
+        updatedCounter <- counterRepository.update(counter.copy(amount = counter.amount + incrementAndGet.amount)).run
+      } yield updatedCounter).pipeTo(sender())
 
-    case GetAmount => counterRepo.find(defaultId).pipeTo(sender)
+    case GetAmount =>
+      (for {
+        optionCounter <- counterRepository.findOne(defaultId).run
+        counter <- if (optionCounter.nonEmpty) Future(optionCounter.get) else Future.failed(InternalServerError())
+      } yield counter).pipeTo(sender())
   }
 }

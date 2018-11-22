932d5e2f75b8d6dc6d575889554289bb1179775d
Dmitry Tverdokhleb <dmitriy.tverdokhleb@sysgears.com>
2018-11-22 11:44:48 +0200

    Move ContactResolver bind to ContactModule

--- packages/server-scala/src/main/scala/modules/contact/guice/modules/ContactModule.scala
+++ packages/server-scala/src/main/scala/modules/contact/guice/modules/ContactModule.scala
@@ -5,12 +5,14 @@ import com.google.inject.Provides
 import com.google.inject.name.{Named, Names}
 import core.guice.injection.GuiceActorRefProvider
 import modules.contact.actor.ContactActor
+import modules.contact.graphql.resolvers.{ContactResolver, ContactResolverImpl}
 import net.codingwell.scalaguice.ScalaModule
 
 class ContactModule extends ScalaModule with GuiceActorRefProvider {
 
   override def configure(): Unit = {
     bind[Actor].annotatedWith(Names.named(ContactActor.name)).to[ContactActor]
+    bind[ContactResolver].to[ContactResolverImpl]
   }
 
   @Provides

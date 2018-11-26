e74a941975af4b6f6cfda9a87cb6b77916ca17a1
Serhii Reznychenko <serhii.reznychenko@sysgears.com>
2018-11-26 15:29:37 +0200

    Add jwt validation with using custom secret

--- modules/core/server-scala/src/main/scala/modules/jwt/validator/JwtValidator.scala
+++ modules/core/server-scala/src/main/scala/modules/jwt/validator/JwtValidator.scala
@@ -3,7 +3,7 @@ package modules.jwt.validator
 import scala.util.Try
 
 trait JwtValidator {
-  def validate(token: String): Try[Unit]
+  def validate(token: String, secret: String): Try[Unit]
 
-  def isValid(token: String): Boolean
+  def isValid(token: String, secret: String): Boolean
 }
--- modules/core/server-scala/src/main/scala/modules/jwt/validator/JwtValidatorImpl.scala
+++ modules/core/server-scala/src/main/scala/modules/jwt/validator/JwtValidatorImpl.scala
@@ -1,18 +1,14 @@
 package modules.jwt.validator
 
-import javax.inject.{Inject, Named}
+import javax.inject.Inject
 import pdi.jwt.Jwt
 import pdi.jwt.algorithms.JwtHmacAlgorithm
 
 import scala.util.Try
 
-class JwtValidatorImpl @Inject()(@Named("jwt.secretKey") secretKey: String,
-                                 algorithm: JwtHmacAlgorithm) extends JwtValidator {
-  override def validate(token: String): Try[Unit] = {
-    Try(Jwt.validate(token, secretKey, Seq(algorithm)))
-  }
+class JwtValidatorImpl @Inject()(algorithm: JwtHmacAlgorithm) extends JwtValidator {
 
-  override def isValid(token: String): Boolean = {
-    Jwt.isValid(token, secretKey, Seq(algorithm))
-  }
+  override def validate(token: String, secret: String): Try[Unit] = Try(Jwt.validate(token, secret, Seq(algorithm)))
+
+  override def isValid(token: String, secret: String): Boolean = Jwt.isValid(token, secret, Seq(algorithm))
 }

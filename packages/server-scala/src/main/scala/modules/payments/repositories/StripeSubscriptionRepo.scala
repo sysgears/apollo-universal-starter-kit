9e0835838f813cec94f05a59982ba7d526ac32f8
Mikael Zulfigarov <mikaelzulfigarov@gmail.com>
2018-11-26 16:18:37 +0200

    Add repository for 'stripe_subscription' table

---
+++ packages/server-scala/src/main/scala/modules/payments/repositories/StripeSubscriptionRepo.scala
@@ -0,0 +1,54 @@
+package modules.payments.repositories
+
+import com.google.inject.Inject
+import common.errors.NotFound
+import modules.payments.models.StripeSubscription
+import slick.jdbc.SQLiteProfile.api._
+
+import scala.concurrent.{ExecutionContext, Future}
+import scala.language.postfixOps
+
+trait StripeSubscriptionRepo {
+  def createSubscription(subscription: StripeSubscription): Future[StripeSubscription]
+  def editSubscription(subscription: StripeSubscription): Future[StripeSubscription]
+  def getSubscriptionByUserId(userId: Long): Future[Option[StripeSubscription]]
+  def getSubscriptionByStripeSubscriptionId(stripeSubscriptionId: String): Future[Option[StripeSubscription]]
+  def getSubscriptionByStripeCustomerId(stripeCustomerId: String): Future[Option[StripeSubscription]]
+  def getCreditCardByUserId(userId: Long): Future[Option[StripeSubscription.CreditCard]]
+}
+
+class StripeSubscriptionRepoImpl @Inject()(db: Database)(implicit executionContext: ExecutionContext) extends StripeSubscriptionRepo {
+  def stripeSubscriptions = TableQuery[StripeSubscription.Table]
+
+  override def createSubscription(subscription: StripeSubscription): Future[StripeSubscription] = db run {
+    stripeSubscriptions returning stripeSubscriptions.map(_.id) into {
+      (subscription, id) => subscription.copy(id = Some(id))
+    } += subscription
+  }
+
+  override def editSubscription(stripeSubscription: StripeSubscription): Future[StripeSubscription] = db run {
+    stripeSubscriptions.filter(_.id === stripeSubscription.id).update(stripeSubscription).flatMap {
+      case 0 => DBIO.failed(NotFound(s"StripeSubscription(id: ${stripeSubscription.id})"))
+      case _ => DBIO.successful(stripeSubscription)
+    }: DBIOAction[StripeSubscription, NoStream, Effect.Write with Effect]
+  }
+
+  override def getSubscriptionByUserId(userId: Long): Future[Option[StripeSubscription]] = db run {
+    stripeSubscriptions.filter(_.userId === userId).take(1).result.headOption
+  }
+
+  override def getSubscriptionByStripeSubscriptionId(stripeSubscriptionId: String): Future[Option[StripeSubscription]] = db run {
+    stripeSubscriptions.filter(_.stripeSubscriptionId === stripeSubscriptionId).take(1).result.headOption
+  }
+
+  override def getSubscriptionByStripeCustomerId(stripeCustomerId: String): Future[Option[StripeSubscription]] = db run {
+    stripeSubscriptions.filter(_.stripeCustomerId === stripeCustomerId).take(1).result.headOption
+  }
+
+  override def getCreditCardByUserId(userId: Long): Future[Option[StripeSubscription.CreditCard]] = db run {
+    for {
+      maybeCCData <- stripeSubscriptions.filter(_.userId === userId).take(1).map(ss => (ss.expiryMonth, ss.expiryYear, ss.last4, ss.brand)).result.headOption
+      maybeCreditCard = maybeCCData map (StripeSubscription.CreditCard(_))
+    } yield maybeCreditCard
+  }
+}

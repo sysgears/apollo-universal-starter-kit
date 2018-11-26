1dd82d282a746346842f088d0b7a6136cabfaba0
Serhii Reznychenko <serhii.reznychenko@sysgears.com>
2018-11-26 15:33:06 +0200

    Create JwtAuthService

---
+++ modules/core/server-scala/src/main/scala/modules/jwt/service/JwtAuthService.scala
@@ -0,0 +1,15 @@
+package modules.jwt.service
+
+import scala.util.Try
+
+trait JwtAuthService[T] {
+  def createAccessToken(content: T): String
+
+  def createRefreshToken(content: T, secret: String): String
+
+  def decodeContent(token: String): Try[T]
+
+  def decodeAccessToken(token: String): Try[T]
+
+  def decodeRefreshToken(token: String, secret: String): Try[T]
+}

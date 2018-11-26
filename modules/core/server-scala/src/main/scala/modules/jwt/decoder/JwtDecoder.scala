824dfe2c5ab29297685d21599d0f94fde29a3aa9
Serhii Reznychenko <serhii.reznychenko@sysgears.com>
2018-11-26 15:27:30 +0200

    Add jwt decoding content-only and decoding, using custom secret

--- modules/core/server-scala/src/main/scala/modules/jwt/decoder/JwtDecoder.scala
+++ modules/core/server-scala/src/main/scala/modules/jwt/decoder/JwtDecoder.scala
@@ -4,4 +4,6 @@ import scala.util.Try
 
 trait JwtDecoder {
   def decode(token: String): Try[String]
+
+  def decode(token: String, secret: String): Try[String]
 }
--- modules/core/server-scala/src/main/scala/modules/jwt/decoder/JwtDecoderImpl.scala
+++ modules/core/server-scala/src/main/scala/modules/jwt/decoder/JwtDecoderImpl.scala
@@ -1,14 +1,14 @@
 package modules.jwt.decoder
 
-import javax.inject.{Inject, Named}
+import javax.inject.Inject
 import pdi.jwt.Jwt
 import pdi.jwt.algorithms.JwtHmacAlgorithm
 
 import scala.util.Try
 
-class JwtDecoderImpl @Inject()(@Named("jwt.secretKey") secretKey: String,
-                               algorithm: JwtHmacAlgorithm) extends JwtDecoder {
-  override def decode(token: String): Try[String] = {
-    Jwt.decodeRaw(token, secretKey, Seq(algorithm))
-  }
+class JwtDecoderImpl @Inject()(algorithm: JwtHmacAlgorithm) extends JwtDecoder {
+
+  override def decode(token: String): Try[String] = Jwt.decodeRaw(token)
+
+  override def decode(token: String, secret: String): Try[String] = Jwt.decodeRaw(token, secret, Seq(algorithm))
 }

c99186065cde2c411ed6a703cdf7891f487b3ef1
Sergey Gilevich <kentserg9453@gmail.com>
2018-11-26 15:48:05 +0200

    Add mutation logoutFromAllDevices for server user resolver

--- packages/server/src/modules/user/access/session/resolvers.js
+++ packages/server/src/modules/user/access/session/resolvers.js
@@ -1,10 +1,19 @@
 import { writeSession } from './sessions';
+import User from '../../sql';
 
 export default () => ({
   Mutation: {
     logout(obj, args, { req }) {
       const session = { ...req.session };
 
+      delete session.userId;
+
+      req.session = writeSession(req, session);
+    },
+    async logoutFromAllDevices(obj, args, { req }) {
+      const session = { ...req.session };
+
+      await User.deleteUserSessions(session.userId);
       delete session.userId;
 
       req.session = writeSession(req, session);

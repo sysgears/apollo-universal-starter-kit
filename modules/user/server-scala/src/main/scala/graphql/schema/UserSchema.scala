ed5db1f5ec39bf05967c5a84a17e003cd84ef34b
Serhii Reznychenko <serhii.reznychenko@sysgears.com>
2018-11-27 19:18:15 +0200

    Update UserSchema according to actor-driven architecture

--- modules/user/server-scala/src/main/scala/graphql/schema/UserSchema.scala
+++ modules/user/server-scala/src/main/scala/graphql/schema/UserSchema.scala
@@ -1,31 +1,38 @@
 package graphql.schema
 
-import common.InputUnmarshallerGenerator
+import akka.actor.ActorSystem
+import akka.stream.ActorMaterializer
+import common.{InputUnmarshallerGenerator, Logger}
+import common.graphql.DispatcherResolver._
 import config.AuthConfig
 import core.graphql.{GraphQLSchema, UserContext}
 import javax.inject.Inject
-import graphql.resolvers.{TokensResolver, UserResolver}
-import model._
+import graphql.resolvers.UserResolver
+import model.{UserPayload, _}
+import modules.common.FieldError
 import sangria.schema.{Argument, Field, InputObjectType, ObjectType, StringType}
 import sangria.macros.derive._
 import sangria.marshalling.FromInput
 
-class UserSchema @Inject()(userResolver: UserResolver,
-                           tokensResolver: TokensResolver,
-                           authConfig: AuthConfig) extends GraphQLSchema
-  with InputUnmarshallerGenerator {
+import scala.concurrent.Future
+
+class UserSchema @Inject()(authConfig: AuthConfig)
+                          (implicit val materializer: ActorMaterializer,
+                           actorSystem: ActorSystem) extends GraphQLSchema
+  with InputUnmarshallerGenerator
+  with Logger {
 
   implicit val registerUserInput: InputObjectType[RegisterUserInput] = deriveInputObjectType(InputObjectTypeName("RegisterUserInput"))
   implicit val confirmRegistrationInput: InputObjectType[ConfirmRegistrationInput] = deriveInputObjectType(InputObjectTypeName("ConfirmRegistrationInput"))
   implicit val resendConfirmationMessageInput: InputObjectType[ResendConfirmationMessageInput] = deriveInputObjectType(InputObjectTypeName("ResendConfirmationMessageInput"))
   implicit val user: ObjectType[UserContext, User] = deriveObjectType(ObjectTypeName("User"), ExcludeFields("password"))
-  implicit val UserPayload: ObjectType[UserContext, UserPayload] = deriveObjectType(ObjectTypeName("UserPayload"))
+  implicit val userPayload: ObjectType[UserContext, UserPayload] = deriveObjectType(ObjectTypeName("UserPayload"))
   implicit val loginUserInput: InputObjectType[LoginUserInput] = deriveInputObjectType(InputObjectTypeName("LoginUserInput"))
+  implicit val authPayload: ObjectType[UserContext, AuthPayload] = deriveObjectType(ObjectTypeName("AuthPayload"))
   implicit val tokens: ObjectType[UserContext, Tokens] = deriveObjectType(ObjectTypeName("Tokens"))
-  implicit val AuthPayload: ObjectType[UserContext, AuthPayload] = deriveObjectType(ObjectTypeName("AuthPayload"))
   implicit val forgotPasswordInput: InputObjectType[ForgotPasswordInput] = deriveInputObjectType(InputObjectTypeName("ForgotPasswordInput"))
   implicit val resetPasswordInput: InputObjectType[ResetPasswordInput] = deriveInputObjectType(InputObjectTypeName("ResetPasswordInput"))
-  implicit val ResetPayload: ObjectType[UserContext, ResetPayload] = deriveObjectType(ObjectTypeName("ResetPayload"))
+  implicit val resetPayload: ObjectType[UserContext, ResetPayload] = deriveObjectType(ObjectTypeName("ResetPayload"))
 
   implicit val registerUserInputUnmarshaller: FromInput[RegisterUserInput] = inputUnmarshaller {
     input =>
@@ -59,64 +66,87 @@ class UserSchema @Inject()(userResolver: UserResolver,
       )
   }
 
-  implicit val forgotPasswordInputUnmarshaller:  FromInput[ForgotPasswordInput] = inputUnmarshaller {
+  implicit val forgotPasswordInputUnmarshaller: FromInput[ForgotPasswordInput] = inputUnmarshaller {
     input =>
       ForgotPasswordInput(
         usernameOrEmail = input("usernameOrEmail").asInstanceOf[String]
       )
   }
 
-  implicit val resetPasswordInputUnmarshaller:  FromInput[ResetPasswordInput] = inputUnmarshaller {
+  implicit val resetPasswordInputUnmarshaller: FromInput[ResetPasswordInput] = inputUnmarshaller {
     input =>
       ResetPasswordInput(
-       token = input("token").asInstanceOf[String],
+        token = input("token").asInstanceOf[String],
         password = input("password").asInstanceOf[String]
       )
   }
 
-
   override def mutations: List[Field[UserContext, Unit]] = List(
     Field(
       name = "register",
-      fieldType = UserPayload,
+      fieldType = userPayload,
       arguments = List(Argument("input", registerUserInput)),
-      resolve = sangriaContext => userResolver.register(sangriaContext.args.arg[RegisterUserInput]("input"), authConfig.skipConfirmation)
+      resolve = sc => resolveWithDispatcher[UserPayload](
+        input = (sc.args.arg[RegisterUserInput]("input"), authConfig.skipConfirmation),
+        userContext = sc.ctx,
+        onException = e => UserPayload(errors = Some(List(FieldError("register", e.getMessage)))),
+        namedResolverActor = UserResolver
+      )
     ),
     Field(
       name = "resendConfirmationMessage",
-      fieldType = UserPayload,
+      fieldType = userPayload,
       arguments = List(Argument("input", resendConfirmationMessageInput)),
-      resolve = sangriaContext => userResolver.resendConfirmationMessage(sangriaContext.args.arg[ResendConfirmationMessageInput]("input"))
+      resolve = sc => resolveWithDispatcher[UserPayload](
+        input = sc.args.arg[ResendConfirmationMessageInput]("input"),
+        userContext = sc.ctx,
+        onException = e => UserPayload(errors = Some(List(FieldError("resendConfirmationMessage", e.getMessage)))),
+        namedResolverActor = UserResolver
+      )
     ),
     Field(
       name = "confirmRegistration",
-      fieldType = AuthPayload,
+      fieldType = authPayload,
       arguments = List(Argument("input", confirmRegistrationInput)),
-      resolve = sangriaContext => userResolver.confirmRegistration(sangriaContext.args.arg[ConfirmRegistrationInput]("input"))
+      resolve = sc => resolveWithDispatcher[AuthPayload](
+        input = sc.args.arg[ConfirmRegistrationInput]("input"),
+        userContext = sc.ctx,
+        onException = e => AuthPayload(errors = Some(List(FieldError("confirmRegistration", e.getMessage)))),
+        namedResolverActor = UserResolver
+      )
     ),
     Field(
       name = "login",
-      fieldType = AuthPayload,
+      fieldType = authPayload,
       arguments = List(Argument("input", loginUserInput)),
-      resolve = sangriaContext => userResolver.login(sangriaContext.args.arg[LoginUserInput]("input"))
-    ),
-    Field(
-      name = "refreshTokens",
-      fieldType = tokens,
-      arguments = List(Argument("refreshToken", StringType)),
-      resolve = sangriaContext => tokensResolver.refreshTokens(sangriaContext.args.arg[String]("refreshToken"))
+      resolve = sc => resolveWithDispatcher[AuthPayload](
+        input = sc.args.arg[LoginUserInput]("input"),
+        userContext = sc.ctx,
+        onException = e => AuthPayload(errors = Some(List(FieldError("login", e.getMessage)))),
+        namedResolverActor = UserResolver
+      )
     ),
     Field(
       name = "forgotPassword",
       fieldType = StringType,
       arguments = List(Argument("input", forgotPasswordInput)),
-      resolve = sangriaContext => userResolver.forgotPassword(sangriaContext.args.arg[ForgotPasswordInput]("input"))
+      resolve = sc => resolveWithDispatcher[String](
+        input = sc.args.arg[ForgotPasswordInput]("input"),
+        userContext = sc.ctx,
+        onException = e => Future.failed(e),
+        namedResolverActor = UserResolver
+      )
     ),
     Field(
       name = "resetPassword",
-      fieldType = ResetPayload,
+      fieldType = resetPayload,
       arguments = List(Argument("input", resetPasswordInput)),
-      resolve = sangriaContext => userResolver.resetPassword(sangriaContext.args.arg[ResetPasswordInput]("input"))
+      resolve = sc => resolveWithDispatcher[ResetPayload](
+        input = sc.args.arg[ResetPasswordInput]("input"),
+        userContext = sc.ctx,
+        onException = e => ResetPayload(errors = Some(List(FieldError("resetPassword", e.getMessage)))),
+        namedResolverActor = UserResolver
+      )
     )
   )
 }

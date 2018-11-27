dd402c3bd7d6c13f3e70ab7f6a47b733f920af8c
Serhii Reznychenko <serhii.reznychenko@sysgears.com>
2018-11-27 19:16:23 +0200

    Add actor bindings in the UserModule configuration

--- modules/user/server-scala/src/main/scala/modules/UserModule.scala
+++ modules/user/server-scala/src/main/scala/modules/UserModule.scala
@@ -1,14 +1,17 @@
 package modules
 
-import graphql.resolvers.{TokensResolver, TokensResolverImpl, UserResolver, UserResolverImpl}
+import akka.actor.Actor
+import com.google.inject.name.Names
+import core.guice.injection.GuiceActorRefProvider
+import graphql.resolvers._
 import repositories.{UserRepo, UserRepoImpl}
 import net.codingwell.scalaguice.ScalaModule
 
-class UserModule extends ScalaModule {
+class UserModule extends ScalaModule with GuiceActorRefProvider {
   
   override def configure() = {
-    bind[UserResolver].to[UserResolverImpl]
     bind[UserRepo].to[UserRepoImpl]
-    bind[TokensResolver].to[TokensResolverImpl]
+    bind[Actor].annotatedWith(Names.named(TokenResolver.name)).to[TokenResolver]
+    bind[Actor].annotatedWith(Names.named(UserResolver.name)).to[UserResolver]
   }
 }

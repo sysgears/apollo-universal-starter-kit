ecd38e58a801597bf75eb653d7f1a4b81282ef30
Serhii Reznychenko <serhii.reznychenko@sysgears.com>
2018-11-27 13:26:37 +0200

    Remove the 'WebsiteConfig' class

--- modules/core/server-scala/src/main/scala/common/config/WebsiteConfig.scala
+++
@@ -1,8 +0,0 @@
-package common.config
-
-import com.google.inject.Inject
-import com.typesafe.config.Config
-
-class WebsiteConfig @Inject()(config: Config)  {
-  val url: String = config.getString("website.url")
-}

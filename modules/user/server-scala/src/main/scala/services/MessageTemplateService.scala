041d2d6264ada8a9284b3bea53b350af3d751a76
Serhii Reznychenko <serhii.reznychenko@sysgears.com>
2018-11-27 17:32:21 +0200

    Add the recover password message template and refactor the confirm registration message template

--- modules/user/server-scala/src/main/scala/services/MessageTemplateService.scala
+++ modules/user/server-scala/src/main/scala/services/MessageTemplateService.scala
@@ -6,12 +6,24 @@ import model.User
 
 class MessageTemplateService {
 
-  def createConfirmRegistrationMessage(user: User, fromEmail: String, followLink: String): Message = {
+  def createConfirmRegistrationMessage(user: User, appName: String, fromEmail: String, followLink: String): Message =
     Message(
-      subject = "Apollo universal starter kit registration.",
-      content = Content().html(s"<p>${user.username}, please follow the link to confirm registration.</p><br><p>$followLink</p>"),
+      subject = "Confirm Email",
+      content = Content().html(
+        s"""<p>Hi, ${user.username}!</p>
+           | <p>Welcome to $appName. Please click the following link to confirm your email:</p>
+           | <p><a href="$followLink">$followLink</a></p>""".stripMargin),
       from = new InternetAddress(fromEmail),
       to = Seq(new InternetAddress(user.email))
     )
-  }
+
+  def createRecoverPasswordMessage(user: User, appName: String, fromEmail: String, followLink: String): Message =
+    Message(
+      subject = "Reset Password",
+      content = Content().html(
+        s"""<p>Please click this link to reset your password:</p>
+           | <a href="$followLink">$followLink</a>""".stripMargin),
+      from = new InternetAddress(fromEmail),
+      to = Seq(new InternetAddress(user.email))
+    )
 }

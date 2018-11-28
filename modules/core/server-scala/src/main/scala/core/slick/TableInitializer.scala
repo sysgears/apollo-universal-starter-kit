e467ce58ca7ba3d3266567bb941c8d48aeee31c9
pavel.grudina <plgrudina@gmail.com>
2018-11-28 14:22:22 +0200

    Implement an abstraction for initializing tables in a database

---
+++ modules/core/server-scala/src/main/scala/core/slick/TableInitializer.scala
@@ -0,0 +1,28 @@
+package core.slick
+
+import slick.jdbc.SQLiteProfile.api._
+import slick.jdbc.meta.MTable
+import slick.lifted.TableQuery
+import slick.relational.RelationalProfile
+
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
+
+abstract class TableInitializer[E <: RelationalProfile#Table[_]](name: String, table: TableQuery[E], database: Database) {
+
+  def create(): Future[Unit] = {
+    database.run(MTable.getTables(name)).flatMap {
+      tables => if (tables.isEmpty) database.run(DBIO.seq(table.schema.create)) else Future.successful()
+    }
+  }
+
+  def drop(): Future[Unit] = {
+    database.run(MTable.getTables(name)).flatMap {
+      tables => if (tables.nonEmpty) database.run(DBIO.seq(table.schema.drop)) else Future.successful()
+    }
+  }
+
+  def seedDatabase(): Future[Unit] = {
+    Future.successful()
+  }
+}

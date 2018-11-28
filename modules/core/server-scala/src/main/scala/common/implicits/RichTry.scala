0baeb5b4dcaa29e7193036c2c2a0e7eb52c831f3
Serhii Reznychenko <serhii.reznychenko@sysgears.com>
2018-11-28 16:10:31 +0200

    Remove 'errorHandler' argument in the 'asFuture' function of 'RichTry' class

--- modules/core/server-scala/src/main/scala/common/implicits/RichTry.scala
+++ modules/core/server-scala/src/main/scala/common/implicits/RichTry.scala
@@ -16,13 +16,12 @@ object RichTry {
     /**
       * Converts this Try to a Future.failed with the given error, if the Try contains error (Try.failed).
       *
-      * @param errorHandler a function that responsible for translating the error from Try to Future
-      * @param ec           an execution context for the Future
+      * @param ec an execution context for the Future
       * @return a Future.failed with a errorHandler output type error.
       */
-    def asFuture(errorHandler: Throwable => Throwable)(implicit ec: ExecutionContext): Future[A] = tryVal match {
+    def asFuture(implicit ec: ExecutionContext): Future[A] = tryVal match {
       case Success(value) => Future.successful(value)
-      case Failure(exception) => Future.failed(errorHandler(exception))
+      case Failure(exception) => Future.failed(exception)
     }
   }
 

46740800892f24429fd6bcb55e36ed61c0c9876c
Mikael Zulfigarov <mikaelzulfigarov@gmail.com>
2018-11-28 10:57:58 +0200

    Connect payments schema with query implementations from resolver class

--- packages/server-scala/src/main/scala/modules/payments/graphql/schema/PaymentsSchema.scala
+++ packages/server-scala/src/main/scala/modules/payments/graphql/schema/PaymentsSchema.scala
@@ -2,30 +2,34 @@ package modules.payments.graphql.schema
 
 import com.google.inject.Inject
 import core.graphql.{GraphQLSchema, UserContext}
+import modules.payments.graphql.resolvers.subscription.StripeSubscriptionResolver
+import modules.payments.graphql.resolvers.subscription.contexts.StripeSubscriptionInputContext
 import sangria.schema.{Argument, Field, OptionType}
 
 import scala.concurrent.{ExecutionContext, Future}
 
-class PaymentsSchema @Inject()()(implicit ec: ExecutionContext) extends GraphQLSchema {
+class PaymentsSchema @Inject()(stripeSubscriptionResolver: StripeSubscriptionResolver)
+                              (implicit ec: ExecutionContext) extends GraphQLSchema {
   import modules.payments.graphql.schema.types.input._
   import modules.payments.graphql.schema.types.output._
   import modules.payments.graphql.schema.types.unmarshallers._
+  import stripeSubscriptionResolver._
 
   override def queries: List[Field[UserContext, Unit]] = List(
     Field(
       name = "stripeSubscription",
       fieldType = OptionType(StripeSubscription),
-      resolve = { ctx => Future.successful(null) /*TODO: Stub*/ }
+      resolve = { ctx => stripeSubscription(StripeSubscriptionInputContext(null /*TODO: Stub. Resolve when userContext in User module will be implemented*/)) }
     ),
     Field(
       name = "stripeSubscriptionProtectedNumber",
       fieldType = OptionType(StripeSubscriberProtectedNumber),
-      resolve = { ctx => Future.successful(null) /*TODO: Stub*/ }
+      resolve = { ctx => stripeSubscriptionProtectedNumber(StripeSubscriptionInputContext(null /*TODO: Stub. Resolve when userContext in User module will be implemented*/)) }
     ),
     Field(
       name = "stripeSubscriptionCard",
       fieldType = OptionType(StripeSubscriptionCard),
-      resolve = { ctx => Future.successful(null) /*TODO: Stub*/ }
+      resolve = { ctx => stripeSubscriptionCard(StripeSubscriptionInputContext(null /*TODO: Stub. Resolve when userContext in User module will be implemented*/)) }
     )
   )
 

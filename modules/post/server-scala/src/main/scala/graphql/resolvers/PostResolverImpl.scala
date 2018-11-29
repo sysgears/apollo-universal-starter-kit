c601fc349db101485db68fd2991c9814ddf0b0f8
Eugene Krotov <e.krotov@hotmail.com>
2018-11-29 16:20:50 +0200

    Implement post resolver.

--- modules/post/server-scala/src/main/scala/graphql/resolvers/PostResolverImpl.scala
+++ modules/post/server-scala/src/main/scala/graphql/resolvers/PostResolverImpl.scala
@@ -9,21 +9,57 @@ import slick.dbio.DBIO
 
 import scala.concurrent.{ExecutionContext, Future}
 
-class PostResolverImpl @Inject()(postRepository: Repository[Post, Int], commentRepository: Repository[Comment, Int])
+class PostResolverImpl @Inject()(postRepository: Repository[Post, Int],
+                                 commentRepository: Repository[Comment, Int])
                                 (implicit executionContext: ExecutionContext) extends PostResolver with Logger {
-  override def post(id: Int): Future[Post] = ???
+  implicit def dbAction[T](action: DBIO[T]): Future[T] = action.run
 
+  //TODO Implement error handler
+  override def post(id: Int): Future[Post] =
+    for {
+      maybePost <- postRepository.findOne(id).run
+      post      <- if (maybePost.nonEmpty) Future.successful(maybePost.get) else Future.successful(null)
+    } yield post
+
+  //TODO Not Implemented
   override def posts(limit: Int, after: Int): Future[Posts] = ???
 
-  override def addPost(input: AddPostInput): Future[Post] = ???
+  override def addPost(input: AddPostInput): Future[Post] =
+    postRepository.save(input)
 
-  override def deletePost(id: Int): Future[Post] = ???
+  override def deletePost(id: Int): Future[Post] = {
+    for {
+      maybePost <- postRepository.findOne(id).run
+      post      <- maybePost
+      _         <- postRepository.delete(post)
+    } yield post
+  }
 
-  override def editPost(input: EditPostInput): Future[Post] = ???
+  override def editPost(input: EditPostInput): Future[Post] =
+    postRepository.update(input)
 
-  override def addComment(input: AddCommentInput): Future[Comment] = ???
+  override def addComment(input: AddCommentInput): Future[Comment] =
+    commentRepository.save(input)
 
-  override def deleteComment(input: DeleteCommentInput): Future[Comment] = ???
+  override def deleteComment(input: DeleteCommentInput): Future[Comment] =
+    for {
+      maybeComment <- commentRepository.findOne(input.id)
+      comment      <- maybeComment
+      _            <- commentRepository.delete(comment)
+    } yield comment
 
-  override def editComment(input: EditCommentInput): Future[Comment] = ???
+  override def editComment(input: EditCommentInput): Future[Comment] =
+    commentRepository.update(input)
+
+  //  //TODO NOT Implemented Subscription method!!!
+  //  override def postUpdated(id: Int): Future[UpdatePostPayload] =
+  //    Future.failed(NotImplementedException)
+  //
+  //  //TODO NOT Implemented Subscription method!!!
+  //  override def postsUpdated(endCursor: Int): Future[UpdatePostPayload] =
+  //    Future.failed(NotImplementedException)
+  //
+  //  //TODO NOT Implemented Subscription method!!!
+  //  override def commentUpdated(postId: Int): Future[UpdateCommentPayload] =
+  //    Future.failed(NotImplementedException)
 }

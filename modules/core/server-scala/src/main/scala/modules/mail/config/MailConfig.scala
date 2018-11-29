3d512c28b94cc15da782cb9a2e2f5f6cd8aa8e53
Serhii Reznychenko <serhii.reznychenko@sysgears.com>
2018-11-29 13:18:30 +0200

    Add singleton annotation to all of the config classes

--- modules/core/server-scala/src/main/scala/modules/mail/config/MailConfig.scala
+++ modules/core/server-scala/src/main/scala/modules/mail/config/MailConfig.scala
@@ -1,8 +1,9 @@
 package modules.mail.config
 
-import com.google.inject.Inject
+import com.google.inject.{Inject, Singleton}
 import com.typesafe.config.Config
 
+@Singleton
 class MailConfig @Inject()(config: Config) {
   val address: String = config.getString("email.address")
 }

438faeec47743e44c972e5a4acefb229f846f4fc
Serhii Reznychenko <serhii.reznychenko@sysgears.com>
2018-11-29 14:10:08 +0200

    Add refresh token validation into the 'refreshTokens' mutation

--- modules/user/server-scala/src/main/scala/graphql/resolvers/TokenResolver.scala
+++ modules/user/server-scala/src/main/scala/graphql/resolvers/TokenResolver.scala
@@ -30,6 +30,7 @@ class TokenResolver @Inject()(userRepository: UserRepository,
         tokenContent <- jwtAuthService.decodeContent(refreshToken).asFuture
         user <- userRepository.findOne(tokenContent.id).run failOnNone NotFound(s"User with id: [${tokenContent.id}] not found.")
         userId <- Future.successful(user.id) failOnNone NotFound(s"Id for user: [${user.username}] is none.")
+        _ <- jwtAuthService.validate(refreshToken, user.password).asFuture
         accessToken = jwtAuthService.createAccessToken(JwtContent(userId))
         refreshToken = jwtAuthService.createRefreshToken(JwtContent(userId), user.password)
       } yield Tokens(accessToken, refreshToken)

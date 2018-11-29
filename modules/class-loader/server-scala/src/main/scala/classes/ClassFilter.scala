18b58665091e31bc864d83fefd50fb111aaa54e5
Dmitry Tverdokhleb <dmitriy.tverdokhleb@sysgears.com>
2018-11-29 11:10:41 +0200

    Remove traits

--- modules/class-loader/server-scala/src/main/scala/classes/ClassFilter.scala
+++
@@ -1,9 +0,0 @@
-package classes
-
-import org.clapper.classutil.ClassInfo
-
-trait ClassFilter {
-  def filterClasses(classes: List[ClassInfo], filter: ClassInfo => Boolean): List[ClassInfo] = {
-    classes.filter(filter)
-  }
-}
--- modules/class-loader/server-scala/src/main/scala/classes/ClassInstantiator.scala
+++
@@ -1,9 +0,0 @@
-package classes
-
-import org.clapper.classutil.ClassInfo
-
-trait ClassInstantiator {
-  def instatiateClasses[T](classes: List[ClassInfo], init: Class[_] => T): List[T] = {
-    classes.map(classInfo => getClass.getClassLoader.loadClass(classInfo.name)).map(init)
-  }
-}

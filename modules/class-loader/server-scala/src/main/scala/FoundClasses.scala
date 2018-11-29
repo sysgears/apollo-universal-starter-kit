ac388c28f61aa48b6c17fdeda1d730b343184e5d
Dmitry Tverdokhleb <dmitriy.tverdokhleb@sysgears.com>
2018-11-29 11:16:53 +0200

    Refactor naming

--- modules/class-loader/server-scala/src/main/scala/FoundClasses.scala
+++ modules/class-loader/server-scala/src/main/scala/FoundClasses.scala
@@ -3,8 +3,8 @@ import java.io.File
 import classes.ClassesMetaInfo
 import org.clapper.classutil.{ClassFinder, ClassInfo}
 
-case class FoundClasses(path: List[String]) extends ClassesMetaInfo {
+case class FoundClasses(paths: List[String]) extends ClassesMetaInfo {
   override def retrieve: List[ClassInfo] = {
-    ClassFinder(path.map(new File(_))).getClasses().toList
+    ClassFinder(paths.map(new File(_))).getClasses().toList
   }
 }
--- modules/class-loader/server-scala/src/main/scala/InitializedClasses.scala
+++ modules/class-loader/server-scala/src/main/scala/InitializedClasses.scala
@@ -1,8 +1,8 @@
 import classes.{ClassInstances, ClassesMetaInfo}
 
-case class InitializedClasses[T](classMetaInfo: ClassesMetaInfo, initializer: Option[Class[_] => T] = None) extends ClassInstances[T] {
+case class InitializedClasses[T](classesMetaInfo: ClassesMetaInfo, initializer: Option[Class[_] => T] = None) extends ClassInstances[T] {
   override def retrieve: List[T] = {
-    classMetaInfo.retrieve.map(classInfo => getClass.getClassLoader.loadClass(classInfo.name)).map {
+    classesMetaInfo.retrieve.map(classInfo => getClass.getClassLoader.loadClass(classInfo.name)).map {
       initializer match {
         case Some(func) => func
         case _ => clazz: Class[_] => clazz.newInstance.asInstanceOf[T]

9b0dc50326d46c30b0cbe658d4e7b2fa1d34a956
Konstantin Musienko <konstantin.musienko@sysgears.com>
2018-11-29 15:42:11 +0200

    Restructure 'retrieve' function in InitializedClasses

--- modules/class-loader/server-scala/src/main/scala/InitializedClasses.scala
+++ modules/class-loader/server-scala/src/main/scala/InitializedClasses.scala
@@ -2,11 +2,10 @@ import classes.{ClassInstances, ClassesMetaInfo}
 
 case class InitializedClasses[T](classesMetaInfo: ClassesMetaInfo, initializer: Option[Class[_] => T] = None) extends ClassInstances[T] {
   override def retrieve: List[T] = {
-    classesMetaInfo.retrieve.map(classInfo => getClass.getClassLoader.loadClass(classInfo.name)).map {
-      initializer match {
-        case Some(func) => func
-        case _ => clazz: Class[_] => clazz.newInstance.asInstanceOf[T]
-      }
+    val initializerFunc = initializer match {
+      case Some(func) => func
+      case _ => clazz: Class[_] => clazz.newInstance.asInstanceOf[T]
     }
+    classesMetaInfo.retrieve.map(classInfo => getClass.getClassLoader.loadClass(classInfo.name)).map(initializerFunc)
   }
 }

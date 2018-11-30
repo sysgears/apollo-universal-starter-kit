0f671ff23667f2e084f910822e71ecfd9c8c06e1
Serhii Reznychenko <serhii.reznychenko@sysgears.com>
2018-11-30 15:35:42 +0200

    Make the 'username' and 'email' fields unique in the user table

--- modules/user/server-scala/src/main/scala/model/UserTable.scala
+++ modules/user/server-scala/src/main/scala/model/UserTable.scala
@@ -12,9 +12,9 @@ object UserTable extends JdbcProfile {
   class UserTable(tag: Tag) extends SlickTable[User](tag, name) with Keyed[Int] {
     def id = column[Int]("ID", O.PrimaryKey, O.AutoInc)
 
-    def username = column[String]("USERNAME")
+    def username = column[String]("USERNAME", O.Unique)
 
-    def email = column[String]("EMAIL")
+    def email = column[String]("EMAIL", O.Unique)
 
     def password = column[String]("PASSWORD")
 

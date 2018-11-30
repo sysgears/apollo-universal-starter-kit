da18510db714291ba5c9ca876365d4cc566813e3
Oleh Tsivan <oleh.tsivan@sysgears.com>
2018-11-30 21:36:42 +0200

    Move bind definition of 'MailService' from 'contact' module into 'mailer'

--- modules/contact/server-scala/src/main/scala/guice/modules/ContactModule.scala
+++ modules/contact/server-scala/src/main/scala/guice/modules/ContactModule.scala
@@ -1,15 +1,10 @@
 package guice.modules
 
-import com.github.jurajburian.mailer.Message
 import graphql.resolvers.{ContactResolver, ContactResolverImpl}
-import models.MailPayload
 import net.codingwell.scalaguice.ScalaModule
-import services.MailService
-import services.ethereal.EtherealMailService
 
 class ContactModule extends ScalaModule {
   override def configure(): Unit = {
     bind[ContactResolver].to[ContactResolverImpl]
-    bind[MailService[Message, MailPayload]].to[EtherealMailService]
   }
 }
--- modules/mailer/server-scala/src/main/scala/modules/MailModule.scala
+++ modules/mailer/server-scala/src/main/scala/modules/MailModule.scala
@@ -2,17 +2,21 @@ package modules
 
 import actor.MailActor
 import akka.actor.{Actor, ActorRef, ActorSystem}
-import com.github.jurajburian.mailer.{Mailer, SessionFactory, SmtpAddress, SmtpStartTls}
+import com.github.jurajburian.mailer._
 import com.google.inject.name.{Named, Names}
 import com.google.inject.{Provides, Singleton}
 import com.typesafe.config.Config
 import core.guice.injection.GuiceActorRefProvider
+import models.MailPayload
 import net.codingwell.scalaguice.ScalaModule
+import services.MailService
+import services.ethereal.EtherealMailService
 
 class MailModule extends ScalaModule with GuiceActorRefProvider {
 
   override def configure(): Unit = {
     bind[Actor].annotatedWith(Names.named(MailActor.name)).to[MailActor]
+    bind[MailService[Message, MailPayload]].to[EtherealMailService]
   }
 
   @Provides

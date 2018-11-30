932459dbf96ea3292bbef79afb035111767ac34e
Pavel Grudina <plgrudina@gmail.com>
2018-11-30 14:49:27 +0200

    Implement an abstraction for database tables initializing [Scala] (#936)
    Implement an abstraction for initializing tables in a database

--- modules/user/server-scala/src/main/scala/repositories/UserSchemaInitializer.scala
+++ modules/user/server-scala/src/main/scala/repositories/UserSchemaInitializer.scala
@@ -1,29 +1,14 @@
 package repositories
 
-import core.slick.{SchemaInitializer, SchemaUtil}
+import core.slick.SchemaInitializer
 import javax.inject.Inject
 import model.UserTable
 import model.UserTable.UserTable
 import slick.jdbc.SQLiteProfile.api._
-import slick.lifted.TableQuery
 
-import scala.concurrent.{ExecutionContext, Future}
+class UserSchemaInitializer @Inject()(database: Database) extends SchemaInitializer[UserTable] {
 
-class UserSchemaInitializer @Inject()(database: Database)
-                                     (implicit executionContext: ExecutionContext) extends SchemaInitializer
-  with SchemaUtil {
-
-  val users: TableQuery[UserTable] = TableQuery[UserTable]
-
-  override def create(): Future[Unit] = {
-    withTable(database, users, UserTable.name, _.isEmpty) {
-      DBIO.seq(users.schema.create)
-    }
-  }
-
-  override def drop(): Future[Unit] = {
-    withTable(database, users, UserTable.name, _.nonEmpty) {
-      DBIO.seq(users.schema.drop)
-    }
-  }
+  override val name: String = UserTable.name
+  override val table = TableQuery[UserTable]
+  override val db = database
 }

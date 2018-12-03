96319e35e4792ffa749156aa0ad8d33bff97a04f
Mikael Zulfigarov <mikaelzulfigarov@gmail.com>
2018-12-03 14:49:30 +0200

    Update stripe subscription schema initializer to use new schema initialization approach

--- modules/payments/server-scala/src/main/scala/repositories/StripeSubscriptionSchemaInitializer.scala
+++ modules/payments/server-scala/src/main/scala/repositories/StripeSubscriptionSchemaInitializer.scala
@@ -1,22 +1,13 @@
 package repositories
 
 import com.google.inject.Inject
-import core.slick.{SchemaInitializer, SchemaUtil}
+import core.slick.SchemaInitializer
 import models.StripeSubscription
 import slick.jdbc.SQLiteProfile.api._
 import slick.lifted.TableQuery
 
-import scala.concurrent.{ExecutionContext, Future}
-
-class StripeSubscriptionSchemaInitializer @Inject()(database: Database)
-                                                   (implicit executionContext: ExecutionContext) extends SchemaInitializer with SchemaUtil {
-  val stripeSubscriptions: TableQuery[StripeSubscription.Table] = TableQuery[StripeSubscription.Table]
-
-  override def create(): Future[Unit] = withTable(database, stripeSubscriptions, StripeSubscription.name, _.isEmpty) {
-    stripeSubscriptions.schema.create
-  }
-
-  override def drop(): Future[Unit] = withTable(database, stripeSubscriptions, StripeSubscription.name, _.nonEmpty) {
-    stripeSubscriptions.schema.drop
-  }
+class StripeSubscriptionSchemaInitializer @Inject()(database: Database) extends SchemaInitializer[StripeSubscription.Table] {
+  override val name: String = StripeSubscription.name
+  override val table = TableQuery[StripeSubscription.Table]
+  override val db = database
 }

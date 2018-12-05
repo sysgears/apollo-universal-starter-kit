d8e9f09b55c5cb0296ee1ab7e17051cdf56d8822
Oleh Tsivan <oleh.tsivan@sysgears.com>
2018-12-05 12:40:34 +0200

    Add an initial implementation for 'fold' function in 'ServerModule'

--- modules/core/server-scala/src/main/scala/common/shapes/ServerModule.scala
+++ modules/core/server-scala/src/main/scala/common/shapes/ServerModule.scala
@@ -1,3 +1,15 @@
 package common.shapes
 
-class ServerModule extends GraphQLShape with SlickSchemaShape
+class ServerModule(modules: Seq[ServerModule] = Seq.empty) extends GraphQLShape
+  with SlickSchemaShape
+  with AkkaRouteShape {
+
+  //TODO: implement generic fold function
+  def fold(): Unit = {
+    modules.foreach(m => this.queries ++= m.queries)
+    modules.foreach(m => this.mutations ++= m.mutations)
+    modules.foreach(m => this.subscriptions ++= m.subscriptions)
+    modules.foreach(m => this.slickSchemas ++= m.slickSchemas)
+    modules.foreach(m => this.routes ++= m.routes)
+  }
+}

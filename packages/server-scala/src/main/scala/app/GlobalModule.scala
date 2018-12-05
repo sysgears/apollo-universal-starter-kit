46f5c38c4c5f07a84ef77ddc7837356074d9b136
Konstantin Musienko <konstantin.musienko@sysgears.com>
2018-12-05 14:50:51 +0200

    Update 'GlobalModule' to inject the 'user' module

--- packages/server-scala/src/main/scala/app/GlobalModule.scala
+++ packages/server-scala/src/main/scala/app/GlobalModule.scala
@@ -6,4 +6,5 @@ import common.shapes.ServerModule
 @Singleton
 class GlobalModule @Inject()()(counter: CounterModule,
                                contact: ContactModule,
-                               upload: UploadModule) extends ServerModule(Seq(counter, contact, upload))
+                               upload: UploadModule,
+                               user: UserModule) extends ServerModule(Seq(counter, contact, upload, user))

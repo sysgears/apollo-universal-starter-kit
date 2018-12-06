13a049e3a796704e4cee701b6995514258efa86e
Serhii Reznychenko <serhii.reznychenko@sysgears.com>
2018-12-06 19:09:48 +0200

    Add Oauth2Module to provide different OAuth2 services

---
+++ modules/user/server-scala/src/main/scala/modules/Oauth2Module.scala
@@ -0,0 +1,24 @@
+package modules
+
+import com.github.scribejava.apis.GoogleApi20
+import com.github.scribejava.core.builder.ServiceBuilder
+import com.github.scribejava.core.oauth.OAuth20Service
+import com.google.inject.Provides
+import config.AuthConfig
+import javax.inject.Named
+import net.codingwell.scalaguice.ScalaModule
+
+import scala.util.Random
+
+class Oauth2Module extends ScalaModule {
+
+  @Provides
+  @Named("google")
+  def counterActor(authConfig: AuthConfig): OAuth20Service =
+    new ServiceBuilder(authConfig.google.clientId)
+      .apiSecret(authConfig.google.clientSecret)
+      .scope(authConfig.google.scope)
+      .state(new Random(authConfig.google.stateSeed).nextString(8))
+      .callback(authConfig.google.callback)
+      .build(GoogleApi20.instance())
+}

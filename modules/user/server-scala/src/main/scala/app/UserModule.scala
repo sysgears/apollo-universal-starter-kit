0e139e53908e935b462f73d607c8b5b706e40951
Oleh Tsivan <oleh.tsivan@sysgears.com>
2018-12-06 18:25:59 +0200

    Include tokens 'graphql' schema to module 'user'

--- modules/user/server-scala/src/main/scala/app/UserModule.scala
+++ modules/user/server-scala/src/main/scala/app/UserModule.scala
@@ -2,15 +2,16 @@ package app
 
 import com.google.inject.Inject
 import common.shapes.ServerModule
-import graphql.schema.UserSchema
+import graphql.schema.{TokenSchema, UserSchema}
 import repositories.UserSchemaInitializer
 
 import scala.collection.mutable.ListBuffer
 
 class UserModule @Inject()(userSchema: UserSchema,
+                           tokenSchema: TokenSchema,
                            userSchemaInitializer: UserSchemaInitializer) extends ServerModule {
 
   slickSchemas ++= ListBuffer(userSchemaInitializer)
 
-  mutations ++= userSchema.mutations
+  mutations ++= userSchema.mutations ++ tokenSchema.mutations
 }

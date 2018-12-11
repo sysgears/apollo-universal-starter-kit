64ae06fb7990b9e6573ac877690a0682abcfe540
Serhii Reznychenko <serhii.reznychenko@sysgears.com>
2018-12-11 14:44:33 +0200

    Add 'FacebookOauth2Response' model class

---
+++ modules/user/server-scala/src/main/scala/model/oauth/FacebookOauth2Response.scala
@@ -0,0 +1,11 @@
+package model.oauth
+
+import spray.json.{DefaultJsonProtocol, RootJsonFormat}
+
+case class FacebookOauth2Response(id: String,
+                                  email: String,
+                                  name: String)
+
+object FacebookOauth2Response extends DefaultJsonProtocol {
+  implicit val facebookOauthResponseFormat: RootJsonFormat[FacebookOauth2Response] = jsonFormat3(FacebookOauth2Response.apply)
+}

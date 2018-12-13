4b1a125659d2cd4d5747917ac10d5c5f6faf7401
Oleh Tsivan <oleh.tsivan@sysgears.com>
2018-12-13 16:36:29 +0200

    Update 'TestHelper' to initialize 'Guice' Injector for each modules.

--- modules/core/server-scala/src/test/scala/TestHelper.scala
+++ modules/core/server-scala/src/test/scala/TestHelper.scala
@@ -1,12 +1,15 @@
 import akka.http.scaladsl.server.Route
 import akka.http.scaladsl.testkit.ScalatestRouteTest
-import common.shapes.ServerModule
+import com.typesafe.config.ConfigFactory
 import common.graphql.schema.GraphQL
-import core.guice.Injecting
 import common.routes.graphql.{GraphQLRoute, HttpHandler, WebSocketHandler}
+import common.shapes.ServerModule
+import core.guice.injection.Injecting
 import modules.session.JWTSessionImpl
 import monix.execution.Scheduler
 import org.scalatest.{BeforeAndAfter, BeforeAndAfterAll, Matchers, WordSpec}
+
+import scala.collection.JavaConverters._
 
 trait TestHelper extends WordSpec
   with ScalatestRouteTest
@@ -14,6 +17,10 @@ trait TestHelper extends WordSpec
   with BeforeAndAfterAll
   with Matchers
   with Injecting {
+
+  private val config = ConfigFactory.load
+  private val loadPaths = config.getList("loadPaths").unwrapped.asScala.map(_.toString).toList
+  initialize(loadPaths)
 
   val endpoint: String = "/graphql"
   implicit val scheduler: Scheduler = inject[Scheduler]

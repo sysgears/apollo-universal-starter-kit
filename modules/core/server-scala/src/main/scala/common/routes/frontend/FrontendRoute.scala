bbcbe1c1e093ed49de7b1559abc1e856554d5a61
Oleh Tsivan <oleh.tsivan@sysgears.com>
2018-12-14 19:44:42 +0200

    Remove unused routes

--- modules/core/server-scala/src/main/scala/common/routes/frontend/FrontendRoute.scala
+++ modules/core/server-scala/src/main/scala/common/routes/frontend/FrontendRoute.scala
@@ -1,18 +1,12 @@
 package common.routes.frontend
 
-import akka.http.scaladsl.model.StatusCodes
 import akka.http.scaladsl.server.Directives._
 import akka.http.scaladsl.server.Route
 
 class FrontendRoute {
 
-  val routes: Route =
-    (get & pathEndOrSingleSlash & redirectToTrailingSlashIfMissing(StatusCodes.TemporaryRedirect)) {
-      getFromResource("web/frontend/index.html")
-    } ~
-      (path("public" / Segment) & get) { str =>
-        getFromResource(s"public/$str")
-      } ~ {
-      getFromResourceDirectory("web/frontend")
-    }
+  val routes: Route = (path("public" / Segment) & get) {
+    str =>
+      getFromResource(s"public/$str")
+  }
 }

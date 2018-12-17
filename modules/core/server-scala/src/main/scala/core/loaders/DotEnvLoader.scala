f80b33bdd0511fc749bf8992f92411f06bddb707
Mikael Zulfigarov <mikaelzulfigarov@gmail.com>
2018-12-17 18:50:09 +0200

    Update .env loader to parse the current dir

--- modules/core/server-scala/src/main/scala/core/loaders/DotEnvLoader.scala
+++ modules/core/server-scala/src/main/scala/core/loaders/DotEnvLoader.scala
@@ -4,11 +4,5 @@ import core.services.conf.dotenv._
 import scala.language.postfixOps
 
 object DotEnvLoader extends DotEnvParser with SysEnvExtenderImplicits {
-  def load(moduleDotEnvPaths: List[String]): Unit = sys.env extend {
-    val rootDotEnv: `.env` = parse(directory = "./")
-    val moduleDotEnvs: List[`.env`] = moduleDotEnvPaths.map(path => parse(directory = path))
-    val moduleDotEnvsConcatenated: `.env` = moduleDotEnvs.foldLeft(`.env` empty)(_ ++ _)
-
-    rootDotEnv ++ moduleDotEnvsConcatenated
-  }
+  def load: Unit = sys.env extend parse(/*Parses current dir by default*/)
 }

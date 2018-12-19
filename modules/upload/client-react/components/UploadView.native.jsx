8c114c97ebf46b4186d235e72c2d1bedff30f176
RomanPeskov <peskovrp@gmail.com>
2018-12-19 14:17:31 +0200

    Implement file uploading

--- modules/upload/client-react/components/UploadView.native.jsx
+++ modules/upload/client-react/components/UploadView.native.jsx
@@ -1,11 +1,25 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { StyleSheet, Text, View } from 'react-native';
+import path from 'path';
+import { DocumentPicker } from 'expo';
+import { ReactNativeFile } from 'apollo-upload-client';
+import * as mime from 'react-native-mime-types';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
-const UploadView = ({ t }) => {
+const UploadView = ({ t, handleUploadFiles }) => {
+  const uploadFile = async () => {
+    const { uri, name } = await DocumentPicker.getDocumentAsync({ copyToCacheDirectory: false });
+    const type = mime.contentType(path.extname(name));
+    if (type) {
+      const imageData = new ReactNativeFile({ uri, name, type });
+      await handleUploadFiles([imageData]);
+    }
+  };
+
   return (
     <View style={styles.container}>
       <View style={styles.element}>
+        <Button title="Upload" onPress={uploadFile} />
         <Text style={styles.box}>{t('nativeMock')}</Text>
       </View>
     </View>
@@ -13,7 +27,8 @@ const UploadView = ({ t }) => {
 };
 
 UploadView.propTypes = {
-  t: PropTypes.func
+  t: PropTypes.func,
+  handleUploadFiles: PropTypes.func
 };
 
 const styles = StyleSheet.create({

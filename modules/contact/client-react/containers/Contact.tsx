3a4d6255954f18a02d9d7feefebeb7cc1204544a
Konstantin Stepakh <kostyantin.stepakh@sysgears.com>
2018-12-19 13:36:46 +0200

    Update contact in new mutation style

--- modules/contact/client-react/containers/Contact.tsx
+++ modules/contact/client-react/containers/Contact.tsx
@@ -1,45 +1,32 @@
 import React from 'react';
-import { FetchResult, compose, graphql } from 'react-apollo';
-
+import { Mutation, MutationFn, FetchResult, compose } from 'react-apollo';
 import { formikMessageHandler, HandleError } from '@module/core-client-react';
 import { translate, TranslateFunction } from '@module/i18n-client-react';
 import ContactView from '../components/ContactView';
 import CONTACT from '../graphql/Contact.graphql';
 import { ContactForm } from '../types';
 
-interface ContactProps {
-  t: TranslateFunction;
-  handleError: HandleError;
-  contact: (values: ContactForm) => Promise<{ errors: Array<{ field: string; message: string }> }>;
-}
+class Contact extends React.Component<{ t: TranslateFunction; handleError: HandleError }> {
+  public onSubmit = (mutate: MutationFn) => async (values: ContactForm) => {
+    const { t, handleError } = this.props;
 
-class Contact extends React.Component<ContactProps> {
-  public onSubmit = async (values: ContactForm) => {
-    const { t, handleError, contact } = this.props;
-    await handleError(() => contact(values), t('serverError'));
+    const {
+      data: { contact }
+    } = (await mutate({ variables: { input: values } })) as FetchResult;
+
+    await handleError(async () => contact, t('serverError'));
   };
 
   public render() {
-    return <ContactView {...this.props} onSubmit={this.onSubmit} />;
+    return (
+      <Mutation mutation={CONTACT}>
+        {mutate => <ContactView {...this.props} onSubmit={this.onSubmit(mutate)} />}
+      </Mutation>
+    );
   }
 }
 
-const ContactWithApollo = compose(
+export default compose(
   translate('contact'),
-  formikMessageHandler,
-  graphql(CONTACT, {
-    props: ({ mutate }) => ({
-      contact: async (values: any) => {
-        try {
-          const {
-            data: { contact }
-          } = (await mutate({ variables: { input: values } })) as FetchResult;
-          return contact;
-        } catch (e) {
-          throw e;
-        }
-      }
-    })
-  })
+  formikMessageHandler
 )(Contact);
-export default ContactWithApollo;

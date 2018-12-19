bf3f58a72f75a666a31927d9c4aa58eb46a0c130
RomanPeskov <peskovrp@gmail.com>
2018-12-19 17:14:00 +0200

    Implement the display list of uploaded files

--- modules/upload/client-react/components/UploadView.native.jsx
+++ modules/upload/client-react/components/UploadView.native.jsx
@@ -4,9 +4,10 @@ import path from 'path';
 import { DocumentPicker } from 'expo';
 import { ReactNativeFile } from 'apollo-upload-client';
 import * as mime from 'react-native-mime-types';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { FontAwesome } from '@expo/vector-icons';
+import { StyleSheet, Text, View, Button, TouchableOpacity, FlatList } from 'react-native';
 
-const UploadView = ({ t, handleUploadFiles }) => {
+const UploadView = ({ t, handleUploadFiles, files, handleRemoveFile }) => {
   const uploadFile = async () => {
     const { uri, name } = await DocumentPicker.getDocumentAsync({ copyToCacheDirectory: false });
     const type = mime.contentType(path.extname(name));
@@ -16,35 +17,82 @@ const UploadView = ({ t, handleUploadFiles }) => {
     }
   };
 
-  return (
+  const renderFileInfo = ({ item: { id, name } }) => {
+    return (
+      <TouchableOpacity style={styles.fileWrapper}>
+        <Text style={styles.text}>{name}</Text>
+        <TouchableOpacity style={styles.iconWrapper} onPress={() => handleRemoveFile(id)}>
+          <FontAwesome name="trash" size={20} style={{ color: '#3B5998' }} />
+        </TouchableOpacity>
+      </TouchableOpacity>
+    );
+  };
+
+  renderFileInfo.propTypes = {
+    item: PropTypes.object
+  };
+
+  return files ? (
     <View style={styles.container}>
-      <View style={styles.element}>
-        <Button title="Upload" onPress={uploadFile} />
-        <Text style={styles.box}>{t('nativeMock')}</Text>
+      <View style={styles.btnContainer}>
+        <View style={styles.btn}>
+          <Button title={t('btnUpload')} onPress={uploadFile} />
+        </View>
       </View>
+      <FlatList data={files} style={styles.list} keyExtractor={item => `${item.id}`} renderItem={renderFileInfo} />
     </View>
-  );
+  ) : null;
 };
 
 UploadView.propTypes = {
   t: PropTypes.func,
-  handleUploadFiles: PropTypes.func
+  handleUploadFiles: PropTypes.func,
+  files: PropTypes.array,
+  handleRemoveFile: PropTypes.func
 };
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center'
   },
-  element: {
-    paddingTop: 30
+  btnContainer: {
+    width: '100%',
+    alignItems: 'center',
+    borderBottomWidth: 1,
+    borderBottomColor: '#000'
   },
-  box: {
-    textAlign: 'center',
-    marginLeft: 15,
-    marginRight: 15
+  btn: {
+    width: 200,
+    height: 50,
+    marginTop: 20
+  },
+  text: {
+    fontSize: 18
+  },
+  iconWrapper: {
+    backgroundColor: 'transparent',
+    width: 50,
+    height: 50,
+    alignItems: 'center',
+    justifyContent: 'center',
+    flexDirection: 'row'
+  },
+  fileWrapper: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    backgroundColor: '#fff',
+    borderBottomColor: '#000',
+    borderBottomWidth: 0.8,
+    height: 50,
+    paddingLeft: 7
+  },
+  list: {
+    marginTop: 5,
+    width: '100%'
   }
 });
 

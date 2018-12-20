3b9eeece2f221eceb5a3e064dcb23acdcdd37a20
Evgenij Olshanskij <gekanchiko@gmail.com>
2018-12-20 16:21:49 +0200

    Simplify entry CommandInvoker. Mock operation functions

--- tools/cli/commands/addModule.js
+++ tools/cli/commands/addModule.js
@@ -21,78 +21,81 @@ const {
  * @param location - The location for a new module [client|server|both].
  * @param finished - The flag about the end of the generating process.
  */
-function addModule(logger, templatesPath, moduleName, options, location, finished = true) {
-  logger.info(`Copying ${location} files…`);
+function addModule({ logger, templatesPath, moduleName, options, location, finished = true }) {
+  console.log(temp);
+  function temp() {
+    logger.info(`Copying ${location} files…`);
 
-  // create new module directory
-  const destinationPath = computeModulesPath(location, options, moduleName);
-  const newModule = shell.mkdir('-p', destinationPath);
+    // create new module directory
+    const destinationPath = computeModulesPath(location, options, moduleName);
+    const newModule = shell.mkdir('-p', destinationPath);
 
-  // continue only if directory does not jet exist
-  if (newModule.code !== 0) {
-    logger.error(chalk.red(`The ${moduleName} directory is already exists.`));
-    process.exit();
-  }
-  //copy and rename templates in destination directory
-  copyFiles(destinationPath, templatesPath, location);
-  renameFiles(destinationPath, moduleName);
+    // continue only if directory does not jet exist
+    if (newModule.code !== 0) {
+      logger.error(chalk.red(`The ${moduleName} directory is already exists.`));
+      process.exit();
+    }
+    //copy and rename templates in destination directory
+    copyFiles(destinationPath, templatesPath, location);
+    renameFiles(destinationPath, moduleName);
 
-  logger.info(chalk.green(`✔ The ${location} files have been copied!`));
+    logger.info(chalk.green(`✔ The ${location} files have been copied!`));
 
-  // get index file path
-  const modulesPath = computeModulesPath(location, options);
-  const indexFullFileName = fs.readdirSync(modulesPath).find(name => name.search(/index/) >= 0);
-  const indexPath = modulesPath + indexFullFileName;
-  let indexContent;
+    // get index file path
+    const modulesPath = computeModulesPath(location, options);
+    const indexFullFileName = fs.readdirSync(modulesPath).find(name => name.search(/index/) >= 0);
+    const indexPath = modulesPath + indexFullFileName;
+    let indexContent;
 
-  try {
-    // prepend import module
-    indexContent =
-      `import ${moduleName} from '${computeModulePackageName(location, options, moduleName)}';\n` +
-      fs.readFileSync(indexPath);
-  } catch (e) {
-    logger.error(chalk.red(`Failed to read ${indexPath} file`));
-    process.exit();
-  }
+    try {
+      // prepend import module
+      indexContent =
+        `import ${moduleName} from '${computeModulePackageName(location, options, moduleName)}';\n` +
+        fs.readFileSync(indexPath);
+    } catch (e) {
+      logger.error(chalk.red(`Failed to read ${indexPath} file`));
+      process.exit();
+    }
 
-  // extract application modules
-  const appModuleRegExp = /Module\(([^()]+)\)/g;
-  const [, appModules] = appModuleRegExp.exec(indexContent) || ['', ''];
+    // extract application modules
+    const appModuleRegExp = /Module\(([^()]+)\)/g;
+    const [, appModules] = appModuleRegExp.exec(indexContent) || ['', ''];
 
-  // add module to app module list
-  shell
-    .ShellString(indexContent.replace(RegExp(appModuleRegExp, 'g'), `Module(${moduleName}, ${appModules})`))
-    .to(indexPath);
-  runPrettier(indexPath);
-
-  if (!options.old) {
-    // get package content
-    const packagePath = computePackagePath(location);
-    const packageContent = `` + fs.readFileSync(packagePath);
-
-    // extract dependencies
-    const dependenciesRegExp = /"dependencies":\s\{([^()]+)\},\n\s+"devDependencies"/g;
-    const [, dependencies] = dependenciesRegExp.exec(packageContent) || ['', ''];
-
-    // insert package and sort
-    const dependenciesSorted = dependencies.split(',');
-    dependenciesSorted.push(`\n    "${computeModulePackageName(location, options, moduleName)}": "^1.0.0"`);
-    dependenciesSorted.sort();
-
-    // add module to package list
+    // add module to app module list
     shell
-      .ShellString(
-        packageContent.replace(
-          RegExp(dependenciesRegExp, 'g'),
-          `"dependencies": {${dependenciesSorted}},\n  "devDependencies"`
-        )
-      )
-      .to(packagePath);
+      .ShellString(indexContent.replace(RegExp(appModuleRegExp, 'g'), `Module(${moduleName}, ${appModules})`))
+      .to(indexPath);
+    runPrettier(indexPath);
 
-    addSymlink(location, moduleName);
-  }
-  if (finished) {
-    logger.info(chalk.green(`✔ Module for ${location} successfully created!`));
+    if (!options.old) {
+      // get package content
+      const packagePath = computePackagePath(location);
+      const packageContent = `` + fs.readFileSync(packagePath);
+
+      // extract dependencies
+      const dependenciesRegExp = /"dependencies":\s\{([^()]+)\},\n\s+"devDependencies"/g;
+      const [, dependencies] = dependenciesRegExp.exec(packageContent) || ['', ''];
+
+      // insert package and sort
+      const dependenciesSorted = dependencies.split(',');
+      dependenciesSorted.push(`\n    "${computeModulePackageName(location, options, moduleName)}": "^1.0.0"`);
+      dependenciesSorted.sort();
+
+      // add module to package list
+      shell
+        .ShellString(
+          packageContent.replace(
+            RegExp(dependenciesRegExp, 'g'),
+            `"dependencies": {${dependenciesSorted}},\n  "devDependencies"`
+          )
+        )
+        .to(packagePath);
+
+      addSymlink(location, moduleName);
+    }
+    if (finished) {
+      logger.info(chalk.green(`✔ Module for ${location} successfully created!`));
+    }
   }
 }
 
--- tools/cli/commands/deleteModule.js
+++ tools/cli/commands/deleteModule.js
@@ -18,81 +18,84 @@ const {
  * @param options - User defined options
  * @param location - The location for a new module [client|server|both].
  */
-function deleteModule(logger, moduleName, options, location) {
-  logger.info(`Deleting ${location} files…`);
-  const modulePath = computeModulesPath(location, options, moduleName);
+function deleteModule({ logger, moduleName, options, location }) {
+  console.log(temp);
+  function temp() {
+    logger.info(`Deleting ${location} files…`);
+    const modulePath = computeModulesPath(location, options, moduleName);
 
-  if (fs.existsSync(modulePath)) {
-    // remove module directory
-    shell.rm('-rf', modulePath);
+    if (fs.existsSync(modulePath)) {
+      // remove module directory
+      shell.rm('-rf', modulePath);
 
-    // in new module structure remove root dir if no submodules exist
-    if (!options.old) {
-      const rootModulePath = computeRootModulesPath(moduleName);
-      if (shell.ls(rootModulePath).length === 0) {
-        shell.rm('-rf', rootModulePath);
+      // in new module structure remove root dir if no submodules exist
+      if (!options.old) {
+        const rootModulePath = computeRootModulesPath(moduleName);
+        if (shell.ls(rootModulePath).length === 0) {
+          shell.rm('-rf', rootModulePath);
+        }
       }
-    }
-    const modulesPath = computeModulesPath(location, options);
+      const modulesPath = computeModulesPath(location, options);
 
-    // get index file path
-    const indexFullFileName = fs.readdirSync(modulesPath).find(name => name.search(/index/) >= 0);
-    const indexPath = modulesPath + indexFullFileName;
-    let indexContent;
+      // get index file path
+      const indexFullFileName = fs.readdirSync(modulesPath).find(name => name.search(/index/) >= 0);
+      const indexPath = modulesPath + indexFullFileName;
+      let indexContent;
 
-    try {
-      indexContent = fs.readFileSync(indexPath);
-    } catch (e) {
-      logger.error(chalk.red(`Failed to read ${indexPath} file`));
-      process.exit();
-    }
+      try {
+        indexContent = fs.readFileSync(indexPath);
+      } catch (e) {
+        logger.error(chalk.red(`Failed to read ${indexPath} file`));
+        process.exit();
+      }
 
-    // extract application modules
-    const appModuleRegExp = /Module\(([^()]+)\)/g;
-    const [, appModules] = appModuleRegExp.exec(indexContent) || ['', ''];
-    const appModulesWithoutDeleted = appModules.split(',').filter(appModule => appModule.trim() !== moduleName);
+      // extract application modules
+      const appModuleRegExp = /Module\(([^()]+)\)/g;
+      const [, appModules] = appModuleRegExp.exec(indexContent) || ['', ''];
+      const appModulesWithoutDeleted = appModules.split(',').filter(appModule => appModule.trim() !== moduleName);
 
-    const contentWithoutDeletedModule = indexContent
-      .toString()
-      // remove module from modules list
-      .replace(appModuleRegExp, `Module(${appModulesWithoutDeleted.toString().trim()})`)
-      // remove module import
-      .replace(
-        RegExp(`import ${moduleName} from '${computeModulePackageName(location, options, moduleName)}';\n`, 'g'),
-        ''
-      );
+      const contentWithoutDeletedModule = indexContent
+        .toString()
+        // remove module from modules list
+        .replace(appModuleRegExp, `Module(${appModulesWithoutDeleted.toString().trim()})`)
+        // remove module import
+        .replace(
+          RegExp(`import ${moduleName} from '${computeModulePackageName(location, options, moduleName)}';\n`, 'g'),
+          ''
+        );
 
-    fs.writeFileSync(indexPath, contentWithoutDeletedModule);
-    runPrettier(indexPath);
+      fs.writeFileSync(indexPath, contentWithoutDeletedModule);
+      runPrettier(indexPath);
 
-    if (!options.old) {
-      // get package content
-      const packagePath = computePackagePath(location);
-      const packageContent = `` + fs.readFileSync(packagePath);
+      if (!options.old) {
+        // get package content
+        const packagePath = computePackagePath(location);
+        const packageContent = `` + fs.readFileSync(packagePath);
 
-      // extract dependencies
-      const dependenciesRegExp = /"dependencies":\s\{([^()]+)\},\n\s+"devDependencies"/g;
-      const [, dependencies] = dependenciesRegExp.exec(packageContent) || ['', ''];
-      const dependenciesWithoutDeleted = dependencies
-        .split(',')
-        .filter(pkg => pkg.indexOf(computeModulePackageName(location, options, moduleName)) < 0);
+        // extract dependencies
+        const dependenciesRegExp = /"dependencies":\s\{([^()]+)\},\n\s+"devDependencies"/g;
+        const [, dependencies] = dependenciesRegExp.exec(packageContent) || ['', ''];
+        const dependenciesWithoutDeleted = dependencies
+          .split(',')
+          .filter(pkg => pkg.indexOf(computeModulePackageName(location, options, moduleName)) < 0);
 
-      // remove module from package list
-      shell
-        .ShellString(
-          packageContent.replace(
-            RegExp(dependenciesRegExp, 'g'),
-            `"dependencies": {${dependenciesWithoutDeleted}},\n  "devDependencies"`
+        // remove module from package list
+        shell
+          .ShellString(
+            packageContent.replace(
+              RegExp(dependenciesRegExp, 'g'),
+              `"dependencies": {${dependenciesWithoutDeleted}},\n  "devDependencies"`
+            )
           )
-        )
-        .to(packagePath);
+          .to(packagePath);
 
-      removeSymlink(location, moduleName);
+        removeSymlink(location, moduleName);
+      }
+
+      logger.info(chalk.green(`✔ Module for ${location} successfully deleted!`));
+    } else {
+      logger.info(chalk.red(`✘ Module ${location} location for ${modulePath} not found!`));
     }
-
-    logger.info(chalk.green(`✔ Module for ${location} successfully deleted!`));
-  } else {
-    logger.info(chalk.red(`✘ Module ${location} location for ${modulePath} not found!`));
   }
 }
 

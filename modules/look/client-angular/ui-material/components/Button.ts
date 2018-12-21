eb4827b6ee20ea1af19de5c7cc9ba9dbb3d08dbe
Aleksey Zadorozhniy <zadorozhniyal@gmail.com>
2018-12-21 16:05:24 +0200

    Add button component

---
+++ modules/look/client-angular/ui-material/components/Button.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'btn',
+  template: `<button mat-raised-button color="primary">Primary</button>`,
+  styles: []
+})
+export class ButtonComponent {}

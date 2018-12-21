533590b9bc1b6d6a747c7105c723512154e7daf9
Serhii Reznychenko <serhii.reznychenko@sysgears.com>
2018-12-21 19:24:52 +0200

    Fix indents in GoogleAuth class

--- modules/authentication/server-scala/src/main/scala/model/google/GoogleAuth.scala
+++ modules/authentication/server-scala/src/main/scala/model/google/GoogleAuth.scala
@@ -7,5 +7,5 @@ case class GoogleAuth(id: Option[String],
                       displayName: String,
                       userId: Int) extends Entity[GoogleAuth, String] {
 
-                        override def withId(id: String): GoogleAuth = this.copy(id = Some(id))
-                      }
+  override def withId(id: String): GoogleAuth = this.copy(id = Some(id))
+}

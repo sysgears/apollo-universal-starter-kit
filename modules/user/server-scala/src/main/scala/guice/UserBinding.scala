fffac6189f8569c35375c92d47e2636558e0bedc
Serhii Reznychenko <serhii.reznychenko@sysgears.com>
2018-12-21 16:25:39 +0200

    Remove 'UserRepository' interface, instead use implementation with custom functions

--- modules/user/server-scala/src/main/scala/guice/UserBinding.scala
+++ modules/user/server-scala/src/main/scala/guice/UserBinding.scala
@@ -7,12 +7,10 @@ import core.guice.injection.GuiceActorRefProvider
 import graphql.resolvers._
 import javax.inject.Named
 import net.codingwell.scalaguice.ScalaModule
-import repositories.{UserRepository, UserRepositoryImpl}
 
 class UserBinding extends ScalaModule with GuiceActorRefProvider {
 
   override def configure() = {
-    bind[UserRepository].to[UserRepositoryImpl]
     bind[Actor].annotatedWith(Names.named(UserResolver.name)).to[UserResolver]
   }
 

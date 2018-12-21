1194e6009a754fb853fe5a791a4456fdeb657731
Serhii Reznychenko <serhii.reznychenko@sysgears.com>
2018-12-21 17:23:32 +0200

    Add 'AuthenticationBinding' to the configure function of the 'GlobalBinding' class

--- packages/server-scala/src/main/scala/guice/GlobalBinding.scala
+++ packages/server-scala/src/main/scala/guice/GlobalBinding.scala
@@ -14,5 +14,6 @@ class GlobalBinding extends ScalaModule {
     install(new FileBinding)
     install(new SangriaBinding)
     install(new ServerModulesBinding)
+    install(new AuthenticationBinding)
   }
 }

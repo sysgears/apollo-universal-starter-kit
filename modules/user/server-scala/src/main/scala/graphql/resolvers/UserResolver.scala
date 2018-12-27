a93ee043914678e0968d1f5d713e8c27f8d148fd
Serhii Reznychenko <serhii.reznychenko@sysgears.com>
2018-12-27 19:14:59 +0200

    Implement 'addUser' function in the UserResolver class

--- modules/user/server-scala/src/main/scala/graphql/resolvers/UserResolver.scala
+++ modules/user/server-scala/src/main/scala/graphql/resolvers/UserResolver.scala
@@ -1,15 +1,32 @@
 package graphql.resolvers
 
-import akka.actor.{Actor, ActorLogging}
-import common.ActorNamed
+import com.google.inject.Inject
+import model.{AddUserInput, User, UserPayload, UserProfile}
+import org.mindrot.jbcrypt.BCrypt
+import repositories.{UserProfileRepository, UserRepository}
+import common.implicits.RichDBIO._
 
-object UserResolver extends ActorNamed {
-  final val name = "UserResolver"
-}
+import scala.concurrent.{ExecutionContext, Future}
 
-class UserResolver extends Actor with ActorLogging {
+class UserResolver @Inject()(userRepository: UserRepository,
+                             userProfileRepository: UserProfileRepository)
+                            (implicit executionContext: ExecutionContext) {
 
-  override def receive: Receive = {
-    case unknownMessage@_ => log.warning(s"Received unknown message: $unknownMessage")
-  }
+  def addUser(input: AddUserInput): Future[UserPayload] = for {
+    user <- userRepository.save(User(
+      username = input.username,
+      email = input.email,
+      password = BCrypt.hashpw(input.password, BCrypt.gensalt),
+      role = input.role,
+      isActive = input.isActive.getOrElse(false)
+    )).run
+    _ <- input.profile.map(
+      profile =>
+        userProfileRepository.save(UserProfile(
+          firstName = profile.firstName,
+          lastName = profile.lastName,
+          fullName = None
+        ).withId(user.id.get)).run
+    ).getOrElse(Future.successful())
+  } yield UserPayload(user = Some(user))
 }

990161a85e8fcb6f98af7c12ab1b8120fed39c69
Serhii Reznychenko <serhii.reznychenko@sysgears.com>
2018-12-27 16:08:39 +0200

    Implement 'addUser' mutation in the user module

--- modules/user/server-scala/src/main/scala/graphql/schema/UserSchema.scala
+++ modules/user/server-scala/src/main/scala/graphql/schema/UserSchema.scala
@@ -5,10 +5,11 @@ import akka.stream.ActorMaterializer
 import common.graphql.UserContext
 import common.{InputUnmarshallerGenerator, Logger}
 import javax.inject.Inject
-import model.{User, UserProfile}
+import model._
 import repositories.UserProfileRepository
-import sangria.schema.{Field, ObjectType, OptionType}
+import sangria.schema.{Argument, Field, InputObjectType, ObjectType, OptionType}
 import sangria.macros.derive._
+import sangria.marshalling.FromInput
 
 import scala.concurrent.ExecutionContext
 
@@ -18,9 +19,9 @@ class UserSchema @Inject()(userProfileRepository: UserProfileRepository)
                            executionContext: ExecutionContext) extends InputUnmarshallerGenerator
   with Logger {
 
-  val userProfile: ObjectType[UserContext, UserProfile] = deriveObjectType(ObjectTypeName("UserProfile"))
+  implicit val userProfile: ObjectType[UserContext, UserProfile] = deriveObjectType(ObjectTypeName("UserProfile"))
 
-  val user: ObjectType[UserContext, User] = deriveObjectType(
+  implicit val user: ObjectType[UserContext, User] = deriveObjectType(
     ObjectTypeName("User"),
     ExcludeFields("password"),
     AddFields(
@@ -31,6 +32,23 @@ class UserSchema @Inject()(userProfileRepository: UserProfileRepository)
     )
   )
 
+  val userPayload: ObjectType[UserContext, UserPayload] = deriveObjectType(ObjectTypeName("UserPayload"))
+
+  implicit val profileInput: InputObjectType[ProfileInput] = deriveInputObjectType(InputObjectTypeName("ProfileInput"))
+  implicit val addUserInput: InputObjectType[AddUserInput] = deriveInputObjectType(InputObjectTypeName("AddUserInput"))
+
+  implicit val addUserInputUnmarshaller: FromInput[AddUserInput] = inputUnmarshaller {
+    input =>
+      AddUserInput(
+        username = input("username").asInstanceOf[String],
+        email = input("email").asInstanceOf[String],
+        role = input("role").asInstanceOf[String],
+        password = input("password").asInstanceOf[String],
+        isActive = input.get("isActive").flatMap(_.asInstanceOf[Option[Boolean]]),
+        profile = input.get("profile").flatMap(_.asInstanceOf[Option[ProfileInput]])
+      )
+  }
+
   def queries: List[Field[UserContext, Unit]] = List(
     //TODO implement stub's functionality
     Field(
@@ -38,6 +56,16 @@ class UserSchema @Inject()(userProfileRepository: UserProfileRepository)
       fieldType = OptionType(user),
       arguments = List.empty,
       resolve = _ => None
+    )
+  )
+
+  def mutations: List[Field[UserContext, Unit]] = List(
+    //TODO implement stub's functionality
+    Field(
+      name = "addUser",
+      fieldType = userPayload,
+      arguments = List(Argument("input", addUserInput)),
+      resolve = sc => UserPayload(Some(User(Some(1), "testusername", "testmail", "testpass", "user", true)))
     )
   )
 }

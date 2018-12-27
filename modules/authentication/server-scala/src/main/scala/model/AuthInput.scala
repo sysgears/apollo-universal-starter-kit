7eeb83c969673277f123a4820f6e5cb4706ef438
Serhii Reznychenko <serhii.reznychenko@sysgears.com>
2018-12-27 16:06:19 +0200

    Add 'AuthInput' model class

---
+++ modules/authentication/server-scala/src/main/scala/model/AuthInput.scala
@@ -0,0 +1,7 @@
+package model
+
+case class AuthInput(certificate: Option[AuthCertificateInput],
+                     facebook: Option[AuthFacebookInput],
+                     google: Option[AuthGoogleInput],
+                     github: Option[AuthGitHubInput],
+                     linkedin: Option[AuthLinkedInInput])

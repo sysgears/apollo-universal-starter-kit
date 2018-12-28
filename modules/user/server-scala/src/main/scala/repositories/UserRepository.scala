686faeee03274e156db11603e01b8b31e5cc510c
Serhii Reznychenko <serhii.reznychenko@sysgears.com>
2018-12-29 00:31:39 +0200

    Add a new 'findAll' function to the UserRepository class

--- modules/user/server-scala/src/main/scala/repositories/UserRepository.scala
+++ modules/user/server-scala/src/main/scala/repositories/UserRepository.scala
@@ -7,6 +7,8 @@ import model.UserTable.UserTable
 import slick.ast.BaseTypedType
 import slick.jdbc.JdbcProfile
 import slick.jdbc.JdbcBackend.Database
+import slick.lifted.ColumnOrdered
+import slick.ast.Ordering
 
 import scala.concurrent.ExecutionContext
 
@@ -18,6 +20,35 @@ class UserRepository @Inject()(override val driver: JdbcProfile, db: Database)
   val pkType = implicitly[BaseTypedType[Int]]
   val tableQuery = TableQuery[UserTable]
   type TableType = UserTable
+
+  def findAll(usernameOrEmailFilter: Option[String],
+              roleFilter: Option[String],
+              isActiveFilter: Option[Boolean],
+              orderingColumn: Option[String],
+              orderDirection: Option[String]): DBIO[Seq[User]] = {
+    val filteringQuery = tableQuery.filter(
+      user =>
+        usernameOrEmailFilter.map(usernameOrEmail =>
+          user.email === usernameOrEmail || user.username === usernameOrEmail).getOrElse(LiteralColumn(true)) &&
+          roleFilter.map(role => user.role === role).getOrElse(LiteralColumn(true)) &&
+          isActiveFilter.map(isActive => user.isActive === isActive).getOrElse(LiteralColumn(true))
+    )
+    val sortingQuery = for {
+      ordering <- orderDirection.map {
+        case "asc" => Ordering(Ordering.Asc)
+        case "desc" => Ordering(Ordering.Desc)
+      }
+      sortedResult <- orderingColumn.map {
+        case "id" => filteringQuery.sortBy(user => ColumnOrdered(user.id, ordering)).result
+        case "username" => filteringQuery.sortBy(user => ColumnOrdered(user.username, ordering)).result
+        case "role" => filteringQuery.sortBy(user => ColumnOrdered(user.role, ordering)).result
+        case "isActive" => filteringQuery.sortBy(user => ColumnOrdered(user.isActive, ordering)).result
+        case "email" => filteringQuery.sortBy(user => ColumnOrdered(user.email, ordering)).result
+      }
+    } yield sortedResult
+
+    sortingQuery.getOrElse(filteringQuery.result)
+  }
 
   def findByUsernameOrEmail(usernameOrEmail: String): DBIO[Option[User]] =
     tableQuery.filter(user => user.email === usernameOrEmail || user.username === usernameOrEmail).result.headOption

e90670690acc1b756ff7ecba3f417c58eda033c3
Evgenij Olshanskij <gekanchiko@gmail.com>
2018-12-28 15:06:01 +0200

    Fix tests

--- packages/client/src/__tests__/index.spec.ts
+++ packages/client/src/__tests__/index.spec.ts
@@ -1,5 +1,5 @@
 import { initRenderer } from '@module/testing-client-react';
-import rootSchema from '@module/core-server-ts/api/rootSchema.graphql';
+import rootSchema from '@module/core-server-ts/graphql/rootSchema.graphql';
 import clientModules from '..';
 import serverModules from '../../../server/src/modules';
 

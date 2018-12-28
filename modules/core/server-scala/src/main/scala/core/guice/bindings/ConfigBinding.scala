17b1b4c3970feb9429e72ee7311df01770a29d2c
Oleh Tsivan <oleh.tsivan@sysgears.com>
2018-12-28 18:44:44 +0200

    Make config and database configurations as a singletons

--- modules/core/server-scala/src/main/scala/core/guice/bindings/ConfigBinding.scala
+++ modules/core/server-scala/src/main/scala/core/guice/bindings/ConfigBinding.scala
@@ -1,11 +1,14 @@
 package core.guice.bindings
 
+import com.google.inject.{Provides, Singleton}
 import com.typesafe.config.{Config, ConfigFactory}
 import net.codingwell.scalaguice.ScalaModule
 
 class ConfigBinding extends ScalaModule {
 
-  override def configure() {
-    bind[Config].toInstance(ConfigFactory.load)
+  @Provides
+  @Singleton
+  def config: Config = {
+    ConfigFactory.load()
   }
 }

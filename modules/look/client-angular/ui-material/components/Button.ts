850b88da67f736152ea31ad9421b21959e783e7f
Aleksey Zadorozhniy <zadorozhniyal@gmail.com>
2019-01-02 11:26:29 +0200

    Add ng-content to button for content projection

--- modules/look/client-angular/ui-material/components/Button.ts
+++ modules/look/client-angular/ui-material/components/Button.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'btn',
-  template: `<button mat-raised-button color="primary">Primary</button>`,
+  template: `<button mat-raised-button color="primary"><ng-content></ng-content></button>`,
   styles: []
 })
 export class ButtonComponent {}

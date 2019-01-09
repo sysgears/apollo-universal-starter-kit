8b318d652966afb33f69b82b9f70a8ddab7044bd
Oleksandr Bazylevskyi <bazylevskyioleksandr@gmail.com>
2019-01-09 17:06:44 +0200

    Move social configs to auth config file

--- config/auth.js
+++ config/auth.js
@@ -15,6 +15,28 @@ export default {
       clientSecret: '7f7374215bb85f37d705c316affa9a2d90ee0407',
       callbackURL: '/auth/github/callback',
       scope: ['user:email']
+    },
+    facebook: {
+      enabled: false,
+      clientID: process.env.FACEBOOK_CLIENTID,
+      clientSecret: process.env.FACEBOOK_CLIENTSECRET,
+      callbackURL: '/auth/facebook/callback',
+      scope: ['email'],
+      profileFields: ['id', 'emails', 'displayName']
+    },
+    linkedin: {
+      enabled: true,
+      clientID: '77vwua4llmj10t',
+      clientSecret: 'bG5cPSzvOmqNLNbH',
+      callbackURL: '/auth/linkedin/callback',
+      scope: ['r_emailaddress', 'r_basicprofile']
+    },
+    google: {
+      enabled: true,
+      clientID: '936744536604-phfojlvtniin07oukj07b33qju8edjdn.apps.googleusercontent.com',
+      clientSecret: 'rq_IhawCIRqWezn4Y_QDah0Z',
+      callbackURL: '/auth/google/callback',
+      scope: ['https://www.googleapis.com/auth/userinfo.email', 'https://www.googleapis.com/auth/userinfo.profile']
     }
   }
 };
--- config/user.js
+++ config/user.js
@@ -11,28 +11,6 @@ export default {
     certificate: {
       devSerial: CERTIFICATE_DEVSERIAL,
       enabled: false
-    },
-    facebook: {
-      enabled: false,
-      clientID: process.env.FACEBOOK_CLIENTID,
-      clientSecret: process.env.FACEBOOK_CLIENTSECRET,
-      callbackURL: '/auth/facebook/callback',
-      scope: ['email'],
-      profileFields: ['id', 'emails', 'displayName']
-    },
-    linkedin: {
-      enabled: false,
-      clientID: process.env.LINKEDIN_CLIENTID,
-      clientSecret: process.env.LINKEDIN_CLIENTSECRET,
-      callbackURL: '/auth/linkedin/callback',
-      scope: ['r_emailaddress', 'r_basicprofile']
-    },
-    google: {
-      enabled: false,
-      clientID: process.env.GOOGLE_CLIENTID,
-      clientSecret: process.env.GOOGLE_CLIENTSECRET,
-      callbackURL: '/auth/google/callback',
-      scope: ['https://www.googleapis.com/auth/userinfo.email', 'https://www.googleapis.com/auth/userinfo.profile']
     }
   }
 };

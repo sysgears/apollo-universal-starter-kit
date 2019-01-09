810eb54b903a1c1a792b6112c2e2b493acb53c78
Serhii Reznychenko <serhii.reznychenko@sysgears.com>
2019-01-09 17:21:24 +0200

    Add default 'None' values for optional fields into the UserProfile class

--- modules/user/server-scala/src/main/scala/model/UserProfile.scala
+++ modules/user/server-scala/src/main/scala/model/UserProfile.scala
@@ -4,8 +4,8 @@ import akka.japi.Option.Some
 import com.byteslounge.slickrepo.meta.Entity
 
 case class UserProfile(id: Option[Int] = None,
-                       firstName: Option[String],
-                       lastName: Option[String],
-                       fullName: Option[String]) extends Entity[UserProfile, Int] {
+                       firstName: Option[String] = None,
+                       lastName: Option[String] = None,
+                       fullName: Option[String] = None) extends Entity[UserProfile, Int] {
   override def withId(id: Int): UserProfile = this.copy(id = Some(id))
 }

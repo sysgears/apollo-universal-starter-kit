36bf21c749483d0b627652a5ff8559a68879ed0d
Alexandr Belokur <alexandr.belokur@sysgears.com>
2019-01-10 16:15:07 +0200

    add generate pdf on server and parse pdf data on client

--- modules/reports/client-react/containers/ServerExportPDF.jsx
+++ modules/reports/client-react/containers/ServerExportPDF.jsx
@@ -1,14 +1,15 @@
-import React, { Component, Fragment } from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
-// import { removeTypename } from '@module/core-common';
 import { translate } from '@module/i18n-client-react';
 import { Query } from 'react-apollo';
-// import { Button } from '@module/look-client-react';
-// import WithExportPDF from './WithExportPDF';
-// import ServerReportDemo from '../components/ServerReportDemo';
-// import ReportsQuery from '../graphql/ReportsQuery.graphql';
 import getReport from '../graphql/getReport.graphql';
+
+function getObjectURLFromArray(array) {
+  const buffer = new window.Uint8Array(array);
+  const blob = new window.Blob([buffer], { type: 'application/pdf' });
+  return window.URL.createObjectURL(blob);
+}
 
 @translate('reports')
 class ServerExportPDF extends Component {
@@ -17,53 +18,17 @@ class ServerExportPDF extends Component {
   };
 
   render() {
-    // const { t } = this.props;
-    // const button = <Button>{t('btn')}</Button>;
+    const { t } = this.props;
     return (
       <Query query={getReport}>
         {({ loading, data: { getReport } }) => {
           if (loading) return null;
-          console.log(getReport);
-          // const b64toBlob = (b64Data, contentType = '', sliceSize = 512) => {
-          //   const byteCharacters = atob(b64Data);
-          //
-          //   const byteArrays = [];
-          //
-          //   for (let offset = 0; offset < byteCharacters.length; offset += sliceSize) {
-          //     const slice = byteCharacters.slice(offset, offset + sliceSize),
-          //       byteNumbers = new Array(slice.length);
-          //     for (let i = 0; i < slice.length; i++) {
-          //       byteNumbers[i] = slice.charCodeAt(i);
-          //     }
-          //     const byteArray = new Uint8Array(byteNumbers);
-          //
-          //     byteArrays.push(byteArray);
-          //   }
-          //
-          //   return new Blob(byteArrays, { type: contentType });
-          // };
-          //
-          // const blob = b64toBlob(getReport, 'application/pdf');
-          // console.log(blob);
-          //
-          // const myReader = new FileReader();
-          // myReader.addEventListener('loadend', function(e) {
-          //   // console.log( e.srcElement.result)
-          //   document.getElementById('Iframe').innerHTML = e.srcElement.result;
-          // });
-          // myReader.readAsText(blob);
+          const pdfURL = getObjectURLFromArray(getReport.data);
 
-          // const iframeEle = document.getElementById("Iframe");
-          // if (iframeEle) {
-          //   iframeEle.contentWindow.print();
-          // }
           return (
-            <Fragment>
-              <div id="Iframe" />
-              {/*<WithExportPDF button={button} visibly={false}>*/}
-              {/*<ServerReportDemo reports={reports} />*/}
-              {/*</WithExportPDF>*/}
-            </Fragment>
+            <a href={pdfURL} target="_blank" rel="noopener noreferrer">
+              {t('btn')}
+            </a>
           );
         }}
       </Query>
--- modules/reports/server-ts/resolvers.js
+++ modules/reports/server-ts/resolvers.js
@@ -1,3 +1,8 @@
+import pdfMake from 'pdfmake/build/pdfmake';
+import pdfFonts from 'pdfmake/build/vfs_fonts';
+
+pdfMake.vfs = pdfFonts.pdfMake.vfs;
+
 export default () => ({
   Query: {
     report(obj, { id }, { Report }) {
@@ -7,50 +12,19 @@ export default () => ({
       return await Report.reports();
     },
     async getReport() {
-      // const path = require('path');
-
-      // const fonts = {
-      //   Roboto: {
-      //     normal: path.join(__dirname, '/fonts/Roboto-Regular.ttf'),
-      //     bold: path.join(__dirname, '/fonts/Roboto-Bold.ttf'),
-      //     italics: path.join(__dirname, '/fonts/Roboto-Italic.ttf')
-      //   }
-      // };
-
-      const pdfMake = require('pdfmake/build/pdfmake.js');
-      const pdfFonts = require('pdfmake/build/vfs_fonts.js');
-      pdfMake.vfs = pdfFonts.pdfMake.vfs;
-
       const docDefinition = {
         content: [
           'First paragraph',
           'Another paragraph, this time a little bit longer to make sure, this line will be divided into at least two lines'
         ]
       };
-
       const pdfDocGenerator = pdfMake.createPdf(docDefinition);
-      // const bufferTest = await pdfDocGenerator.getBuffer();
 
-      const blobTest = await new Promise(res => {
-        pdfDocGenerator.getBlob(blob => {
-          console.log('blob', blob);
-          res(blob);
+      return await new Promise(res => {
+        pdfDocGenerator.getBuffer(buffer => {
+          res(buffer);
         });
       });
-      console.log('blobTest', blobTest);
-      return blobTest;
-      // return await pdfDocGenerator.getBuffer(async (buffer) => {
-      //   console.log('buffer', buffer);
-      //   return buffer
-      // });
-
-      // console.log('bufferTest', bufferTest);
-      // return bufferTest;
-
-      // const pdfDoc = printer.createPdfKitDocument(docDefinition);
-      // console.log('pdfDoc', pdfDoc)
-      // // pdfDoc.pipe(fs.createWriteStream('document.pdf'));
-      // pdfDoc.end();
     }
   },
   Mutation: {},

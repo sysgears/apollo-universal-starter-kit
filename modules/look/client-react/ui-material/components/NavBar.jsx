2070787aecce82741e2becb02547db831bc5da06
Philip Balo <f.g.balo@gmail.com>
2019-01-14 17:24:03 +0200

    Add NavBar

--- modules/look/client-react/ui-material/components/NavBar.jsx
+++ modules/look/client-react/ui-material/components/NavBar.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 // import { NavLink } from 'react-router-dom';
-import {
-  Container,
-  Navbar
-  // Nav,
-  // NavItem
-} from 'reactstrap';
+import Grid from '@material-ui/core/Grid';
+// import {
+//   Container,
+//   Navbar,
+//   Nav,
+//   NavItem
+// } from 'reactstrap';
 
 // import settings from '../../../../../settings';
 
@@ -14,27 +15,28 @@ const ref = { modules: null };
 export const onAppCreate = modules => (ref.modules = modules);
 
 const NavBar = () => (
-  <Navbar color="faded" light>
-    <Container>
-      {/* <Nav>
-        <NavLink to="/" className="navbar-brand">
-          {settings.app.name}
-        </NavLink>
-        {ref.modules.navItems}
-      </Nav>
+  <Grid container>{ref.modules.navItems}</Grid>
+  // <Navbar color="faded" light>
+  //   <Container>
+  //     <Nav>
+  //       <NavLink to="/" className="navbar-brand">
+  //         {settings.app.name}
+  //       </NavLink>
+  //       {ref.modules.navItems}
+  //     </Nav>
 
-      <Nav className="justify-content-end">
-        {ref.modules.navItemsRight}
-        {__DEV__ && (
-          <NavItem>
-            <a href="/graphiql" className="nav-link">
-              GraphiQL
-            </a>
-          </NavItem>
-        )}
-      </Nav> */}
-    </Container>
-  </Navbar>
+  //     <Nav className="justify-content-end">
+  //       {ref.modules.navItemsRight}
+  //       {__DEV__ && (
+  //         <NavItem>
+  //           <a href="/graphiql" className="nav-link">
+  //             GraphiQL
+  //           </a>
+  //         </NavItem>
+  //       )}
+  //     </Nav>
+  //   </Container>
+  // </Navbar>
 );
 
 export default NavBar;

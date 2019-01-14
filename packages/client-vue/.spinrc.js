827195a3af2e28c24203262a7eb60424a89c0ffc
Oleksandr Bazylevskyi <bazylevskyioleksandr@gmail.com>
2019-01-14 18:14:02 +0200

    Fix build errors

--- packages/client-vue/.spinrc.js
+++ packages/client-vue/.spinrc.js
@@ -1,5 +1,3 @@
-const url = require('url');
-
 const config = {
   builders: {
     web: {
@@ -11,7 +9,7 @@ const config = {
         __CLIENT__: true
       },
       // Wait for backend to start prior to letting webpack load frontend page
-      waitOn: [`tcp:${process.env.SERVER_HOST || 'localhost:8080'}`],
+      waitOn: ['tcp:localhost:8080'],
       enabled: true
     },
     test: {
@@ -23,15 +21,13 @@ const config = {
     }
   },
   options: {
-    stack: ['apollo', 'vue', 'vuex', 'styled-components', 'css', 'sass', 'less', 'es6', 'webpack', 'i18next'],
+    stack: ['apollo', 'css', 'sass', 'less', 'ts', 'webpack', 'i18next', 'vue'],
     cache: '../../.cache',
     ssr: false,
     webpackDll: true,
-    reactHotLoader: false,
     defines: {
       __DEV__: process.env.NODE_ENV !== 'production',
-      __API_URL__: '"/graphql"',
-      'process.env.STRIPE_PUBLIC_KEY': !!process.env.STRIPE_PUBLIC_KEY ? `"${process.env.STRIPE_PUBLIC_KEY}"` : undefined
+      __API_URL__: '"/graphql"'
     },
     webpackConfig: {
       devServer: {

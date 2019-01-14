dc1585dd0624869d2e85cea83ce515c91753b8cb
Alexandr Belokur <alexandr.belokur@sysgears.com>
2019-01-14 18:50:51 +0200

    Add function, that creates table in pdf from sql database

--- modules/reports/server-ts/pdfReport/generateBufferPDF.js
+++ modules/reports/server-ts/pdfReport/generateBufferPDF.js
@@ -1,7 +1,32 @@
 import PdfPrinter from 'pdfmake';
 import path from 'path';
 
-import docDefinition from './docDefinition';
+function createTable(array) {
+  const titles = Object.keys(array[0]);
+  const capitilizedTitles = titles.map(title => title[0].toUpperCase() + title.slice(1));
+  const columnAmount = titles.length;
+  const columnWidths = [...Array(columnAmount).keys()].map(i => (i === 0 ? 'auto' : '*'));
+  const docDefinition = {
+    content: [
+      {
+        table: {
+          widths: columnWidths,
+          body: []
+        }
+      }
+    ]
+  };
+  const tableBody = docDefinition.content[0].table.body;
+
+  tableBody.push(capitilizedTitles);
+
+  array.forEach(element => {
+    const values = Object.values(element);
+    tableBody.push(values);
+  });
+
+  return docDefinition;
+}
 
 const fonts = {
   Roboto: {
@@ -12,8 +37,9 @@ const fonts = {
   }
 };
 
-export default function generateBufferPDF() {
+export default function generateBufferPDF(reports) {
   const printer = new PdfPrinter(fonts);
+  const docDefinition = createTable(reports);
   const doc = printer.createPdfKitDocument(docDefinition);
   const chunks = [];
 

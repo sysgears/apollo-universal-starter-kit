236120739f34cf964b22643a577f5535b295b150
Oleksandr Samofan <samofan77@gmail.com>
2019-01-16 19:43:16 +0200

    Add read package.json and check dependencies functionality

--- tools/customRules/rules/recursiveDependenciesRule.ts
+++ tools/customRules/rules/recursiveDependenciesRule.ts
@@ -1,6 +1,11 @@
 /* tslint:disable */
 import * as ts from 'typescript';
 import * as Lint from 'tslint';
+import * as fs from "fs";
+import * as path from "path";
+
+import { findImports, ImportKind } from "tsutils";
+
 
 export class Rule extends Lint.Rules.AbstractRule {
   public static FAILURE_STRING = 'Cant find this dependency in the tree of dependencies';
@@ -22,24 +27,102 @@ export class Rule extends Lint.Rules.AbstractRule {
     );
   }
 }
+interface PackageJson {
+  dependencies?: Dependencies;
+  devDependencies?: Dependencies;
+  peerDependencies?: Dependencies;
+  optionalDependencies?: Dependencies;
+}
+
+interface Dependencies extends Object {
+  [name: string]: any;
+}
 
 // The walker takes care of all the work.
 class RecursiveDependenciesWalker extends Lint.RuleWalker {
-  public visitImportDeclaration(node: ts.ImportDeclaration) {
-      //let dependencies: Set<string> | undefined;
-      if (ts.isStringLiteral(node.moduleSpecifier) && node.moduleSpecifier.text && !ts.isExternalModuleNameRelative(node.moduleSpecifier.text)) {
-          const packageNameParts = node.moduleSpecifier.text.split(/\//g);
-          if (node.moduleSpecifier.text[0] === '@'){
-              //packageNameParts[0]
-          }
+
+  public walk(sourceFile: ts.SourceFile) {
+    let dependencies: Set<string> | undefined;
+    for (const name of findImports(sourceFile, ImportKind.All)) {
+      if (!ts.isExternalModuleNameRelative(name.text)) {
+        const packageNameParts = name.text.split(/\//g);
+        const moduleName = name.text[0] === '@' ? packageNameParts[0] : `${packageNameParts[0]}/${packageNameParts[1]}`;
+        if (dependencies === undefined) {
+          dependencies = this.getDependencies(sourceFile.fileName);
+        }
+        return dependencies.has(moduleName);
       }
-
-      // const {parent, ...rest} = node;
-      //console.log('import11', rest,  );
-
-    // call the base version of this visitor to actually parse this node
-    super.visitImportDeclaration(node);
+    }
   }
 
+
+  private getDependencies(fileName: string): Set<string> {
+    const result = new Set<string>();
+    const packageJsonPath = this.findPackageJson(path.resolve(path.dirname(fileName)));
+    if (packageJsonPath !== undefined) {
+      try {
+        // don't use require here to avoid caching
+        // remove BOM from file content before parsing
+        const content = JSON.parse(
+          fs.readFileSync(packageJsonPath, "utf8").replace(/^\uFEFF/, ""),
+        ) as PackageJson;
+        console.log('content', content);
+        if (content.dependencies !== undefined) {
+          this.addDependencies(result, content.dependencies);
+        }
+        if (content.peerDependencies !== undefined) {
+          this.addDependencies(result, content.peerDependencies);
+        }
+        if (content.devDependencies !== undefined) {
+          this.addDependencies(result, content.devDependencies);
+        }
+        if (content.optionalDependencies !== undefined) {
+          this.addDependencies(result, content.optionalDependencies);
+        }
+      } catch {
+        // treat malformed package.json files as empty
+      }
+
+      result.forEach((dependency) => {
+        console.log('dependency', dependency);
+        if (dependency.includes('@module')){
+            this.findRelatedModulePackageJson(packageJsonPath);
+        }
+      });
+
+    }
+
+    return result;
+  }
+
+  private findRelatedModulePackageJson(current: string): void {
+    const rootDir = path.dirname(path.dirname(current));
+    const dir = fs.readdirSync(rootDir)
+    console.log('dir', dir);
+  }
+
+  private addDependencies(result: Set<string>, dependencies: Dependencies) {
+    for (const name in dependencies) {
+      if (dependencies.hasOwnProperty(name)) {
+        result.add(name);
+      }
+    }
+  }
+
+  private findPackageJson(current: string): string | undefined {
+    let prev: string;
+    do {
+      const fileName = path.join(current, "package.json");
+      if (fs.existsSync(fileName)) {
+        return fileName;
+      }
+
+      prev = current;
+      current = path.dirname(current);
+      console.log('prev', prev);
+      console.log('current', current);
+    } while (prev !== current);
+    return undefined;
+  }
 
 }

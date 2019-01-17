651b73f8816514cea9227b5f522a414b70291db9
Alexandr Belokur <alexandr.belokur@sysgears.com>
2019-01-17 16:37:00 +0200

    Remove method for get single report in sql

--- modules/reports/server-ts/sql.js
+++ modules/reports/server-ts/sql.js
@@ -1,15 +1,7 @@
 import { knex } from '@module/database-server-ts';
 
 export default class Report {
-  report(id) {
-    return knex
-      .select('id', 'name', 'phone', 'email')
-      .from('report')
-      .where('id', '=', id)
-      .first();
-  }
-
-  reports() {
+  report() {
     return knex
       .select('id', 'name', 'phone', 'email')
       .from('report')

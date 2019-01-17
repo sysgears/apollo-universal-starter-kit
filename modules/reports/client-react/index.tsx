d237d0f9fb670bd48d0eb5c987a2adeb6b6c8db7
Alexandr Belokur <alexandr.belokur@sysgears.com>
2019-01-17 17:18:21 +0200

    Rename reports to report in client index

--- modules/reports/client-react/index.tsx
+++ modules/reports/client-react/index.tsx
@@ -4,21 +4,21 @@ import { translate, TranslateFunction } from '@module/i18n-client-react';
 import { MenuItem } from '@module/look-client-react';
 import ClientModule from '@module/module-client-react';
 
-import Reports from './containers/Reports';
+import Report from './containers/Report';
 import resources from './locales';
 
-const NavLinkWithI18n = translate('reports')(({ t }: { t: TranslateFunction }) => (
-  <NavLink to="/reports" className="nav-link" activeClassName="active">
+const NavLinkWithI18n = translate('report')(({ t }: { t: TranslateFunction }) => (
+  <NavLink to="/report" className="nav-link" activeClassName="active">
     {t('navLink')}
   </NavLink>
 ));
 
 export default new ClientModule({
-  route: [<Route exact path="/reports" component={Reports} />],
+  route: [<Route exact path="/report" component={Report} />],
   navItem: [
-    <MenuItem key="/reports">
+    <MenuItem key="/report">
       <NavLinkWithI18n />
     </MenuItem>
   ],
-  localization: [{ ns: 'reports', resources }]
+  localization: [{ ns: 'report', resources }]
 });

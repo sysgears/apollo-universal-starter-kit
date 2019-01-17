96e4cb2af46644a28bb0989622d543c9529bf053
Alexandr Belokur <alexandr.belokur@sysgears.com>
2019-01-17 11:20:28 +0200

    Clean up WithExportPDF container

--- modules/reports/client-react/containers/WithExportPDF.jsx
+++ modules/reports/client-react/containers/WithExportPDF.jsx
@@ -3,56 +3,19 @@ import ReactToPrint from 'react-to-print';
 
 import PropTypes from 'prop-types';
 
-const noVisibly = { position: 'fixed', right: -1000 };
-
-const left = { display: 'flex', justifyContent: 'flex-start' };
-const center = { display: 'flex', justifyContent: 'center' };
-const right = { display: 'flex', justifyContent: 'flex-end' };
-
 class ExportPDF extends Component {
   static propTypes = {
     children: PropTypes.node.isRequired,
-    button: PropTypes.element.isRequired,
-    visibly: PropTypes.bool,
-    positionButton: PropTypes.string
+    button: PropTypes.element.isRequired
   };
-
-  state = {
-    visibly: { display: 'block' },
-    positionButton: {}
-  };
-
-  componentDidMount() {
-    const { visibly = true, positionButton = 'left' } = this.props;
-
-    if (!visibly) {
-      this.setState({
-        visibly: noVisibly
-      });
-    }
-
-    switch (positionButton) {
-      case 'left':
-        return this.setState({ positionButton: left });
-      case 'center':
-        return this.setState({ positionButton: center });
-      case 'right':
-        return this.setState({ positionButton: right });
-    }
-  }
 
   render() {
     const { children, button } = this.props;
-    const { visibly, positionButton } = this.state;
 
     return (
       <Fragment>
-        <div style={visibly}>
-          <div ref={el => (this.ref = el)}>{children}</div>
-        </div>
-        <div style={positionButton}>
-          <ReactToPrint trigger={() => button} content={() => this.ref} />
-        </div>
+        <div ref={el => (this.ref = el)}>{children}</div>
+        <ReactToPrint trigger={() => button} content={() => this.ref} />
       </Fragment>
     );
   }

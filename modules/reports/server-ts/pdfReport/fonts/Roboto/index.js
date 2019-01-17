f1c7c968fed281a2cda8c0a324ea8435339cdf04
Alexandr Belokur <alexandr.belokur@sysgears.com>
2019-01-17 11:31:30 +0200

    Add separate folder for Robot font

---
+++ modules/reports/server-ts/pdfReport/fonts/Roboto/index.js
@@ -0,0 +1,12 @@
+import path from 'path';
+
+const fonts = {
+  Roboto: {
+    normal: path.join(__dirname, './Roboto-Regular.ttf'),
+    bold: path.join(__dirname, './Roboto-Medium.ttf'),
+    italics: path.join(__dirname, './Roboto-Italic.ttf'),
+    bolditalics: path.join(__dirname, './Roboto-MediumItalic.ttf')
+  }
+};
+
+export default fonts;

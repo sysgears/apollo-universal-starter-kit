804f307d05d24dcff74b448225e217c0ba9f8953
Philip Balo <f.g.balo@gmail.com>
2019-01-21 17:03:49 +0200

    Default config

--- config/user.js
+++ config/user.js
@@ -46,9 +46,9 @@ export default {
       scope: ['r_emailaddress', 'r_basicprofile']
     },
     google: {
-      enabled: true,
-      clientID: '936744536604-phfojlvtniin07oukj07b33qju8edjdn.apps.googleusercontent.com',
-      clientSecret: 'rq_IhawCIRqWezn4Y_QDah0Z',
+      enabled: false,
+      clientID: process.env.GOOGLE_CLIENTID,
+      clientSecret: process.env.GOOGLE_CLIENTSECRET,
       callbackURL: '/auth/google/callback',
       scope: ['https://www.googleapis.com/auth/userinfo.email', 'https://www.googleapis.com/auth/userinfo.profile']
     }

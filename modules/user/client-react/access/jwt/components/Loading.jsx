6cf53cd1613dc8e4ca5e7478406df24f3f63cdf3
Sergey Gilevich <kentserg9453@gmail.com>
2019-01-21 11:27:29 +0200

    Fix Loading component for fixing bug with JWT auth

--- modules/user/client-react/access/jwt/components/Loading.jsx
+++ modules/user/client-react/access/jwt/components/Loading.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import { LayoutCenter } from '@module/look-client-react';
 
-export default (
+const Loading = () => (
   <LayoutCenter>
     <div className="text-center">App is loading...</div>
   </LayoutCenter>
 );
+
+export default Loading;

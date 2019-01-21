1101a34f894557fd58625d5648f291cde355d2c6
Serhii Reznychenko <serhii.reznychenko@sysgears.com>
2019-01-21 17:44:45 +0200

    Move user's authentication functionality to the UserResolver class

--- modules/user/server-scala/src/main/scala/graphql/resolvers/UserResolver.scala
+++ modules/user/server-scala/src/main/scala/graphql/resolvers/UserResolver.scala
@@ -1,16 +1,38 @@
 package graphql.resolvers
 
-import com.google.inject.Inject
-import common.errors.NotFound
-import model._
-import org.mindrot.jbcrypt.BCrypt
-import repositories.{UserProfileRepository, UserRepository}
+import java.sql.SQLException
+
 import common.implicits.RichDBIO._
 import common.implicits.RichFuture._
+import common.implicits.RichTry._
+import com.github.jurajburian.mailer.Message
+import com.google.inject.Inject
+import common.FieldError
+import common.config.AppConfig
+import common.errors.{AlreadyExists, NotFound}
+import org.mindrot.jbcrypt.BCrypt
+import repositories.{UserProfileRepository, UserRepository}
+import config.{AuthConfig, MailConfig}
+import errors.Unauthenticated
+import jwt.model.{JwtContent, Tokens}
+import jwt.service.JwtAuthService
+import model._
+import model.auth.AuthPayload
+import models.MailPayload
+import services.{MailService, MessageTemplateService}
 
 import scala.concurrent.{ExecutionContext, Future}
 
-class UserResolver @Inject()(userRepository: UserRepository, userProfileRepository: UserProfileRepository)(
+class UserResolver @Inject()(
+    userRepository: UserRepository,
+    userProfileRepository: UserProfileRepository,
+    jwtAuthService: JwtAuthService[JwtContent],
+    mailService: MailService[Message, MailPayload],
+    messageTemplateService: MessageTemplateService,
+    mailConfig: MailConfig,
+    authConfig: AuthConfig,
+    appConfig: AppConfig
+)(
     implicit executionContext: ExecutionContext
 ) {
 
@@ -75,4 +97,112 @@ class UserResolver @Inject()(userRepository: UserRepository, userProfileReposito
   }
 
   def user(id: Int): Future[UserPayload] = userRepository.findOne(id).run.map(user => UserPayload(user))
+
+  def register(input: RegisterUserInput, skipConfirmation: Boolean): Future[UserPayload] = {
+    for {
+      createdUser <- userRepository
+        .save(
+          User(
+            username = input.username,
+            email = input.email,
+            role = "user",
+            isActive = skipConfirmation,
+            password = BCrypt.hashpw(input.password, BCrypt.gensalt)
+          )
+        )
+        .run
+      mailingResult <- if (!skipConfirmation) {
+        mailService.send(
+          messageTemplateService.createConfirmRegistrationMessage(
+            createdUser,
+            appConfig.name,
+            mailConfig.address,
+            appConfig.url + "/confirmation/" + jwtAuthService.createAccessToken(JwtContent(createdUser.id.get))
+          )
+        )
+      } else Future.successful(MailPayload())
+    } yield UserPayload(Some(createdUser), mailingResult.errors)
+  }.recover {
+    case e: SQLException =>
+      val usernameWarning =
+        if (e.getMessage.contains("column USERNAME is not unique"))
+          Some(FieldError("username", "Username already exists."))
+        else None
+      val emailWarning =
+        if (e.getMessage.contains("column EMAIL is not unique")) Some(FieldError("email", "E-mail already exists."))
+        else None
+      UserPayload(errors = Some(List(usernameWarning, emailWarning).flatten))
+  }
+
+  def resendConfirmationMesage(input: ResendConfirmationMessageInput): Future[UserPayload] =
+    for {
+      user <- userRepository.findByUsernameOrEmail(input.usernameOrEmail).run failOnNone NotFound(
+        s"User with username or email: [${input.usernameOrEmail}] not found."
+      )
+      _ <- if (!user.isActive) Future.successful()
+      else Future.failed(AlreadyExists(s"User with id: [${user.id}] is active"))
+      _ <- if (BCrypt.checkpw(input.password, user.password)) Future.successful()
+      else Future.failed(Unauthenticated())
+      accessToken = jwtAuthService.createAccessToken(JwtContent(user.id.get))
+      mailingResult <- mailService.send(
+        messageTemplateService.createConfirmRegistrationMessage(
+          user,
+          appConfig.name,
+          mailConfig.address,
+          appConfig.url + "/confirmation/" + accessToken
+        )
+      )
+    } yield UserPayload(Some(user), mailingResult.errors)
+
+  def login(input: LoginUserInput): Future[AuthPayload] = {
+    for {
+      user <- userRepository.findByUsernameOrEmail(input.usernameOrEmail).run failOnNone NotFound(
+        s"User with username or email: [${input.usernameOrEmail}] not found."
+      )
+      _ <- if (BCrypt.checkpw(input.password, user.password)) Future.successful()
+      else Future.failed(Unauthenticated())
+      accessToken = jwtAuthService.createAccessToken(JwtContent(user.id.get))
+      refreshToken = jwtAuthService.createRefreshToken(JwtContent(user.id.get), user.password)
+    } yield AuthPayload(Some(user), Some(Tokens(accessToken, refreshToken)))
+  }.recover {
+    case _: NotFound =>
+      AuthPayload(errors = Some(FieldError("usernameOrEmail", "Please enter a valid username or e-mail.") :: Nil))
+    case _: Unauthenticated =>
+      AuthPayload(errors = Some(FieldError("password", "Please enter a valid password.") :: Nil))
+  }
+
+  def forgotPassword(input: ForgotPasswordInput): Future[String] =
+    for {
+      user <- userRepository.findByEmail(input.email).run failOnNone NotFound(
+        s"User with username or email: [${input.email}] not found."
+      )
+      token = jwtAuthService.createAccessToken(JwtContent(user.id.get))
+      _ <- mailService.send(
+        messageTemplateService.createRecoverPasswordMessage(
+          user,
+          appConfig.name,
+          mailConfig.address,
+          appConfig.url + "/reset-password/" + token
+        )
+      )
+    } yield token
+
+  def resetPassword(input: ResetPasswordInput): Future[ResetPayload] = {
+    if (input.password != input.passwordConfirmation)
+      Future.successful(ResetPayload(Some(FieldError("password", "Passwords are not the same.") :: Nil)))
+    else if (input.password.length < authConfig.passwordMinLength)
+      Future.successful(
+        ResetPayload(
+          Some(FieldError("password", s"Password length must be more than ${authConfig.passwordMinLength}.") :: Nil)
+        )
+      )
+    else
+      for {
+        tokenContent <- jwtAuthService.decodeAccessToken(input.token).asFuture
+        user <- userRepository.findOne(tokenContent.id).run failOnNone NotFound(
+          s"User with id: [${tokenContent.id}] not found."
+        )
+        _ <- userRepository.update(user.copy(password = BCrypt.hashpw(input.password, BCrypt.gensalt))).run
+      } yield ResetPayload()
+  }
 }

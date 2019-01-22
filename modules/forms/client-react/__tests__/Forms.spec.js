615f7a3f3665cf1831897cfad05a7c9ddfcee810
Konstantin Stepakh <k.stepah@gmail.com>
2019-01-22 17:05:15 +0200

    Update tests for FormErrors

--- modules/forms/client-react/__tests__/Forms.spec.js
+++ modules/forms/client-react/__tests__/Forms.spec.js
@@ -4,7 +4,7 @@ import { step } from 'mocha-steps';
 import { FormErrors } from '../FormErrors';
 
 describe('Class FormErrors works', () => {
-  const graphQLErrors = {
+  const graphQLError = {
     graphQLErrors: [
       {
         extensions: {
@@ -28,19 +28,68 @@ describe('Class FormErrors works', () => {
       }
     ]
   };
-  const noGraphQLErrors = {
+
+  const graphQLErrors = {
+    graphQLErrors: [
+      {
+        extensions: {
+          code: 'FAILED_PASSWORD',
+          exception: {
+            errors: { usernameOrEmail: 'Please enter a valid password.' },
+            stacktrace: [
+              'Error: Failed valid user password',
+              '    at _callee2$ (~/apollo-universal-starter-kit/node_modules/@module/user-server-ts/auth/password/resolvers.js:29:1)',
+              '    at tryCatch (~/apollo-universal-starter-kit/node_modules/@babel/runtime/node_modules/regenerator-runtime/runtime.js:62:40)',
+              '    at Generator.invoke [as _invoke] (~/apollo-universal-starter-kit/node_modules/@babel/runtime/node_modules/regenerator-runtime/runtime.js:288:22)',
+              '    at Generator.prototype.(anonymous function) [as next] (~/apollo-universal-starter-kit/node_modules/@babel/runtime/node_modules/regenerator-runtime/runtime.js:114:21)',
+              '    at asyncGeneratorStep (~/apollo-universal-starter-kit/node_modules/@babel/runtime/helpers/asyncToGenerator.js:3:24)',
+              '    at _next (~/apollo-universal-starter-kit/node_modules/@babel/runtime/helpers/asyncToGenerator.js:25:9)'
+            ]
+          },
+          locations: [{ line: 2, column: 3 }],
+          message: 'Failed valid user password',
+          path: ['login']
+        }
+      },
+      {
+        extensions: {
+          code: 'FAILED_PASSWORD',
+          exception: {
+            errors: { email: 'E-mail already exists.' },
+            stacktrace: [
+              'Error: Failed valid user password',
+              '    at _callee2$ (~/apollo-universal-starter-kit/node_modules/@module/user-server-ts/auth/password/resolvers.js:29:1)',
+              '    at tryCatch (~/apollo-universal-starter-kit/node_modules/@babel/runtime/node_modules/regenerator-runtime/runtime.js:62:40)',
+              '    at Generator.invoke [as _invoke] (~/apollo-universal-starter-kit/node_modules/@babel/runtime/node_modules/regenerator-runtime/runtime.js:288:22)',
+              '    at Generator.prototype.(anonymous function) [as next] (~/apollo-universal-starter-kit/node_modules/@babel/runtime/node_modules/regenerator-runtime/runtime.js:114:21)',
+              '    at asyncGeneratorStep (~/apollo-universal-starter-kit/node_modules/@babel/runtime/helpers/asyncToGenerator.js:3:24)',
+              '    at _next (~/apollo-universal-starter-kit/node_modules/@babel/runtime/helpers/asyncToGenerator.js:25:9)'
+            ]
+          },
+          locations: [{ line: 2, column: 3 }],
+          message: 'Failed reset password',
+          path: ['register']
+        }
+      }
+    ]
+  };
+
+  const clientError = {
     type: 'FAILED_PASSWORD',
     message: 'Failed valid user password',
     errors: { usernameOrEmail: 'Please enter a valid password.' }
   };
   const messageForAlertForm = 'Test message';
 
-  step('Class FormErrors works with graphQLErrors', () => {
-    const errors = new FormErrors(messageForAlertForm, graphQLErrors).errors;
+  step('Class FormErrors works with one graphQLError', () => {
+    const errors = new FormErrors(messageForAlertForm, graphQLError).errors;
     expect(errors).to.have.all.keys('usernameOrEmail', 'errorMsg');
   });
-  step('Class FormErrors works with other errors', () => {
-    const errors = new FormErrors(messageForAlertForm, noGraphQLErrors).errors;
-    expect(Object.keys(errors)).to.have.lengthOf(1);
+  step('Class FormErrors works with two graphQLErrors', () => {
+    const errors = new FormErrors(messageForAlertForm, graphQLErrors).errors;
+    expect(errors).to.have.all.keys('usernameOrEmail', 'email', 'errorMsg');
+  });
+  step('Class FormErrors works with client error', () => {
+    expect(() => new FormErrors(messageForAlertForm, clientError)).to.throw();
   });
 });

ffd523e82fbe8b03d0072f3ff39bb5ebe80f3268
Konstantin Stepakh <k.stepah@gmail.com>
2019-01-23 17:56:08 +0200

    Update GraphQLErrors

--- modules/forms/client-react/__tests__/Forms.spec.js
+++ modules/forms/client-react/__tests__/Forms.spec.js
@@ -1,10 +1,64 @@
 import { ApolloError } from 'apollo-client';
+import { GraphQLError } from 'graphql';
 import { expect } from 'chai';
 import { step } from 'mocha-steps';
 
 import { FormError } from '../FormError';
 
 describe('Class FormError works', () => {
+  const passwordGraphQLError = new GraphQLError(
+    'Failed valid user password',
+    undefined,
+    undefined,
+    undefined,
+    ['login'],
+    undefined,
+    {
+      code: 'FAILED_PASSWORD',
+      exception: {
+        errors: { usernameOrEmail: 'Please enter a valid password.' },
+        stacktrace: [
+          'Error: Failed valid user password',
+          '    at _callee2$ (~/apollo-universal-starter-kit/node_modules/@module/user-server-ts/auth/password/resolvers.js:29:1)',
+          '    at tryCatch (~/apollo-universal-starter-kit/node_modules/@babel/runtime/node_modules/regenerator-runtime/runtime.js:62:40)',
+          '    at Generator.invoke [as _invoke] (~/apollo-universal-starter-kit/node_modules/@babel/runtime/node_modules/regenerator-runtime/runtime.js:288:22)',
+          '    at Generator.prototype.(anonymous function) [as next] (~/apollo-universal-starter-kit/node_modules/@babel/runtime/node_modules/regenerator-runtime/runtime.js:114:21)',
+          '    at asyncGeneratorStep (~/apollo-universal-starter-kit/node_modules/@babel/runtime/helpers/asyncToGenerator.js:3:24)',
+          '    at _next (~/apollo-universal-starter-kit/node_modules/@babel/runtime/helpers/asyncToGenerator.js:25:9)'
+        ]
+      },
+      locations: [{ line: 2, column: 3 }],
+      message: 'Failed valid user password',
+      path: ['login']
+    }
+  );
+  const emailGraphQLError = new GraphQLError(
+    'Failed valid user email',
+    undefined,
+    undefined,
+    undefined,
+    ['login'],
+    undefined,
+    {
+      code: 'FAILED_EMAIL',
+      exception: {
+        errors: { email: 'E-mail already exists.' },
+        stacktrace: [
+          'Error: Failed reset email',
+          '    at _callee2$ (~/apollo-universal-starter-kit/node_modules/@module/user-server-ts/auth/password/resolvers.js:29:1)',
+          '    at tryCatch (~/apollo-universal-starter-kit/node_modules/@babel/runtime/node_modules/regenerator-runtime/runtime.js:62:40)',
+          '    at Generator.invoke [as _invoke] (~/apollo-universal-starter-kit/node_modules/@babel/runtime/node_modules/regenerator-runtime/runtime.js:288:22)',
+          '    at Generator.prototype.(anonymous function) [as next] (~/apollo-universal-starter-kit/node_modules/@babel/runtime/node_modules/regenerator-runtime/runtime.js:114:21)',
+          '    at asyncGeneratorStep (~/apollo-universal-starter-kit/node_modules/@babel/runtime/helpers/asyncToGenerator.js:3:24)',
+          '    at _next (~/apollo-universal-starter-kit/node_modules/@babel/runtime/helpers/asyncToGenerator.js:25:9)'
+        ]
+      },
+      locations: [{ line: 2, column: 3 }],
+      message: 'Failed reset email',
+      path: ['register']
+    }
+  );
+
   const apolloErrorWithNetworkError = {
     graphQLErrors: undefined,
     networkError: new Error('Test networkError'),
@@ -12,77 +66,15 @@ describe('Class FormError works', () => {
   };
 
   const apolloErrorWithGraphQLError = {
-    graphQLErrors: [
-      {
-        extensions: {
-          code: 'FAILED_PASSWORD',
-          exception: {
-            errors: { usernameOrEmail: 'Please enter a valid password.' },
-            stacktrace: [
-              'Error: Failed valid user password',
-              '    at _callee2$ (~/apollo-universal-starter-kit/node_modules/@module/user-server-ts/auth/password/resolvers.js:29:1)',
-              '    at tryCatch (~/apollo-universal-starter-kit/node_modules/@babel/runtime/node_modules/regenerator-runtime/runtime.js:62:40)',
-              '    at Generator.invoke [as _invoke] (~/apollo-universal-starter-kit/node_modules/@babel/runtime/node_modules/regenerator-runtime/runtime.js:288:22)',
-              '    at Generator.prototype.(anonymous function) [as next] (~/apollo-universal-starter-kit/node_modules/@babel/runtime/node_modules/regenerator-runtime/runtime.js:114:21)',
-              '    at asyncGeneratorStep (~/apollo-universal-starter-kit/node_modules/@babel/runtime/helpers/asyncToGenerator.js:3:24)',
-              '    at _next (~/apollo-universal-starter-kit/node_modules/@babel/runtime/helpers/asyncToGenerator.js:25:9)'
-            ]
-          },
-          locations: [{ line: 2, column: 3 }],
-          message: 'Failed valid user password',
-          path: ['login']
-        }
-      }
-    ],
+    graphQLErrors: [passwordGraphQLError],
     networkError: null,
     message: 'GraphQL error: Failed password'
   };
 
   const apolloErrorWithGraphQLErrors = {
-    graphQLErrors: [
-      {
-        extensions: {
-          code: 'FAILED_PASSWORD',
-          exception: {
-            errors: { usernameOrEmail: 'Please enter a valid password.' },
-            stacktrace: [
-              'Error: Failed valid user password',
-              '    at _callee2$ (~/apollo-universal-starter-kit/node_modules/@module/user-server-ts/auth/password/resolvers.js:29:1)',
-              '    at tryCatch (~/apollo-universal-starter-kit/node_modules/@babel/runtime/node_modules/regenerator-runtime/runtime.js:62:40)',
-              '    at Generator.invoke [as _invoke] (~/apollo-universal-starter-kit/node_modules/@babel/runtime/node_modules/regenerator-runtime/runtime.js:288:22)',
-              '    at Generator.prototype.(anonymous function) [as next] (~/apollo-universal-starter-kit/node_modules/@babel/runtime/node_modules/regenerator-runtime/runtime.js:114:21)',
-              '    at asyncGeneratorStep (~/apollo-universal-starter-kit/node_modules/@babel/runtime/helpers/asyncToGenerator.js:3:24)',
-              '    at _next (~/apollo-universal-starter-kit/node_modules/@babel/runtime/helpers/asyncToGenerator.js:25:9)'
-            ]
-          },
-          locations: [{ line: 2, column: 3 }],
-          message: 'Failed valid user password',
-          path: ['login']
-        }
-      },
-      {
-        extensions: {
-          code: 'FAILED_PASSWORD',
-          exception: {
-            errors: { email: 'E-mail already exists.' },
-            stacktrace: [
-              'Error: Failed valid user password',
-              '    at _callee2$ (~/apollo-universal-starter-kit/node_modules/@module/user-server-ts/auth/password/resolvers.js:29:1)',
-              '    at tryCatch (~/apollo-universal-starter-kit/node_modules/@babel/runtime/node_modules/regenerator-runtime/runtime.js:62:40)',
-              '    at Generator.invoke [as _invoke] (~/apollo-universal-starter-kit/node_modules/@babel/runtime/node_modules/regenerator-runtime/runtime.js:288:22)',
-              '    at Generator.prototype.(anonymous function) [as next] (~/apollo-universal-starter-kit/node_modules/@babel/runtime/node_modules/regenerator-runtime/runtime.js:114:21)',
-              '    at asyncGeneratorStep (~/apollo-universal-starter-kit/node_modules/@babel/runtime/helpers/asyncToGenerator.js:3:24)',
-              '    at _next (~/apollo-universal-starter-kit/node_modules/@babel/runtime/helpers/asyncToGenerator.js:25:9)'
-            ]
-          },
-          locations: [{ line: 2, column: 3 }],
-          message: 'Failed reset password',
-          path: ['register']
-        }
-      }
-    ],
+    graphQLErrors: [passwordGraphQLError, emailGraphQLError],
     networkError: null,
-    message: 'GraphQL error: Failed password'
+    message: 'GraphQL error: Failed password and email'
   };
 
   const clientError = new Error('Test client Error');

6e382b5b219d43336d46693191f3c760cf94b238
Tony Worm <verdverm@gmail.com>
2019-01-28 00:18:16 -0700

    Feature, enable certificate / serial based access (#1017)

---
+++ modules/user/server-ts/access/serial/index.js
@@ -0,0 +1,35 @@
+import { AuthenticationError } from 'apollo-server-errors';
+
+import AccessModule from '../AccessModule';
+import settings from '../../../../../settings';
+
+import User from '../../sql';
+
+const getCurrentUser = async ({ req }) => {
+  const authorization = req && req.headers['authorization'];
+  const parts = authorization && authorization.split(' ');
+  const serial = parts && parts.length === 2 && parts[1];
+  if (serial) {
+    const user = await User.getUserWithSerial(serial)
+    if (user) {
+      return user;
+    }
+  }
+};
+
+const createContextFunc = async ({ req, connectionParams, webSocket, context }) => {
+  try {
+    context.user = context.user || (await getCurrentUser({ req, connectionParams, webSocket }));
+  } catch (e) {
+    throw new AuthenticationError(e);
+  }
+};
+
+export default new AccessModule(
+  settings.user.auth.access.serial.enabled
+    ? {
+        createContextFunc: [createContextFunc]
+      }
+    : {}
+);
+

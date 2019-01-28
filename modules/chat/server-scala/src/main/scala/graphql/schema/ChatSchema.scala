95854c15a5e25dd8ed5a2363f171f25f5a97c015
pavel.grudina <plgrudina@gmail.com>
2019-01-28 17:54:24 +0200

    Fixed bug with schema initialization

--- modules/chat/server-scala/src/main/scala/graphql/schema/ChatSchema.scala
+++ modules/chat/server-scala/src/main/scala/graphql/schema/ChatSchema.scala
@@ -1,7 +1,5 @@
 package graphql.schema
 
-import java.sql.Timestamp
-
 import akka.actor.ActorSystem
 import akka.stream.ActorMaterializer
 import common.graphql.UserContext
@@ -9,11 +7,9 @@ import common.{InputUnmarshallerGenerator, Logger}
 import graphql.resolver.ChatResolver
 import javax.inject.Inject
 import models.{AddMessageInput, _}
-import sangria.ast.StringValue
 import sangria.macros.derive._
 import sangria.marshalling.FromInput
 import sangria.schema._
-import sangria.validation.Violation
 
 import scala.concurrent.ExecutionContext
 
@@ -30,7 +26,7 @@ class ChatSchema @Inject()(
       AddMessageInput(
         text = input("text").asInstanceOf[String],
         userId = input("userId").asInstanceOf[Option[Int]],
-        uuid = input("uuid").asInstanceOf[String],
+        uuid = input("uuid").asInstanceOf[Option[String]],
         quotedId = input("quotedId").asInstanceOf[Option[Int]],
         attachment = input("attachment").asInstanceOf[Option[ScalarType[Unit]]]
       )
@@ -47,26 +43,9 @@ class ChatSchema @Inject()(
 
   object Types {
 
-    case object TimestampCoerceViolation extends Violation {
-      override def errorMessage: String = "Error during parsing Timestamp"
-    }
-
-    implicit val GraphQLTimestamp: ScalarType[Timestamp] = new ScalarType[Timestamp](
-      name = "Timestamp",
-      coerceOutput = (ts, _) => ts.toString,
-      coerceInput = {
-        case StringValue(ts, _, _, _, _) => Right(Timestamp.valueOf(ts))
-        case _ => Left(TimestampCoerceViolation)
-      },
-      coerceUserInput = {
-        case s: String => Right(Timestamp.valueOf(s))
-        case _ => Left(TimestampCoerceViolation)
-      }
-    )
-
-    implicit val attachmentFile: ScalarType[Unit] =
+    implicit val UploadAttachment: ScalarType[Unit] =
       new ScalarType[Unit](
-        name = "attachment",
+        name = "UploadAttachment",
         coerceOutput = (_, _) => Unit,
         coerceUserInput = _ => Right(Unit),
         coerceInput = _ => Right(Unit)
@@ -83,10 +62,10 @@ class ChatSchema @Inject()(
       name = "AddMessageInput",
       fields = List(
         InputField(name = "text", fieldType = StringType),
-//        InputField(userId = "userId", fieldType = OptionInputType(IntType)),
-//        InputField(uuid = "uuid", fieldType = StringType),
-//        InputField(quotedId = "quotedId", fieldType = OptionInputType(IntType)),
-//        InputField(attachment = "attachment", fieldType = OptionInputType(attachmentFile))
+        InputField(name = "userId", fieldType = OptionInputType(IntType)),
+        InputField(name = "uuid", fieldType = OptionInputType(StringType)),
+        InputField(name = "quotedId", fieldType = OptionInputType(IntType)),
+        InputField(name = "attachment", fieldType = OptionInputType(UploadAttachment))
       )
     )
 
@@ -101,11 +80,11 @@ class ChatSchema @Inject()(
       arguments = List(
         Argument(name = "input", argumentType = Types.AddMessageInput)
       ),
-      resolve = sc => chatResolver.addMessage(sc.args.arg[AddMessageInput]("input"))
+      resolve = sc => chatResolver.addMessage(sc.args.arg[AddMessageInput]("input"), sc.ctx.filesData)
     ),
     Field(
       name = "deleteMessage",
-      fieldType = Types.Message,
+      fieldType = OptionType(Types.Message),
       arguments = List(
         Argument(name = "id", argumentType = IntType)
       ),
@@ -124,9 +103,10 @@ class ChatSchema @Inject()(
   def queries: List[Field[UserContext, Unit]] = List(
     Field(
       name = "messages",
-      fieldType = Types.Messages,
+      fieldType = OptionType(Types.Messages),
       arguments = List(
-        Argument(name = "input", argumentType = Types.AddMessageInput)
+        Argument(name = "limit", argumentType = IntType),
+        Argument(name = "after", argumentType = IntType),
       ),
       resolve = sc =>
         chatResolver.messages(
@@ -136,7 +116,7 @@ class ChatSchema @Inject()(
     ),
     Field(
       name = "message",
-      fieldType = Types.Message,
+      fieldType = OptionType(Types.Message),
       arguments = List(
         Argument(name = "id", argumentType = IntType)
       ),

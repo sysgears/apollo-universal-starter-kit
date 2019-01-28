5ea7c544f6dc794ec20e3066e4ea2787c4afe780
Philip Balo <f.g.balo@gmail.com>
2019-01-28 18:33:18 +0200

    Complete Paganation component

--- modules/look/client-react/ui-material/components/Pagination.jsx
+++ modules/look/client-react/ui-material/components/Pagination.jsx
@@ -3,11 +3,10 @@ import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import TablePagination from '@material-ui/core/TablePagination';
 import IconButton from '@material-ui/core/IconButton';
-// import FirstPageIcon from '@material-ui/icons/FirstPage';
+import FirstPageIcon from '@material-ui/icons/FirstPage';
 import KeyboardArrowLeft from '@material-ui/icons/KeyboardArrowLeft';
 import KeyboardArrowRight from '@material-ui/icons/KeyboardArrowRight';
-// import LastPageIcon from '@material-ui/icons/LastPage';
-// import Grid from '@material-ui/core/Grid';
+import LastPageIcon from '@material-ui/icons/LastPage';
 import { Button } from '.';
 
 const actionsStyles = theme => ({
@@ -18,66 +17,48 @@ const actionsStyles = theme => ({
   }
 });
 
-class TablePaginationActions extends React.Component {
-  // handleFirstPageButtonClick = event => {
-  //   this.props.onChangePage(event, 0);
-  // };
-
-  handleBackButtonClick = event => {
-    console.log('handleBackButtonClick page --->', this.props.page);
-
-    this.props.onChangePage(event, this.props.page + 1);
+const TablePaginationActions = ({ classes, count, page, rowsPerPage, onChangePage }) => {
+  const handleFirstPageButtonClick = event => {
+    onChangePage(event, 0);
   };
 
-  handleNextButtonClick = event => {
-    console.log('handleNextButtonClick page --->', this.props.page);
-
-    this.props.onChangePage(event, this.props.page + 1);
+  const handleBackButtonClick = event => {
+    onChangePage(event, page - 1);
   };
 
-  // handleLastPageButtonClick = event => {
-  //   this.props.onChangePage(
-  //     event,
-  //     Math.max(0, Math.ceil(this.props.count / this.props.rowsPerPage) - 1)
-  //   );
-  // };
+  const handleNextButtonClick = event => {
+    onChangePage(event, page + 1);
+  };
 
-  render() {
-    const { classes, count, page, rowsPerPage } = this.props;
+  const handleLastPageButtonClick = event => {
+    onChangePage(event, Math.max(0, Math.ceil(count / rowsPerPage) - 1));
+  };
 
-    console.log('this.props.page --->', this.props.page);
-    console.log('Math.ceil(count / rowsPerPage) --->', Math.ceil(count / rowsPerPage));
-
-    return (
-      <div className={classes.root}>
-        {/* <IconButton
-          onClick={this.handleFirstPageButtonClick}
-          disabled={page === 0}
-          aria-label="First Page"
-        >
-          <FirstPageIcon />
-        </IconButton> */}
-        <IconButton onClick={this.handleBackButtonClick} disabled={page === 0} aria-label="Previous Page">
-          <KeyboardArrowLeft />
-        </IconButton>
-        <IconButton
-          onClick={this.handleNextButtonClick}
-          disabled={page >= Math.ceil(count / rowsPerPage)}
-          aria-label="Next Page"
-        >
-          <KeyboardArrowRight />
-        </IconButton>
-        {/* <IconButton
-          onClick={this.handleLastPageButtonClick}
-          disabled={page >= Math.ceil(count / rowsPerPage) - 1}
-          aria-label="Last Page"
-        >
-          <LastPageIcon />
-        </IconButton> */}
-      </div>
-    );
-  }
-}
+  return (
+    <div className={classes.root}>
+      <IconButton onClick={handleFirstPageButtonClick} disabled={page === 0} aria-label="First Page">
+        <FirstPageIcon />
+      </IconButton>
+      <IconButton onClick={handleBackButtonClick} disabled={page === 0} aria-label="Previous Page">
+        <KeyboardArrowLeft />
+      </IconButton>
+      <IconButton
+        onClick={handleNextButtonClick}
+        disabled={page >= Math.ceil(count / rowsPerPage) - 1}
+        aria-label="Next Page"
+      >
+        <KeyboardArrowRight />
+      </IconButton>
+      <IconButton
+        onClick={handleLastPageButtonClick}
+        disabled={page >= Math.ceil(count / rowsPerPage) - 1}
+        aria-label="Last Page"
+      >
+        <LastPageIcon />
+      </IconButton>
+    </div>
+  );
+};
 
 TablePaginationActions.propTypes = {
   classes: PropTypes.object.isRequired,
@@ -92,70 +73,49 @@ const TablePaginationActionsWrapped = withStyles(actionsStyles, {
   withTheme: true
 })(TablePaginationActions);
 
-export default class Pagination extends React.Component {
-  state = {
-    page: 0
+const Pagination = ({
+  itemsPerPage,
+  handlePageChange,
+  hasNextPage,
+  pagination,
+  total,
+  loadMoreText,
+  defaultPageSize,
+  currentPage
+}) => {
+  const handleChangePage = (event, page) => {
+    handlePageChange(pagination, page + 1);
   };
-  componentDidMount() {
-    console.log('componentDidMount --->', 'componentDidMount');
-  }
 
-  handleChangePage = (event, page) => {
-    console.log('Pagination page --->', page);
-    this.setState({ page }, this.props.handlePageChange(this.props.pagination, page + 1));
-  };
-
-  // handleChangeRowsPerPage = event => {
-  //   this.setState({ rowsPerPage: event.target.value });
-  // };
-
-  render() {
-    const { page } = this.state;
-    console.log('STATE page --->', page);
-    const {
-      itemsPerPage,
-      handlePageChange,
-      hasNextPage,
-      pagination,
-      total,
-      loadMoreText,
-      defaultPageSize
-    } = this.props;
-
-    return (
-      <Fragment>
-        {pagination === 'relay' ? (
-          hasNextPage ? (
+  return (
+    <Fragment>
+      {pagination === 'relay' ? (
+        hasNextPage ? (
+          <div>
             <div>
-              <div>
-                <small>
-                  ({itemsPerPage} / {total})
-                </small>
-              </div>
-              <Button id="load-more" color="primary" onClick={() => handlePageChange(pagination)}>
-                {loadMoreText}
-              </Button>
+              <small>
+                ({itemsPerPage} / {total})
+              </small>
             </div>
-          ) : null
-        ) : (
-          <TablePagination
-            component="div"
-            rowsPerPageOptions={[]}
-            count={total}
-            rowsPerPage={defaultPageSize}
-            page={page}
-            onChangePage={this.handleChangePage}
-            // SelectProps={{
-            //   native: true
-            // }}
-            // onChangeRowsPerPage={this.handleChangeRowsPerPage}
-            ActionsComponent={TablePaginationActionsWrapped}
-          />
-        )}
-      </Fragment>
-    );
-  }
-}
+            <Button id="load-more" color="primary" onClick={() => handlePageChange(pagination)}>
+              {loadMoreText}
+            </Button>
+          </div>
+        ) : null
+      ) : (
+        <TablePagination
+          component="div"
+          rowsPerPageOptions={[]}
+          count={total}
+          rowsPerPage={defaultPageSize}
+          page={currentPage}
+          onChangePage={handleChangePage}
+          ActionsComponent={TablePaginationActionsWrapped}
+        />
+      )}
+    </Fragment>
+  );
+};
 
 Pagination.propTypes = {
   itemsPerPage: PropTypes.number,
@@ -164,5 +124,12 @@ Pagination.propTypes = {
   pagination: PropTypes.string,
   total: PropTypes.number,
   loadMoreText: PropTypes.string,
-  defaultPageSize: PropTypes.number
+  defaultPageSize: PropTypes.number,
+  currentPage: PropTypes.number
 };
+
+Pagination.defaultProps = {
+  currentPage: 0
+};
+
+export default Pagination;

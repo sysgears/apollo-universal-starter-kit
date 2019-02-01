425d24a64da1f05f9fdb396f483f6e8679d26e03
Philip Balo <f.g.balo@gmail.com>
2019-02-01 15:10:38 +0200

    Change settings

--- modules/look/client-react/look.ts
+++ modules/look/client-react/look.ts
@@ -1,5 +1,5 @@
-export * from './ui-bootstrap';
-// export * from './ui-material';
+// export * from './ui-bootstrap';
+export * from './ui-material';
 // export * from './ui-antd';
 
 export { default as LayoutCenter } from './LayoutCenter';

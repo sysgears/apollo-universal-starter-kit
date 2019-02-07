cdeb514e8d382fecc4152d9c841c54c6b8807cd0
Eugene Krotov <e.krotov@hotmail.com>
2019-02-07 12:52:28 +0200

    Change CrudRepository to JpaRepository

--- modules/counter/server-java/src/main/java/graphql/repository/CounterRepository.java
+++ modules/counter/server-java/src/main/java/graphql/repository/CounterRepository.java
@@ -1,9 +1,9 @@
 package graphql.repository;
 
 import graphql.model.Counter;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CounterRepository extends CrudRepository<Counter, Integer> {
+public interface CounterRepository extends JpaRepository<Counter, Integer> {
 }
--- modules/user/server-java/src/main/java/graphql/repository/UserRepository.java
+++ modules/user/server-java/src/main/java/graphql/repository/UserRepository.java
@@ -1,9 +1,9 @@
 package graphql.repository;
 
 import graphql.model.User;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository extends CrudRepository<User, Integer> {
+public interface UserRepository extends JpaRepository<User, Integer> {
 }

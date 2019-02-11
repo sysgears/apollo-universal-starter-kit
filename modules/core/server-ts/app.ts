3cc57b887471736b1bc7ce28b43990462dc1eead
Tony Worm <verdverm@gmail.com>
2019-02-10 19:59:11 -0700

    initial prototype for SOFA integration

--- modules/core/server-ts/app.ts
+++ modules/core/server-ts/app.ts
@@ -1,6 +1,8 @@
 import express from 'express';
 import path from 'path';
 import { GraphQLSchema } from 'graphql';
+import sofa, { OpenAPI } from 'sofa-api';
+
 import { isApiExternal } from '@gqlapp/core-common';
 import ServerModule from '@gqlapp/module-server-ts';
 
@@ -25,6 +27,33 @@ export const createServerApp = (schema: GraphQLSchema, modules: ServerModule) =>
     const graphqlServer = createApolloServer(schema, modules);
     graphqlServer.applyMiddleware({ app, path: __API_URL__, cors: { credentials: true, origin: true } });
   }
+
+  const openApi = OpenAPI({
+    schema,
+    info: {
+      title: 'Example API',
+      version: '3.0.0'
+    }
+  });
+
+  app.use(
+    '/api',
+    sofa({
+      schema,
+      onRoute: info => {
+        // console.log(info);
+        openApi.addRoute(info, {
+          basePath: '/api'
+        });
+      },
+      context: async ({ req, res }) => {
+        return modules.createContext(req, res);
+      }
+    })
+  );
+
+  // writes every recorder route
+  openApi.save('./swagger.yml');
 
   app.get('/graphiql', (req, res, next) => graphiqlMiddleware(req, res, next));
   app.use(websiteMiddleware(schema, modules));

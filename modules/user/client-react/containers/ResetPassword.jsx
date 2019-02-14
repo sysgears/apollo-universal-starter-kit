8c3ed25e358685635561efa5ec4395940b2f2ead
Philip Balo <f.g.balo@gmail.com>
2019-02-14 17:15:09 +0200

    Change ResetPassword container

--- modules/user/client-react/containers/ResetPassword.jsx
+++ modules/user/client-react/containers/ResetPassword.jsx
@@ -1,52 +1,50 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { graphql, compose } from 'react-apollo';
+import { Mutation } from 'react-apollo';
 import { translate } from '@gqlapp/i18n-client-react';
 
 import { FormError } from '@gqlapp/forms-client-react';
 import ResetPasswordView from '../components/ResetPasswordView';
 import RESET_PASSWORD from '../graphql/ResetPassword.graphql';
 
-class ResetPassword extends React.Component {
-  static propTypes = {
-    resetPassword: PropTypes.func,
-    t: PropTypes.func,
-    history: PropTypes.object,
-    match: PropTypes.object
-  };
-  onSubmit = async values => {
-    const { t, resetPassword, history, match } = this.props;
+const ResetPassword = props => {
+  const { t, resetPassword, history, match } = props;
 
+  const onSubmit = async values => {
     try {
       await resetPassword({ ...values, token: match.params.token });
     } catch (e) {
       throw new FormError(t('resetPass.errorMsg'), e);
     }
-
     history.push('/login');
   };
 
-  render() {
-    return <ResetPasswordView {...this.props} onSubmit={this.onSubmit} />;
-  }
-}
+  return <ResetPasswordView {...props} onSubmit={onSubmit} />;
+};
 
-const ResetPasswordWithApollo = compose(
-  translate('user'),
+ResetPassword.propTypes = {
+  resetPassword: PropTypes.func,
+  t: PropTypes.func,
+  history: PropTypes.object,
+  match: PropTypes.object
+};
 
-  graphql(RESET_PASSWORD, {
-    props: ({ mutate }) => ({
-      resetPassword: async ({ password, passwordConfirmation, token }) => {
-        const {
-          data: { resetPassword }
-        } = await mutate({
-          variables: { input: { password, passwordConfirmation, token } }
-        });
+const ResetPasswordWithApollo = props => {
+  return (
+    <Mutation mutation={RESET_PASSWORD}>
+      {mutate => {
+        const resetPassword = async ({ password, passwordConfirmation, token }) => {
+          const {
+            data: { resetPassword }
+          } = await mutate({
+            variables: { input: { password, passwordConfirmation, token } }
+          });
+          return resetPassword;
+        };
+        return <ResetPassword {...props} resetPassword={resetPassword} />;
+      }}
+    </Mutation>
+  );
+};
 
-        return resetPassword;
-      }
-    })
-  })
-)(ResetPassword);
-
-export default ResetPasswordWithApollo;
+export default translate('user')(ResetPasswordWithApollo);

7fa59639b753d0746136186f6042df939a04da4f
Philip Balo <f.g.balo@gmail.com>
2019-02-15 14:55:11 +0200

    Changed RegisterView component

--- modules/user/client-react/components/RegisterView.web.jsx
+++ modules/user/client-react/components/RegisterView.web.jsx
@@ -8,13 +8,10 @@ import RegisterForm from '../components/RegisterForm';
 
 import settings from '../../../../settings';
 
-class RegisterView extends React.PureComponent {
-  static propTypes = {
-    t: PropTypes.func,
-    onSubmit: PropTypes.func
-  };
+const RegisterView = props => {
+  const { t, onSubmit } = props;
 
-  renderMetaData = t => (
+  const renderMetaData = () => (
     <Helmet
       title={`${settings.app.name} - ${t('reg.title')}`}
       meta={[
@@ -26,18 +23,20 @@ class RegisterView extends React.PureComponent {
     />
   );
 
-  render() {
-    const { t, onSubmit } = this.props;
-    return (
-      <PageLayout>
-        {this.renderMetaData(t)}
-        <LayoutCenter>
-          <h1 className="text-center">{t('reg.form.title')}</h1>
-          <RegisterForm onSubmit={onSubmit} />
-        </LayoutCenter>
-      </PageLayout>
-    );
-  }
-}
+  return (
+    <PageLayout>
+      {renderMetaData(t)}
+      <LayoutCenter>
+        <h1 className="text-center">{t('reg.form.title')}</h1>
+        <RegisterForm onSubmit={onSubmit} />
+      </LayoutCenter>
+    </PageLayout>
+  );
+};
+
+RegisterView.propTypes = {
+  t: PropTypes.func,
+  onSubmit: PropTypes.func
+};
 
 export default translate('user')(RegisterView);

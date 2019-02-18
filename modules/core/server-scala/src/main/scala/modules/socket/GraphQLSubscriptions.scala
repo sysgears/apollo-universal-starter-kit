3606f114c03d0fa7330bdb3dd3f78c899017aecc
Oleh Tsivan <oleh.tsivan@sysgears.com>
2019-02-18 12:25:17 +0200

    Add synchronization for functionality related to cancel

--- modules/core/server-scala/src/main/scala/modules/socket/GraphQLSubscriptions.scala
+++ modules/core/server-scala/src/main/scala/modules/socket/GraphQLSubscriptions.scala
@@ -8,20 +8,24 @@ case class GraphQLSubscriptions() {
 
   private[this] val subscriptions: TrieMap[String, Cancelable] = TrieMap.empty[String, Cancelable]
 
+  private[this] var wsClosed = false
+
   /** Adds a new operationId/cancelable pair to subscriptions.
     *  If the subscriptions already contains a
     *  mapping for the operationId, it will be overridden by the new cancelable.
     */
-  def add(id: String, cancelable: Cancelable): Unit = {
-    this.subscriptions.update(id, cancelable)
+  def add(id: String, cancelable: Cancelable): Unit = synchronized {
+    if (!wsClosed) this.subscriptions.update(id, cancelable)
+    else cancelable.cancel
   }
 
   def cancel(id: String): Unit = {
     this.subscriptions.get(id).foreach(_.cancel)
   }
 
-  def cancelAll(): Unit = {
+  def cancelAll(): Unit = synchronized {
     this.subscriptions.foreach(_._2.cancel)
+    wsClosed = true
   }
 
   def size: Int = {

6f02763f2625b1a4b2e16e2b94bb9a1c206793ac
Eugene Krotov <e.krotov@hotmail.com>
2019-02-21 20:25:51 +0200

    Fix serverCounter mutation null value.

--- modules/counter/server-java/src/main/java/graphql/schema/CounterMutation.java
+++ modules/counter/server-java/src/main/java/graphql/schema/CounterMutation.java
@@ -11,6 +11,8 @@ import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.concurrent.CompletableFuture;
+
 import static graphql.repository.SeedCounterDB.COUNTER_ID;
 
 @Component
@@ -26,14 +28,16 @@ public class CounterMutation implements GraphQLMutationResolver {
 
     @Transactional
     @Async("resolverThreadPoolTaskExecutor")
-    public Counter addServerCounter(Integer amount) {
-        Counter counter = counterRepository.findById(COUNTER_ID).get(); //TODO Unsafe
-        Integer currentAmount = counter.getAmount();
-        counter.setAmount(currentAmount + amount);
-        logger.debug("Update amount");
-        Counter savedCounter = counterRepository.save(counter);
-        logger.debug("Publish element [Counter]");
-        counterPubSubService.publish(savedCounter);
-        return savedCounter;
+    public CompletableFuture<Counter> addServerCounter(Integer amount) {
+        return counterRepository.findOneById(COUNTER_ID)
+            .thenApplyAsync(counter -> {
+                Integer currentAmount = counter.getAmount();
+                counter.setAmount(currentAmount + amount);
+                logger.debug("Update amount");
+                Counter savedCounter = counterRepository.save(counter);
+                logger.debug("Publish element [Counter]");
+                counterPubSubService.publish(savedCounter);
+                return savedCounter;
+            });
     }
 }

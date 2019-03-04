eb24efb3b67ede03f024022a382173d8b725e0e3
Eugene Krotov <e.krotov@hotmail.com>
2019-03-04 13:47:58 +0200

    Update User mutation addUser

--- modules/user/server-java/src/main/java/graphql/schema/UserMutation.java
+++ modules/user/server-java/src/main/java/graphql/schema/UserMutation.java
@@ -32,25 +32,66 @@ public class UserMutation implements GraphQLMutationResolver {
     @Async("resolverThreadPoolTaskExecutor")
     public CompletableFuture<UserPayload> addUser(AddUserInput input) {
         logger.debug("Add User: [{}]", input);
+
+        UserProfile profile = input.getProfile().map(profileInput -> UserProfile.builder()
+                .firstName(profileInput.getFirstName().orElse(""))
+                .lastName(profileInput.getLastName().orElse(""))
+                .fullName(profileInput.getFirstName().orElse("") + " " + profileInput.getLastName().orElse(""))
+                .build()).orElse(null);
+
+        UserAuth auth = input.getAuth().map(authInput -> {
+
+            CertificateAuth certificateAuth = authInput.getCertificate().map(authCertificateInput -> CertificateAuth.builder()
+                    .serial(authCertificateInput.getSerial().orElse(null))
+                    .build()).orElse(null);
+
+            LinkedInAuth linkedInAuth = authInput.getLinkedin().map(authLinkedInInput ->
+                    LinkedInAuth.builder()
+                            .lnId(authLinkedInInput.getLnId().orElse(null))
+                            .displayName(authLinkedInInput.getDisplayName().orElse(null))
+                            .build()).orElse(null);
+
+            GoogleAuth googleAuth = authInput.getGoogle().map(authGoogleInput ->
+                    GoogleAuth.builder()
+                            .googleId(authGoogleInput.getGoogleId().orElse(null))
+                            .displayName(authGoogleInput.getDisplayName().orElse(null))
+                            .build()).orElse(null);
+
+            GithubAuth githubAuth = authInput.getGithub().map(authGitHubInput ->
+                    GithubAuth.builder()
+                            .ghId(authGitHubInput.getGhId().orElse(null))
+                            .displayName(authGitHubInput.getDisplayName().orElse(null))
+                            .build()).orElse(null);
+
+            FacebookAuth facebookAuth = authInput.getFacebook().map(authFacebookInput ->
+                    FacebookAuth.builder()
+                            .fbId(authFacebookInput.getFbId().orElse(null))
+                            .displayName(authFacebookInput.getDisplayName().orElse(null))
+                            .build()).orElse(null);
+
+            return UserAuth.builder()
+                    .certificate(certificateAuth)
+                    .linkedin(linkedInAuth)
+                    .github(githubAuth)
+                    .google(googleAuth)
+                    .facebook(facebookAuth)
+                    .build();
+        }).orElse(null);
+
+
         User user = User.builder()
                 .password(passwordEncoder.encode(input.getPassword()))
                 .email(input.getEmail())
                 .role(input.getRole())
                 .username(input.getUsername())
                 .isActive(input.getIsActive().orElse(false))
-                //TODO Add profile and auth
-//                .profile(input.getProfile().ifPresent(profile -> {
-//                     UserProfile.builder()
-//                            .firstName(profile.getFirstName().orElse(null))
-//                            .lastName(profile.getLastName().orElse(null))
-//                            .fullName(profile.getFirstName().orElse(null) + " " + profile.getLastName().orElse(null))
-//                            .build();
-//                })
-//                )
+                .profile(profile)
+                .auth(auth)
                 .build();
 
+        User savedUser = userRepository.save(user);
         return CompletableFuture.supplyAsync(() -> UserPayload.builder()
-                .user(user)
+                .user(savedUser)
                 .build());
     }
 
@@ -112,8 +153,8 @@ public class UserMutation implements GraphQLMutationResolver {
                     return userRepository.save(maybeUser);
                 })
                 .thenApplyAsync(updatedUser -> UserPayload.builder()
-                                                .user(updatedUser)
-                                                .build());
+                        .user(updatedUser)
+                        .build());
     }
 
     @Transactional
@@ -122,7 +163,7 @@ public class UserMutation implements GraphQLMutationResolver {
         logger.debug("Delete User by ID: [{}]", id);
         return userRepository.findOneById(id)
                 .thenApplyAsync(maybeUser -> {
-                            userRepository.delete(maybeUser);
+                    userRepository.delete(maybeUser);
                     return UserPayload.builder()
                             .user(maybeUser)
                             .build();

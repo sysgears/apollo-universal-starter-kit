5e5003f6668bc7026b05b4e668470bfe3c8106ac
Mitja Debeljak <mitjade@gmail.com>
2019-03-11 14:09:11 +0100

    merge latest

--- tools/templates/module/client-react/index.native.tsx
+++ tools/templates/module/client-react/index.native.tsx
@@ -4,7 +4,7 @@ import { createStackNavigator } from 'react-navigation';
 import { translate } from '@gqlapp/i18n-client-react';
 import ClientModule from '@gqlapp/module-client-react-native';
 
-import { HeaderTitle, IconButton } from '../../../packages/client/src/modules/common/components/native';
+import { HeaderTitle, IconButton } from '@gqlapp/look-client-react-native';
 import $Module$ from './containers/$Module$';
 import resources from './locales';
 

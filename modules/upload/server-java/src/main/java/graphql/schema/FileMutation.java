b8a665ec54c3785847369381348cdeb428df6db1
pavel.grudina <plgrudina@gmail.com>
2019-03-18 17:51:21 +0200

    Implemented 'uploadFiles' resolve function

--- modules/upload/server-java/src/main/java/graphql/schema/FileMutation.java
+++ modules/upload/server-java/src/main/java/graphql/schema/FileMutation.java
@@ -1,16 +1,22 @@
 package graphql.schema;
 
 import com.coxautodev.graphql.tools.GraphQLMutationResolver;
-import graphql.model.FileUpload;
+import graphql.model.File;
 import graphql.repository.FileRepository;
+import graphql.services.FileService;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.List;
+import java.util.Optional;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.atomic.AtomicInteger;
 
 @Component
 public class FileMutation implements GraphQLMutationResolver {
@@ -20,11 +26,36 @@ public class FileMutation implements GraphQLMutationResolver {
     @Autowired
     private FileRepository fileRepository;
 
-    public CompletableFuture<Boolean> uploadFiles(final List<FileUpload> files) {
-        return CompletableFuture.supplyAsync(() -> false);
+    @Autowired
+    private FileService fileService;
+
+    public CompletableFuture<Boolean> uploadFiles(List<MultipartFile> files) {
+        AtomicInteger successfullyUploadCounter = new AtomicInteger();
+       return CompletableFuture.supplyAsync(() -> {
+            files.parallelStream().forEach(file -> {
+                try {
+                    String hashedFileName = fileService.hashAppend(file.getOriginalFilename());
+                    java.io.File uploadedFile = new java.io.File(fileService.publicDirPath.toString()+hashedFileName);
+                    file.transferTo(uploadedFile);
+                    fileRepository.save(new File(
+                            0,
+                            hashedFileName,
+                            fileService.getFileExtension(hashedFileName),
+                            file.getSize(),
+                            uploadedFile.getPath()
+                            ));
+                    successfullyUploadCounter.getAndIncrement();
+                } catch (IOException e) {
+                    logger.debug("Unsuccessful upload file with [name=" + file.getOriginalFilename() + "]");
+                }
+            });
+            return files.size() == successfullyUploadCounter.get();
+        }
+        );
     }
 
     @Async("repositoryThreadPoolTaskExecutor")
+    @Transactional
     public Boolean removeFile(Integer id) {
         logger.debug("Started removing a file with [id=" + id + "]");
         Optional<File> maybeFile = fileRepository.findById(id);

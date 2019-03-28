b7e6d9fb16c94750aacda3ed462a7e6d524fd13d
Victor Vlasenko <victor.vlasenko@sysgears.com>
2019-03-28 19:02:13 +0200

    Clean up vue and angular packages deps

--- packages/client-vue/babel.config.js
+++ packages/client-vue/babel.config.js
@@ -1,18 +1,6 @@
 module.exports = {
   compact: false,
-  presets: ['babel-preset-vue', ['@babel/preset-env', { modules: false }]],
-  plugins: [
-    '@babel/plugin-transform-destructuring',
-    '@babel/plugin-transform-regenerator',
-    '@babel/plugin-transform-runtime',
-    ['@babel/plugin-proposal-decorators', { legacy: true }],
-    ['@babel/plugin-proposal-class-properties', { loose: true }],
-    '@babel/plugin-proposal-object-rest-spread',
-    'syntax-jsx',
-    'transform-vue-jsx',
-    'jsx-event-modifiers',
-    'jsx-v-model'
-  ],
+  presets: ['@vue/babel-preset-app'],
   env: {
     production: {
       compact: true

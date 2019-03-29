b4c91727a3af8beb1ce0be67408c865013be25d7
Alexandr Belokur <alexandr.belokur@sysgears.com>
2019-03-29 18:26:21 +0200

    Create editUserController

--- modules/user/server-ts/controllers/index.js
+++ modules/user/server-ts/controllers/index.js
@@ -1,9 +1,11 @@
 import getUsersController from './getUsers';
 import addUserController from './addUser';
+import editUserController from './editUser';
 
 const restApi = [
   { route: '/getUsers', controller: getUsersController, method: 'GET' },
-  { route: '/addUser', controller: addUserController, method: 'POST' }
+  { route: '/addUser', controller: addUserController, method: 'POST' },
+  { route: '/editUser', controller: editUserController, method: 'PUT' }
 ];
 
 export default restApi;

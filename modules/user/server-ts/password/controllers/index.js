ae9ea772f0b65c6b1feac16df9de845c9e7eb5a7
Alexandr Belokur <alexandr.belokur@sysgears.com>
2019-04-01 18:58:53 +0300

    Create forgotPasswordController

--- modules/user/server-ts/password/controllers/index.js
+++ modules/user/server-ts/password/controllers/index.js
@@ -1,9 +1,11 @@
 import registerController from './register';
 import loginController from './login';
+import forgotPasswordController from './forgotPassword';
 
 const restApi = [
   { route: '/register', controller: registerController, method: 'POST' },
-  { route: '/login', controller: loginController, method: 'POST' }
+  { route: '/login', controller: loginController, method: 'POST' },
+  { route: '/forgotPassword', controller: forgotPasswordController, method: 'POST' }
 ];
 
 export default restApi;

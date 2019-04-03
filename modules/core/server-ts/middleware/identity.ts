fc9a9f7c948884b28a491daf8be0711730636f0d
Alexandr Belokur <alexandr.belokur@sysgears.com>
2019-04-03 14:58:53 +0300

    Fix identity middleware

--- modules/core/server-ts/middleware/identity.ts
+++ modules/core/server-ts/middleware/identity.ts
@@ -3,7 +3,7 @@ import jwt from 'jsonwebtoken';
 
 import settings from '../../../../settings';
 
-const identityMiddleware = (req: express.Request, res: express.Response) => {
+const identityMiddleware = (req: express.Request, res: express.Response, next: any) => {
   const authorization = req && req.headers.authorization;
   const parts = authorization && authorization.split(' ');
   const token = parts && parts.length === 2 && parts[1];
@@ -11,6 +11,7 @@ const identityMiddleware = (req: express.Request, res: express.Response) => {
     const identity = jwt.verify(token, settings.auth.secret);
     res.locals.identity = identity;
   }
+  next();
 };
 
 export default identityMiddleware;

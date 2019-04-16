2935c015cca22cda8929fb9f7f3819cf888ba313
Oleksandr Bazylevskyi <bazylevskyioleksandr@gmail.com>
2019-04-16 12:03:36 +0300

    Delete previous GraphQl setup

--- modules/core/server-ts/app.ts
+++ modules/core/server-ts/app.ts
@@ -1,12 +1,8 @@
 import express, { Express } from 'express';
 import path from 'path';
 import { GraphQLSchema } from 'graphql';
-
-// import { isApiExternal } from '@gqlapp/core-common';
 import ServerModule, { TWare, TCreateGraphQLContext } from '@gqlapp/module-server-ts';
 
-// import graphiqlMiddleware from './middleware/graphiql';
-// import createApolloServer from './graphql';
 import errorMiddleware from './middleware/error';
 
 type TApplyWare = (ware: TWare) => void;
@@ -22,12 +18,6 @@ export const createServerApp = (schema: GraphQLSchema, modules: ServerModule) =>
   beforeware.forEach(applyWare);
   middleware.forEach(applyWare);
 
-  // if (!isApiExternal) {
-  //   const graphqlServer = createApolloServer(schema, modules);
-  //   graphqlServer.applyMiddleware({ app, path: __API_URL__, cors: { credentials: true, origin: true } });
-  // }
-
-  // app.get('/graphiql', (req, res, next) => graphiqlMiddleware(req, res, next));
   app.use('/', express.static(__FRONTEND_BUILD_DIR__, { maxAge: '180 days' }));
 
   if (__DEV__) {

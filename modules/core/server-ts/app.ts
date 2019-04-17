36f0ef09833fbb142fc4f1c45c8cb012fb181b8e
Oleksandr Bazylevskyi <bazylevskyioleksandr@gmail.com>
2019-04-17 12:34:34 +0300

    Remove GraphQL setup from createServerApp function

--- modules/core/server-ts/app.ts
+++ modules/core/server-ts/app.ts
@@ -1,39 +1,32 @@
 import express from 'express';
 import path from 'path';
 import { GraphQLSchema } from 'graphql';
+import ServerModule, { MiddlewareCallback, CreateGraphQLContext } from '@gqlapp/module-server-ts';
 
-import { isApiExternal } from '@gqlapp/core-common';
-import ServerModule from '@gqlapp/module-server-ts';
-
-import graphiqlMiddleware from './middleware/graphiql';
 import websiteMiddleware from './middleware/website';
-import createApolloServer from './graphql';
 import errorMiddleware from './middleware/error';
+
+type ApplyMiddleWare = (fn: MiddlewareCallback) => void;
 
 export const createServerApp = (schema: GraphQLSchema, modules: ServerModule) => {
   const app = express();
   // Don't rate limit heroku
   app.enable('trust proxy');
+  const { appContext, beforeware, middleware } = modules;
+  const createGraphQLContext: CreateGraphQLContext = (req, res) => modules.createContext(req, res);
+  const applyMiddleWare: ApplyMiddleWare = fn => fn(app, appContext, { createGraphQLContext, schema });
 
-  modules.beforeware.forEach(applyBeforeware => applyBeforeware(app, modules.appContext));
-  modules.middleware.forEach(applyMiddleware => applyMiddleware(app, modules.appContext));
+  beforeware.forEach(applyMiddleWare);
+  middleware.forEach(applyMiddleWare);
 
-  if (__DEV__) {
-    app.get('/servdir', (req, res) => res.send(process.cwd() + path.sep));
-  }
-
-  if (!isApiExternal) {
-    const graphqlServer = createApolloServer(schema, modules);
-    graphqlServer.applyMiddleware({ app, path: __API_URL__, cors: { credentials: true, origin: true } });
-  }
-
-  app.get('/graphiql', (req, res, next) => graphiqlMiddleware(req, res, next));
   app.use(websiteMiddleware(schema, modules));
   app.use('/', express.static(__FRONTEND_BUILD_DIR__, { maxAge: '180 days' }));
 
   if (__DEV__) {
     app.use('/', express.static(__DLL_BUILD_DIR__, { maxAge: '180 days' }));
+    app.get('/servdir', (req, res) => res.send(process.cwd() + path.sep));
     app.use(errorMiddleware);
   }
+
   return app;
 };

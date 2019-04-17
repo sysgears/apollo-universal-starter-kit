60736865223b3203444bda9bfa19025f9f8d02d3
Philip Balo <f.g.balo@gmail.com>
2019-04-17 18:35:24 +0300

    Add functionality show stack list

--- tools/cli/commands/deleteStack.js
+++ tools/cli/commands/deleteStack.js
@@ -4,17 +4,21 @@ import deleteStack from '../helpers/deleteStack';
 
 import { STACK_LIST, BASE_PATH } from '../config';
 
-const handleDeleteStackList = (stackList, logger) => {
-  const formatStackList = stackList.map(stack => stack.toLowerCase());
-  checkStackList(formatStackList, logger);
-};
-
-const checkStackList = (stackList, logger) => {
+const handleDeleteStackList = (stackList, logger, isShowStackList) => {
   const existsStackList = fs
     .readdirSync(`${BASE_PATH}/packages`)
     .filter(stack => stack !== 'common' && stack !== 'mobile')
     .map(stack => (stack === 'client' ? 'react' : STACK_LIST[stack]));
 
+  if (isShowStackList) {
+    logger.info(chalk.yellow(`List exists stack of technology: ${existsStackList.join(', ')}`));
+  }
+
+  const formatStackList = stackList.map(stack => stack.toLowerCase());
+  checkStackList(formatStackList, existsStackList, logger);
+};
+
+const checkStackList = (stackList, existsStackList, logger) => {
   const notExistsStackList = stackList
     .reduce((prev, curr) => {
       return existsStackList.includes(curr) ? [...prev] : [...prev, curr];
@@ -23,7 +27,6 @@ const checkStackList = (stackList, logger) => {
       logger.error(chalk.red(`The stack of technology ${stack} not exists.`));
       return stack;
     });
-  // console.log('notExistsStackList --->', notExistsStackList);
 
   if (notExistsStackList.length) {
     logger.error(chalk.yellow(`Please enter correct stack of technology`));

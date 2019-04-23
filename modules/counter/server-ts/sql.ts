00882c36943afb1ef3aa294ff4073ddf89f8866d
Mithilesh Kohale <beingtmk@gmail.com>
2019-04-24 03:48:13 +0530

    Started adding ObjectionJS with Counter Module

--- modules/counter/server-ts/sql.ts
+++ modules/counter/server-ts/sql.ts
@@ -1,11 +1,31 @@
 import knex from '../../../packages/server/src/sql/connector';
+import { Model } from 'objection';
 
-export default class Counter {
+// Give the knex instance to objection.
+Model.knex(knex);
+
+// Counter model.
+export default class Counter extends Model {
+  static get tableName() {
+    return 'counter';
+  }
+
+  static get jsonSchema() {
+    return {
+      type: 'object',
+
+      properties: {
+        id: { type: 'integer' },
+        amount: { type: 'integer' }
+      }
+    };
+  }
+
   public counterQuery() {
-    return knex('counter').first();
+    return Counter.query().first();
   }
 
   public addCounter(amount: number) {
-    return knex('counter').increment('amount', amount);
+    return Counter.query().increment('amount', amount);
   }
 }

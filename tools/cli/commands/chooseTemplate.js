959fc9a4f0303e7d327c304103df763c2a602fd9
Philip Balo <f.g.balo@gmail.com>
2019-05-03 10:45:23 +0300

    Add discriptions for choosestack functionality

--- tools/cli/commands/chooseTemplate.js
+++ tools/cli/commands/chooseTemplate.js
@@ -3,13 +3,20 @@ import * as inquirer from 'inquirer';
 import { deleteStack } from '../helpers/util';
 import { STACK_MAP, BASE_PATH } from '../config';
 
+/**
+ * Generates a list of technologies for the library 'inquirer'
+ * and returns a list of selected technologies
+ */
 const chooseTemplate = async () => {
+  // getting a list of existing technologies
   const existingStackList = fs
     .readdirSync(`${BASE_PATH}/packages`)
     .filter(stack => Object.keys(STACK_MAP).includes(stack));
 
+  // creating a list of options with existing technologies for 'inquirer'
   const choices = existingStackList.reduce((prev, curr) => [...prev, { name: STACK_MAP[curr].title }], []);
 
+  // creating options for 'inquirer'
   const questions = [
     {
       type: 'checkbox',
@@ -25,17 +32,19 @@ const chooseTemplate = async () => {
       }
     }
   ];
+
+  // getting a list of selected technologies using 'inquirer'
   const { stackList } = await inquirer.prompt(questions);
 
   let unusedStack = [];
 
+  // creating list of unused technologies
   for (let stack in STACK_MAP) {
     if (!stackList.includes(STACK_MAP[stack].title)) {
       unusedStack = [...unusedStack, ...STACK_MAP[stack].subdirs];
     }
   }
 
-  // Add client and mobile stacks in next step
   deleteStack(unusedStack);
 };
 

f3202606002ecbe32f4a87b29c01233fb07ca19d
Victor Vlasenko <victor.vlasenko@sysgears.com>
2019-05-19 11:25:44 +0300

    Remove canvas-prebuilt from server deps

--- packages/common/.zenrc.js
+++ packages/common/.zenrc.js
@@ -1,14 +1,4 @@
 const config = {
-  builders: {
-    test: {
-      roles: ['test'],
-      defines: {
-        __DEV__: false,
-        __API_URL__: '"/graphql"',
-        __TEST__: true
-      }
-    }
-  },
   options: {
     cache: '../../.cache'
   }

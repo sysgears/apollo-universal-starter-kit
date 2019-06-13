ac2fa1a1c8ad45d209e80f2c17491e999a6b7b96
Philip Balo <f.g.balo@gmail.com>
2019-06-13 12:16:43 +0300

    Remrite User DataRootComponent

--- modules/user/client-react/containers/DataRootComponent.jsx
+++ modules/user/client-react/containers/DataRootComponent.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import { withApollo } from 'react-apollo';
 
@@ -11,48 +11,46 @@ import CURRENT_USER_QUERY from '../graphql/CurrentUserQuery.graphql';
 
 // TODO: shouldn't be needed at all when React Apollo will allow rendering
 // all queries as loading: true during SSR
-class DataRootComponent extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { ready: settings.auth.session.enabled };
-  }
 
-  async componentDidMount() {
-    if (!this.state.ready && (await getItem('refreshToken'))) {
-      const { client } = this.props;
-      let result;
-      try {
-        result = await client.readQuery({ query: CURRENT_USER_QUERY });
-      } catch (e) {
-        // We have no current user in the cache, we need to load it to properly draw UI
-      }
-      if (!result || !result.currentUser) {
-        // If we don't have current user but have refresh token, this means our Apollo Cache
-        // might be invalid: we received this Apollo Cache from server in __APOLLO_STATE__
-        // as generated during server-sider rendering. Server had no idea about our client-side
-        // access token and refresh token. In this case we need to trigger our JWT link
-        // by sending network request
-        const {
-          data: { currentUser }
-        } = await client.query({
-          query: CURRENT_USER_QUERY,
-          fetchPolicy: 'network-only'
-        });
-        if (currentUser) {
-          // If we have received current user, then we had invalid Apollo Cache previously
-          // and we should discard it
-          await client.clearStore();
-          await client.writeQuery({ query: CURRENT_USER_QUERY, data: { currentUser } });
+const DataRootComponent = props => {
+  const [isReady, setIsReady] = useState(settings.auth.session.enabled);
+
+  useEffect(() => {
+    (async () => {
+      if (!isReady && (await getItem('refreshToken'))) {
+        const { client } = props;
+        let result;
+        try {
+          result = await client.readQuery({ query: CURRENT_USER_QUERY });
+        } catch (e) {
+          // We have no current user in the cache, we need to load it to properly draw UI
+        }
+        if (!result || !result.currentUser) {
+          // If we don't have current user but have refresh token, this means our Apollo Cache
+          // might be invalid: we received this Apollo Cache from server in __APOLLO_STATE__
+          // as generated during server-sider rendering. Server had no idea about our client-side
+          // access token and refresh token. In this case we need to trigger our JWT link
+          // by sending network request
+          const {
+            data: { currentUser }
+          } = await client.query({
+            query: CURRENT_USER_QUERY,
+            fetchPolicy: 'network-only'
+          });
+          if (currentUser) {
+            // If we have received current user, then we had invalid Apollo Cache previously
+            // and we should discard it
+            await client.clearStore();
+            await client.writeQuery({ query: CURRENT_USER_QUERY, data: { currentUser } });
+          }
         }
       }
-    }
-    this.setState({ ready: true });
-  }
+      setIsReady(true);
+    })();
+  }, []);
 
-  render() {
-    return this.state.ready ? this.props.children : <Loading />;
-  }
-}
+  return isReady ? props.children : <Loading />;
+};
 
 DataRootComponent.propTypes = {
   client: PropTypes.object,

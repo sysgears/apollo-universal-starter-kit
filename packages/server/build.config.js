6067edb9b579ccc35cb0a154b9e5b7d44edf5b1e
Victor Vlasenko <victor.vlasenko@sysgears.com>
2019-06-15 12:21:38 +0300

    Safely escape build.config.js values
    Fixes: #1106

--- packages/server/build.config.js
+++ packages/server/build.config.js
@@ -6,12 +6,12 @@ const config = {
   __SERVER__: true,
   __DEV__: process.env.NODE_ENV !== 'production',
   __TEST__: false,
-  'process.env.NODE_ENV': `"${process.env.NODE_ENV || 'development'}"`,
+  'process.env.NODE_ENV': process.env.NODE_ENV || 'development',
   __SERVER_PORT__: process.env.SERVER_PORT || process.env.PORT || 8080,
-  __API_URL__: `"${process.env.API_URL || '/graphql'}"`,
-  __WEBSITE_URL__: `"${process.env.WEBSITE_URL || 'http://localhost:3000'}"`,
-  __CDN_URL__: `"${process.env.CDN_URL || ''}"`,
-  __FRONTEND_BUILD_DIR__: `"${path.resolve(process.env.FRONTEND_BUILD_DIR || '../client/build')}"`
+  __API_URL__: process.env.API_URL || '/graphql',
+  __WEBSITE_URL__: process.env.WEBSITE_URL || 'http://localhost:3000',
+  __CDN_URL__: process.env.CDN_URL || '',
+  __FRONTEND_BUILD_DIR__: path.resolve(process.env.FRONTEND_BUILD_DIR || '../client/build')
 };
 
 module.exports = config;

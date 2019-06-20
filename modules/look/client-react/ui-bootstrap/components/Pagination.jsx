2d47c9a3aa4bf8e9daa9a951b3a58587b7403584
Philip Balo <f.g.balo@gmail.com>
2019-06-20 10:05:33 +0300

    Rewrote Look module bootstrap layout Pagination

--- modules/look/client-react/ui-bootstrap/components/Pagination.jsx
+++ modules/look/client-react/ui-bootstrap/components/Pagination.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import {
   Pagination as RSPagination,
@@ -8,64 +8,45 @@ import {
 
 import Button from './Button';
 
-export default class Pagination extends React.Component {
-  static propTypes = {
-    itemsPerPage: PropTypes.number,
-    handlePageChange: PropTypes.func,
-    hasNextPage: PropTypes.bool,
-    pagination: PropTypes.string,
-    total: PropTypes.number,
-    loadMoreText: PropTypes.string,
-    defaultPageSize: PropTypes.number
+const Pagination = props => {
+  const [pageNumber, setPageNumber] = useState(1);
+
+  const onItemClick = pageNumber => {
+    const { handlePageChange, pagination } = props;
+    setPageNumber(pageNumber);
+    handlePageChange(pagination, pageNumber);
   };
 
-  state = { pageNumber: 1, pagination: this.props.pagination };
-
-  static getDerivedStateFromProps(nextProps, prevState) {
-    return nextProps.pagination !== prevState.pagination ? { pageNumber: 1, pagination: nextProps.pagination } : null;
-  }
-
-  onItemClick = pageNumber => {
-    const { handlePageChange, pagination } = this.props;
-    this.setState({ pageNumber: pageNumber }, handlePageChange(pagination, pageNumber));
-  };
-
-  showPreviousPage = e => {
+  const showPreviousPage = e => {
     e.preventDefault();
-    const { handlePageChange, pagination } = this.props;
-    const { pageNumber } = this.state;
+    const { handlePageChange, pagination } = props;
     if (pageNumber > 1) {
-      this.setState(prevState => {
-        const newPageNumber = prevState.pageNumber - 1;
+      setPageNumber(prevPageNumber => {
+        const newPageNumber = prevPageNumber - 1;
         handlePageChange(pagination, newPageNumber);
-        return {
-          pageNumber: newPageNumber
-        };
+        return newPageNumber;
       });
     }
   };
 
-  showNextPage = (e, pagesArray) => {
+  const showNextPage = (e, pagesArray) => {
     e.preventDefault();
-    const { handlePageChange, pagination } = this.props;
-    const { pageNumber } = this.state;
+    const { handlePageChange, pagination } = props;
     if (pageNumber < pagesArray.length) {
-      this.setState(prevState => {
-        const newPageNumber = prevState.pageNumber + 1;
+      setPageNumber(prevPageNumber => {
+        const newPageNumber = prevPageNumber + 1;
         handlePageChange(pagination, newPageNumber);
-        return {
-          pageNumber: newPageNumber
-        };
+        return newPageNumber;
       });
     }
   };
 
-  renderPaginationItems(pagesArray) {
-    return pagesArray.map(pageNumber => (
+  const renderPaginationItems = pagesArray => {
+    return pagesArray.map(itemNumber => (
       <RSPaginationItem
-        key={pageNumber.toString()}
-        onClick={() => this.onItemClick(pageNumber)}
-        active={this.state.pageNumber === pageNumber}
+        key={itemNumber.toString()}
+        onClick={() => onItemClick(itemNumber)}
+        active={pageNumber === itemNumber}
       >
         <RSPaginationLink
           href="#"
@@ -73,61 +54,57 @@ export default class Pagination extends React.Component {
             e.preventDefault();
           }}
         >
-          {pageNumber}
+          {itemNumber}
         </RSPaginationLink>
       </RSPaginationItem>
     ));
-  }
+  };
 
-  render() {
-    const { pageNumber } = this.state;
-    const {
-      itemsPerPage,
-      handlePageChange,
-      hasNextPage,
-      pagination,
-      total,
-      loadMoreText,
-      defaultPageSize
-    } = this.props;
-    if (pagination === 'relay') {
-      return hasNextPage ? (
+  const { itemsPerPage, handlePageChange, hasNextPage, pagination, total, loadMoreText, defaultPageSize } = props;
+  if (pagination === 'relay') {
+    return hasNextPage ? (
+      <div>
         <div>
-          <div>
-            <small>
-              ({itemsPerPage} / {total})
-            </small>
-          </div>
-          <Button id="load-more" color="primary" onClick={() => handlePageChange(pagination, null)}>
-            {loadMoreText}
-          </Button>
+          <small>
+            ({itemsPerPage} / {total})
+          </small>
         </div>
-      ) : null;
-    } else {
-      const pagesArray = Array(Math.ceil(total / defaultPageSize))
-        .fill(1)
-        .map((x, y) => x + y);
-      return (
-        <RSPagination className="float-right">
-          <RSPaginationItem disabled={pageNumber <= 1}>
-            <RSPaginationLink
-              previous
-              href="#"
-              onClick={this.showPreviousPage}
-              className={'bootstrap-pagination-previous'}
-            />
-          </RSPaginationItem>
-          {this.renderPaginationItems(pagesArray)}
-          <RSPaginationItem disabled={pageNumber >= pagesArray.length}>
-            <RSPaginationLink
-              next
-              href="#"
-              onClick={e => this.showNextPage(e, pagesArray)}
-              className={'bootstrap-pagination-next'}
-            />
-          </RSPaginationItem>
-        </RSPagination>
-      );
-    }
+        <Button id="load-more" color="primary" onClick={() => handlePageChange(pagination, null)}>
+          {loadMoreText}
+        </Button>
+      </div>
+    ) : null;
+  } else {
+    const pagesArray = Array(Math.ceil(total / defaultPageSize))
+      .fill(1)
+      .map((x, y) => x + y);
+    return (
+      <RSPagination className="float-right">
+        <RSPaginationItem disabled={pageNumber <= 1}>
+          <RSPaginationLink previous href="#" onClick={showPreviousPage} className={'bootstrap-pagination-previous'} />
+        </RSPaginationItem>
+        {renderPaginationItems(pagesArray)}
+        <RSPaginationItem disabled={pageNumber >= pagesArray.length}>
+          <RSPaginationLink
+            next
+            href="#"
+            onClick={e => showNextPage(e, pagesArray)}
+            className={'bootstrap-pagination-next'}
+          />
+        </RSPaginationItem>
+      </RSPagination>
+    );
   }
-}
+};
+
+Pagination.propTypes = {
+  itemsPerPage: PropTypes.number,
+  handlePageChange: PropTypes.func,
+  hasNextPage: PropTypes.bool,
+  pagination: PropTypes.string,
+  total: PropTypes.number,
+  loadMoreText: PropTypes.string,
+  defaultPageSize: PropTypes.number
+};
+
+export default Pagination;

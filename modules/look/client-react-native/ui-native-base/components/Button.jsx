11c30438df8312ca913d9acc4da801b134cd6c10
Philip Balo <f.g.balo@gmail.com>
2019-06-24 18:34:55 +0300

    Rewrote btn native base

--- modules/look/client-react-native/ui-native-base/components/Button.jsx
+++ modules/look/client-react-native/ui-native-base/components/Button.jsx
@@ -4,26 +4,23 @@ import { Button as NBButton, Text } from 'native-base';
 import { StyleSheet } from 'react-native';
 import ButtonStyles from '../styles/Button';
 
-class Button extends React.Component {
-  render() {
-    const { textStyle, children, onClick, onPress, type, size, ...props } = this.props;
-    const btnProps = {
-      ...props,
-      [type]: true,
-      [size]: true,
-      block: true,
-      onPress: onPress || onClick
-    };
+const Button = ({ textStyle, children, onClick, onPress, type, size, ...restProps }) => {
+  const btnProps = {
+    ...restProps,
+    [type]: true,
+    [size]: true,
+    block: true,
+    onPress: onPress || onClick
+  };
 
-    return (
-      <NBButton {...btnProps}>
-        <Text style={[styles.btnText, textStyle]} numberOfLines={1}>
-          {children}
-        </Text>
-      </NBButton>
-    );
-  }
-}
+  return (
+    <NBButton {...btnProps}>
+      <Text style={[styles.btnText, textStyle]} numberOfLines={1}>
+        {children}
+      </Text>
+    </NBButton>
+  );
+};
 
 const styles = StyleSheet.create(ButtonStyles);
 

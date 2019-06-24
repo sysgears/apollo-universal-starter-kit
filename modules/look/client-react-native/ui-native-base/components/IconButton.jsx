b455ab2f5348d48f6588383eeb221f606f96d46e
Philip Balo <f.g.balo@gmail.com>
2019-06-24 18:36:46 +0300

    Rewrote IconButton native base

--- modules/look/client-react-native/ui-native-base/components/IconButton.jsx
+++ modules/look/client-react-native/ui-native-base/components/IconButton.jsx
@@ -3,18 +3,13 @@ import PropTypes from 'prop-types';
 import { Button, Icon } from 'native-base';
 import { Feather } from '@expo/vector-icons';
 
-class IconButton extends React.Component {
-  render() {
-    const { iconName, style, iconColor, iconSize, ...props } = this.props;
-    return (
-      <Button transparent style={[{ alignSelf: 'center' }, style]} {...props}>
-        <Icon>
-          <Feather name={iconName} size={iconSize} color={iconColor} />
-        </Icon>
-      </Button>
-    );
-  }
-}
+const IconButton = ({ iconName, style, iconColor, iconSize, ...restProps }) => (
+  <Button transparent style={[{ alignSelf: 'center' }, style]} {...restProps}>
+    <Icon>
+      <Feather name={iconName} size={iconSize} color={iconColor} />
+    </Icon>
+  </Button>
+);
 
 IconButton.propTypes = {
   iconName: PropTypes.string,

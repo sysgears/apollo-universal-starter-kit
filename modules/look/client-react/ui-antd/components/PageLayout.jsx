f6c5c3c7ea89d35b265b83c0eec13b5b30d38805
Philip Balo <f.g.balo@gmail.com>
2019-06-25 14:46:29 +0300

    Rewrote PageLayout

--- modules/look/client-react/ui-antd/components/PageLayout.jsx
+++ modules/look/client-react/ui-antd/components/PageLayout.jsx
@@ -10,32 +10,26 @@ import styles from '../styles/styles.less';
 
 const { Header, Content, Footer } = Layout;
 
-class PageLayout extends React.Component {
-  render() {
-    const { children, navBar } = this.props;
-
-    return (
-      <Layout>
-        {navBar !== false && (
-          <Header className="no-print">
-            <NavBar />
-          </Header>
-        )}
-        {__SERVER__ && __DEV__ && (
-          <Helmet>
-            <style type="text/css">{styles._getCss()}</style>
-          </Helmet>
-        )}
-        <Content id="content" style={{ background: '#fff', padding: 24 }}>
-          {children}
-        </Content>
-        <Footer className="no-print" style={{ textAlign: 'center' }}>
-          &copy; {new Date().getFullYear()}. {settings.app.name}.
-        </Footer>
-      </Layout>
-    );
-  }
-}
+const PageLayout = ({ children, navBar }) => (
+  <Layout>
+    {navBar !== false && (
+      <Header className="no-print">
+        <NavBar />
+      </Header>
+    )}
+    {__SERVER__ && __DEV__ && (
+      <Helmet>
+        <style type="text/css">{styles._getCss()}</style>
+      </Helmet>
+    )}
+    <Content id="content" style={{ background: '#fff', padding: 24 }}>
+      {children}
+    </Content>
+    <Footer className="no-print" style={{ textAlign: 'center' }}>
+      &copy; {new Date().getFullYear()}. {settings.app.name}.
+    </Footer>
+  </Layout>
+);
 
 PageLayout.propTypes = {
   children: PropTypes.node,
--- modules/look/client-react/ui-bootstrap/components/PageLayout.jsx
+++ modules/look/client-react/ui-bootstrap/components/PageLayout.jsx
@@ -17,29 +17,24 @@ const Footer = styled.footer`
   height: ${footerHeight};
 `;
 
-class PageLayout extends React.Component {
-  render() {
-    const { children, navBar } = this.props;
-    return (
-      <section className="d-flex flex-column flex-grow-1">
-        {__SERVER__ && __DEV__ && (
-          <Helmet>
-            <style type="text/css">{styles._getCss()}</style>
-          </Helmet>
-        )}
-        <section className="d-flex flex-column flex-grow-1 flex-shrink-0">
-          <section className="d-flex flex-column no-print">{navBar !== false && <NavBar />}</section>
-          <Container id="content">{children}</Container>
-        </section>
-        <Footer className="d-flex flex-shrink-0 justify-content-center no-print">
-          <span>
-            &copy; {new Date().getFullYear()}. {settings.app.name}.
-          </span>
-        </Footer>
-      </section>
-    );
-  }
-}
+const PageLayout = ({ children, navBar }) => (
+  <section className="d-flex flex-column flex-grow-1">
+    {__SERVER__ && __DEV__ && (
+      <Helmet>
+        <style type="text/css">{styles._getCss()}</style>
+      </Helmet>
+    )}
+    <section className="d-flex flex-column flex-grow-1 flex-shrink-0">
+      <section className="d-flex flex-column no-print">{navBar !== false && <NavBar />}</section>
+      <Container id="content">{children}</Container>
+    </section>
+    <Footer className="d-flex flex-shrink-0 justify-content-center no-print">
+      <span>
+        &copy; {new Date().getFullYear()}. {settings.app.name}.
+      </span>
+    </Footer>
+  </section>
+);
 
 PageLayout.propTypes = {
   children: PropTypes.node,

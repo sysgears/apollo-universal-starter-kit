f11ddb7e5f9b29c2c0ab180add4eaeace6ae0ca2
Philip Balo <f.g.balo@gmail.com>
2019-06-27 12:44:23 +0300

    Rewrote withMessagesSubscription

--- modules/chat/client-react/containers/withMessagesSubscription.jsx
+++ modules/chat/client-react/containers/withMessagesSubscription.jsx
@@ -1,32 +1,26 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import { Subscription } from 'react-apollo';
 
 import MESSAGES_SUBSCRIPTION from '../graphql/MessagesSubscription.graphql';
 
 export default Component => {
-  return class WithMessagesSubscription extends React.Component {
-    static propTypes = {
-      messages: PropTypes.object
-    };
+  return props => {
+    const { messages } = props;
 
-    render() {
-      const { messages } = this.props;
-      if (messages) {
-        const endCursor = messages.pageInfo.endCursor;
-        return (
-          <Subscription subscription={MESSAGES_SUBSCRIPTION} variables={{ endCursor }}>
-            {({ data, loading }) => {
-              if (!loading) {
-                return <Component {...this.props} messagesUpdated={data.messagesUpdated} />;
-              }
-              return <Component {...this.props} />;
-            }}
-          </Subscription>
-        );
-      } else {
-        return <Component {...this.props} />;
-      }
+    if (messages) {
+      const endCursor = messages.pageInfo.endCursor;
+      return (
+        <Subscription subscription={MESSAGES_SUBSCRIPTION} variables={{ endCursor }}>
+          {({ data, loading }) => {
+            if (!loading) {
+              return <Component {...props} messagesUpdated={data.messagesUpdated} />;
+            }
+            return <Component {...props} />;
+          }}
+        </Subscription>
+      );
+    } else {
+      return <Component {...props} />;
     }
   };
 };

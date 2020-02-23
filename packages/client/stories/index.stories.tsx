ad7ef3bab4998b43879d17e32c865ecee5316c2f
DeanC <>
2020-02-23 18:44:46 -0500

    Make storybook work with client counter.

--- packages/client/stories/index.stories.tsx
+++ packages/client/stories/index.stories.tsx
@@ -1,15 +1,39 @@
 import React from 'react';
-
 import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
 import { linkTo } from '@storybook/addon-links';
+import { ApolloProvider } from 'react-apollo';
+import { createApolloClient } from '@gqlapp/core-common';
+import i18n from '@gqlapp/i18n-client-react';
+import ClientModule from '@gqlapp/module-client-react';
+import clientCounter from '@gqlapp/counter-client-react/clientCounter';
+import CounterModule from '@gqlapp/counter-client-react/CounterModule';
 
-import { ClientCounterView } from '@gqlapp/counter-client-react/clientCounter/components/ClientCounterView';
+const modules = new ClientModule(i18n, new CounterModule(clientCounter));
+(async () => {
+  await modules.createApp(module);
+})();
+
+const client = createApolloClient({
+  createLink: modules.createLink,
+  clientResolvers: modules.resolvers
+});
 
 const { Button, Welcome } = require('@storybook/react/demo');
+const CounterComponent: React.FC = () => modules.counterComponent;
 
 storiesOf('Welcome', module).add('to Storybook', () => <Welcome showApp={linkTo('Button')} />);
-
 storiesOf('Button', module)
   .add('with text', () => <Button onClick={action('clicked')}>Hello Button</Button>)
-  .add('with some emoji', () => <ClientCounterView text="123">abc</ClientCounterView>);
+  .add('with some emoji', () => (
+    <Button onClick={action('clicked')}>
+      <span role="img" aria-label="so cool">
+        😀 😎 👍 💯
+      </span>
+    </Button>
+  ));
+storiesOf('Counters', module).add('clientCounter', () => (
+  <ApolloProvider client={client}>
+    <CounterComponent />
+  </ApolloProvider>
+));

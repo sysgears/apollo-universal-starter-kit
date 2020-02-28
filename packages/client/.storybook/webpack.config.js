9b9093fb70599f7f2acae286ab9c7a1b3c066957
DeanC <>
2020-02-28 08:00:43 -0500

    Add storybook sources addon

--- packages/client/.storybook/webpack.config.js
+++ packages/client/.storybook/webpack.config.js
@@ -7,5 +7,10 @@ module.exports = async ({ config, mode }) => {
   result.resolve = webpackConfig.resolve;
   result.watchOptions = webpackConfig.watchOptions;
   result.plugins = [...config.plugins, ...webpackConfig.plugins];
+  result.module.rules.push({
+    test: /\.stories\.tsx?$/,
+    loaders: [require.resolve('@storybook/source-loader')],
+    enforce: 'pre',
+  });
   return result;
 };

671690bd2f2b66ab23f45702a026f85cc17b9c4f
Dean Cirielli <dcirielli5@gmail.com>
2020-03-02 10:55:06 -0500

    Feature/add storybook client counter, plus button text knobs & locale/lang selection (#1186)
    * add helpers and some docs
    
    * Make storybook work with client counter.
    
    * Cleanup code
    
    * Remove extraneous new ClientModule() call
    
    * Lint format
    
    * Add storybook knobs
    
    * Add storybook sources addon
    
    * Get storybook addon locale selector working
    
    Co-authored-by: Tony Worm <1390600+verdverm@users.noreply.github.com>
    Co-authored-by: Victor Vlasenko <victor.vlasenko@sysgears.com>

--- packages/client/stories/index.stories.tsx
+++ packages/client/stories/index.stories.tsx
@@ -1,15 +1,57 @@
 import React from 'react';
-
+import { ApolloProvider } from 'react-apollo';
 import { storiesOf } from '@storybook/react';
+import addons from '@storybook/addons';
 import { action } from '@storybook/addon-actions';
 import { linkTo } from '@storybook/addon-links';
+import { withKnobs, text } from '@storybook/addon-knobs';
+import { LOCALE_EVENT_NAME } from 'storybook-addon-locale';
+import i18next from 'i18next';
+import i18n from '@gqlapp/i18n-client-react';
+import { createApolloClient } from '@gqlapp/core-common';
+import ClientModule from '@gqlapp/module-client-react';
+import clientCounter from '@gqlapp/counter-client-react/clientCounter';
+import config from '@gqlapp/config';
 
-import { ClientCounterView } from '@gqlapp/counter-client-react/clientCounter/components/ClientCounterView';
+// Trigger locale change using  storybook-addon-locale's event emitter
+const channel = addons.getChannel();
+channel.on(LOCALE_EVENT_NAME, l => {
+  i18next.changeLanguage(`${l.locale}`); // fully qualified i18n names apparently needed
+});
 
-const { Button, Welcome } = require('@storybook/react/demo');
-
+// Standard storybook Welcome and buttons, but with knobs enabled for text label button.
+const { Welcome, Button } = require('@storybook/react/demo');
 storiesOf('Welcome', module).add('to Storybook', () => <Welcome showApp={linkTo('Button')} />);
-
 storiesOf('Button', module)
-  .add('with text', () => <Button onClick={action('clicked')}>Hello Button</Button>)
-  .add('with some emoji', () => <ClientCounterView text="123">abc</ClientCounterView>);
+  .addDecorator(withKnobs)
+  .add('with text', () => {
+    const msgTxt = text('Label', 'Hello Button');
+    return <Button onClick={action('clicked')}>{`${msgTxt}`}</Button>;
+  })
+  .add('with some emoji', () => (
+    <Button onClick={action('clicked')}>
+      <span role="img" aria-label="so cool">
+        😀 😎 👍 💯
+      </span>
+    </Button>
+  ));
+
+// Client counter with i18n translation & storybook locale addon for language selection
+const clientCounterModule = new ClientModule(i18n, clientCounter);
+(async () => clientCounterModule.createApp(module))();
+const CounterComponent: React.FC = () => clientCounterModule.counterComponent;
+const client = createApolloClient({
+  createLink: clientCounterModule.createLink,
+  clientResolvers: clientCounterModule.resolvers
+});
+storiesOf('Client Counters', module)
+  .addParameters({
+    locales: config.i18n.langList,
+    defaultLocale: config.i18n.langList[0],
+    enableLocaleLockButton: false
+  })
+  .add('Apollo Link', () => (
+    <ApolloProvider client={client}>
+      <CounterComponent />
+    </ApolloProvider>
+  ));

b496f6b66aa35b189b56f9ac4ecc4f3be21a6c5a
vitalii <vitalii.mordak@sysgears.com>
2020-07-27 17:37:39 +0300

    added counter graphQL resolvers

---
+++ modules/counter/server-java/src/main/java/com/sysgears/counter/resolvers/CounterMutationResolver.java
@@ -0,0 +1,35 @@
+package com.sysgears.counter.resolvers;
+
+import com.sysgears.core.subscription.Publisher;
+import com.sysgears.counter.constant.CounterConstants;
+import com.sysgears.counter.model.Counter;
+import com.sysgears.counter.repository.CounterRepository;
+import graphql.kickstart.tools.GraphQLMutationResolver;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.concurrent.CompletableFuture;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class CounterMutationResolver implements GraphQLMutationResolver {
+    private final CounterRepository repository;
+    private final Publisher<Counter> publisher;
+
+    @Transactional
+    public CompletableFuture<Counter> addServerCounter(int amount) {
+        log.debug("Updating counter");
+        return repository.findById(CounterConstants.ID)
+                .thenApply(
+                        counter -> {
+                            counter.increaseAmount(amount);
+                            repository.save(counter);
+                            publisher.publish(counter);
+                            return counter;
+                        }
+                );
+    }
+}
---
+++ modules/counter/server-java/src/main/java/com/sysgears/counter/resolvers/CounterQueryResolver.java
@@ -0,0 +1,23 @@
+package com.sysgears.counter.resolvers;
+
+import com.sysgears.counter.constant.CounterConstants;
+import graphql.kickstart.tools.GraphQLQueryResolver;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import com.sysgears.counter.model.Counter;
+import org.springframework.stereotype.Service;
+import com.sysgears.counter.repository.CounterRepository;
+
+import java.util.concurrent.CompletableFuture;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class CounterQueryResolver implements GraphQLQueryResolver {
+    private final CounterRepository repository;
+
+    public CompletableFuture<Counter> serverCounter() {
+        log.debug("Get counter");
+        return repository.findById(CounterConstants.ID);
+    }
+}

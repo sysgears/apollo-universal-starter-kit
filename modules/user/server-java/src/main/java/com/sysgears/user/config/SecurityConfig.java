0f410aa1952d8e398e02534aa4553bca54dcface
Vitalii <vitalikmordak@gmail.com>
2021-01-04 18:05:04 +0200

    added cors configuration

--- modules/user/server-java/src/main/java/com/sysgears/user/config/SecurityConfig.java
+++ modules/user/server-java/src/main/java/com/sysgears/user/config/SecurityConfig.java
@@ -6,6 +6,9 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
@@ -18,6 +21,24 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         // todo: Security disabled
-        http.csrf().disable().authorizeRequests().anyRequest().permitAll();
+        http
+                .csrf().disable()
+                .authorizeRequests().anyRequest().permitAll()
+                .and().cors().configurationSource(securityCorsConfiguration());
+
+    }
+
+    @Bean
+    public CorsConfigurationSource securityCorsConfiguration() {
+        final CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOriginPattern("*");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+
+        return source;
     }
 }

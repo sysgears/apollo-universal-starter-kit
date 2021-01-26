f1c397b9f79b4f9480f1e4b60e684cab269b186c
Vitalii <vitalikmordak@gmail.com>
2021-01-26 13:55:18 +0200

    upgraded switch expression

--- modules/user/server-java/src/main/java/com/sysgears/user/resolvers/UserSubscriptionResolver.java
+++ modules/user/server-java/src/main/java/com/sysgears/user/resolvers/UserSubscriptionResolver.java
@@ -9,7 +9,6 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.reactivestreams.Publisher;
 import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Service;
 
 import java.util.Optional;
 import java.util.concurrent.CompletableFuture;
@@ -24,24 +23,18 @@ public class UserSubscriptionResolver implements GraphQLSubscriptionResolver {
         return CompletableFuture.supplyAsync(() -> subscriber.subscribe(event -> {
             if (filter.isEmpty()) return true;
 
-            switch (event.getMutation()) {
-                case DELETE_USER:
-                    return true;
-
-                case ADD_USER:
-                case EDIT_USER:
-                    return filter.get().getIsActive().map(isActive ->
-                            event.getUser().getIsActive().equals(isActive)).orElse(true) &&
-                            filter.get().getRole().map(role ->
-                                    event.getUser().getRole().toLowerCase().equals(role.toLowerCase())).orElse(true) &&
-                            (filter.get().getSearchText().map(searchText ->
-                                    event.getUser().getEmail().toLowerCase().equals(searchText.toLowerCase())).orElse(true) ||
-                                    filter.get().getSearchText().map(searchText ->
-                                            event.getUser().getUsername().toLowerCase().equals(searchText.toLowerCase())).orElse(true));
-
-                default:
-                    return false;
-            }
+            return switch (event.getMutation()) {
+                case DELETE_USER -> true;
+                case ADD_USER, EDIT_USER -> filter.get().getIsActive().map(isActive ->
+                        event.getUser().getIsActive().equals(isActive)).orElse(true) &&
+                        filter.get().getRole().map(role ->
+                                event.getUser().getRole().equalsIgnoreCase(role)).orElse(true) &&
+                        (filter.get().getSearchText().map(searchText ->
+                                event.getUser().getEmail().equalsIgnoreCase(searchText)).orElse(true) ||
+                                filter.get().getSearchText().map(searchText ->
+                                        event.getUser().getUsername().equalsIgnoreCase(searchText)).orElse(true));
+                default -> false;
+            };
         }).map(event -> new UpdateUserPayload(event.getMutation().getOperation(), event.getUser())));
     }
 }

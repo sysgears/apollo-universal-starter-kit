bd583555d29f44fd2077e3a58eea74df73c81915
piff-saad <pooriya.safaei.80@gmail.com>
2023-02-12 07:46:19 -0800

    deleteModule API updated

--- tools/cli/commands/deleteModule.js
+++ tools/cli/commands/deleteModule.js
@@ -20,19 +20,76 @@ const {
  * @param moduleName - The name of a new module
  * @param old - The flag that describes if the command invoked for a new structure or not
  */
-function deleteModule({ logger, packageName, moduleName, old }) {
-  const modulePackageName = getModulePackageName(packageName, old);
-  const modulePath = computeModulePath(modulePackageName, old, moduleName);
-  const params = { logger, moduleName, modulePath, packageName, modulePackageName, old };
+function deleteModule(logger, templatePath, module, location) {
+  logger.info(`Deleting ${location} files…`);
+
+  // pascalize
+  const Module = pascalize(module);
+  const startPath = `${__dirname}/../../..`;
+  const modulePath = `${startPath}/packages/${location}/src/modules/${module}`;
+  const commonGraphqlFile = 'commonGraphql.js';
+  const commonGraphqlPath = `${startPath}/packages/${location}/src/modules/common/components/web/${commonGraphqlFile}`;
 
   if (fs.existsSync(modulePath)) {
-    deleteTemplates(params);
-    removeFromModules(params);
-    if (!old) removeDependency(params);
+    // remove module directory
+    shell.rm('-rf', modulePath);
 
-    logger.info(chalk.green(`✔ Module ${moduleName} for package ${packageName} successfully deleted!`));
+    // change to destination directory
+    shell.cd(`${startPath}/packages/${location}/src/modules/`);
+
+    // get module input data
+    const path = `${startPath}/packages/${location}/src/modules/index.js`;
+    let data = fs.readFileSync(path);
+
+    // extract Feature modules
+    const re = /Feature\(([^()]+)\)/g;
+    const match = re.exec(data);
+    const modules = match[1].split(',').filter(featureModule => featureModule.trim() !== module);
+
+    // remove import module line
+    const lines = data
+      .toString()
+      .split('\n')
+      .filter(line => line.match(`import ${module} from './${module}';`) === null);
+    fs.writeFileSync(path, lines.join('\n'));
+
+    // remove module from Feature function
+    //shell.sed('-i', re, `Feature(${modules.toString().trim()})`, 'index.js');
+    shell
+      .ShellString(shell.cat('index.js').replace(RegExp(re, 'g'), `Feature(${modules.toString().trim()})`))
+      .to('index.js');
+
+    if (location === 'server') {
+      // change to database migrations directory
+      shell.cd(`${startPath}/packages/${location}/src/database/migrations`);
+      // check if any migrations files for this module exist
+      if (shell.find('.').filter(file => file.search(`_${Module}.js`) > -1).length > 0) {
+        let okMigrations = shell.rm(`*_${Module}.js`);
+        if (okMigrations) {
+          logger.info(chalk.green(`✔ Database migrations files successfully deleted!`));
+        }
+      }
+
+      // change to database seeds directory
+      shell.cd(`${startPath}/packages/${location}/src/database/seeds`);
+      // check if any seed files for this module exist
+      if (shell.find('.').filter(file => file.search(`_${Module}.js`) > -1).length > 0) {
+        let okSeeds = shell.rm(`*_${Module}.js`);
+        if (okSeeds) {
+          logger.info(chalk.green(`✔ Database seed files successfully deleted!`));
+        }
+      }
+    }
+
+    // continue only if directory does not jet exist
+    logger.info(chalk.green(`✔ Module for ${location} successfully deleted!`));
   } else {
-    logger.info(chalk.red(`✘ Module ${moduleName} for package ${packageName} not found!`));
+    logger.info(chalk.red(`✘ Module ${location} location for ${modulePath} not found!`));
+  }
+
+  if (fs.existsSync(commonGraphqlPath)) {
+    const graphqlQuery = `${Module}Query`;
+    deleteModuleFromCommonGraphqlFile(module, commonGraphqlPath, graphqlQuery);
   }
 }
 
